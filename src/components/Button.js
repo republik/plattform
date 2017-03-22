76e5be6c0d8e275cc7b80d1b66de310f377ea1d3
Thomas Preusse <tpreusse@users.noreply.github.com>
2017-03-22 10:24:11 +0100

    use colors in button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {css, merge, simulate} from 'glamor'
+import * as colors from '../colors'
 
 const fieldStyle = css({
   verticalAlign: 'bottom',
@@ -8,22 +9,16 @@ const fieldStyle = css({
   fontSize: 14,
   height: 40,
   boxSizing: 'border-box',
-  backgroundColor: 'rgb(9,81,138)',
-  border: 'solid rgb(9,81,138) 1px',
+  backgroundColor: colors.primary,
+  border: 'none',
   color: '#fff',
   ':hover': {
-    backgroundColor: '#fff',
-    color: 'rgb(9,81,138)'
+    backgroundColor: colors.primaryHover
   }
 })
 const primaryStyle = css({
   fontSize: 18,
-  height: 50,
-  color: '#fff',
-  ':hover': {
-    backgroundColor: '#fff',
-    color: 'rgb(9,81,138)'
-  }
+  height: 50
 })
 
 const Button = ({onClick, type, children, primary, simulate: sim}) => {
