4e8048ee7e52d39b449d90643d4c231bbe3ab0a4
Thomas Preusse <tpreusse@users.noreply.github.com>
2017-04-03 17:25:52 +0200

    new container padding

--- src/components/Grid/index.js
+++ src/components/Grid/index.js
@@ -3,10 +3,10 @@ import {css} from 'glamor'
 import {onlyS, mUp} from '../../theme/mediaQueries'
 
 export const GUTTER = 42
-export const CONTENT_PADDING = 20
+export const CONTENT_PADDING = 15
 
-export const NARROW_CONTENT_MAX_WIDTH = 650
-export const CONTENT_MAX_WIDTH = 1200
+export const NARROW_CONTENT_MAX_WIDTH = 680
+export const CONTENT_MAX_WIDTH = 1230
 
 const styles = {
   container: css({
