d383977bcdfdc00d001a00672218a57cc802e6fa
Thomas Preusse <tpreusse@users.noreply.github.com>
2017-04-04 01:07:00 +0200

    export breakpoints

--- src/theme/mediaQueries.js
+++ src/theme/mediaQueries.js
@@ -1,3 +1,6 @@
-export const onlyS = '@media only screen and (max-width: 767px)'
-export const mUp = '@media only screen and (min-width: 768px)'
-export const lUp = '@media only screen and (min-width: 1025px)'
+export const mBreakPoint = 768
+export const lBreakPoint = 1025
+
+export const onlyS = `@media only screen and (max-width: ${mBreakPoint - 1}px)`
+export const mUp = `@media only screen and (min-width: ${mBreakPoint}px)`
+export const lUp = `@media only screen and (min-width: ${lBreakPoint}px)`
