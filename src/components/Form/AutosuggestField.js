43ba3a55398c7c8c617a25f5c0516c10e9612190
Thomas Preusse <tpreusse@users.noreply.github.com>
2017-05-03 17:44:51 +0200

    migrate to prop-types

--- src/components/Form/AutosuggestField.js
+++ src/components/Form/AutosuggestField.js
@@ -1,4 +1,5 @@
-import React, {Component, PropTypes} from 'react'
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
 import Autocomplete from 'react-autocomplete'
 import * as colors from '../../theme/colors'
 import Field, {fieldHeight} from './Field'
--- src/components/Grid/index.js
+++ src/components/Grid/index.js
@@ -1,4 +1,5 @@
-import React, {PropTypes} from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 import {css} from 'glamor'
 import {onlyS, mUp} from '../../theme/mediaQueries'
 
