1882065d8afc4fa4d803a5697ef24fa43b7c05dd
Thomas Preusse <tpreusse@users.noreply.github.com>
2017-06-28 15:55:55 +0200

    rm unused grid
    - no longer a substantial copy, rm LICENSE too

--- src/components/Grid/docs.md
+++ src/components/Grid/docs.md
@@ -15,48 +15,4 @@ Container definieren die maximale Breite des Grids und kümmeren sich um das hor
 </NarrowContainer>
 ```
 
-## Grid & Spans
-
-Je nach verfügbarer Fläche nutzen wir ein zwei oder 18 Spalten Grid.
-
-### Props
-
-- `s` Spaltenbreite für kleine Displays. Brüche von 2, z.B. `1/2`
-- `m` Spaltenbreite ab mittel grossem Display. Brüche von 18, z.B. `5/18`
-
-```hint
-`<Span>`s müssen in einem `<Grid>` sein
-```
-
-```react
-<Grid>
-  <Span s='1/2' m='6/18'>Foo</Span>
-  <Span s='1/2' m='12/18'>Bar</Span>
-</Grid>
-```
-
-### Offset
-
-```react
-<Grid>
-  <Span m='1/18' />
-  <Span m='10/18'>
-    Er hörte leise Schritte hinter sich. Das bedeutete nichts Gutes. Wer würde ihm schon folgen, spät in der Nacht und dazu noch in dieser engen Gasse mitten im übel beleumundeten Hafenviertel? Gerade jetzt, wo er das Ding seines Lebens gedreht hatte und mit der Beute verschwinden wollte! Hatte einer seiner zahllosen Kollegen dieselbe Idee gehabt, ihn beobachtet und abgewartet, um ihn nun um die Früchte seiner Arbeit zu erleichtern?
-  </Span>
-</Grid>
-```
-
-### Pull Right
-
-```react
-<Grid>
-  <Span s='2/2' m='5/18' style={{float: 'right'}}>
-    Ein rechter Sidebar,<br />
-    der zuerst kommt auf Mobile.
-  </Span>
-  <Span s='2/2' m='1/18' />
-  <Span s='2/2' m='10/18'>
-    Er hörte leise Schritte hinter sich. Das bedeutete nichts Gutes. Wer würde ihm schon folgen, spät in der Nacht und dazu noch in dieser engen Gasse mitten im übel beleumundeten Hafenviertel? Gerade jetzt, wo er das Ding seines Lebens gedreht hatte und mit der Beute verschwinden wollte! Hatte einer seiner zahllosen Kollegen dieselbe Idee gehabt, ihn beobachtet und abgewartet, um ihn nun um die Früchte seiner Arbeit zu erleichtern?
-  </Span>
-</Grid>
-```
+Auf ein mehrspaltiges Grid haben wir vorerst verzichtet.
--- src/components/Grid/index.js
+++ src/components/Grid/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {css} from 'glamor'
-import {onlyS, mUp} from '../../theme/mediaQueries'
 
 export const GUTTER = 42
 export const CONTENT_PADDING = 15
@@ -25,41 +24,7 @@ const styles = {
     maxWidth: NARROW_CONTENT_MAX_WIDTH,
     marginLeft: 'auto',
     marginRight: 'auto'
-  }),
-  grid: css({
-    clear: 'both',
-    overflow: 'auto',
-    zoom: '1',
-    marginLeft: `-${GUTTER / 2}px`,
-    width: `calc(100% + ${GUTTER}px)`
-  }),
-  span: css({
-    boxSizing: 'border-box',
-    float: 'left',
-    paddingLeft: `${GUTTER / 2}px`,
-    paddingRight: `${GUTTER / 2}px`,
-    minHeight: 1
-  }),
-  s1of2: css({[onlyS]: {width: '50%'}}),
-  s2of2: css({[onlyS]: {width: '100%'}}),
-  m1of18: css({[mUp]: {width: `${100 / 18 * 1}%`}}),
-  m2of18: css({[mUp]: {width: `${100 / 18 * 2}%`}}),
-  m3of18: css({[mUp]: {width: `${100 / 18 * 3}%`}}),
-  m4of18: css({[mUp]: {width: `${100 / 18 * 4}%`}}),
-  m5of18: css({[mUp]: {width: `${100 / 18 * 5}%`}}),
-  m6of18: css({[mUp]: {width: `${100 / 18 * 6}%`}}),
-  m7of18: css({[mUp]: {width: `${100 / 18 * 7}%`}}),
-  m8of18: css({[mUp]: {width: `${100 / 18 * 8}%`}}),
-  m9of18: css({[mUp]: {width: `${100 / 18 * 9}%`}}),
-  m10of18: css({[mUp]: {width: `${100 / 18 * 10}%`}}),
-  m11of18: css({[mUp]: {width: `${100 / 18 * 11}%`}}),
-  m12of18: css({[mUp]: {width: `${100 / 18 * 12}%`}}),
-  m13of18: css({[mUp]: {width: `${100 / 18 * 13}%`}}),
-  m14of18: css({[mUp]: {width: `${100 / 18 * 14}%`}}),
-  m15of18: css({[mUp]: {width: `${100 / 18 * 15}%`}}),
-  m16of18: css({[mUp]: {width: `${100 / 18 * 16}%`}}),
-  m17of18: css({[mUp]: {width: `${100 / 18 * 17}%`}}),
-  m18of18: css({[mUp]: {width: `${100 / 18 * 18}%`}})
+  })
 }
 
 export const Container = ({children, ...props}) => (
@@ -81,40 +46,4 @@ export const NarrowContainer = ({children, ...props}) => (
 
 NarrowContainer.propTypes = {
   children: PropTypes.node
-}
-
-export const Grid = ({children}) => (
-  <div {...styles.grid}>
-    {children}
-  </div>
-)
-
-Grid.propTypes = {
-  children: PropTypes.node
-}
-
-const fractionToClassName = (frac = '') => frac.replace(/\//, 'of')
-
-export const Span = ({children, style, s, m}) => {
-  return (
-    <div style={style} {...styles.span}
-      {...styles[`s${fractionToClassName(s)}`]}
-      {...styles[`m${fractionToClassName(m)}`]}>
-      {children}
-    </div>
-  )
-}
-
-Span.propTypes = {
-  children: PropTypes.node,
-  s: PropTypes.oneOf(['1/2', '2/2']),
-  m: PropTypes.oneOf([
-    '1/18', '2/18', '3/18', '4/18', '5/18', '6/18', '7/18', '8/18', '9/18',
-    '10/18', '11/18', '12/18', '13/18', '14/18', '15/18', '16/18', '17/18', '18/18'
-  ])
-}
-
-Span.defaultProps = {
-  s: '2/2',
-  m: '18/18'
 }
