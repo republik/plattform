2ce8a46d0648aca76e8863a8b2a21b34304a97e4
Thomas Preusse <tpreusse@users.noreply.github.com>
2017-06-28 22:59:47 +0200

    port catalog style from Bodara

--- src/catalogTheme.js
+++ src/catalogTheme.js
@@ -1,71 +1,24 @@
 import {fontFamilies} from './theme/fonts'
 
 export default {
-  // Colors
-  background: '#fff',
-  pageHeadingBackground: '#fff',
+  pageHeadingBackground: 'rgb(245,245,245)',
   pageHeadingTextColor: '#000',
-  pageHeadingHeight: 200,
   brandColor: '#000',
-  sidebarColor: '#fff',
-  sidebarColorActive: '#000',
-  sidebarColorText: '#999',
-  sidebarColorTextActive: '#000',
-  sidebarColorLine: '#fff',
-  sidebarColorHeading: '#000',
-  textColor: '#000',
-  codeColor: '#00263E',
-  linkColor: '#FF5555',
-  textMedium: '#000',
-  bgLight: '#fff',
-  bgDark: '#f2f2f2',
-  lightColor: '#ccc',
 
-  codeStyles: {
-    tag: {color: '#FF5555'},
-    punctuation: {color: '#535353'},
-    script: {color: '#3F7397'},
-    function: {color: '#FF5555'},
-    keyword: {color: '#3F7397'},
-    string: {color: '#00263E'}
-  },
-
-  // Patterns
-  checkerboardPatternLight: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAAAAACoWZBhAAAAF0lEQVQI12P4BAI/QICBFCaYBPNJYQIAkUZftTbC4sIAAAAASUVORK5CYII=',
-  checkerboardPatternDark: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAAAAACoWZBhAAAAFklEQVQI12NQBQF2EGAghQkmwXxSmADZJQiZ2ZZ46gAAAABJRU5ErkJggg==',
-
-  // Fonts
-  fontFamily: fontFamilies.serifRegular,
+  fontFamily: fontFamilies.sansSerifRegular,
   fontHeading: fontFamilies.sansSerifMedium,
-  fontMono: "'Source Code Pro', monospace",
+  textColor:  'rgba(0,0,0,0.8)',
+  textMedium: 'rgba(0,0,0,0.8)',
+  linkColor: 'rgb(0,180,0)',
 
-  fontXs:  12,
-  fontS:   14,
-  fontM:   16,
-  fontL:   21,
-  fontXl:  28,
-  fontXxl: 38,
+  sidebarColor: 'rgb(245,245,245)',
+  sidebarColorTextActive: 'rgb(60,100,90)', // rgb(60,100,90)
+  sidebarColorText: '#000',
+  
+  lightColor: 'rgba(60,100,90,0.5)',
 
-  sizeS:   8,
-  sizeM:   15,
-  sizeL:   21,
-  sizeXl:  28,
-  sizeXxl: 38,
+  background:'rgb(245,245,245)',
+  baseFontSize: 20,
 
-
-  // The vertical grid unit. Margin, padding, and line-height are set to multiples
-  // of this value. This is the value that determines the baseline for our vertical
-  // rhythm. The default value of 6px allows more fine tuned designs that still
-  // obey vertical rhythm.
-  baseVerticalUnit: 4,
-
-  // The value that multiplies the baseVerticalUnit to get the baseLineHeight.
-  // This gives type an ideal lineHeight.
-  baseLineMulti: 10,
-
-  // Base font size in pixels.
-  baseFontSize: 16,
-
-  // Modular scale ratio that is used to figure out all the different font sizes
-  msRatio: 1.2
+  bgLight: 'rgb(245,245,245)'
 };
