d5d5e2bade71ada27ed962163c5f86879487416d
Thomas Preusse <tpreusse@users.noreply.github.com>
2017-06-30 16:51:03 +0200

    rm unused colors

--- src/theme/colors.js
+++ src/theme/colors.js
@@ -4,10 +4,6 @@ export const secondary = '#4B6359'
 export const secondaryBg = '#F6F8F7' // 5%
 export const disabled = '#B7C1BD'
 
-export const error = '#9F2500'
-
-export const background = '#fff'
 export const text = '#282828'
-export const textLabel = '#ccc'
-
+export const error = '#9F2500'
 export const divider = '#DADDDC'
