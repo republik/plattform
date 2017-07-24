bee288b2b402e1407f09a0dbdec592c5d1d6a90c
Lukas Buenger <lukasbuenger@gmail.com>
2017-07-24 16:40:47 +0200

    tooling, round #2

---
+++ app/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Head from 'next/head'
+import { fontFamilies } from '@project-r/styleguide'
+
+export default ({ children }) =>
+  <main>
+    <Head>
+      <title>Project R — Admin Interface</title>
+      <meta
+        name="viewport"
+        content="initial-scale=1.0, width=device-width"
+      />
+      <style>{`
+        html, body {
+          padding: 0;
+          margin: 0;
+          font-family: ${fontFamilies.sansSerifRegular}
+        }
+      `}</style>
+    </Head>
+    {children}
+  </main>
