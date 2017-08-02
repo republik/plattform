8c4f50a7edc2e582e0baeaa6dae7e00fd79ac3a0
Thomas Preusse <tpreusse@users.noreply.github.com>
2017-08-02 10:12:19 +0200

    chore: explicit exports

--- src/lib.js
+++ src/lib.js
@@ -3,10 +3,7 @@ import * as allMediaQueries from './theme/mediaQueries'
 export {default as colors} from './theme/colors'
 export const mediaQueries = allMediaQueries
 
-export {
-  fontFamilies,
-  fontFaces
-} from './theme/fonts'
+export {fontFamilies, fontFaces} from './theme/fonts'
 
 export {default as Logo} from './components/Logo'
 export {default as BrandMark} from './components/Logo/BrandMark'
@@ -14,5 +11,14 @@ export {default as Button} from './components/Button'
 export {default as Field} from './components/Form/Field'
 export {default as Radio} from './components/Form/Radio'
 export {default as Checkbox} from './components/Form/Checkbox'
-export * from './components/Grid'
-export * from './components/Typography'
+
+export {Container, NarrowContainer} from './components/Grid'
+export {
+  linkRule, A,
+  H1, H2,
+  Lead,
+  P,
+  Label,
+  Quote,
+  Interaction
+} from './components/Typography'
