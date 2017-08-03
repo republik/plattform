3f9b1dfbe21fb1f1c434023183175d8ef3dcfe29
Lukas Buenger <lukasbuenger@gmail.com>
2017-08-03 14:23:45 +0200

    refactoring finished

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,27 @@ import React, { Component } from 'react'
 import { Raw, resetKeyGenerator } from 'slate'
 
 import App from '../lib/App'
-import Editor from '../lib/editor/components/Editor'
-import lorem from '../lib/editor/templates/lorem.json'
+
+import createEditor from '../lib/editor'
+import Typography from '../lib/editor/modules/Typography'
+import Document from '../lib/editor/modules/Document'
+import Link from '../lib/editor/modules/Link'
+import Image from '../lib/editor/modules/Image'
+import StyleguideTheme from '../lib/themes/styleguide'
+
+import lorem from '../lib/templates/lorem.json'
+
+const { Editor } = createEditor({
+  Typography,
+  Document,
+  Link,
+  Image
+})(StyleguideTheme)
 
 const getInitialState = () => {
   resetKeyGenerator()
   return {
-    state: Raw.deserialize(
-      lorem,
-      { terse: true }
-    )
+    state: Raw.deserialize(lorem, { terse: true })
   }
 }
 
@@ -30,7 +41,7 @@ export default class Index extends Component {
       <App>
         <Editor
           state={this.state.state}
-          onCommit={this.commitHandler.bind(this)}
+          onChange={this.commitHandler.bind(this)}
         />
       </App>
     )
