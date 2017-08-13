c52c5fe4d0539f41d3af47a3eb65fe7c95ffe4a7
patte <p@tte.io>
2017-08-13 22:46:46 +0200

    first draft: merge
    add merge to schema with description
    add merge migration
    add github helper allowing to commit with multiple parents
    
    TODOs:
      - handle non-fast-forward HEAD updates (aka auto-branching)

--- graphql/schema.js
+++ graphql/schema.js
@@ -16,6 +16,13 @@ type RootMutations {
 
   commit(login: String!, repository: String!, branch: String!, path: String!, commitOid: String!, message: String!, content: String!): Commit!
 
+  # creates a merge commits with the provided parents, message and content.
+  # The content is first submitted as a blob and a new tree is created
+  # setting the blob at path. The tree is based on the tree of the first
+  # parent's commit.
+  # The HEAD of the provided branch is updated to point to the merge-commit.
+  merge(login: String!, repository: String!, branch: String!, path: String!, content: String!, parents: [String!]!, message: String!): Commit!
+
   # Inform about my uncommited changes on the path.
   # Use path without branch prefix.
   uncommittedChanges(login: String!, repository: String!, path: String! action: Action): Boolean!
