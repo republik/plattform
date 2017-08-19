f0afda0fac3c87abca6a799ad98a1406ac866f73
patte <p@tte.io>
2017-08-20 01:58:27 +0200

    resolver for repo commits

--- graphql/resolvers/Repo.js
+++ graphql/resolvers/Repo.js
@@ -1,4 +1,78 @@
+const { createGithubFetchForUser } = require('../../lib/github')
+const GitHub = require('github-api')
+
 module.exports = {
+  commits: async (repo, args, {user}) => {
+    const refsQuery = `
+      query repository(
+        $login: String!,
+        $repoName: String!,
+        $first: Int
+      ) {
+        repository(owner: $login, name: $repoName) {
+          refs(refPrefix: "refs/heads/", first: $first) {
+            nodes {
+              name
+              target {
+                ... on Commit {
+                  oid
+                }
+              }
+            }
+          }
+        }
+      }
+    `
+    const [login, repoName] = repo.id.split('/')
+    const variables = {
+      login,
+      repoName,
+      first: 100
+    }
+
+    const {
+      errors,
+      data: {
+        repository: {
+          refs: {
+            nodes: refs
+          }
+        }
+      }
+    } = await createGithubFetchForUser(user)({ query: refsQuery, variables })
+    if (errors) {
+      throw new Error(JSON.stringify(errors))
+    }
+
+    const github = new GitHub({
+      token: user.githubAccessToken
+    })
+
+    return Promise.all(
+      refs.map(({ target: { oid } }) => {
+        return github
+          .getRepo(login, repoName)
+          .listCommits({
+            sha: oid,
+            per_page: 100
+          })
+          .then(response => response ? response.data : response)
+          .then(commits => commits
+            .map(({ sha, parents, commit: { message, author } }) => ({
+              id: sha,
+              parentIds: parents
+                ? parents.map(parent => parent.sha)
+                : [],
+              message,
+              author,
+              date: new Date(author.date)
+            }))
+          )
+      })
+    )
+      .then(commits => [].concat.apply([], ...commits))
+      .then(commits => [...new Set(commits)])
+  },
   uncommittedChanges: async ({owner: login, name: repository}, {path}, {redis, pgdb}) => {
     const key = `${login}/${repository}/${path}`
     const userIds = await redis.zrangeAsync(key, 0, -1)
