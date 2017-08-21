03a8b78a64da7cf5a43f7cecab3a8788cde589fb
Lukas Buenger <lukasbuenger@gmail.com>
2017-08-21 15:35:52 +0200

    module marks including styles

--- components/editor/styles/index.js
+++ components/editor/styles/index.js
@@ -1,20 +1,31 @@
 import { colors } from '@project-r/styleguide'
 
+const button = {
+  cursor: 'pointer',
+  color: colors.secondary,
+  transition: 'color 0.1s, opacity 0.1s',
+  '&[data-active="true"]': {
+    color: colors.primary
+  },
+  '&[data-disabled="true"]': {
+    opacity: 0.3,
+    color: colors.secondary
+  },
+  '&[data-active="false"][data-disabled="false"]:hover': {
+    color: colors.text
+  }
+}
+
 export default {
+  button,
+  markButton: {
+    textAlign: 'center',
+    display: 'inline-block',
+    width: '30px',
+    ...button
+  },
   blockButton: {
     display: 'block',
-    cursor: 'pointer',
-    color: colors.secondary,
-    transition: 'color 0.1s, opacity 0.1s',
-    '&[data-active="true"]': {
-      color: colors.primary
-    },
-    '&[data-disabled="true"]': {
-      opacity: 0.3,
-      color: colors.secondary
-    },
-    '&[data-active="false"][data-disabled="false"]:hover': {
-      color: colors.text
-    }
+    ...button
   }
 }
