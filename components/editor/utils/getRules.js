6a46c3800c475d3fed0d9f07d6272b9b740c3119
Thomas Preusse <tpreusse@users.noreply.github.com>
2017-08-28 00:09:45 +0200

    get serialization rules

--- components/editor/utils/getRules.js
+++ components/editor/utils/getRules.js
@@ -1,4 +1,9 @@
-export default plugins => plugins.reduce(
+const getRules = plugins => plugins.reduce(
   (all, plugin) => all.concat((plugin.schema || {}).rules).filter(Boolean),
   []
 )
+
+export default getRules
+
+export const getSerializationRules = plugins =>
+  getRules(plugins).filter(r => r.fromMdast || r.toMdast)
