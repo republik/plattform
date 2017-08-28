5ae350accf1a02bed399fd81e1a2e1e2cbeb35c8
Thomas Preusse <tpreusse@users.noreply.github.com>
2017-08-28 18:05:36 +0200

    rm old editor

--- pages/lorem.js
+++ pages/lorem.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react'
-import { Raw, resetKeyGenerator } from 'slate'
+import { resetKeyGenerator } from 'slate'
 import App from '../lib/App'
-import lorem from '../lib/editor/templates/lorem.json'
 import Editor, {serializer} from '../components/editor/NewsletterEditor'
 
 const getInitialState = () => {
   resetKeyGenerator()
   return {
-    state: Raw.deserialize(lorem, { terse: true })
-    // state: serializer.deserialize('# Lorem ipsum')
+    state: serializer.deserialize(`# Lorem ipsum
+
+> Lorem sipsum dolor sit amet, consectetur adipiscing elit. Proin vestibulum dui eget tellus fermentum, eu lobortis libero lacinia. Maecenas commodo lacus dignissim, aliquet risus non, scelerisque dui. Aliquam at massa rutrum ante laoreet pharetra non a nulla. Praesent imperdiet egestas dapibus. Nunc quis lorem vehicula, pharetra nibh quis, dignissim felis. Fusce in justo pharetra, lacinia lorem in, dignissim sem. Phasellus lacinia turpis massa. Etiam eu condimentum diam.
+
+![](/static/example.jpg)
+
+Nullam et metus mauris. Quisque scelerisque massa commodo, dapibus tortor in, condimentum dui. Integer pellentesque, dolor quis condimentum bibendum, lacus neque rutrum orci, vel egestas quam augue ac est. Mauris auctor fringilla neque. Maecenas varius pulvinar mattis. Donec aliquet odio quis nibh rhoncus, nec pretium odio vestibulum. Donec dictum ut lectus non posuere. Integer orci eros, scelerisque ac urna a, rutrum tincidunt lectus. Vestibulum volutpat enim non purus tincidunt dignissim. Praesent tincidunt magna eget augue ultrices mollis. Morbi sodales suscipit dui tincidunt malesuada. Integer ac mattis purus. Nulla ornare porta nisi sed fermentum. Praesent ut porttitor massa. Maecenas vestibulum felis purus, et bibendum lorem tempus sit amet. In commodo, ex sit amet posuere porttitor, metus turpis aliquet augue, eu feugiat leo dolor ut lorem.`)
   }
 }
 
