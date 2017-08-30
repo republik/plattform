dd9335b3ac907979476286e8a7cea2e45254fdb4
patte <p@tte.io>
2017-08-30 22:26:33 +0200

    milestones: add migration placeMilestone

---
+++ graphql/resolvers/RootMutations/placeMilestone.js
@@ -0,0 +1,41 @@
+const github = require('../../../lib/github')
+
+module.exports = async (
+  _,
+  { repoId, commitId, name, message },
+  { pgdb, req }
+) => {
+  if (!req.user) {
+    throw new Error('you need to signIn first')
+  }
+  if (!req.user.githubAccessToken) {
+    throw new Error('you need to sign in to github first')
+  }
+
+  const tag = await github.tag(
+    req.user,
+    repoId,
+    name,
+    message,
+    commitId
+  )
+
+  await github.createRef(
+    req.user.githubAccessToken,
+    repoId,
+    `refs/tags/${name}`,
+    tag.object.sha
+  )
+
+  return {
+    name: tag.tag,
+    message: tag.message,
+    commit: {
+      id: tag.object.sha
+    },
+    author: {
+      name: tag.tagger.name,
+      email: tag.tagger.email
+    }
+  }
+}
