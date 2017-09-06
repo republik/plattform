ba8b5279ccca499d14323a32b8910b990587c87e
Thomas Preusse <tpreusse@users.noreply.github.com>
2017-09-07 00:16:11 +0200

    facebook preview & customization

--- components/editor/MetaData.js
+++ components/editor/MetaData.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { css } from 'glamor'
-import { Map } from 'immutable'
+import { Map, Set } from 'immutable'
 
 import { Interaction, Field, colors } from '@project-r/styleguide'
 import AutosizeInput from 'react-textarea-autosize'
@@ -22,68 +22,158 @@ const styles = {
     minHeight: 40,
     paddingTop: '7px !important',
     paddingBottom: '6px !important'
+  }),
+  fbContainer: css({
+    backgroundColor: '#fff',
+    color: '#000',
+    width: 476
+  }),
+  fbImage: css({
+    width: 476,
+    height: 249,
+    backgroundSize: 'cover'
+  }),
+  fbText: css({
+    padding: '10px 12px',
+    maxHeight: 120,
+    overflow: 'hidden'
+  }),
+  fbTitle: css({
+    fontFamily: 'Georgia, serif',
+    fontSize: 18,
+    fontWeight: 500,
+    lineHeight: '22px',
+    maxHeight: 110,
+    overflow: 'hidden',
+    marginBottom: 5,
+    wordWrap: 'break-word'
+  }),
+  fbDescription: css({
+    fontFamily: 'sans-serif',
+    fontSize: 12,
+    lineHeight: '16px',
+    maxHeight: 80,
+    overflow: 'hidden'
+  }),
+  fbDomain: css({
+    fontFamily: 'sans-serif',
+    fontSize: 11,
+    lineHeight: '11px',
+    textTransform: 'uppercase',
+    color: '#90949c',
+    paddingTop: 9
   })
 }
 
-const Form = ({state, onChange, t}) => {
-  const defaultValues = Map({
-    title: '',
-    description: '',
-    image: ''
-  })
+const FBPreview = ({data}) => (
+  <div {...styles.fbContainer}>
+    <div {...styles.fbImage} style={{
+      backgroundImage: `url(${data.get('facebookImage') || data.get('image')})`
+    }} />
+    <div {...styles.fbText}>
+      <div {...styles.fbTitle}>
+        {data.get('facebookTitle') || data.get('title')}
+      </div>
+      <div {...styles.fbDescription}>
+        {data.get('facebookDescription') || data.get('description')}
+      </div>
+      <div {...styles.fbDomain}>
+        republik.ch
+      </div>
+    </div>
+  </div>
+)
+
+const Form = ({onInputChange, data, t}) => (
+  <div>
+    {data.map((value, key) => {
+      const label = t(`metaData/field/${key}`, undefined, key)
+
+      if (key.match(/image/i)) {
+        return <ImageInput key={key}
+          label={label}
+          src={value}
+          onChange={onInputChange(key)} />
+      }
+      let renderInput
+      if (key.match(/description/i)) {
+        renderInput = ({ref, ...inputProps}) => (
+          <AutosizeInput {...styles.autoSize}
+            {...inputProps}
+            inputRef={ref} />
+        )
+      }
+      return (
+        <Field
+          key={key}
+          label={label}
+          name={key}
+          value={value}
+          renderInput={renderInput}
+          black
+          onChange={onInputChange(key)} />
+      )
+    }).toArray()}
+  </div>
+)
+
+const MetaData = ({state, onChange, t}) => {
   const node = state.document
-  const data = defaultValues.merge(
-    node.data
+
+  const genericKeys = Set([
+    'slug',
+    'emailSubject',
+    'title',
+    'description',
+    'image'
+  ])
+  const gernericDefaultValues = Map(genericKeys.map(key => [key, '']))
+  const genericData = gernericDefaultValues.merge(
+    node.data.filter((_, key) => genericKeys.has(key))
   )
+
+  const fbKeys = Set([
+    'facebookTitle',
+    'facebookDescription',
+    'facebookImage'
+  ])
+  const fbDefaultValues = Map(fbKeys.map(key => [key, '']))
+  const fbData = fbDefaultValues.merge(
+    node.data.filter((_, key) => fbKeys.has(key))
+  )
+
+  const onInputChange = key => (_, value) => {
+    const newData = node.data.remove('auto')
+
+    onChange(
+      state
+        .transform()
+        .setNodeByKey(node.key, {
+          data: value
+            ? newData.set(key, value)
+            : newData.remove(key)
+        })
+        .apply()
+    )
+  }
 
   return (
     <div {...styles.container}>
       <div {...styles.center}>
-        <Interaction.H2>{t('metaData/title')}</Interaction.H2>
-        {data.map((value, key) => {
-          const label = t(`metaData/field/${key}`, undefined, key)
-          const onInputChange = (_, value) => {
-            const newData = node.data.remove('auto')
-
-            onChange(
-              state
-                .transform()
-                .setNodeByKey(node.key, {
-                  data: value
-                    ? newData.set(key, value)
-                    : newData.remove(key)
-                })
-                .apply()
-            )
-          }
-          if (key.match(/image/i)) {
-            return <ImageInput key={key}
-              label={label}
-              src={value}
-              onChange={onInputChange} />
-          }
-          let renderInput
-          if (key.match(/description/i)) {
-            renderInput = ({ref, ...inputProps}) => (
-              <AutosizeInput {...styles.autoSize}
-                {...inputProps}
-                inputRef={ref} />
-            )
-          }
-          return (
-            <Field
-              key={key}
-              label={label}
-              name={key}
-              value={value}
-              renderInput={renderInput}
-              black
-              onChange={onInputChange} />
-          )
-        }).toArray()}
+        <Interaction.H2>
+          {t('metaData/title')}
+        </Interaction.H2>
+        <br />
+        <Form data={genericData} onInputChange={onInputChange} t={t} />
+        <br /><br />
+        <Interaction.H3>
+          Facebook Vorschau
+        </Interaction.H3>
+        <FBPreview data={node.data} />
+        <Form data={fbData} onInputChange={onInputChange} t={t} />
       </div>
     </div>
   )
 }
 
-export default withT(Form)
+export default withT(MetaData)
