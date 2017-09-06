912b6e1d175e8b34a3b1ff822e5e377cfd0c821d
patte <p@tte.io>
2017-09-06 21:57:45 +0200

    document.meta: cleanup unused TreeUtils

--- graphql/resolvers/Document.js
+++ graphql/resolvers/Document.js
@@ -1,40 +1,5 @@
-// const Immutable = require('immutable')
-// https://github.com/lukasbuenger/immutable-treeutils/issues/8
-// const TreeUtils = require('immutable-treeutils')
-// const TreeUtils = require('../../lib/TreeUtils')
-
 module.exports = {
   meta: async (doc, args) => {
     return doc.mdast.meta
-
-    // TODO migrate to mdast
-    // wait until decision about how meta data is embedded in document
-    /*
-
-    const treeUtils = new TreeUtils(
-      Immutable.Seq.of(),
-      'id',
-      'nodes'
-    )
-    const documentTree = Immutable.fromJS(JSON.parse(doc.content))
-    const headerNodes = treeUtils.find(
-      documentTree,
-      node => node.get('type') === 'H1'
-    )
-
-    if (!headerNodes) {
-      return {title: 'empty'}
-    }
-
-    const title = documentTree
-      .getIn(headerNodes)
-      .get('nodes')
-      .first()
-      .get('text')
-
-    return {
-      title
-    }
-    */
   }
 }
