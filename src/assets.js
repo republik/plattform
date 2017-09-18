b5a70e59435205dededd7b4da9bf40ab695acc37
Thomas Preusse <tpreusse@users.noreply.github.com>
2017-09-18 14:45:35 +0200

    get ride of global state and regex all together

--- src/assets.js
+++ src/assets.js
@@ -1,7 +1,5 @@
 const { githubRest } = require('../lib/github')
 
-const blobRegex = /.*\/(.*?)\./g
-
 // Because githubs get-contents is limited to 1MB the current
 // approach is to extract the blobs sha from the filename,
 // get the file via get-blob, and send the content directly as response.
@@ -18,11 +16,10 @@ module.exports = (server) => {
       path
     } = req.params
 
-    const match = blobRegex.exec(path)
-    if (!match || !match[1]) {
-      return res.status(404).end('invalid url')
-    }
-    const blobSha = match[1]
+    const blobSha = path
+      .split('/')
+      .pop()
+      .split('.')[0]
 
     const result = await githubRest.gitdata.getBlob({
       owner: login,
