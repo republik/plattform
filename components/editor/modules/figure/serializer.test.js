559ef44f8b365ee7bccc7d56023467babfcad6be
Thomas Preusse <tpreusse@users.noreply.github.com>
2017-09-18 15:25:41 +0200

    adapt figure serialization test

--- components/editor/modules/figure/serializer.test.js
+++ components/editor/modules/figure/serializer.test.js
@@ -1,39 +1,24 @@
 import test from 'tape'
-import img, { FIGURE } from './'
-import {PARAGRAPH} from '../paragraph'
+import figure, { FIGURE } from './'
 import getRules from '../../utils/getRules'
 import MarkdownSerializer from '../../../../lib/serializer'
 
 const serializer = new MarkdownSerializer({
-  rules: getRules(img.plugins).concat({
-    match: (object) => object.kind === 'block' && object.type === PARAGRAPH,
-    matchMdast: (node) => node.type === 'paragraph',
-    fromMdast: (node, index, parent, visitChildren) => ({
-      kind: 'block',
-      type: PARAGRAPH,
-      nodes: visitChildren(node)
-    }),
-    toMdast: (object, index, parent, visitChildren) => ({
-      type: 'paragraph',
-      children: visitChildren(object)
-    })
-  })
+  rules: getRules(figure.plugins)
 })
 
-test('image serialization', assert => {
-  const state = serializer.deserialize('![Alt](example.com/img.jpg "Title")')
+test('figure serialization', assert => {
+  const md = `<section><h6>${FIGURE}</h6>
+
+![Alt](example.com/img.jpg)
+
+<hr /></section>`
+  const state = serializer.deserialize(md)
   const node = state.document.nodes.first()
 
-  assert.equal(node.kind, 'block')
-  assert.equal(node.type, PARAGRAPH)
+  assert.equal(node.getIn(['data', 'src']), 'example.com/img.jpg')
+  assert.equal(node.getIn(['data', 'alt']), 'Alt')
 
-  const img = node.nodes.find(node => node.type === FIGURE)
-  assert.ok(img)
-
-  assert.equal(img.getIn(['data', 'src']), 'example.com/img.jpg')
-  assert.equal(img.getIn(['data', 'alt']), 'Alt')
-  assert.equal(img.getIn(['data', 'title']), 'Title')
-
-  assert.equal(serializer.serialize(state).trimRight(), '![Alt](example.com/img.jpg "Title")')
+  assert.equal(serializer.serialize(state).trimRight(), md)
   assert.end()
 })
