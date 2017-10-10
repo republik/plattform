e488fc6d447f76907016b86e981faef187637970
Thomas Preusse <tpreusse@users.noreply.github.com>
2017-10-10 22:51:56 +0200

    improve preview

--- components/Publication/PublishForm.js
+++ components/Publication/PublishForm.js
@@ -94,9 +94,25 @@ mutation publish(
 }
 `
 
+const PADDING_X = 5
+
 const PREVIEW_SIZES = [
-  {label: 'Mobile', width: 320, height: 480},
-  {label: 'Desktop', width: '100%', height: 600}
+  {
+    label: 'mobile',
+    width: 320,
+    height: 480,
+    paddingTop: 40,
+    paddingBottom: 40,
+    borderRadius: 10
+  },
+  {
+    label: 'desktop',
+    width: 1024,
+    height: 800,
+    paddingTop: PADDING_X,
+    paddingBottom: PADDING_X,
+    borderRadius: 3
+  }
 ]
 
 class PublishForm extends Component {
@@ -118,6 +134,22 @@ class PublishForm extends Component {
       css: '',
       size: PREVIEW_SIZES[0]
     }
+    this.containerRef = ref => { this.container = ref }
+
+    this.measure = () => {
+      this.setState(() => ({
+        width:
+          this.container &&
+          this.container.getBoundingClientRect().width
+      }))
+    }
+  }
+  componentDidMount () {
+    window.addEventListener('resize', this.measure)
+    this.measure()
+  }
+  componentWillUnmount () {
+    window.removeEventListener('resize', this.measure)
   }
   transferCSS () {
     this.setState({
@@ -136,186 +168,213 @@ class PublishForm extends Component {
     const scheduledAtError = scheduledAtDate === null && t('publish/label/scheduledAt')
 
     return (
-      <Loader loading={loading} error={error} render={() => {
-        const { commit, commit: { document: { meta } } } = repo
+      <div ref={this.containerRef}>
+        <Loader loading={loading} error={error} render={() => {
+          const { commit, commit: { document: { meta } } } = repo
 
-        const errors = [
-          !meta.slug && t('publish/validation/slug/empty'),
-          !meta.publishDate && t('publish/validation/publishDate/empty'),
-          (updateMailchimp && !meta.emailSubject) && t('publish/validation/emailSubject/empty')
-        ].filter(Boolean)
-        const hasErrors = errors.length > 0
+          const errors = [
+            !meta.slug && t('publish/validation/slug/empty'),
+            !meta.publishDate && t('publish/validation/publishDate/empty'),
+            (updateMailchimp && !meta.emailSubject) && t('publish/validation/emailSubject/empty')
+          ].filter(Boolean)
+          const hasErrors = errors.length > 0
 
-        const {
-          size,
-          css
-        } = this.state
+          const {
+            size,
+            css,
+            width
+          } = this.state
 
-        return (
-          <div>
-            <Label>{t('publish/commit/selected')}</Label>
-            <Interaction.P>
-              {commit.message}
-            </Interaction.P>
-            <Label>
-              {commit.author.name}<br />
-              {timeFormat(new Date(commit.date))}
-            </Label>
-            <Interaction.P>
+          const scale = width
+            ? Math.min(1, width / (size.width + PADDING_X * 2))
+            : 1
+
+          return (
+            <div>
+              <Label>{t('publish/commit/selected')}</Label>
+              <Interaction.P>
+                {commit.message}
+              </Interaction.P>
               <Label>
-                <Link
-                  route='repo/tree'
-                  params={{
-                    repoId: repoId.split('/')
+                {commit.author.name}<br />
+                {timeFormat(new Date(commit.date))}
+              </Label>
+              <Interaction.P>
+                <Label>
+                  <Link
+                    route='repo/tree'
+                    params={{
+                      repoId: repoId.split('/')
+                    }}
+                  >
+                    <a {...linkRule}>
+                      {t('publish/commit/change')}
+                    </a>
+                  </Link>
+                </Label>
+              </Interaction.P>
+
+              <br /><br />
+
+              {hasErrors && (
+                <Interaction.P style={{color: colors.error}}>
+                  {t('publish/validation/hasErrors')}
+                  <ul>
+                    {errors.map((error, i) => (
+                      <li key={i}>
+                        {error}
+                      </li>
+                    ))}
+                  </ul>
+                  <br /><br />
+                </Interaction.P>
+              )}
+
+              <Checkbox checked={prepublication} onChange={(_, value) => {
+                this.setState({
+                  prepublication: value
+                })
+              }}>
+                {t('publish/label/prepublication')}
+              </Checkbox>
+              <br />
+              <br />
+              <Checkbox checked={updateMailchimp} onChange={(_, value) => {
+                this.setState({
+                  updateMailchimp: value
+                })
+              }}>
+                {t('publish/label/updateMailchimp')}
+              </Checkbox>
+              <br />
+              <br />
+              <Checkbox checked={scheduled} onChange={(_, value) => {
+                this.setState({
+                  scheduled: value
+                })
+              }}>
+                {t('publish/label/scheduled')}
+              </Checkbox>
+
+              {scheduled && <Field
+                renderInput={(inputProps) => (
+                  <MaskedInput
+                    {...inputProps}
+                    {...styles.mask}
+                    placeholderChar={'_'}
+                    mask={'11.11.1111 11:11'} />
+                )}
+                label={t('publish/label/scheduledAt')}
+                value={scheduledAt}
+                error={scheduledAtError}
+                onChange={(_, value) => {
+                  this.setState({
+                    scheduledAt: value
+                  })
+                }} />}
+
+              <br /><br /><br />
+
+              {publishing ? (
+                <div style={{textAlign: 'center'}}>
+                  <InlineSpinner />
+                </div>
+              ) : (
+                <div>
+                  {!!this.state.error && (
+                    <ErrorMessage error={this.state.error} />
+                  )}
+                  <Button block primary disabled={hasErrors} onClick={() => {
+                    if (scheduled && scheduledAtError) {
+                      return
+                    }
+                    this.setState(() => ({publishing: true}))
+                    this.props.publish({
+                      repoId,
+                      commitId: commit.id,
+                      prepublication,
+                      updateMailchimp,
+                      scheduledAt: scheduled ? scheduledAtDate : undefined
+                    }).then(() => {
+                      Router.pushRoute('repo/tree', {
+                        repoId: repoId.split('/')
+                      })
+                    }).catch((error) => {
+                      this.setState(() => ({
+                        publishing: false,
+                        error: error
+                      }))
+                    })
+                  }}>
+                    {t('publish/trigger')}
+                  </Button>
+                </div>
+              )}
+              <br /><br />
+              <Interaction.H2>{t('publish/preview/title')}</Interaction.H2>
+
+              <Interaction.P>
+                {intersperse(
+                  PREVIEW_SIZES.map(previewSize => {
+                    const label = t(
+                      `publish/preview/${previewSize.label}`,
+                      undefined,
+                      previewSize.label
+                    )
+                    if (previewSize === size) {
+                      return [
+                        label,
+                        scale !== 1 && ` (${Math.round(scale * 10) / 10}x)`
+                      ].filter(Boolean).join(' ')
+                    }
+                    return (
+                      <A
+                        key={label}
+                        href='#'
+                        onClick={e => {
+                          e.preventDefault()
+                          this.setState({size: previewSize})
+                        }}
+                      >
+                        {label}
+                      </A>
+                    )
+                  }),
+                  () => ' '
+                )}
+              </Interaction.P>
+
+              <div style={{
+                // transition: 'padding 400ms, border-radius 400ms, width 400ms',
+                paddingLeft: PADDING_X,
+                paddingRight: PADDING_X,
+                paddingTop: size.paddingTop,
+                paddingBottom: size.paddingBottom,
+                borderRadius: size.borderRadius,
+                backgroundColor: '#eee',
+                width: size.width + PADDING_X * 2,
+                transformOrigin: '0% 0%',
+                transform: `scale(${scale})`
+              }}>
+                <Frame
+                  frameBorder='0'
+                  allowTransparency='true'
+                  contentDidMount={() => this.transferCSS()}
+                  head={[
+                    <style key='glamor'>{css}</style>
+                  ]}
+                  style={{
+                    width: '100%',
+                    height: size.height
                   }}
                 >
-                  <a {...linkRule}>
-                    {t('publish/commit/change')}
-                  </a>
-                </Link>
-              </Label>
-            </Interaction.P>
-
-            <br /><br />
-
-            {hasErrors && (
-              <Interaction.P style={{color: colors.error}}>
-                {t('publish/validation/hasErrors')}
-                <ul>
-                  {errors.map((error, i) => (
-                    <li key={i}>
-                      {error}
-                    </li>
-                  ))}
-                </ul>
-                <br /><br />
-              </Interaction.P>
-            )}
-
-            <Checkbox checked={prepublication} onChange={(_, value) => {
-              this.setState({
-                prepublication: value
-              })
-            }}>
-              {t('publish/label/prepublication')}
-            </Checkbox>
-            <br />
-            <br />
-            <Checkbox checked={updateMailchimp} onChange={(_, value) => {
-              this.setState({
-                updateMailchimp: value
-              })
-            }}>
-              {t('publish/label/updateMailchimp')}
-            </Checkbox>
-            <br />
-            <br />
-            <Checkbox checked={scheduled} onChange={(_, value) => {
-              this.setState({
-                scheduled: value
-              })
-            }}>
-              {t('publish/label/scheduled')}
-            </Checkbox>
-
-            {scheduled && <Field
-              renderInput={(inputProps) => (
-                <MaskedInput
-                  {...inputProps}
-                  {...styles.mask}
-                  placeholderChar={'_'}
-                  mask={'11.11.1111 11:11'} />
-              )}
-              label={t('publish/label/scheduledAt')}
-              value={scheduledAt}
-              error={scheduledAtError}
-              onChange={(_, value) => {
-                this.setState({
-                  scheduledAt: value
-                })
-              }} />}
-
-            <br /><br /><br />
-
-            {publishing ? (
-              <div style={{textAlign: 'center'}}>
-                <InlineSpinner />
+                  {renderMdast(commit.document.content, newsletterTemplate)}
+                </Frame>
               </div>
-            ) : (
-              <div>
-                {!!this.state.error && (
-                  <ErrorMessage error={this.state.error} />
-                )}
-                <Button block primary disabled={hasErrors} onClick={() => {
-                  if (scheduled && scheduledAtError) {
-                    return
-                  }
-                  this.setState(() => ({publishing: true}))
-                  this.props.publish({
-                    repoId,
-                    commitId: commit.id,
-                    prepublication,
-                    updateMailchimp,
-                    scheduledAt: scheduled ? scheduledAtDate : undefined
-                  }).then(() => {
-                    Router.pushRoute('repo/tree', {
-                      repoId: repoId.split('/')
-                    })
-                  }).catch((error) => {
-                    this.setState(() => ({
-                      publishing: false,
-                      error: error
-                    }))
-                  })
-                }}>
-                  {t('publish/trigger')}
-                </Button>
-              </div>
-            )}
-            <br /><br />
-            <Interaction.H2>Vorschau</Interaction.H2>
-
-            <Interaction.P>
-              {intersperse(
-                PREVIEW_SIZES.map(previewSize => {
-                  if (previewSize === size) {
-                    return previewSize.label
-                  }
-                  return (
-                    <A
-                      key={previewSize.label}
-                      href='#'
-                      onClick={e => {
-                        e.preventDefault()
-                        this.setState({size: previewSize})
-                      }}
-                    >
-                      {previewSize.label}
-                    </A>
-                  )
-                }),
-                () => ' '
-              )}
-            </Interaction.P>
-
-            <Frame
-              frameBorder='0'
-              allowTransparency='true'
-              contentDidMount={() => this.transferCSS()}
-              head={[
-                <style key='glamor'>{css}</style>
-              ]}
-              style={{
-                border: '1px solid black',
-                width: size.width,
-                height: size.height
-              }}
-            >
-              {renderMdast(commit.document.content, newsletterTemplate)}
-            </Frame>
-          </div>
-        )
-      }} />
+            </div>
+          )
+        }} />
+      </div>
     )
   }
 }
