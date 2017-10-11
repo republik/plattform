9adb73d5939112ff88f6f9274fe340e444ae5224
Thomas Preusse <tpreusse@users.noreply.github.com>
2017-10-11 11:54:02 +0200

    nest root rule

--- components/Templates/NewsletterEmail/index.js
+++ components/Templates/NewsletterEmail/index.js
@@ -47,90 +47,92 @@ const schema = {
   rules: [
     {
       matchMdast: matchType('root'),
-      component: Container
-    },
-    {
-      matchMdast: matchZone('COVER'),
-      component: Cover,
-      getData: node => {
-        const img = node.children[0].children[0]
-        return {
-          alt: img.alt,
-          src: img.url
-        }
-      },
+      component: Container,
       rules: [
         {
-          matchMdast: matchImageParagraph,
-          component: () => null,
-          isVoid: true
-        },
-        {
-          matchMdast: matchHeading(1),
-          component: Title
-        },
-        {
-          matchMdast: matchType('blockquote'),
-          component: Lead,
-          rules: [
-            paragraph
-          ]
-        }
-      ]
-    },
-    {
-      matchMdast: matchZone('CENTER'),
-      component: Center,
-      rules: [
-        paragraph,
-        {
-          matchMdast: matchHeading(2),
-          component: H2
-        },
-        {
-          matchMdast: matchHeading(3),
-          component: H3
-        },
-        {
-          matchMdast: matchZone('FIGURE'),
-          component: Figure,
+          matchMdast: matchZone('COVER'),
+          component: Cover,
+          getData: node => {
+            const img = node.children[0].children[0]
+            return {
+              alt: img.alt,
+              src: img.url
+            }
+          },
           rules: [
             {
               matchMdast: matchImageParagraph,
-              component: Image,
-              getData: node => ({
-                src: node.children[0].url,
-                alt: node.children[0].alt
-              }),
+              component: () => null,
               isVoid: true
             },
             {
-              matchMdast: matchParagraph,
-              component: Caption,
-              getData: (node, parent) => (parent && parent.data) || {},
-              rules: paragraph.rules
+              matchMdast: matchHeading(1),
+              component: Title
+            },
+            {
+              matchMdast: matchType('blockquote'),
+              component: Lead,
+              rules: [
+                paragraph
+              ]
             }
           ]
         },
         {
-          matchMdast: matchType('blockquote'),
-          component: Blockquote,
+          matchMdast: matchZone('CENTER'),
+          component: Center,
           rules: [
-            paragraph
-          ]
-        },
-        {
-          matchMdast: matchType('list'),
-          component: List,
-          getData: node => ({
-            ordered: node.ordered,
-            start: node.start
-          }),
-          rules: [
+            paragraph,
             {
-              matchMdast: matchType('listItem'),
-              component: ListItem,
-              rules: [paragraph]
+              matchMdast: matchHeading(2),
+              component: H2
+            },
+            {
+              matchMdast: matchHeading(3),
+              component: H3
+            },
+            {
+              matchMdast: matchZone('FIGURE'),
+              component: Figure,
+              rules: [
+                {
+                  matchMdast: matchImageParagraph,
+                  component: Image,
+                  getData: node => ({
+                    src: node.children[0].url,
+                    alt: node.children[0].alt
+                  }),
+                  isVoid: true
+                },
+                {
+                  matchMdast: matchParagraph,
+                  component: Caption,
+                  getData: (node, parent) => (parent && parent.data) || {},
+                  rules: paragraph.rules
+                }
+              ]
+            },
+            {
+              matchMdast: matchType('blockquote'),
+              component: Blockquote,
+              rules: [
+                paragraph
+              ]
+            },
+            {
+              matchMdast: matchType('list'),
+              component: List,
+              getData: node => ({
+                ordered: node.ordered,
+                start: node.start
+              }),
+              rules: [
+                {
+                  matchMdast: matchType('listItem'),
+                  component: ListItem,
+                  rules: [paragraph]
+                }
+              ]
             }
           ]
         }
