9c23f33e9f2a3c560785431438365eb333149c3a
patte <p@tte.io>
2017-10-16 15:50:56 +0200

    vote on comments

---
+++ graphql/resolvers/_RootMutations/discussion/upvoteComment.js
@@ -0,0 +1,9 @@
+const Roles = require('../../../../lib/Roles')
+const voteComment = require('./lib/voteComment')
+
+module.exports = async (_, args, { pgdb, user, t }) => {
+  Roles.ensureUserHasRole(user, 'member')
+  const { id } = args
+  const vote = 1
+  return voteComment(id, vote, pgdb, user, t)
+}
