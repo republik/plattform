8f6e27098cf9d29fbc651c22667934aa36c786c8
Daniel Pfänder <daniel.pfaender@project-r.construction>
2017-10-18 18:15:05 +0200

    Clean up commented code

--- graphql/resolvers/_RootQuerys/me.js
+++ graphql/resolvers/_RootQuerys/me.js
@@ -28,15 +28,10 @@ module.exports = async (_, args, { user, pgdb }) => {
   return {
     ...user,
     name,
-    /* async address(user, args, { pgdb }) {
-      if (!user.addressId) return null
-      return pgdb.public.addresses.findOne({ id: user.addressId })
-    }, */
     address: await pgdb.public.addresses.findOne({ id: user.addressId }),
     initials: () => getInitials(name),
     publicUser: {...user, name}
     // TODO: Implement memberships
     // TODO: Implement pledges
-    // TODO: Implement testimonial
   }
 }
