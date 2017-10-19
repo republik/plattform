45f8971b18fbc4f85313a071d1c17d7d6df1c75d
patte <p@tte.io>
2017-10-19 19:30:58 +0200

    PublicUser testimonial resolver, remove mock

--- graphql/resolvers/_RootQuerys/publicUser.js
+++ graphql/resolvers/_RootQuerys/publicUser.js
@@ -1,9 +1,10 @@
-const graphqlTools = require('graphql-tools')
 const Roles = require('../../../lib/Roles')
+const createUser = require('../../../lib/factories/createUser')
 
 module.exports = async (_, args, { pgdb, user }) => {
   const { id } = args
   const profileUser = await pgdb.public.users.findOne({ id })
+    .then(user => createUser(user))
   if (!profileUser) {
     return null
   }
@@ -17,19 +18,8 @@ module.exports = async (_, args, { pgdb, user }) => {
     return {
       ...profileUser,
       email,
-      // TODO: Replace mocks below with actual data.
-      testimonial: {
-        id: '58ef3a0d-82c1-4ead-add5-ec08f6f0d75f',
-        name: 'Daniel Pfänder',
-        quote:
-          'Journalismus heißt, etwas zu drucken, von dem jemand will, dass es nicht gedruckt wird. Alles andere ist Public Relations.',
-        role: 'Entwickler',
-        image:
-          'https://assets.republik.ch/testimonials/58ef3a0d-82c1-4ead-add5-ec08f6f0d75f_384x384.jpeg',
-        sequenceNumber: 754
-      },
-      badges: () => new graphqlTools.MockList([0, 5]),
-      latestComments: () => new graphqlTools.MockList([20, 50])
+      badges: [], // TODO
+      latestComments: [] // TODO
     }
   }
 }
