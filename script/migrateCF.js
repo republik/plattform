0db4060a03a6d0fd2cbfd7536ee091d450d8d0de
patte <p@tte.io>
2017-10-20 14:08:34 +0200

    make migrateCF script work on heroku, describe usage

--- script/migrateCF.js
+++ script/migrateCF.js
@@ -1,27 +1,47 @@
 /*
- * this script migrates the tables from crowdfunding-backend
- * node script/migrateCF.js PG_USERNAME
+ * This script migrates the crowdfunding-backend DB to the republik-backend DB schema.
+ * This script is not idempotent, don't run it twice.
+ * usage:
+ * node script/migrateCF.js [PG_USERNAME]
+ *
+ * If you provide PG_USERNAME, this script first clones the local DB named
+ * postgres (crowdfunding default) to the local DB named republik.
+ * Then it continues to convert that cloned DB. This doesn't work on heroku.
+ *
+ * If you omitt PG_USERNAME this script expects a DB in the crowdfunding-backend schema at
+ * DATABASE_URL and converts it to match the schema necessary for republik-backend.
+ *
+ * To get the data to heroku either: (all commands in this repos root, with heroku remote present)
+ *   - do the convertion locally and push your local DB to heroku:
+ *     heroku pg:push postgres://PG_USERNAME@localhost:5432/republik HEROKU_DATABASE_URL
+ *     heroku restart
+ *   - clone the crowdfunding DB to the republik DB, then convert:
+ *     heroku pg:copy r-cf-api::HEROKU_POSTGRESQL_OLIVE_URL HEROKU_DATABASE_URL -a republik-api-staging
+ *     heroku run node script/migrateCF.js
+ *     heroku restart
  */
 
 const exec = require('child-process-promise').exec
 const PgDb = require('../lib/pgdb')
 
 const username = process.argv[2]
-if (!username) {
-  throw new Error('your (postgres) username must be provided as the first argument to this script')
-}
 
 Promise.resolve().then(async () => {
-  await exec(`dropdb republik`)
-    .then(r => console.log(r.stdout, r.stderr))
-  await exec(`createdb republik`)
-    .then(r => console.log(r.stdout, r.stderr))
+  if (username) {
+    await exec(`dropdb republik`)
+      .then(r => console.log(r.stdout, r.stderr))
+    await exec(`createdb republik`)
+      .then(r => console.log(r.stdout, r.stderr))
 
-  const DATABASE_URL = `postgres://${username}@localhost:5432/republik`
-  await exec(`pg_dump postgres://${username}@localhost:5432/postgres | psql ${DATABASE_URL}`)
-    .then(r => console.log(r.stdout, r.stderr))
+    const DATABASE_URL = `postgres://${username}@localhost:5432/republik`
 
-  process.env.DATABASE_URL = DATABASE_URL
+    await exec(`pg_dump postgres://${username}@localhost:5432/postgres | psql ${DATABASE_URL}`)
+      .then(r => console.log(r.stdout, r.stderr))
+
+    process.env.DATABASE_URL = DATABASE_URL
+  } else {
+    require('dotenv').config()
+  }
   const pgdb = await PgDb.connect()
 
   await pgdb.run(`
