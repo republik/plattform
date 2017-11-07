5166158b3e8e058e8e3e474239edb5ff3c430d01
Daniel Pfänder <daniel.pfaender@project-r.construction>
2017-11-07 12:12:19 +0100

    Bump styleguide version to 3.3.0 and use styelguide RawHtml/Loader/Spinner

--- components/Auth/SignIn.js
+++ components/Auth/SignIn.js
@@ -6,12 +6,12 @@ import {compose} from 'redux'
 import withT from '../../lib/withT'
 import {validate as isEmail} from 'email-validator'
 import ErrorMessage from '../ErrorMessage'
-import {InlineSpinner} from '../Spinner'
-import RawHtml from '../RawHtml'
 
 import {
   Button, Interaction,
-  Field
+  Field,
+  InlineSpinner,
+  RawHtml
 } from '@project-r/styleguide'
 
 import Poller from './Poller'
