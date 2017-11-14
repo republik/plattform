3b1574eb3fe0eeafd6adf710677c8d4e604036b0
Lukas Buenger <lukasbuenger@gmail.com>
2017-11-14 12:30:07 +0100

    tests fixed

--- components/editor/modules/embed/serializer.test.js
+++ components/editor/modules/embed/serializer.test.js
@@ -30,30 +30,19 @@ paragraphModule.name = 'paragraph'
 const serializer = embedModule.helpers.serializer
 
 test('embed serialization', assert => {
-  const md = `<section><h6>${TYPE}</h6>
-\`\`\`
-{
-  "embedType": "youtube",
-  "embedData": {
-    "id": "8bl19RoR7lc"
-  }
-}
-\`\`\`
+  const md = `<section><h6>EMBED</h6>
 
-[https://www.youtube.com/watch?v=8bl19RoR7lc](https://www.youtube.com/watch?v=8bl19RoR7lc)
+[https://www.youtube.com/watch?v=8bl19RoR7lc](https://www.youtube.com/watch?v=8bl19RoR7lc "https&#x3A;//www.youtube.com/watch?v=8bl19RoR7lc")
 
 <hr /></section>`
   const value = serializer.deserialize(md)
-  const node = value.document.nodes.first()
+  const embed = value.document.nodes.first()
 
-  const embed = node.nodes.first()
   assert.equal(embed.kind, 'block')
   assert.equal(embed.type, 'EMBED')
 
-  assert.equal(embed.getIn(['data', 'embedTyoe']), 'youtube')
-  assert.equal(embed.getIn(['data', 'embedData', 'id']), '8bl19RoR7lc')
-  assert.equal(embed.getIn(['data', 'embedData', 'originalUrl']), 'https://www.youtube.com/watch?v=8bl19RoR7lc')
-
+  assert.equal(embed.getIn(['data', 'url']), 'https://www.youtube.com/watch?v=8bl19RoR7lc')
+  //
   assert.equal(serializer.serialize(value).trimRight(), md)
   assert.end()
 })
