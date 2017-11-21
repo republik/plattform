67b3eeb06baf438499e596067cc889b7fc1362e4
Lukas Buenger <lukasbuenger@gmail.com>
2017-11-21 15:14:34 +0100

    helpers to display declarative prop forms. incl tests and docs

---
+++ components/editor/SidebarForm.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { css } from 'glamor'
+
+const defaultGetWidth = () => '100%'
+
+const GUTTER = 20
+const styles = {
+  grid: css({
+    clear: 'both',
+    width: `calc(100% + ${GUTTER}px)`,
+    margin: `0 -${GUTTER / 2}px`,
+    ':after': {
+      content: '""',
+      display: 'table',
+      clear: 'both'
+    }
+  }),
+  span: css({
+    float: 'left',
+    paddingLeft: `${GUTTER / 2}px`,
+    paddingRight: `${GUTTER / 2}px`,
+    minHeight: 1,
+    width: '50%'
+  })
+}
+
+export default ({ children, getWidth = defaultGetWidth, ...props }) => {
+  const wrappedChildren = children.map(
+    (child, index) => (
+      <div
+        key={`input-${index}`}
+        {...styles.span}
+        style={{width: getWidth()}}>
+        {child}
+      </div>
+    )
+  )
+  return (
+    <div {...styles.grid}>
+      {wrappedChildren}
+    </div>
+  )
+}
---
+++ components/editor/utils/createOnFieldChange.test.js
@@ -0,0 +1,95 @@
+import test from 'tape'
+import createOnFieldChange from './createOnFieldChange'
+import { Value } from 'slate'
+
+const rawDoc = {
+  'nodes': [
+    {
+      kind: 'block',
+      type: 'paragraph',
+      nodes: [
+        {
+          kind: 'text',
+          leaves: [
+            {
+              text: 'Hello Node w/o data!'
+            }
+          ]
+        }
+      ]
+    },
+    {
+      kind: 'block',
+      type: 'paragraph',
+      data: {
+        foo: 'bar'
+      },
+      nodes: [
+        {
+          kind: 'text',
+          leaves: [
+            {
+              text: 'Hello Node with data!'
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}
+
+const initialState = Value.fromJSON({
+  document: rawDoc
+})
+
+test('utils.createOnFieldChange: handler called with value', assert => {
+  assert.plan(1)
+  const paragraphNode = initialState.document.nodes.first()
+
+  const onChange = change => {
+    assert.equal(
+      change.value.document.nodes.first().data.get('foo'),
+      'bar',
+      'sets the data[key] property to the second argument passed to the handler function'
+    )
+  }
+
+  createOnFieldChange(onChange, initialState, paragraphNode, 'foo', null, 'bar')
+})
+
+test('utils.createOnFieldChange: handler called without value', assert => {
+  assert.plan(1)
+
+  const paragraphNodeWithData = initialState.document.nodes.get(1)
+
+  const onChange = change => {
+    assert.equal(
+      change.value.document.nodes.get(1).data.has('foo'),
+      false,
+      'removes the data[key] property if the second argument passed to the handler is falsy'
+    )
+  }
+
+  createOnFieldChange(onChange, initialState, paragraphNodeWithData, 'foo', null, undefined)
+})
+
+test('utils.createOnFieldChange: partial application', assert => {
+  assert.plan(1)
+
+  const paragraphNode = initialState.document.nodes.first()
+
+  const onChange = () => true
+
+  const args = [onChange, initialState, paragraphNode, 'foo', null, 'bar']
+
+  const result = args.reduce(
+    (fn, arg) => fn(arg),
+    createOnFieldChange
+  )
+
+  assert.equal(
+    typeof result !== 'function',
+    true,
+    'works'
+  )
+})
