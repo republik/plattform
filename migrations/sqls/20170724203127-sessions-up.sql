f4dd4b9bc2b0333927a8f632906441620e2b5295
patte <p@tte.io>
2017-11-23 15:55:00 +0100

    migrate db migrations and seeds to backend-modules

--- migrations/sqls/20170724203127-sessions-up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE "sessions" (
-  "sid" varchar NOT NULL COLLATE "default",
-  "sess" jsonb NOT NULL,
-  "expire" timestamp(6) NOT NULL
-)
-WITH (OIDS=FALSE);
-ALTER TABLE "sessions" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
---
+++ script/db-migrate-all.js
@@ -0,0 +1,20 @@
+//
+// This script runs db-migrate on multiple migrations directories
+//
+// usage
+// node script/db-migrate-all.js [db-migrate args]
+//
+require('dotenv').config()
+const { dbMigrateAll } = require('@orbiting/backend-modules-scripts')
+
+const migrationDirs = [
+  'node_modules/@orbiting/backend-modules-auth/migrations'
+]
+
+dbMigrateAll(migrationDirs)
+  .then(() => {
+    process.exit()
+  }).catch(e => {
+    console.log(e)
+    process.exit(1)
+  })
