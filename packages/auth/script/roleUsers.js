c8dc6337649e483ad1cded4de15f7d48e2b6b9d1
patte <p@tte.io>
2017-11-30 19:50:11 +0100

    feat(backend-modules-auth): roleUsers: input via stdin, create users
    affects: @orbiting/backend-modules-auth

--- packages/auth/script/roleUsers.js
+++ packages/auth/script/roleUsers.js
@@ -2,16 +2,21 @@
 // This script adds/removes a role to/from users.
 //
 // usage
-// auth  node script/roleUsers.js supporter emails.json [remove]
-// The second paramenter must be a path (relative to this file) to a json
-// file containing an array with user emails.
-// If a user is not found, he/she is skipped.
+// cat seeds/seeds.example.json | node script/roleUsers.js add/remove [create]
 //
+// This script expects a json with users and their roles on stdin.
+// For the expected format check seeds/seeds.example.json
+//
+// Provide add or remove as the first parameter whether you want the specified
+// roles to be added or removed from the users.
+//
+// Only if you provide --create non-existing users will be created.
 //
 
 // use pogi directly to not create a circular dependency to backend-modules-base
 const { PgDb } = require('pogi')
 const Roles = require('../lib/Roles')
+const rw = require('rw')
 
 require('dotenv').config()
 
@@ -20,38 +25,71 @@ const {
 } = process.env
 
 PgDb.connect({connectionString: DATABASE_URL}).then(async (pgdb) => {
-  const role = process.argv[2]
-  const emailsPath = process.argv[3]
 
-  if (!role) {
-    throw new Error('first parameter must be the role to add')
-  }
-  if (!emailsPath) {
-    throw new Error('second parameter must be a path to a json file containing emails')
-  }
-  const emails = require(emailsPath)
-  if (!emails || emails.length === 0) {
-    throw new Error('emails input invalid')
-
+  const input = JSON.parse(rw.readFileSync('/dev/stdin', 'utf8'))
+  if (!input || !input.users || !input.users.length) {
+    console.log('please provide a users array on stdin!')
+    process.exit(1)
   }
 
-  let skippedEmails = []
-  for(let email of emails) {
-    const user = await pgdb.public.users.findOne({email})
-    if (!user) {
-      skippedEmails.push(email)
-    } else {
-      let newUser
-      if (process.argv[4]) {
-        newUser = await Roles.removeUserFromRoll(user.id, role, pgdb)
-      } else {
-        newUser = await Roles.addUserToRole(user.id, role, pgdb)
+  const operation = process.argv[2]
+  if(operation !== 'add' && operation !== 'remove') {
+    console.error('add or remove must be passed as the first param!')
+    process.exit(1)
+  }
+  const add = operation === 'add'
+
+  const create = !!process.argv[3]
+
+  const transaction = await pgdb.transactionBegin()
+  let promises = []
+  try {
+    let skippedEmails = []
+    for(let user of input.users) {
+      let existingUser = await transaction.public.users.findOne({
+        email: user.email
+      })
+      if (!existingUser) {
+        if (create) {
+          existingUser = await transaction.public.users.insertAndGet({
+            ...user
+          })
+        } else {
+          skippedEmails.push(user.email)
+        }
       }
-      console.log(newUser)
+      if(existingUser) {
+        for(let role of user.roles) {
+          if(add) {
+            promises.push(
+              Roles.addUserToRole(existingUser.id, role, transaction)
+            )
+          } else {
+            promises.push(
+              Roles.removeUserFromRoll(existingUser.id, role, transaction)
+            )
+          }
+        }
+      }
     }
+
+    await Promise.all(promises)
+    console.log(await transaction.public.users.find({ email: input.users.map( u => u.email ) }))
+
+    if (skippedEmails.length > 0) {
+      console.log('no user found for the following emails:')
+      console.log('(use this script with --create to automatically create them)')
+      console.log(skippedEmails.join('\n'))
+    }
+
+    await transaction.transactionCommit()
+
+  } catch (e) {
+    await transaction.transactionRollback()
+    console.warn('transaction rollback')
+    throw e
   }
-  console.log('no user found for the following emails:')
-  console.log(skippedEmails.join('\n'))
+
 
   console.log('job done!')
 }).then(() => {
