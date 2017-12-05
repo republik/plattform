65a370e4dd61558b8c4fa7bdd1e106c99156092f
Lukas Buenger <lukasbuenger@gmail.com>
2017-12-05 13:08:53 +0100

    Log removed

--- pages/repo/edit.js
+++ pages/repo/edit.js
@@ -326,9 +326,6 @@ class EditorPage extends Component {
       t
     } = this.props
     const { editorState } = this.state
-    console.log(this.editor.serializer.serialize(editorState, {
-      mdast: true
-    }))
 
     const message = window.prompt(t('commit/promtMessage'))
     if (!message) {
