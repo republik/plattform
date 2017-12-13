e1f411453446dd5df6c03af0637a147fb4621fd3
patte <p@tte.io>
2017-12-13 17:38:14 +0100

    stripe createSubscription helper

---
+++ graphql/modules/crowdfundings/lib/payments/stripe/createSubscription.js
@@ -0,0 +1,30 @@
+const getClients = require('./clients')
+
+module.exports = async ({
+  plan,
+  userId,
+  companyId,
+  pgdb
+}) => {
+  const { accounts } = await getClients(pgdb)
+
+  const account = accounts.find(a => a.company.id === companyId)
+  if (!account) {
+    throw new Error(`could not find account for companyId: ${companyId}`)
+  }
+
+  const customer = await pgdb.public.stripeCustomers.findOne({
+    userId,
+    companyId
+  })
+  if (!customer) {
+    throw new Error(`could not find stripeCustomer for userId: ${userId} companyId: ${companyId}`)
+  }
+
+  return account.stripe.subscriptions.create({
+    customer: customer.id,
+    items: [
+      { plan }
+    ]
+  })
+}
