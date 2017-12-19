0c523561abbcb2ccb93c394234f7022199d6e423
Thomas Preusse <tpreusse@users.noreply.github.com>
2017-12-19 10:59:42 +0100

    fix(User): only send valid uuids to db
    affects: @orbiting/backend-modules-auth

--- packages/auth/graphql/resolvers/_queries/user.js
+++ packages/auth/graphql/resolvers/_queries/user.js
@@ -1,14 +1,17 @@
 const transformUser = require('../../../lib/transformUser')
 const Roles = require('../../../lib/Roles')
+const isUUID = require('is-uuid')
 
 module.exports = async (_, args, { pgdb, user: me }) => {
   const { slug } = args
   if (!slug) {
     return null
   }
-  const user = await pgdb.public.users.findOne({
-    or: [{id: slug}, {username: slug}]
-  }, { skipUndefined: true })
+  const user = await pgdb.public.users.findOne(
+    isUUID.v4(slug)
+      ? {id: slug}
+      : {username: slug}
+  )
   if (
     !user ||
     (!user.hasPublicProfile && !Roles.userIsMe(user, me))
