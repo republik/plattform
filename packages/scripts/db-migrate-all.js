48ab811ead813fc03ef71b585c8a9e1f37a5807e
patte <p@tte.io>
2017-12-27 20:19:53 +0100

    fix(db-migrate-all): running down (or reset) requires reversing the migrations
    affects: @orbiting/backend-modules-scripts

--- packages/scripts/db-migrate-all.js
+++ packages/scripts/db-migrate-all.js
@@ -1,9 +1,15 @@
 const exec = require('child-process-promise').exec
 
-module.exports = (migrationDirs) => Promise.resolve().then( async () => {
+module.exports = (_migrationDirs) => Promise.resolve().then( async () => {
+
+  const args = process.argv.slice(2).join(' ')
+
+  const migrationDirs = ['down', 'reset'].includes(args)
+    ? _migrationDirs.slice().reverse()
+    : _migrationDirs
 
   for(let migrationDir of migrationDirs) {
-    const command = `yarn run db-migrate --migrations-dir ${migrationDir} ${process.argv.slice(2).join(' ')}`
+    const command = `yarn run db-migrate --migrations-dir ${migrationDir} ${args}`
     await exec(command)
       .then( result => {
         console.log(command, '\n', result.stdout);
