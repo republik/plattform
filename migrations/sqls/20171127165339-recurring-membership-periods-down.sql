523f40bddc91487e07aefc7fd2b9e25a5f9958e9
patte <p@tte.io>
2017-12-27 20:15:08 +0100

    cleanup recurring-membership-periods-down.sql

--- migrations/sqls/20171127165339-recurring-membership-periods-down.sql
+++ migrations/sqls/20171127165339-recurring-membership-periods-down.sql
@@ -1,7 +1,7 @@
-drop table "membershipPeriodPayments";
+-- drop table "membershipPeriodPayments";
+
+-- drop index if exists "membershipPeriodPayments_pledgeId_idx";
+-- drop index if exists "membershipPeriodPayments_createdAt_idx";
 
 drop table "membershipPeriods";
-
-drop index if exists "membershipPeriodPayments_pledgeId_idx";
-drop index if exists "membershipPeriodPayments_createdAt_idx";
 
