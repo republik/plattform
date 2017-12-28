df7796d4d1c992086cd6f2fae0cfb674366a2fd8
patte <p@tte.io>
2017-12-28 21:37:43 +0100

    honour pagination in script/pullRedis

--- script/pullRedis.js
+++ script/pullRedis.js
@@ -28,69 +28,84 @@ Promise.resolve().then(async () => {
 
   const now = new Date()
 
-  const repos = await getRepos(null, { first: 100 }, { user, redis })
-  const allLatestPublications = await Promise.all(
-    repos.nodes.map(repo => getLatestPublications(repo))
-  )
-    .then(arr => arr.filter(arr2 => arr2.length > 0))
-  // console.log(util.inspect(allLatestPublications, {depth: null}))
-
-  let redisOps = []
-
-  for (let publications of allLatestPublications) {
-    redisOps.push(
-      redis.saddAsync('repos:ids', publications[0].repo.id)
+  let pageInfo
+  let pageCounter = 1
+  do {
+    console.info(`requesting repos (page ${pageCounter}) ...`)
+    pageCounter += 1
+    const repos = await getRepos(null, {
+      first: 20,
+      ...(pageInfo && pageInfo.hasNextPage)
+        ? { after: pageInfo.endCursor }
+        : { }
+    }, {
+      user, redis
+    })
+    pageInfo = repos.pageInfo
+    const allLatestPublications = await Promise.all(
+      repos.nodes.map(repo => getLatestPublications(repo))
     )
-    for (let publication of publications) {
-      const { repo, commit, sha, meta: { scheduledAt: _scheduledAt } } = publication
+      .then(arr => arr.filter(arr2 => arr2.length > 0))
 
-      const doc = await getDocument(
-        { id: commit.id, repo },
-        { oneway: true },
-        { user, redis }
-      )
-      const payload = JSON.stringify({
-        doc,
-        sha
-      })
+    let redisOps = []
 
-      const scheduledAt = _scheduledAt && _scheduledAt > now
-        ? _scheduledAt
-        : null
-      let ref = publication.name.indexOf('prepublication') > -1
-        ? 'prepublication'
-        : 'publication'
-      if (scheduledAt) {
-        ref = `scheduled-${ref}`
-      }
-
-      const key = `repos:${repo.id}/${ref}`
+    for (let publications of allLatestPublications) {
       redisOps.push(
-        redis.setAsync(key, payload)
+        redis.saddAsync('repos:ids', publications[0].repo.id)
       )
-      if (scheduledAt) {
-        redisOps.push(
-          redis.zaddAsync(`repos:scheduledIds`, scheduledAt.getTime(), key)
+      for (let publication of publications) {
+        const { repo, commit, sha, meta: { scheduledAt: _scheduledAt } } = publication
+
+        const doc = await getDocument(
+          { id: commit.id, repo },
+          { oneway: true },
+          { user, redis }
         )
-      } else {
-        if (ref === 'publication') {
+        const payload = JSON.stringify({
+          doc,
+          sha
+        })
+
+        const scheduledAt = _scheduledAt && _scheduledAt > now
+          ? _scheduledAt
+          : null
+        let ref = publication.name.indexOf('prepublication') > -1
+          ? 'prepublication'
+          : 'publication'
+        if (scheduledAt) {
+          ref = `scheduled-${ref}`
+        }
+
+        const key = `repos:${repo.id}/${ref}`
+        redisOps.push(
+          redis.setAsync(key, payload)
+        )
+        if (scheduledAt) {
           redisOps.push(
-            redis.setAsync(`repos:${repo.id}/prepublication`, payload)
+            redis.zaddAsync(`repos:scheduledIds`, scheduledAt.getTime(), key)
           )
+        } else {
+          if (ref === 'publication') {
+            redisOps.push(
+              redis.setAsync(`repos:${repo.id}/prepublication`, payload)
+            )
+          }
         }
       }
     }
-  }
-  redisOps.push(
-    redis.publishAsync(schedulingChannelKey, 'refresh')
-  )
-  const lock = await redlock().lock(lockKey, 1000)
-  await Promise.all(redisOps)
-  await lock.unlock()
-    .catch((err) => {
-      console.error(err)
-    })
-}).then(() => {
+    redisOps.push(
+      redis.publishAsync(schedulingChannelKey, 'refresh')
+    )
+    const lock = await redlock().lock(lockKey, 1000)
+    await Promise.all(redisOps)
+    await lock.unlock()
+      .catch((err) => {
+        console.error(err)
+      })
+  } while (pageInfo && pageInfo.hasNextPage)
+  console.info('done!')
+}
+).then(() => {
   process.exit()
 }).catch(e => {
   console.log(e)
