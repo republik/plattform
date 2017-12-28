9fedce53fa226cb04f0ab108ed6eb7d9eec6516c
patte <p@tte.io>
2017-12-28 11:44:40 +0100

    prettify tap output, fix force test ending

--- __tests__/zz_close.test.js
+++ __tests__/zz_close.test.js
@@ -1,7 +1,12 @@
 const test = require('tape-async')
 const { disconnect } = require('./helpers.js')
+const {
+  FORCE_TEST_END
+} = process.env
 
 test('close', t => {
+  if (FORCE_TEST_END) {
+    disconnect()
+  }
   t.end()
-  disconnect()
 })
