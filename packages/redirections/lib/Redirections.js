a865b44d60944814f8c33ecf1ba30afef6100e7d
patte <p@tte.io>
2018-01-04 14:25:25 +0100

    feat(redirections): upsert all redirections with the same ressource
    affects: @orbiting/backend-modules-redirections

--- packages/redirections/lib/Redirections.js
+++ packages/redirections/lib/Redirections.js
@@ -1,19 +1,33 @@
 const upsert = async (
-  redirection
-, {
-  pgdb
-}) => {
+  redirection,
+  { pgdb }
+) => {
+  const now = new Date()
+  if (redirection.resource) {
+    await pgdb.public.redirections.update({
+      resource: redirection.resource
+    }, {
+      target: redirection.target,
+      status: redirection.status,
+      updatedAt: now
+    })
+  }
   const existingRedir = await pgdb.public.redirections.findOne({
     source: redirection.source
   })
   if(existingRedir) {
     return pgdb.public.redirections.update({
       source: redirection.source
-    },
-      redirection
-    )
+    }, {
+      ...redirection,
+      updatedAt: now
+    })
   } else {
-    return pgdb.public.redirections.insert(redirection)
+    return pgdb.public.redirections.insert({
+      ...redirection,
+      updatedAt: now,
+      createdAt: now,
+    })
   }
 }
 
