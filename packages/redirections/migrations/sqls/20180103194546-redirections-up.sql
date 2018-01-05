9996b60b1fdd730a6d9eb7543beeb3433ad8cd3a
patte <p@tte.io>
2018-01-05 17:04:02 +0100

    fix(redirections): add missing DB indices and extensions
    affects: @orbiting/backend-modules-redirections

--- packages/redirections/migrations/sqls/20180103194546-redirections-up.sql
+++ packages/redirections/migrations/sqls/20180103194546-redirections-up.sql
@@ -1,3 +1,6 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE EXTENSION IF NOT EXISTS "pg_trgm";
+
 create table "redirections" (
   "id"                  uuid primary key not null default uuid_generate_v4(),
   "source"              text not null,
@@ -9,3 +12,6 @@ create table "redirections" (
   UNIQUE("source")
 );
 create index "redirections_source_idx" on "redirections" using GIN ("source" gin_trgm_ops);
+create index "redirections_target_idx" on "redirections" using GIN ("target" gin_trgm_ops);
+create index "redirections_resource_tags_idx" ON "redirections" using GIN ("resource");
+
