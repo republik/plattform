1cc41fc7eda60f87775d5137d530a7b6e26f3629
patte <p@tte.io>
2018-01-06 12:42:45 +0100

    tryCatch for Commit.document resolver

--- graphql/resolvers/Commit.js
+++ graphql/resolvers/Commit.js
@@ -14,28 +14,29 @@ module.exports = {
     { oneway },
     { user, redis }
   ) => {
-    if (existingDocument) {
-      return existingDocument
-    }
+    try {
+      if (existingDocument) {
+        return existingDocument
+      }
 
-    let mdast
+      let mdast
 
-    const redisKey = `repos:${repoId}/commits/${commitId}/document/mdast`
-    const redisMdast = await redis.getAsync(redisKey)
-    if (redisMdast) {
-      debug('document: redis HIT (%s)', redisKey)
-      mdast = JSON.parse(redisMdast)
-    } else {
-      debug('document: redis MISS (%s)', redisKey)
-      const { githubApolloFetch } = await createGithubClients()
-      const [login, repoName] = repoId.split('/')
+      const redisKey = `repos:${repoId}/commits/${commitId}/document/mdast`
+      const redisMdast = await redis.getAsync(redisKey)
+      if (redisMdast) {
+        debug('document: redis HIT (%s)', redisKey)
+        mdast = JSON.parse(redisMdast)
+      } else {
+        debug('document: redis MISS (%s)', redisKey)
+        const { githubApolloFetch } = await createGithubClients()
+        const [login, repoName] = repoId.split('/')
 
-      const {
-        data: {
-          repository
-        }
-      } = await githubApolloFetch({
-        query: `
+        const {
+          data: {
+            repository
+          }
+        } = await githubApolloFetch({
+          query: `
           query document(
             $login: String!,
             $repoName: String!,
@@ -50,39 +51,48 @@ module.exports = {
             }
           }
         `,
-        variables: {
-          login,
-          repoName,
-          blobExpression: `${commitId}:article.md`
+          variables: {
+            login,
+            repoName,
+            blobExpression: `${commitId}:article.md`
+          }
+        })
+
+        if (!repository.blob) {
+          console.warn(`no document found for ${repoId}`)
+          return {
+            content: {},
+            meta: {}
+          }
+        }
+
+        mdast = MDAST.parse(repository.blob.text)
+        await redis.setAsync(redisKey, JSON.stringify(mdast))
+      }
+
+      // prefix image urls
+      // - editor specific
+      const prefixUrl = createPrefixUrl(repoId, oneway)
+      visit(mdast, 'image', node => {
+        node.url = prefixUrl(node.url)
+      })
+      Object.keys(mdast.meta).forEach(key => {
+        if (key.match(/image/i)) {
+          mdast.meta[key] = prefixUrl(mdast.meta[key])
         }
       })
 
-      if (!repository.blob) {
-        console.warn(`no document found for ${repoId}`)
-        return {
-          content: {},
-          meta: {}
+      return {
+        content: mdast
+      }
+    } catch (e) {
+      console.error(e)
+      return {
+        content: {
+          document: null,
+          meta: null
         }
       }
-
-      mdast = MDAST.parse(repository.blob.text)
-      await redis.setAsync(redisKey, JSON.stringify(mdast))
-    }
-
-    // prefix image urls
-    // - editor specific
-    const prefixUrl = createPrefixUrl(repoId, oneway)
-    visit(mdast, 'image', node => {
-      node.url = prefixUrl(node.url)
-    })
-    Object.keys(mdast.meta).forEach(key => {
-      if (key.match(/image/i)) {
-        mdast.meta[key] = prefixUrl(mdast.meta[key])
-      }
-    })
-
-    return {
-      content: mdast
     }
   }
 }
