29eb4a80b66259c84eff4062ccfdef43ca631fa3
Daniel Pfänder <daniel.pfaender@project-r.construction>
2018-01-06 18:44:38 +0100

    fix: Make email container a full HTML document again (now that we've forked email vs. web)

--- src/templates/EditorialNewsletter/email/Container.js
+++ src/templates/EditorialNewsletter/email/Container.js
@@ -1,57 +1,57 @@
 import React from 'react'
-import { css } from 'glamor'
 import { Mso } from 'mdast-react-render/lib/email'
 import Header from './Header'
 import SG from '../../../theme/env'
 
-const styles = {
-  container: {
-    fontFamily: 'serif',
-    fontSize: 18,
-    color: '#000',
-    WebkitFontSmoothing: 'antialiased',
-    backgroundColor: '#fff',
-    width: '100%'
-  }
-}
-
 export default ({ children, attributes = {}, meta }) => (
-  <div
-    {...css(styles.container)}
-    style={{ margin: 0, padding: 0, backgroundColor: '#fff' }}
-  >
-    <style
-      type="text/css"
-      dangerouslySetInnerHTML={{
-        __html: `
-    ${SG.FONT_FACES}
-    mso{
-      display: none;
-    }
-  `
-      }}
-    />
-    <Mso>
-      {`
+  <html>
+    <head>
+      <meta charSet="UTF-8" />
+      <meta httpEquiv="x-ua-compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width,initial-scale=1" />
+      <Mso gte="15">
+        {`
+        <xml>
+          <o:officedocumentsettings>
+            <o:allowpng />
+            <o:pixelsperinch>96</o:pixelsperinch>
+          </o:officedocumentsettings>
+        </xml>
+        `}
+      </Mso>
+      <title>*|MC:SUBJECT|*</title>
+      <style
+        type="text/css"
+        dangerouslySetInnerHTML={{
+          __html: `
+        ${SG.FONT_FACES}
+      `
+        }}
+      />
+    </head>
+    <body style={{ margin: 0, padding: 0, backgroundColor: '#fff' }}>
+      <Mso>
+        {`
     <div>
       <table cellspacing="0" cellpadding="0" border="0" width="800">
         <tr>
           <td>
     `}
-    </Mso>
-    <table border="0" cellPadding="0" cellSpacing="0" width="100%">
-      <tbody>
-        <Header meta={meta} />
-        {children}
-      </tbody>
-    </table>
-    <Mso>
-      {`
+      </Mso>
+      <table border="0" cellPadding="0" cellSpacing="0" width="100%">
+        <tbody>
+          <Header meta={meta} />
+          {children}
+        </tbody>
+      </table>
+      <Mso>
+        {`
           </td>
         </tr>
       </table>
     </div>
     `}
-    </Mso>
-  </div>
+      </Mso>
+    </body>
+  </html>
 )
