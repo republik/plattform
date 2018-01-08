16cb803887a338af8f124450bd3f539963eb1dd5
patte <p@tte.io>
2018-01-08 23:15:29 +0100

    publish: metaFieldResolver, resolvedDocument for mailchimp, editorialNewsletter

--- graphql/resolvers/_mutations/publish.js
+++ graphql/resolvers/_mutations/publish.js
@@ -33,9 +33,10 @@ const {
   graphql: { resolvers: { queries: { documents: getPublishedDocuments } } },
   lib: {
     html: { get: getHTML },
-    resolve: { contentUrlResolver, metaUrlResolver }
+    resolve: { contentUrlResolver, metaUrlResolver, metaFieldResolver }
   }
 } = require('@orbiting/backend-modules-documents')
+const uniq = require('lodash/uniq')
 
 module.exports = async (
   _,
@@ -83,12 +84,14 @@ module.exports = async (
   // check if all references (link, format, dossiert, etc.) in the document can be resolved
   // for front: warn if related document cannot be resolved
   // for newsletter, preview email: stop publication
-  const unresolvedRepoIds = []
+  let unresolvedRepoIds = []
   const allDocs = await getPublishedDocuments(null, { scheduledAt: scheduledAt || now }, context)
-  const _doc = JSON.parse(JSON.stringify(doc))
-  contentUrlResolver(_doc, allDocs._all, allDocs._usernames, unresolvedRepoIds)
-  metaUrlResolver(_doc.content.meta, allDocs._all, allDocs._usernames, unresolvedRepoIds)
-  if (unresolvedRepoIds.length && (!ignoreUnresolvedRepoIds || doc.content.meta.template === 'newsletter')) {
+  const resolvedDoc = JSON.parse(JSON.stringify(doc))
+  contentUrlResolver(resolvedDoc, allDocs._all, allDocs._usernames, unresolvedRepoIds)
+  metaUrlResolver(resolvedDoc.content.meta, allDocs._all, allDocs._usernames, unresolvedRepoIds)
+  metaFieldResolver(resolvedDoc.content.meta, allDocs._all, unresolvedRepoIds)
+  unresolvedRepoIds = uniq(unresolvedRepoIds)
+  if (unresolvedRepoIds.length && (!ignoreUnresolvedRepoIds || doc.content.meta.template === 'editorialNewsletter')) {
     return {
       unresolvedRepoIds
     }
@@ -136,6 +139,42 @@ module.exports = async (
   // remember if slug changed
   if (!prepublication && !scheduledAt) {
     await handleRedirection(repoId, doc.content.meta, context)
+  }
+
+  // get/create campaign on mailchimp
+  // fail early if mailchimp not available
+  let campaignId
+  if (updateMailchimp) {
+    const { title, emailSubject } = doc.content.meta
+    if (!title || !emailSubject) {
+      throw new Error('updateMailchimp missing title or subject', { title, emailSubject })
+    }
+    const campaignKey = `repos:${repoId}/mailchimp/campaignId`
+    let campaignId = await redis.getAsync(campaignKey)
+    if (!campaignId) {
+      if (repoMeta && repoMeta.mailchimpCampaignId) {
+        campaignId = repoMeta.mailchimpCampaignId
+      }
+    }
+    if (campaignId) {
+      const { status } = await getCampaign({ id: campaignId })
+      if (status === 404) {
+        campaignId = null
+      }
+    }
+    if (!campaignId) {
+      const createResponse = await createCampaign({ title, subject: emailSubject })
+      const { id, status } = createResponse
+      if (status !== 'save') {
+        throw new Error('Mailchimp: could not create campaign', createResponse)
+      }
+      campaignId = id
+      await redis.setAsync(campaignKey, campaignId)
+      await editRepoMeta(null, {
+        repoId,
+        mailchimpCampaignId: campaignId
+      }, context)
+    }
   }
 
   // calc version number
@@ -263,41 +302,9 @@ module.exports = async (
   })
     .then(response => response.data)
 
-  // TODO: move to top to get a potential throw early
-  if (updateMailchimp) {
-    const { title, emailSubject } = doc.content.meta
-    if (!title || !emailSubject) {
-      throw new Error('updateMailchimp missing title or subject', { title, emailSubject })
-    }
-    const campaignKey = `repos:${repoId}/mailchimp/campaignId`
-    let campaignId = await redis.getAsync(campaignKey)
-    if (!campaignId) {
-      if (repoMeta && repoMeta.mailchimpCampaignId) {
-        campaignId = repoMeta.mailchimpCampaignId
-      }
-    }
-    if (campaignId) {
-      const { status } = await getCampaign({ id: campaignId })
-      if (status === 404) {
-        campaignId = null
-      }
-    }
-    if (!campaignId) {
-      const createResponse = await createCampaign({ title, subject: emailSubject })
-      const { id, status } = createResponse
-      if (status !== 'save') {
-        throw new Error('Mailchimp: could not create campaign', createResponse)
-      }
-      campaignId = id
-      await redis.setAsync(campaignKey, campaignId)
-      await editRepoMeta(null, {
-        repoId,
-        mailchimpCampaignId: campaignId
-      }, context)
-    }
-
-    const html = getHTML(doc)
-
+  // do the mailchimp update
+  if (campaignId) {
+    const html = getHTML(resolvedDoc)
     const updateResponse = await updateCampaignContent({
       campaignId,
       html
