57771951d8e02f0d768eebaacc7b09af764052d1
Daniel Pfänder <daniel.pfaender@project-r.construction>
2018-01-10 14:59:53 +0100

    refactor

--- graphql/resolvers/_queries/statistics.js
+++ graphql/resolvers/_queries/statistics.js
@@ -1,12 +1,10 @@
-module.exports = (_, args, { pgdb }) => {
-  return {
-    memberCount: async () => {
-      const result = await pgdb.public.query(
-        'SELECT count(*) FROM memberships, users ' +
-          'WHERE memberships."userId"=users.id AND ' +
-          "users.email!='jefferson@project-r.construction'"
-      )
-      return parseInt(result[0].count)
-    }
+module.exports = (_, args, { pgdb }) => ({
+  memberCount: async () => {
+    const result = await pgdb.public.query(
+      'SELECT count(*) FROM memberships, users ' +
+        'WHERE memberships."userId"=users.id AND ' +
+        "users.email!='jefferson@project-r.construction'"
+    )
+    return parseInt(result[0].count)
   }
-}
+})
