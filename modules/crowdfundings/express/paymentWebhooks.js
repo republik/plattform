08f60c2d50eea6e468ccb69dabd7fa6ef843e54c
patte <p@tte.io>
2018-01-12 21:52:35 +0100

    ignore stripe test webhooks in production

--- modules/crowdfundings/express/paymentWebhooks.js
+++ modules/crowdfundings/express/paymentWebhooks.js
@@ -7,50 +7,56 @@ const debug = require('debug')('crowdfundings:webhooks:all')
 
 const getWebhookHandler = require('../lib/payments/stripe/webhookHandler')
 
+const {
+  STRIPE_TEST_MODE
+} = process.env
+
 module.exports = async (server, pgdb, t) => {
   const handleWebhook = await getWebhookHandler({ pgdb, t })
+
+  const handleStripeWebhook = async (req, res, connected) => {
+    const body = JSON.parse(req.body.toString('utf8'))
+
+    // stripe sends test payments to test and prod endpoints
+    // ignore test events on production
+    if (!body.livemode && !STRIPE_TEST_MODE) {
+      debug('webhookHandler ignoring test event')
+      return res.sendStatus(200)
+    }
+
+    await pgdb.public.paymentsLog.insert({
+      method: 'STRIPE',
+      pspPayload: body
+    })
+
+    let code
+    try {
+      code = await handleWebhook({
+        req,
+        connected
+      })
+    } catch (e) {
+      code = 500
+    }
+    return res.sendStatus(code)
+  }
 
   // https://stripe.com/docs/webhooks
   server.post('/payments/stripe',
     bodyParser.raw({type: '*/*'}),
     async (req, res) => {
       debug('stripe: %O', req.body)
-      await pgdb.public.paymentsLog.insert({
-        method: 'STRIPE',
-        pspPayload: JSON.parse(req.body.toString('utf8'))
-      })
-
-      let code
-      try {
-        code = await handleWebhook({
-          req
-        })
-      } catch (e) {
-        code = 500
-      }
-      return res.sendStatus(code)
-    })
+      return handleStripeWebhook(req, res, false)
+    }
+  )
 
   server.post('/payments/stripe/connected',
     bodyParser.raw({type: '*/*'}),
     async (req, res) => {
       debug('stripe:connected %O', req.body)
-      await pgdb.public.paymentsLog.insert({
-        method: 'STRIPE',
-        pspPayload: JSON.parse(req.body.toString('utf8'))
-      })
-
-      let code
-      try {
-        code = await handleWebhook({
-          req,
-          connected: true
-        })
-      } catch (e) {
-        code = 500
-      }
-      return res.sendStatus(code)
-    })
+      return handleStripeWebhook(req, res, true)
+    }
+  )
 
   // https://e-payment-postfinance.v-psp.com/de/guides/integration%20guides/e-commerce/transaction-feedback#servertoserver-feedback
   server.get('/payments/pf', async (req, res) => {
