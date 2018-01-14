23b238cd2bf645dde976b6e2d624ec2f979f16f1
patte <p@tte.io>
2018-01-14 05:54:01 +0100

    fix(documents): undefined param
    affects: @orbiting/backend-modules-documents

--- packages/documents/graphql/resolvers/Document.js
+++ packages/documents/graphql/resolvers/Document.js
@@ -11,14 +11,14 @@ module.exports = {
     // - alt check info.path for documents / document being the root
     //   https://gist.github.com/tpreusse/f79833a023706520da53647f9c61c7f6
     if (doc._all) {
-      contentUrlResolver(doc, doc._all, doc._usernames, null, urlPrefix)
+      contentUrlResolver(doc, doc._all, doc._usernames, undefined, urlPrefix)
     }
     return doc.content
   },
   meta (doc, { urlPrefix }, context, info) {
     const meta = getMeta(doc)
     if (doc._all) {
-      metaUrlResolver(meta, doc._all, doc._usernames, null, urlPrefix)
+      metaUrlResolver(meta, doc._all, doc._usernames, undefined, urlPrefix)
     }
     return meta
   }
