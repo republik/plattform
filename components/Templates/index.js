cacea977cb066c1ee073933219779db9d09e0fdb
Thomas Preusse <tpreusse@users.noreply.github.com>
2018-01-14 08:48:01 +0100

    show dnt in publikator

--- components/Templates/index.js
+++ components/Templates/index.js
@@ -7,16 +7,18 @@ import createFrontSchema from '@project-r/styleguide/lib/templates/Front'
 import createFormatSchema from '@project-r/styleguide/lib/templates/Format'
 import createDiscussionSchema from '@project-r/styleguide/lib/templates/Discussion'
 
+import { t } from '../../lib/withT'
+
 const schemas = {
   // first is default schema for the editor
   // - for Project R this should be the newsletter
   newsletter: newsletterSchema,
   editorialNewsletter: editorialNewsletterSchema(),
   neutrum: neutrumSchema,
-  article: createArticleSchema(),
-  front: createFrontSchema(),
-  format: createFormatSchema(),
-  discussion: createDiscussionSchema()
+  article: createArticleSchema({t}),
+  front: createFrontSchema({t}),
+  format: createFormatSchema({t}),
+  discussion: createDiscussionSchema({t})
 }
 
 export const getSchema = template => {
