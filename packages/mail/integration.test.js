fb753bd969569ab90543ee7e54e8bffb14e18af2
Pascal Kaufmann <kaufmae@me.com>
2018-01-17 14:56:15 +0100

    refactor(mail): add tests

--- packages/mail/integration.test.js
+++ packages/mail/integration.test.js
@@ -3,6 +3,8 @@ const test = require('tape-async')
 const configure = require('./lib/configure')
 const getNewsletterSettings = require('./lib/getNewsletterSettings')
 const updateNewsletterSubscription = require('./lib/updateNewsletterSubscription')
+const updateNewsletterSubscriptions = require('./lib/updateNewsletterSubscriptions')
+const unsubscribeFromAllNewsletters = require('./lib/unsubscribeFromAllNewsletters')
 
 const prepare = () => {
   configure([
@@ -12,65 +14,96 @@ const prepare = () => {
   ])
 }
 
+const user = (role) => ({
+  email: 'test2@test.project-r.construction',
+  roles: role ? [role] : []
+})
+
 test('member user settings which does not exist on mailchimp', async (t) => {
   prepare()
-  const settings = await getNewsletterSettings({
-    email: 'kaufmae@me.com',
-    roles: ['member']
-  }, {
-    t: console.log
-  })
-  t.equal(settings.subscriptions.length, 3)
+  const member = await getNewsletterSettings(user('member'))
+  t.ok(member.status)
+  t.equal(member.subscriptions.length, 3)
   t.end()
 })
 
-test('user settings which does not exist on mailchimp', async (t) => {
-  prepare()
-  const settings = await getNewsletterSettings({
-    email: 'kaufmae@me.com',
-    roles: []
-  }, {
-    t: console.log
-  })
-  t.equal(settings.subscriptions.length, 3)
-  t.end()
-})
-
-test('change user settings of user which is not on mailchimp', async (t) => {
+test('change interest settings of user which is not subscribed', async (t) => {
   prepare()
   const settings = await updateNewsletterSubscription({
-    user: {
-      email: 'kaufmae@me.com',
-      roles: ['member']
-    },
+    user: user('member'),
     name: 'DAILY',
     subscribed: true,
     status: ''
-  }, {
-    t: console.log
   })
   t.equal(settings.subscribed, true)
   t.equal(settings.isEligible, true)
   t.equal(settings.name, 'DAILY')
+
+  const member = await getNewsletterSettings(user('member'))
+  t.equal(member.status, 'pending')
+  const subscribedSubscriptions = member.subscriptions
+    .filter(({ subscribed }) => subscribed)
+  t.equal(subscribedSubscriptions.length, 0)
   t.end()
 })
 
-// TODO: How to test here with postgres?!
-// const updateUserOnMailchimp = require('./lib/updateUserOnMailchimp')
-// test('add user to mailchimp', async (t) => {
-//   updateUserOnMailchimp({
-//     userId,
-//     pgdb,
-//     hasJustPaid,
-//     isNew
-//   })
-//
-//   const settings = await getNewsletterSettings({
-//     email: 'kaufmae@me.com',
-//     roles: []
-//   }, {
-//     t: console.log
-//   })
-//   t.equal(settings.subscriptions.length, 3)
-//   t.end()
-// })
+test('illegible role for interest', async (t) => {
+  prepare()
+  try {
+    await updateNewsletterSubscription({
+      user: user(),
+      name: 'DAILY',
+      subscribed: true,
+      status: 'subscribed'
+    })
+    t.fail()
+  } catch (e) {
+    t.ok(true)
+  }
+
+  t.end()
+})
+
+test('subscribe email to 1 of 3 interests', async (t) => {
+  prepare()
+  const interests = {}
+  interests[process.env.MAILCHIMP_INTEREST_NEWSLETTER_PROJECTR] = true
+  await updateNewsletterSubscriptions({
+    user: user(),
+    interests
+  })
+  const member = await getNewsletterSettings(user())
+  t.equal(member.status, 'subscribed')
+  const subscribedSubscriptions = member.subscriptions
+    .filter(({ subscribed }) => subscribed)
+  t.equal(subscribedSubscriptions.length, 2)
+  t.end()
+})
+
+test('change interest setting of 1 interest', async (t) => {
+  prepare()
+  const settings = await updateNewsletterSubscription({
+    user: user(),
+    name: 'PROJECTR',
+    subscribed: true
+  })
+  t.equal(settings.subscribed, true)
+  t.equal(settings.isEligible, true)
+  t.equal(settings.name, 'PROJECTR')
+
+  const member = await getNewsletterSettings(user())
+  t.equal(member.status, 'pending')
+  const subscribedSubscriptions = member.subscriptions
+    .filter(({ subscribed }) => subscribed)
+  t.equal(subscribedSubscriptions.length, 0)
+  t.end()
+})
+
+test('unsubscripe from all newsletters', async (t) => {
+  prepare()
+  const member = await unsubscribeFromAllNewsletters({
+    email: 'test@test.project-r.construction'
+  })
+  t.notEqual(member.status, 'subscribed')
+  t.end()
+})
