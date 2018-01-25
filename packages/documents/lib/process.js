b45c683e33718a298f3dbfa12ea68d894268fb38
patte <p@tte.io>
2018-01-25 12:16:30 +0100

    fix(documents): devide processRepoImageUrls in processRepoImageUrlsInContent and -InMeta
    affects: @orbiting/backend-modules-documents

--- packages/documents/lib/process.js
+++ packages/documents/lib/process.js
@@ -1,9 +1,12 @@
 const visit = require('unist-util-visit')
 
-const processRepoImageUrls = (mdast, fn) => {
+const processRepoImageUrlsInContent = (mdast, fn) => {
   visit(mdast, 'image', node => {
     node.url = fn(node.url)
   })
+}
+
+const processRepoImageUrlsInMeta = (mdast, fn) => {
   if (mdast.meta) {
     Object.keys(mdast.meta).forEach(key => {
       if (key.match(/image/i)) {
@@ -24,7 +27,7 @@ const processRepoImageUrls = (mdast, fn) => {
 // TODO migrate if embeds moved into it's own module
 const embedImageKeys = ['userProfileImageUrl', 'image', 'thumbnail']
 
-const processImageUrls = (mdast, fn) => {
+const processImageUrlsInContent = (mdast, fn) => {
   visit(mdast, 'zone', node => {
     if (node.data && node.identifier.indexOf('EMBED') > -1) {
       for (let key of embedImageKeys) {
@@ -37,6 +40,7 @@ const processImageUrls = (mdast, fn) => {
 }
 
 module.exports = {
-  processRepoImageUrls,
-  processImageUrls
+  processRepoImageUrlsInContent,
+  processRepoImageUrlsInMeta,
+  processImageUrlsInContent
 }
