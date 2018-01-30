e2b8460f327aa2ba373f7f6d3a7e0e2bc4af55f7
patte <p@tte.io>
2018-01-30 18:57:48 +0100

    add lost rematchPayments

---
+++ modules/crowdfundings/graphql/resolvers/_mutations/rematchPayments.js
@@ -0,0 +1,29 @@
+const { Roles } = require('@orbiting/backend-modules-auth')
+const matchPayments = require('../../../lib/payments/matchPayments')
+
+module.exports = async (_, args, {pgdb, req, t}) => {
+  Roles.ensureUserHasRole(req.user, 'supporter')
+
+  const transaction = await pgdb.transactionBegin()
+  try {
+    const {
+      numMatchedPayments,
+      numUpdatedPledges,
+      numPaymentsSuccessful
+    } = await matchPayments(transaction, t)
+
+    await transaction.transactionCommit()
+    const result = `
+rematchPayments result:
+num matched payments: ${numMatchedPayments}
+num updated pledges: ${numUpdatedPledges}
+num payments successfull: ${numPaymentsSuccessful}
+    `
+    console.log(result)
+    return result
+  } catch (e) {
+    await transaction.transactionRollback()
+    console.info('transaction rollback', { req: req._log(), args, error: e })
+    throw e
+  }
+}
