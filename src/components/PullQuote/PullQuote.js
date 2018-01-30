5320b0b616c32534ef74c0ce61822271758a3090
Daniel Pfänder <daniel.pfaender@project-r.construction>
2018-01-30 14:35:00 +0100

    fix(PullQuote): Increase vertical margins

--- src/components/PullQuote/PullQuote.js
+++ src/components/PullQuote/PullQuote.js
@@ -1,14 +1,25 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { css } from 'glamor'
-import { mUp } from '../../theme/mediaQueries'
+import { mUp, onlyS } from '../../theme/mediaQueries'
 import { Breakout } from '../Center'
 
 export const IMAGE_SIZE = 155
 
 const styles = {
   container: css({
-    margin: '0 auto'
+    margin: '40px auto',
+    [mUp]: {
+      margin: '60px auto',
+    }
+  }),
+  containerFloat: css({
+    // Vertical desktop margins are currently handled in Center.
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    [onlyS]: {
+      margin: '40px auto'
+    }
   }),
   figure: css({
     [mUp]: {
@@ -40,11 +51,12 @@ const getBreakoutSize = (size, hasFigure) => {
 
 const PullQuote = ({ children, attributes, hasFigure, size }) => {
   const textAlign = !hasFigure && size === 'narrow' ? 'center' : 'inherit'
+  const containerStyle = size === 'float' ? styles.containerFloat : styles.container
 
   return (
     <Breakout attributes={attributes} size={getBreakoutSize(size, hasFigure)}>
       <blockquote
-        {...styles.container}
+        {...containerStyle}
         {...(hasFigure ? styles.figure : {})}
         style={{ textAlign }}
       >
