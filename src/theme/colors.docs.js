45c39794958d147cc7f5e1964c37a3789891b0c3
Thomas Preusse <tpreusse@users.noreply.github.com>
2018-02-15 13:20:10 +0100

    docs(Charts): explain colors

--- src/theme/colors.docs.js
+++ src/theme/colors.docs.js
@@ -23,5 +23,18 @@ export default () => (
     <ColorSpecimen span={1} name='editorial' value={colors.editorial} />
     <ColorSpecimen span={1} name='meta' value={colors.meta} />
     <ColorSpecimen span={1} name='social' value={colors.social} />
+
+    <h2>Charts</h2>
+    <p>Most charts will use sequential colors, e.g. bars all in the frist sequential color and one highlight in the second. Opposite colors are used for diverging ranges, with an optional neutral color in the middle.</p>
+    {colors.sequential3.map((c, i) =>
+      <ColorSpecimen span={1} name={`sequential.${i}`} value={c} />)}
+    {[].concat(colors.opposite3).reverse().map((c, i) =>
+      <ColorSpecimen span={1} name={`opposite.${2 - i}`} value={c} />)}
+    <ColorSpecimen span={1} name='neutral' value={colors.neutral} />
+    <br />
+    <h3>Discrete</h3>
+    <p>For categories, e.g. 10 lines in a chart, each representing a country.</p>
+    {colors.discrete.map((c, i) =>
+      <ColorSpecimen span={1} name={`discrete.${i}`} value={c} />)}
   </Page>
 )
