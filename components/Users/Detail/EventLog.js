3a30b5d6596e014826a8c224053c694d7dea09ee
Lukas Buenger <lukasbuenger@gmail.com>
2018-03-28 15:14:55 +0200

    event log love

--- components/Users/Detail/EventLog.js
+++ components/Users/Detail/EventLog.js
@@ -1,32 +1,66 @@
 import React from 'react'
-import { swissTime } from '../../../lib/utils/formats'
+import { Table, Row, Cell } from '../../Layout/Table'
+import { colors, Label } from '@project-r/styleguide'
 
-const dateTimeFormat = swissTime.format(
-  '%e. %B %Y %H.%M Uhr'
+const rowStyles = index => ({
+  maxHeight: '40px',
+  backgroundColor:
+    index % 2 > 0 ? colors.secondaryBg : 'none'
+})
+
+const headStyles = {
+  maxHeight: '40px',
+  backgroundColor: '#fff',
+  borderBottom: `1px solid ${colors.divider}`
+}
+
+const displayDate = rawDate => {
+  const date = new Date(rawDate)
+  return `${date.getDate()}.${date.getMonth() +
+    1}.${date.getFullYear()}`
+}
+
+export default ({ items, ...props }) => (
+  <Table {...props}>
+    <Row style={headStyles}>
+      <Cell flex="0 0 15%">
+        <Label>Action</Label>
+      </Cell>
+      <Cell flex="0 0 15%">
+        <Label>Date</Label>
+      </Cell>
+      <Cell flex="0 0 20%">
+        <Label>E-Mail</Label>
+      </Cell>
+      <Cell flex="0 0 40%">
+        <Label>User Agent</Label>
+      </Cell>
+      <Cell flex="0 0 10%">
+        <Label>Current session</Label>
+      </Cell>
+    </Row>
+    {items.map((entry, index) => (
+      <Row key={`user-${index}`} style={rowStyles(index)}>
+        <Cell flex="0 0 15%">
+          {entry.type.split('_').join(' ')}
+        </Cell>
+        <Cell flex="0 0 15%">
+          {displayDate(entry.createdAt)}
+        </Cell>
+        <Cell flex="0 0 20%">
+          {entry.archivedSession.email}
+        </Cell>
+        <Cell flex="0 0 40%">
+          {entry.archivedSession.userAgent}
+        </Cell>
+        <Cell flex="0 0 10%">
+          {' '}
+          {entry.activeSession &&
+          entry.activeSession.isCurrent
+            ? 'YES'
+            : 'NO'}
+        </Cell>
+      </Row>
+    ))}
+  </Table>
 )
-
-// const dateFormat = swissTime.format('%e. %B %Y')
-
-export default props =>
-  props.entries.map(entry => (
-    <div>
-      <span>
-        {entry.type.split('_').join(' ')}
-      </span>
-      <span>
-        {dateTimeFormat(
-          new Date(entry.createdAt)
-        )}
-      </span>
-      <span>{entry.archivedSession.email}</span>
-      <span>
-        {entry.archivedSession.userAgent}
-      </span>
-      <span>
-        {entry.activeSession &&
-        entry.activeSession.isCurrent
-          ? 'YES'
-          : 'NO'}
-      </span>
-    </div>
-  ))
