6fff448cfae98cd21f7877b0435b51950d347d02
patte <p@tte.io>
2018-04-04 17:32:04 +0200

    refactor(republik-discussion): remove unused discussion._id

--- servers/republik/graphql/resolvers/_queries/discussion.js
+++ servers/republik/graphql/resolvers/_queries/discussion.js
@@ -1,16 +1,2 @@
-const {
-  getSelectionArgsForField,
-  getUUIDForSelectionArgs
-} = require('../../../lib/graphql')
-
-module.exports = async (_, args, { pgdb }, info) => {
-  const { id } = args
-  const discussion = await pgdb.public.discussions.findOne({ id })
-  if (discussion && info) {
-    const selectionArgs = getSelectionArgsForField(info, 'comments')
-    if (Object.keys(selectionArgs).length > 0) {
-      discussion._id = getUUIDForSelectionArgs(selectionArgs, discussion.id)
-    }
-  }
-  return discussion
-}
+module.exports = async (_, { id }, { pgdb }) =>
+  pgdb.public.discussions.findOne({ id })
