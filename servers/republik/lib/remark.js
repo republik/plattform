0cbf7bc0c4941653c78ddeb1ae7aa8c6f247b172
patte <p@tte.io>
2018-04-11 18:57:03 +0200

    refactor(republik): comment.content mdast parsing: don't remove JSLinks, neither strip html

--- servers/republik/lib/remark.js
+++ servers/republik/lib/remark.js
@@ -1,27 +1,9 @@
 const unified = require('unified')
 const remarkParse = require('remark-parse')
 const remarkBreaks = require('remark-breaks')
-const remarkStripHTML = require('remark-strip-html')
-const select = require('unist-util-select')
 
 // https://github.com/remarkjs/remark/tree/master/packages/remark-parse#turning-off-a-tokenizer
 delete remarkParse.Parser.prototype.blockTokenizers.table
-
-// TODO: move into it's own package - with tests
-const isJSLink = /^javascript:/
-function remarkRemoveJSLinks (_, options = {}) {
-  return (ast) => {
-    select(ast, 'link').forEach((node) => {
-      if (isJSLink.test(node.url)) {
-        node.type = 'text'
-        node.value = node.children[0].value
-        delete node.url
-        delete node.title
-        delete node.children
-      }
-    })
-  }
-}
 
 const parser = unified()
   .use(remarkParse, {
@@ -31,7 +13,5 @@ const parser = unified()
     footnotes: false
   })
   .use(remarkBreaks)
-  .use(remarkStripHTML)
-  .use(remarkRemoveJSLinks)
 
 module.exports.parse = md => parser.runSync(parser.parse(md))
