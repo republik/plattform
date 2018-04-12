5e558f626a35b831c1a52cb509611da75fc1e242
patte <p@tte.io>
2018-04-12 11:23:16 +0200

    fix(republik): count active memberships only in membershipStats

--- servers/republik/graphql/resolvers/_queries/membershipStats.js
+++ servers/republik/graphql/resolvers/_queries/membershipStats.js
@@ -2,7 +2,8 @@ module.exports = (_, args, { pgdb }) => ({
   count: async () => {
     const { PARKING_USER_ID } = process.env
     return pgdb.public.memberships.count({
-      'userId !=': PARKING_USER_ID
+      'userId !=': PARKING_USER_ID,
+      active: true
     })
   }
 })
