face72e1ae1cf9eb3f079816d5fac90bba12361d
patte <p@tte.io>
2018-04-16 17:17:00 +0200

    fix(republik): typo

--- servers/republik/modules/crowdfundings/express/paymentWebhooks.js
+++ servers/republik/modules/crowdfundings/express/paymentWebhooks.js
@@ -61,7 +61,7 @@ module.exports = async (server, pgdb, t) => {
   // https://e-payment-postfinance.v-psp.com/de/guides/integration%20guides/e-commerce/transaction-feedback#servertoserver-feedback
   server.get('/payments/pf', async (req, res) => {
     const { query: body } = req
-    debug(`pf %O`, body)
+    debug('pf %O', body)
 
     await pgdb.public.paymentsLog.insert({
       method: 'POSTFINANCECARD',
