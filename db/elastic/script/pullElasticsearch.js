68f2622c81b99f9cccc0f9c87a628f07e9894bba
Patrick Venetz <venetz@gmail.com>
2018-04-20 18:10:31 +0200

    feat(elastic): Popuplate "comments" and "documents" indices
    Also creates aliases. First attempts to have scripts parts (inserts,
    mapping) modularized.

--- db/elastic/script/pullElasticsearch.js
+++ db/elastic/script/pullElasticsearch.js
@@ -2,17 +2,19 @@ require('@orbiting/backend-modules-env').config()
 
 const elasticsearch = require('@orbiting/backend-modules-base/lib/elastic')
 const PgDb = require('@orbiting/backend-modules-base/lib/pgdb')
+
+const inserts = require('./inserts')
+const mappings = [
+  require('./mapping/documents'),
+  require('./mapping/comments')
+]
+
 const elastic = elasticsearch.client()
-
-const documentsMapping = require('./mapping/documents')
-
-const insertsDict = require('./inserts')
-const inserts = Object.keys(insertsDict).map(key => insertsDict[key])
 
 const flush = process.argv[2] === '--flush'
 
-const indexName = `republik-${new Date().getTime()}`
-const aliasName = 'republik'
+const getAliasName = (type) => `republik-${type}`
+const getIndexName = (alias) => `${alias}-${new Date().getTime()}`
 
 PgDb.connect().then(async pgdb => {
   if (flush) {
@@ -22,21 +24,27 @@ PgDb.connect().then(async pgdb => {
     })
   }
 
-  await elastic.indices.create({
-    index: indexName,
-    body: {
-      aliases: {
-        [aliasName]: {}
-      },
-      mappings: {
-        ...documentsMapping
-      }
-    }
-  })
+  await Promise.all(mappings.map(async mapping => {
+    const type = Object.keys(mapping).shift()
+    const alias = getAliasName(type)
+    const index = getIndexName(alias)
 
-  await Promise.all(
-    inserts.map(insert => insert({indexName, elastic, pgdb}))
-  )
+    console.log('creating index', { alias, index })
+    await elastic.indices.create({
+      index,
+      body: {
+        aliases: {
+          [alias]: {}
+        },
+        mappings: {
+          ...mapping
+        }
+      }
+    })
+
+    console.log('populating index', { alias, index })
+    await inserts[type]({indexName: index, elastic, pgdb})
+  }))
 
   // TODO when to remove old/previous index?
 }
