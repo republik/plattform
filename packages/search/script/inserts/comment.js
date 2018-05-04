6c8831920590c68926c21e32f97b3f949991b813
Patrick Venetz <venetz@gmail.com>
2018-05-04 16:13:55 +0200

    refactor(elastic): Use indexPgTables lib to populate ES with "comments"

--- packages/search/script/inserts/comment.js
+++ packages/search/script/inserts/comment.js
@@ -1,32 +1,6 @@
-module.exports = async ({ indexName, type, elastic, pgdb }) => {
-  const stats = { [type]: { added: 0, total: 0 } }
-  const statsInterval = setInterval(
-    () => { console.log(indexName, stats) },
-    1 * 1000
-  )
+const bulk = require('../../lib/indexPgTable')
 
-  const comments = await pgdb.public.comments.find()
-
-  stats[type].total = comments.length
-
-  const esDocuments = comments.map(comment => ({
-    id: comment.id,
-    body: {
-      ...comment,
-      type
-    }
-  }))
-
-  for (let doc of esDocuments) {
-    await elastic.create({
-      ...doc,
-      index: indexName,
-      type
-    })
-    stats[type].added++
-  }
-
-  clearInterval(statsInterval)
-
-  console.log(indexName, stats)
+module.exports = async ({ pgdb, ...rest }) => {
+  const resource = { table: pgdb.public.comments }
+  return bulk.index({ resource, ...rest })
 }
