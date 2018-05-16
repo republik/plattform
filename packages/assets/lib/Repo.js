fc16cb867b61b5d45b34c44304d9778d86aea97f
Patrick Venetz <venetz@gmail.com>
2018-05-16 14:29:40 +0200

    fix(assets): Configure, reduce concurrency of upload assets

--- packages/assets/lib/Repo.js
+++ packages/assets/lib/Repo.js
@@ -5,7 +5,10 @@ const {
   upload: uploadS3,
   getHead
 } = require('./s3')
-const debug = require('debug')('assets')
+const Promise = require('bluebird')
+const debug = require('debug')('assets:lib:Repo')
+
+const CONCURRENCY = 2
 
 const {
   ASSETS_SERVER_BASE_URL,
@@ -22,46 +25,48 @@ const uploadImages = async (repoId, paths) => {
   const { githubRest } = await createGithubClients()
   const [owner, repo] = repoId.split('/')
 
-  await Promise.all(
-    paths.map( async (path) => {
-      // the filename is the blobSha
-      const blobSha = path
-        .split('/')
-        .pop()
-        .split('.')[0]
+  await Promise.map(paths, async path => {
+    // the filename is the blobSha
+    const blobSha = path
+      .split('/')
+      .pop()
+      .split('.')[0]
 
-      const s3Path = getS3PathForGithubPath(repoId, path)
+    const s3Path = getS3PathForGithubPath(repoId, path)
 
-      if(!await getHead({
-        bucket: AWS_S3_BUCKET,
-        path: s3Path
-      })) {
-        let result
-        let error
-        try {
-          result = await githubRest.gitdata.getBlob({
-            owner,
-            repo,
-            sha: blobSha
-          })
-        } catch(e) {
-          error = e
-          result = null
-        }
-        if (error || !result || !result.data || !result.data.content) {
-          console.error('getBlob failed for ', {owner, repo, blobSha, path, error})
-          throw new Error('getBlob failed for: '+ s3Path)
-        }
-        return uploadS3({
-          stream: Buffer.from(result.data.content, 'base64'),
-          path: s3Path,
-          bucket: AWS_S3_BUCKET
+    if (!await getHead({ bucket: AWS_S3_BUCKET, path: s3Path })) {
+      let result
+      let error
+
+      try {
+        result = await githubRest.gitdata.getBlob({
+          owner,
+          repo,
+          sha: blobSha
         })
-      } else {
-        debug('uploadImages exists %s', s3Path)
+      } catch (e) {
+        error = e
+        result = null
       }
-    })
-  )
+
+      if (error || !result || !result.data || !result.data.content) {
+        console.error(
+          'getBlob failed for ',
+          { owner, repo, blobSha, path, error }
+        )
+        throw new Error(`getBlob failed for: ${s3Path}`)
+      }
+
+      return uploadS3({
+        stream: Buffer.from(result.data.content, 'base64'),
+        path: s3Path,
+        bucket: AWS_S3_BUCKET
+      })
+    } else {
+      debug('uploadImages exists %s', s3Path)
+    }
+  },
+  { concurrency: CONCURRENCY })
 }
 
 module.exports = {
