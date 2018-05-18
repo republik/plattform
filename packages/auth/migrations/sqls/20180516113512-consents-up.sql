8ae3d43d08e0dc26dc09f5d1ae234d74276b7a89
patte <p@tte.io>
2018-05-18 15:05:43 +0200

    fix(consent): make ip column optional in db

--- packages/auth/migrations/sqls/20180516113512-consents-up.sql
+++ packages/auth/migrations/sqls/20180516113512-consents-up.sql
@@ -2,6 +2,6 @@ CREATE TABLE "consents" (
   "id"            uuid primary key not null default uuid_generate_v4(),
   "userId"        uuid references users(id),
   "policy"        text NOT NULL,
-  "ip"            text NOT NULL,
+  "ip"            text,
   "createdAt"     timestamptz NOT NULL default now()
 );
