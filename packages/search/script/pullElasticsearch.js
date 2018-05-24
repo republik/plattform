4ae7444ce3161d640e789fc6b7239a88d0f2b201
Patrick Venetz <venetz@gmail.com>
2018-05-24 21:53:29 +0200

    feat(search): Extend pullElasticsearch to allow zero-downtime indexing

--- packages/search/script/pullElasticsearch.js
+++ packages/search/script/pullElasticsearch.js
@@ -1,45 +1,44 @@
 require('@orbiting/backend-modules-env').config()
+const debug = require('debug')('search:scripts:pullElasticsearch')
 
 const elasticsearch = require('@orbiting/backend-modules-base/lib/elastic')
 const PgDb = require('@orbiting/backend-modules-base/lib/pgdb')
 
 const inserts = require('./inserts')
 const mappings = require('../lib/indices')
+const { getIndexAlias, getIndexDated } = require('../lib/utils')
 
 const elastic = elasticsearch.client()
 
 const flush = process.argv[2] === '--flush'
 
-// format: '2018-05-01T13-43-07'
-const dateString = new Date()
-  .toISOString()
-  .replace(/:/g, '-')
-  .split('.')
-  .shift()
-  .toLowerCase()
-
-const getAliasName = (type) => `republik-${type.toLowerCase()}`
-const getIndexName = (alias) => `${alias}-${dateString}`
-
 PgDb.connect().then(async pgdb => {
-  if (flush) {
-    console.log('flushing all old indices...')
-    await elastic.indices.delete({
-      index: 'republik-*'
+  await Promise.all(mappings.list.map(async ({ type, name, mapping }) => {
+    const readAlias = getIndexAlias(name, 'read')
+    const writeAlias = getIndexAlias(name, 'write')
+    const index = getIndexDated(name)
+
+    debug('updating write alias', { writeAlias, index })
+    const hasWriteAlias = await elastic.indices.existsAlias({
+      name: writeAlias
     })
-  }
 
-  await Promise.all(mappings.list.map(async ({ mapping }) => {
-    const type = Object.keys(mapping).shift()
-    const alias = getAliasName(type)
-    const index = getIndexName(alias)
+    if (hasWriteAlias) {
+      await elastic.indices.updateAliases({
+        body: {
+          actions: [
+            { remove: { index: '_all', alias: writeAlias } }
+          ]
+        }
+      })
+    }
 
-    console.log('creating index', { alias, index })
+    debug('creating index', { writeAlias, index })
     await elastic.indices.create({
       index,
       body: {
         aliases: {
-          [alias]: {}
+          [writeAlias]: {}
         },
         mappings: {
           ...mapping
@@ -51,23 +50,53 @@ PgDb.connect().then(async pgdb => {
       }
     })
 
-    console.log('populating index', { alias, index })
-    await inserts.dict[type.toLowerCase()]({
+    debug('populating index', { writeAlias, index })
+    await inserts.dict[name]({
       indexName: index,
       type,
       elastic,
       pgdb
     })
 
+    debug('enabeling refresh', { writeAlias, index })
     await elastic.indices.putSettings({
       index,
       body: {
         refresh_interval: null // Reset refresh_interval to default
       }
     })
-  }))
 
-  // TODO when to remove old/previous index?
+    debug('updating read alias', { readAlias, index })
+    await elastic.indices.updateAliases({
+      body: {
+        actions: [
+          { remove: { index: '_all', alias: readAlias } },
+          { add: { index, alias: readAlias } }
+        ]
+      }
+    })
+
+    const indices = await elastic.indices.getAlias({
+      index: getIndexAlias(name, '*')
+    })
+
+    const deletable = []
+
+    Object.keys(indices).forEach(name => {
+      if (Object.keys(indices[name].aliases).length === 0) {
+        deletable.push(name)
+      }
+    })
+
+    debug('unlinked indices', deletable)
+
+    if (flush) {
+      debug('deleting indices')
+      await elastic.indices.delete({
+        index: deletable
+      })
+    }
+  }))
 }
 ).then(() => {
   process.exit()
