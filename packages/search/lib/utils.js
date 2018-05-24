7da018c823ebd00eb8cd9685396c8f948a701ef0
Patrick Venetz <venetz@gmail.com>
2018-05-24 21:47:37 +0200

    feat(search): Add utils to get alias and index name in a generic manner

--- packages/search/lib/utils.js
+++ packages/search/lib/utils.js
@@ -1,5 +1,29 @@
 const _ = require('lodash')
 
+const ES_INDEX_PREFIX = 'republik' // TODO: Put into config
+
+/**
+ * @example "2018-05-24-17-05-23"
+ * @return {String} [description]
+ */
+const sanitizedDate =
+  () => new Date().toISOString().slice(0, -5).replace(/[^\d]/g, '-')
+
+/**
+ * @param  {String} name An index name without prefix
+ * @param  {String} type Operation, like "read", "write"
+ * @return {String}      An alias name for an index
+ */
+const getIndexAlias =
+  (name, type) => [ES_INDEX_PREFIX, name, type].filter(Boolean).join('-')
+
+/**
+ * @param  {String} name An index name without prefix
+ * @return {String}      An index name with a date timestamp
+ */
+const getIndexDated =
+  (name) => [ES_INDEX_PREFIX, name, sanitizedDate()].filter(Boolean).join('-')
+
 /**
  * A filter to remove nodes from an mdast with a predicate. Will remove a node
  * if returns true.
@@ -29,5 +53,7 @@ const mdastFilter = function (node, predicate = () => false) {
 }
 
 module.exports = {
+  getIndexAlias,
+  getIndexDated,
   mdastFilter
 }
