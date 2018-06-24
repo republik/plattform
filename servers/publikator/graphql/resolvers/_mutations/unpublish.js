92c925c9014dc97820a6689b5c6ca93ad9d9024a
Patrick Recher <p@tte.io>
2018-06-25 00:51:57 +0200

    fix(search): remove obsolete redis calls in unpublish

--- servers/publikator/graphql/resolvers/_mutations/unpublish.js
+++ servers/publikator/graphql/resolvers/_mutations/unpublish.js
@@ -31,14 +31,11 @@ module.exports = async (
     ...scheduledRefs
   ]
 
-  // const lock = await redlock().lock(lockKey, 1000)
-  await Promise.all([
-    ...scheduledRefs.map(ref => redis.zremAsync(`repos:scheduledIds`, `repos:${repoId}/${ref}`)),
-    ...refs.map(ref => redis.delAsync(`repos:${repoId}/${ref}`)),
-    ...refs.map(ref => deleteRef(repoId, `tags/${ref}`, true))
-  ])
+  await Promise.all(
+    refs.map(ref => deleteRef(repoId, `tags/${ref}`, true))
+  )
     .catch(e => {
-      console.error('Error: one or more promises failed:')
+      console.error('Error: could not delete ref on github')
       console.error(e)
     })
 
