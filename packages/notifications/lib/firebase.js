8b92279c8e481d98c7042ab003a0079b841e556c
Patrick Recher <p@tte.io>
2018-06-27 15:42:10 +0200

    feat(notifications): add firebase-sdk, add early test script

---
+++ packages/notifications/lib/firebase.js
@@ -0,0 +1,19 @@
+const admin = require('firebase-admin')
+
+const {
+  FIREBASE_PROJECT_ID,
+  FIREBASE_CLIENT_EMAIL,
+  FIREBASE_PRIVATE_KEY,
+  FIREBASE_DATABASE_URL
+} = process.env
+
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: FIREBASE_PROJECT_ID,
+    clientEmail: FIREBASE_CLIENT_EMAIL,
+    privateKey: FIREBASE_PRIVATE_KEY
+  }),
+  databaseURL: FIREBASE_DATABASE_URL
+})
+
+module.exports = admin
