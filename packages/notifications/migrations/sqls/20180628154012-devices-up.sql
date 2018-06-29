bd975448f8f446fd07d357b06aa2ea800424dd33
Patrick Recher <p@tte.io>
2018-06-29 16:19:23 +0200

    fix(notifications): typo

--- packages/notifications/migrations/sqls/20180628154012-devices-up.sql
+++ packages/notifications/migrations/sqls/20180628154012-devices-up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE "devices" (
   "id"            uuid primary key not null default uuid_generate_v4(),
   "userId"        uuid not null references users(id),
-  "sessionId"     test not null unique references sessions(sid) ON UPDATE CASCADE ON DELETE CASCADE,
+  "sessionId"     text not null unique references sessions(sid) ON UPDATE CASCADE ON DELETE CASCADE,
   "token"         text unique,
   "information"   jsonb,
   "createdAt"     timestamptz default now(),
