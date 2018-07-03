b1148d0bf749bc9385252ed42e95c204442e5718
Patrick Recher <p@tte.io>
2018-07-03 16:20:06 +0200

    refactor(notifications): remove new API try again

--- packages/notifications/lib/app.js
+++ packages/notifications/lib/app.js
@@ -31,54 +31,5 @@ module.exports = {
     } else {
       debug('no receipients found for publish: %O', args)
     }
-    /* This request would use the "new" API instead of the "legacy" one.
-     * Sadly, it doesn't allow to send to multiple devices simultaniously
-    if (deviceTokens.length > 0) {
-      const notification = {
-        title,
-        body
-      }
-      const message = {
-        data: {
-          url,
-          icon,
-          type
-        },
-        android: {
-          notification: {
-            ...notification,
-            icon,
-            largeIcon: icon
-          }
-        },
-        apns: {
-          payload: {
-            aps: {
-              alert: {
-                ...notification
-              }
-            },
-          }
-        }
-      }
-      debug('#recipients %d, message: %O, result: %O', deviceTokens.length, message)
-      console.log(deviceTokens)
-      const results = await Promise.all(
-        deviceTokens.map( token =>
-          firebase.messaging().send({
-            ...message,
-            token
-          })
-            .catch((error) => {
-              const util = require('util')
-              console.log('Error sending message:', util.inspect(error, 2, {depth: null}))
-            })
-        )
-      )
-      debug('results: %O', results)
-    } else {
-      debug('no receipients found for publish: %O', args)
-    }
-    */
   }
 }
