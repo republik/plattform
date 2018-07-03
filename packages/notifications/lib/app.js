dd02755f6b4a8748e91cc874b09cda2956ba1432
Patrick Recher <p@tte.io>
2018-07-03 11:41:35 +0200

    feat(notifications): debug information in publish

--- packages/notifications/lib/app.js
+++ packages/notifications/lib/app.js
@@ -1,27 +1,34 @@
 const firebase = require('./firebase')
+const debug = require('debug')('notifications:publish')
 
 module.exports = {
-  async publish ({ userIds, title, body, url, icon, type }, { pgdb }) {
+  async publish (args, { pgdb }) {
+    const { userIds, title, body, url, icon, type } = args
+
     const deviceTokens = await pgdb.public.devices.findOneFieldOnly(
       { userId: userIds },
       'token'
     )
 
     if (deviceTokens.length > 0) {
+      const message = {
+        notification: {
+          title,
+          body
+        },
+        data: {
+          url,
+          icon,
+          type
+        }
+      }
+      debug('#recipients %d, message: %O', deviceTokens.length, message)
       await firebase.messaging().sendToDevice(
         deviceTokens,
-        {
-          notification: {
-            title,
-            body
-          },
-          data: {
-            url,
-            icon,
-            type
-          }
-        }
+        message
       )
+    } else {
+      debug('no receipients found for publish: %O', args)
     }
   }
 }
