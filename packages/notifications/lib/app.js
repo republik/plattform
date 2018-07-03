0a30b6dc138d6f1e00fd85145f43a12442c90a4b
Patrick Recher <p@tte.io>
2018-07-03 16:19:19 +0200

    refactor(notifications): try sending messages with the "new" API instead of the "legacy" one

--- packages/notifications/lib/app.js
+++ packages/notifications/lib/app.js
@@ -6,12 +6,11 @@ module.exports = {
     const { userIds, title, body, url, icon, type } = args
 
     const deviceTokens = await pgdb.public.devices.find(
-      { userId: userIds },
+      { userId: userIds }
     )
-      .then( devices => devices
-        .map( d => d.token )
+      .then(devices => devices
+        .map(d => d.token)
       )
-
     if (deviceTokens.length > 0) {
       const message = {
         notification: {
@@ -32,5 +31,54 @@ module.exports = {
     } else {
       debug('no receipients found for publish: %O', args)
     }
+    /* This request would use the "new" API instead of the "legacy" one.
+     * Sadly, it doesn't allow to send to multiple devices simultaniously
+    if (deviceTokens.length > 0) {
+      const notification = {
+        title,
+        body
+      }
+      const message = {
+        data: {
+          url,
+          icon,
+          type
+        },
+        android: {
+          notification: {
+            ...notification,
+            icon,
+            largeIcon: icon
+          }
+        },
+        apns: {
+          payload: {
+            aps: {
+              alert: {
+                ...notification
+              }
+            },
+          }
+        }
+      }
+      debug('#recipients %d, message: %O, result: %O', deviceTokens.length, message)
+      console.log(deviceTokens)
+      const results = await Promise.all(
+        deviceTokens.map( token =>
+          firebase.messaging().send({
+            ...message,
+            token
+          })
+            .catch((error) => {
+              const util = require('util')
+              console.log('Error sending message:', util.inspect(error, 2, {depth: null}))
+            })
+        )
+      )
+      debug('results: %O', results)
+    } else {
+      debug('no receipients found for publish: %O', args)
+    }
+    */
   }
 }
