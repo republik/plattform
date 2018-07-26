61b608f8f36d27a05bb587ea2c721296aa89bab9
Daniel Pfänder <daniel.pfaender@project-r.construction>
2018-07-26 11:14:27 +0200

    fix(Radio): consistent outer circle color

--- src/components/Form/Radio.js
+++ src/components/Form/Radio.js
@@ -29,7 +29,7 @@ const Radio = ({checked, disabled}) => (
     {checked && (
       <circle fill={disabled ? colors.disabled : colors.primary} cx='12' cy='12' r='6' />
     )}
-    <circle fill='none' stroke={disabled ? colors.disabled : checked ? colors.primary : colors.divider}
+    <circle fill='none' stroke={checked && !disabled ? colors.primary : colors.divider}
       cx='12' cy='12' r='11.5' />
   </svg>
 )
