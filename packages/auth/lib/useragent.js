fd7a9ee1023b189f9dfb3daf4f19e405283378e0
Patrick Recher <p@tte.io>
2018-07-29 15:58:06 +0200

    fix(app): detect app version in user-agent

--- packages/auth/lib/useragent.js
+++ packages/auth/lib/useragent.js
@@ -1,14 +1,16 @@
 const useragent = require('useragent')
 
+const appRegex = /RepublikApp\/(.*)/
+
 module.exports.detect = (ua) => {
   if (!ua) {
     return
   }
 
-  // ToDo: Check what useragent.parse(ua) returns once it's regular ua with RepublikApp appended
-  // - currently the app only sends RepublikApp and the result is Other 0.0 / Other 0.0
-  if (ua.match(/RepublikApp/)) {
-    return 'Republik App'
+  const match = ua.match(appRegex)
+  if (match) {
+    const version = match[1]
+    return `Republik App (${version})`
   }
 
   return useragent.parse(ua).toString()
