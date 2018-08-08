f7cfc2653bf8ef8b4a0a59f3a072fc140da771a5
Patrick Recher <p@tte.io>
2018-08-09 00:00:29 +0200

    fix(auth): clean bundleVersion from user-agent

--- packages/auth/lib/useragent.js
+++ packages/auth/lib/useragent.js
@@ -9,7 +9,7 @@ module.exports.detect = (ua) => {
 
   const match = ua.match(appRegex)
   if (match) {
-    const version = match[1]
+    const version = match[1].split('/')[0] // remove bundleVersion
     return `Republik App (${version})`
   }
 
