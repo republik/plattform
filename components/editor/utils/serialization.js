005ef32b40e80418c2f6719f630ca370f748c0ed
Daniel Pfänder <daniel.pfaender@project-r.construction>
2018-08-21 17:32:35 +0200

    support Subject in title module

--- components/editor/utils/serialization.js
+++ components/editor/utils/serialization.js
@@ -7,3 +7,10 @@ export const findOrCreate = (nodes, targetNode, newProps) => {
     ...newProps
   }
 }
+
+export const findNode = (nodes, targetNode) => {
+  const match = node => Object.keys(targetNode)
+    .every(key => node[key] === targetNode[key])
+  const node = nodes.find(match)
+  return node
+}
