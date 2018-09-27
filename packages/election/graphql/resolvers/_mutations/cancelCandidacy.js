34e8e2cb1d8641ae530b815dfe540e16272e18d9
Andreas Moor <amoor@fastmail.net>
2018-09-27 12:04:21 +0200

    feat(election): fix cancelCandidacy endpoint

--- packages/election/graphql/resolvers/_mutations/cancelCandidacy.js
+++ packages/election/graphql/resolvers/_mutations/cancelCandidacy.js
@@ -9,7 +9,7 @@ module.exports = async (_, { slug }, { pgdb, user: me, req }) => {
   ensureSignedIn(req)
   Roles.ensureUserIsInRoles(me, ['admin'])
 
-  const election = await getElections(pgdb, me, { slug })[0]
+  const election = (await getElections(pgdb, me, {slug}))[0]
 
   if (!election) {
     throw new Error(`No election for slug ${slug}`)
@@ -18,5 +18,5 @@ module.exports = async (_, { slug }, { pgdb, user: me, req }) => {
   await pgdb.public.electionCandidacies
     .deleteOne({ userId: me.id, electionId: election.id })
 
-  return getElections(pgdb, me, { slug })[0]
+  return (await getElections(pgdb, me, {slug}))[0]
 }
