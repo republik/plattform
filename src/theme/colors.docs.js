eaa3cf61d2f05d9ed02190875a14987e2ffee367
Thomas Preusse <tpreusse@users.noreply.github.com>
2018-10-05 13:06:08 +0200

    docs(colors): use markdown for proper headline and paragraph style

--- src/theme/colors.docs.js
+++ src/theme/colors.docs.js
@@ -1,43 +1,52 @@
-import React from 'react'
-import {Page, ColorSpecimen, CodeSpecimen} from 'catalog'
+import React, { Fragment } from 'react'
+import { markdown, ColorSpecimen, CodeSpecimen } from 'catalog'
 import colors from './colors'
 
-export default () => (
-  <Page>
-    <CodeSpecimen lang='js'>
-      {`import {colors} from '@project-r/styleguide'`}
-    </CodeSpecimen>
-    <ColorSpecimen span={3} name='primary' value={colors.primary} />
-    <ColorSpecimen span={2} name='secondary' value={colors.secondary} />
-    <ColorSpecimen span={1} name='disabled' value={colors.disabled} />
-    <ColorSpecimen span={3} name='primaryBg' value={colors.primaryBg} />
-    <ColorSpecimen span={2} name='secondaryBg' value={colors.secondaryBg} />
-    <ColorSpecimen span={1} name='divider' value={colors.divider} />
+export default () =>  markdown`
 
-    <ColorSpecimen span={4} name='text' value={colors.text} />
-    <ColorSpecimen span={2} name='lightText' value={colors.lightText} />
+${<Fragment>
+  <CodeSpecimen lang='js'>
+    {`import { colors } from '@project-r/styleguide'`}
+  </CodeSpecimen>
+  <ColorSpecimen span={3} name='primary' value={colors.primary} />
+  <ColorSpecimen span={2} name='secondary' value={colors.secondary} />
+  <ColorSpecimen span={1} name='disabled' value={colors.disabled} />
+  <ColorSpecimen span={3} name='primaryBg' value={colors.primaryBg} />
+  <ColorSpecimen span={2} name='secondaryBg' value={colors.secondaryBg} />
+  <ColorSpecimen span={1} name='divider' value={colors.divider} />
 
-    <ColorSpecimen span={1} name='error' value={colors.error} />
-    <ColorSpecimen span={1} name='online' value={colors.online} />
-    <ColorSpecimen span={1} name='social' value={colors.social} />
+  <ColorSpecimen span={4} name='text' value={colors.text} />
+  <ColorSpecimen span={2} name='lightText' value={colors.lightText} />
 
-    <h2>Formats</h2>
-    <ColorSpecimen span={1} name='editorial' value={colors.editorial} />
-    <ColorSpecimen span={1} name='feuilleton' value={colors.feuilleton} />
-    <ColorSpecimen span={1} name='scribble' value={colors.scribble} />
-    <ColorSpecimen span={1} name='meta' value={colors.meta} />
+  <ColorSpecimen span={1} name='error' value={colors.error} />
+  <ColorSpecimen span={1} name='online' value={colors.online} />
+  <ColorSpecimen span={1} name='social' value={colors.social} />
+</Fragment>}
 
-    <h2>Charts</h2>
-    <p>Most charts will use sequential colors, e.g. bars all in the frist sequential color and one highlight in the second. Opposite colors are used for diverging ranges, with an optional neutral color in the middle.</p>
-    {colors.sequential3.map((c, i) =>
-      <ColorSpecimen span={1} name={`sequential.${i}`} value={c} />)}
-    {[].concat(colors.opposite3).reverse().map((c, i) =>
-      <ColorSpecimen span={1} name={`opposite.${2 - i}`} value={c} />)}
-    <ColorSpecimen span={1} name='neutral' value={colors.neutral} />
-    <br />
-    <h3>Discrete</h3>
-    <p>For categories, e.g. 10 lines in a chart, each representing a country.</p>
-    {colors.discrete.map((c, i) =>
-      <ColorSpecimen span={1} name={`discrete.${i}`} value={c} />)}
-  </Page>
-)
+## Formats
+
+${<Fragment>
+  <ColorSpecimen span={1} name='editorial' value={colors.editorial} />
+  <ColorSpecimen span={1} name='feuilleton' value={colors.feuilleton} />
+  <ColorSpecimen span={1} name='scribble' value={colors.scribble} />
+  <ColorSpecimen span={1} name='meta' value={colors.meta} />
+</Fragment>}
+
+## Charts
+
+Most charts will use sequential colors, e.g. bars all in the frist sequential color and one highlight in the second. Opposite colors are used for diverging ranges, with an optional neutral color in the middle.
+
+${colors.sequential3.map((c, i) =>
+  <ColorSpecimen span={1} name={`sequential.${i}`} value={c} />)}
+${[].concat(colors.opposite3).reverse().map((c, i) =>
+  <ColorSpecimen span={1} name={`opposite.${2 - i}`} value={c} />)}
+${<ColorSpecimen span={1} name='neutral' value={colors.neutral} />}
+
+### Discrete
+
+For categories, e.g. 10 lines in a chart, each representing a country.
+
+${colors.discrete.map((c, i) =>
+  <ColorSpecimen span={1} name={`discrete.${i}`} value={c} />)}
+
+`
