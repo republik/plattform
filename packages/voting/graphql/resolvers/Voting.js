3d4f7b87ee3ea5627f7fc12b635597c00d059464
Patrick Recher <p@tte.io>
2018-10-09 10:58:47 +0200

    fix(votings): make old results queryable with adapted API

--- packages/voting/graphql/resolvers/Voting.js
+++ packages/voting/graphql/resolvers/Voting.js
@@ -51,11 +51,5 @@ module.exports = {
     return ballot.updatedAt
   }
   /* either voting.result is freezed into crowdfunding by countVoting
-     or it must remain null. For live voting stats, the counts query from
-     count voting has to be migrated into an apollo resolver, to provide
-     VoteResult.options
-  result (voting, args, {pgdb, user}) {
-    return voting.result || {voting}
-  }
-  */
+     or it must remain null. */
 }
