dff818b7f31581ddc60b3a1786301647304b65c3
Daniel Pfänder <daniel.pfaender@project-r.construction>
2018-10-23 11:20:06 +0200

    fix(Discussion): !!collapsable

--- servers/publikator/lib/Discussion.js
+++ servers/publikator/lib/Discussion.js
@@ -6,7 +6,7 @@ const upsert = async (repoMeta, docMeta, context) => {
   const settings = {
     title: docMeta.title,
     documentPath: docMeta.path,
-    collapsable: docMeta.collapsable !== undefined ? docMeta.collapsable : false,
+    collapsable: !!docMeta.collapsable,
     ...docMeta.commentsMaxLength
       ? { maxLength: docMeta.commentsMaxLength }
       : { },
