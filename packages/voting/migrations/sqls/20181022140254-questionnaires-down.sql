f1348b20cc2d912d08d01ab0295c3a8865deb210
Patrick Recher <p@tte.io>
2018-10-23 20:17:52 +0200

    fix(voting): add down migration

--- packages/voting/migrations/sqls/20181022140254-questionnaires-down.sql
+++ packages/voting/migrations/sqls/20181022140254-questionnaires-down.sql
@@ -1,4 +1,13 @@
+DROP TABLE IF EXISTS "answers";
+
+DROP TRIGGER IF EXISTS check_answer ON answers;
+DROP FUNCTION IF EXISTS check_answer_trg();
+
 DROP TABLE IF EXISTS "questions";
 DROP DOMAIN question_type;
+
+DROP TABLE IF EXISTS "questionnaireSubmissions";
+
 DROP TABLE IF EXISTS "questionnaireMembershipRequirements";
 DROP TABLE IF EXISTS "questionnaires";
+
