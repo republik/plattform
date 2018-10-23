ca6677a2f66c12369fb50834dc7b5a8665e65fbf
Daniel Pfänder <daniel.pfaender@project-r.construction>
2018-10-23 11:00:20 +0200

    fix(Discussion): default false value for collapsable

--- servers/publikator/lib/Discussion.js
+++ servers/publikator/lib/Discussion.js
@@ -6,7 +6,7 @@ const upsert = async (repoMeta, docMeta, context) => {
   const settings = {
     title: docMeta.title,
     documentPath: docMeta.path,
-    collapsable: docMeta.collapsable,
+    collapsable: docMeta.collapsable !== undefined ? docMeta.collapsable : false,
     ...docMeta.commentsMaxLength
       ? { maxLength: docMeta.commentsMaxLength }
       : { },
