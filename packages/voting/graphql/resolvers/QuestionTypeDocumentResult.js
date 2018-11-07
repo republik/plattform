0df9b3131acc92ab10eaab9160ecef834fc37619
Patrick Recher <p@tte.io>
2018-11-07 21:20:25 +0100

    fix(voting): only request content for document.result.doc if requested

--- packages/voting/graphql/resolvers/QuestionTypeDocumentResult.js
+++ packages/voting/graphql/resolvers/QuestionTypeDocumentResult.js
@@ -1,10 +1,23 @@
-const { graphql: { resolvers: { queries: { document: getDocument } } } } = require('@orbiting/backend-modules-documents')
+const search = require('@orbiting/backend-modules-search/graphql/resolvers/_queries/search')
+const getFieldList = require('graphql-list-fields')
 
 module.exports = {
-  async document (result, args, context) {
+  async document (result, args, context, info) {
+    if (result && result.document) {
+      return result.document
+    }
     if (!result || !result.path) {
       return null
     }
-    return getDocument(null, { path: result.path }, context)
+    const fields = getFieldList(info, true)
+    const withoutContent = fields.indexOf('content') === -1
+    return search(null, {
+      withoutContent,
+      filter: {
+        path: result.path,
+        type: 'Document'
+      }
+    }, context)
+      .then(docCon => docCon.nodes[0] && docCon.nodes[0].entity)
   }
 }
