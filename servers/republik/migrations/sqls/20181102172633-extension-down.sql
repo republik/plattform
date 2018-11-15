6c12218fd484a0d454c29c360a30592c263d9585
Patrick Venetz <venetz@gmail.com>
2018-11-15 09:28:38 -0300

    fix(crowdfundings): Update migration SQL stmt
    - Remove action, additionalDaysActions tables
    - Add rules attribute to packages

--- servers/republik/migrations/sqls/20181102172633-extension-down.sql
+++ servers/republik/migrations/sqls/20181102172633-extension-down.sql
@@ -1,6 +1,5 @@
 ALTER TABLE "pledgeOptions" DROP COLUMN IF EXISTS "customization" ;
 ALTER TABLE "membershipPeriods" DROP COLUMN IF EXISTS "kind" ;
-ALTER TABLE "packages" DROP COLUMN IF EXISTS "custom" ;
-
-DROP TABLE IF EXISTS "actions" ;
-DROP TABLE IF EXISTS "additionalDaysActions" ;
+ALTER TABLE "packages"
+  DROP COLUMN IF EXISTS "custom",
+  DROP COLUMN IF EXISTS "rules" ;
--- servers/republik/migrations/sqls/20181102172633-extension-up.sql
+++ servers/republik/migrations/sqls/20181102172633-extension-up.sql
@@ -1,23 +1,6 @@
-CREATE TABLE "additionalDaysActions" (
-  "id"                       uuid primary key not null default uuid_generate_v4(),
-  "beginDate"                timestamptz not null,
-  "endDate"                  timestamptz not null,
-  "membershipTypeId"         uuid not null references "membershipTypes",
-  "minCreatedAt"             timestamptz not null,
-  "maxCreatedAt"             timestamptz not null,
-  "additionalDaysBeforeDate" timestamptz not null
-) ;
-
--- generic possibility:
-CREATE TABLE "actions" (
-  "id"                       uuid primary key not null default uuid_generate_v4(),
-  "beginDate"                timestamptz not null,
-  "endDate"                  timestamptz not null,
-  "config"                   jsonb not null
-) ;
-
 ALTER TABLE "packages"
-  ADD COLUMN "custom" boolean NOT NULL DEFAULT false;
+  ADD COLUMN "custom" boolean NOT NULL DEFAULT false,
+  ADD COLUMN "rules" jsonb NOT NULL DEFAULT '[]'::jsonb ;
 
 ALTER TABLE "membershipPeriods"
   ADD COLUMN "kind" character varying
