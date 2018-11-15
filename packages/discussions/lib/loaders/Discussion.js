f4f2a4add5f9f527b5821abf7277ba2f199eee4e
Patrick Recher <p@tte.io>
2018-11-15 19:42:07 +0100

    refactor(dataloader): simplify loading of connected primary keys (discussionPreferences)

--- packages/discussions/lib/loaders/Discussion.js
+++ packages/discussions/lib/loaders/Discussion.js
@@ -1,36 +1,23 @@
 const createDataLoader = require('@orbiting/backend-modules-dataloader')
-const uniq = require('lodash/uniq')
-
-const getDiscussionPreferences = (discussionId, userIds, context) =>
-  context.pgdb.public.discussionPreferences.find({
-    userId: userIds,
-    discussionId
-  })
-    .then(dps => dps
-      .map(async (dp) => ({
-        ...dp,
-        credential: dp.credentialId && await context.loaders.User.credential.load(dp.credentialId)
-      }))
-    )
 
 module.exports = (context) => ({
   byId: createDataLoader(ids =>
     context.pgdb.public.discussions.find({ id: ids })
   ),
   Commenter: {
-    discussionPreferences: createDataLoader((keyObjs) =>
-      Promise.all(
-        uniq(keyObjs.map(o => o.discussionId))
-          .map(discussionId => {
-            const userIds = keyObjs
-              .filter(o => o.discussionId === discussionId)
-              .map(o => o.userId)
-            return getDiscussionPreferences(discussionId, userIds, context)
-          })
-      )
-        .then(promises => promises
-          .reduce((agg, result) => agg.concat(result), [])
+    discussionPreferences: createDataLoader(keyObjs =>
+      context.pgdb.public.discussionPreferences.find({
+        or: keyObjs.map(keyObj => ({
+          and: keyObj
+        }))
+      })
+        .then(dps => dps
+          .map(async (dp) => ({
+            ...dp,
+            credential: dp.credentialId && await context.loaders.User.credential.load(dp.credentialId)
+          }))
         )
+
     )
   }
 })
