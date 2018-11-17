21d422f649a273c600162208bcbecf6377704499
Patrick Recher <p@tte.io>
2018-11-17 15:30:19 +0100

    fix(document): resolve linkedDiscussion

--- packages/documents/graphql/resolvers/Meta.js
+++ packages/documents/graphql/resolvers/Meta.js
@@ -1,4 +1,13 @@
 module.exports = {
   ownDiscussion: ({ repoId }, args, { loaders }) =>
-    loaders.Discussion.byRepoId.load(repoId)
+    loaders.Discussion.byRepoId.load(repoId),
+  linkedDiscussion: ({ discussion }, args, { loaders }) => {
+    if (!discussion) {
+      return null
+    }
+    const repoId = typeof discussion === 'object'
+      ? discussion.meta.repoId
+      : discussion
+    return loaders.Discussion.byRepoId.load(repoId)
+  }
 }
