bc772a62b81e00beef1c1b0677c92af628089e13
Patrick Venetz <venetz@gmail.com>
2018-11-20 12:15:52 -0300

    feat(crowdfundings): Added fn getLatestPeriod helper to lib/utils

--- servers/republik/modules/crowdfundings/lib/utils.js
+++ servers/republik/modules/crowdfundings/lib/utils.js
@@ -1,17 +1,23 @@
-// Finds latest endDate in a series of membershipPeriods
-const getLatestEndDate =
+// Finds latest period in a series of membershipPeriods
+const getLatestPeriod =
   periods => periods
-    .map(p => p.endDate)
+    .map(p => p)
     .reduce(
       (accumulator, currentValue) => {
         if (!accumulator) {
           return currentValue
         }
 
-        return currentValue > accumulator ? currentValue : accumulator
+        return currentValue.createdAt > accumulator.createdAt
+          ? currentValue
+          : accumulator
       }
     )
 
+// Finds latest endDate in a series of membershipPeriods
+const getLatestEndDate = periods => getLatestPeriod(periods).endDate
+
 module.exports = {
-  getLatestEndDate
+  getLatestEndDate,
+  getLatestPeriod
 }
