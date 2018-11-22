41800fb897b98237c6b1123b93a465f370a3652c
Andreas Moor <amoor@fastmail.net>
2018-11-22 15:36:54 +0100

    perf: Delete sessions & devices for Android app notifications that were flagged as "unregistered"

---
+++ packages/notifications/lib/utils.js
@@ -0,0 +1,7 @@
+export const deleteSessionForDevices = async (tokens, pgdb) => {
+  return pgdb.query(`
+    DELETE FROM sessions s WHERE s.id IN (
+                                         SELECT d."sessionId" FROM devices d WHERE d.id IN (:tokens)
+                                         )
+  `, {tokens})
+}
