324da30a1d28d3a5a905892b0483a22e9bd3fb47
Patrick Venetz <venetz@gmail.com>
2018-11-22 22:36:28 -0300

    fix(base): Add debug logging to lib/redis

--- packages/base/lib/redis.js
+++ packages/base/lib/redis.js
@@ -1,3 +1,4 @@
+const debug = require('debug')('base:lib:redis')
 const redis = require('redis')
 const bluebird = require('bluebird')
 const { REDIS_URL } = process.env
@@ -5,6 +6,7 @@ const { REDIS_URL } = process.env
 bluebird.promisifyAll(redis.RedisClient.prototype)
 bluebird.promisifyAll(redis.Multi.prototype)
 
+debug('creating client', { REDIS_URL })
 const client = redis.createClient(REDIS_URL)
 
 client.__defaultExpireSeconds = 3 * 7 * 24 * 60 * 60 // 3 weeks
