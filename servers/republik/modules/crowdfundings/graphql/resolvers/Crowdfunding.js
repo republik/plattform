17245cfce3a8b1229254016767ce591c7f773105
Patrick Recher <p@tte.io>
2018-11-26 16:10:02 +0100

    refactor(crowdfundings): only calc requested stats, fix memberships count for periods

--- servers/republik/modules/crowdfundings/graphql/resolvers/Crowdfunding.js
+++ servers/republik/modules/crowdfundings/graphql/resolvers/Crowdfunding.js
@@ -10,85 +10,18 @@ module.exports = {
       orderBy: ['people asc', 'money asc']
     })
   },
-  async status (crowdfunding, {forceUpdate}, {pgdb}) {
+  async status (crowdfunding, { forceUpdate }, { pgdb }) {
     if (!forceUpdate && crowdfunding.result && crowdfunding.result.status) {
       const { status } = crowdfunding.result
       return {
         ...status,
+        forceUpdate,
         memberships: status.memberships || 0
       }
     }
-    const [money, memberships, people] = await Promise.all([
-      // money
-      pgdb.public.queryOneField(`
-        SELECT
-          SUM(pl.total)
-        FROM
-          pledges pl
-        JOIN
-          packages pa
-          ON pl."packageId" = pa.id
-        JOIN
-          crowdfundings cf
-          ON
-            pa."crowdfundingId" = cf.id AND
-            cf.id = :crowdfundingId
-        WHERE
-          pl.status = 'SUCCESSFUL'
-      `, {
-        crowdfundingId: crowdfunding.id
-      }) || 0,
-      // memberships
-      pgdb.public.queryOneField(`
-        SELECT
-          COUNT(
-            DISTINCT(
-              m.id
-            )
-          )
-        FROM
-          memberships m
-        JOIN
-          pledges pl
-          ON m."pledgeId" = pl.id
-        JOIN
-          packages pa
-          ON pl."packageId" = pa.id
-        JOIN
-          crowdfundings cf
-          ON
-            pa."crowdfundingId" = cf.id AND
-            cf.id = :crowdfundingId
-      `, {
-        crowdfundingId: crowdfunding.id
-      }) || 0,
-      // people
-      pgdb.public.queryOneField(`
-        SELECT
-          COUNT(
-            DISTINCT(
-              u.id
-            )
-          )
-        FROM
-          pledges pl
-        JOIN
-          users u
-          ON pl."userId" = u.id
-        JOIN
-          packages pa
-          ON pl."packageId" = pa.id
-        JOIN
-          crowdfundings cf
-          ON pa."crowdfundingId" = cf.id
-        WHERE
-          pl.status = 'SUCCESSFUL' AND
-          cf.id = :crowdfundingId
-      `, {
-        crowdfundingId: crowdfunding.id
-      }) || 0
-    ])
-    return {money, memberships, people}
+    return { // for downstream resolvers
+      crowdfunding
+    }
   },
   hasEnded (crowdfunding) {
     const now = new Date()
