973528a796dbe116370f37aa41d04d9722337e83
Patrick Recher <3500621+patte@users.noreply.github.com>
2018-12-04 15:00:38 +0100

    whitespacce
    Co-Authored-By: garamond <amoor@fastmail.net>

--- packages/notifications/lib/utils.js
+++ packages/notifications/lib/utils.js
@@ -1,7 +1,7 @@
 const deleteSessionForDevices = async (tokens, pgdb) => {
   return pgdb.query(`
     DELETE FROM sessions s WHERE s.id IN (
-                                         SELECT d."sessionId" FROM devices d WHERE d.id IN (:tokens)
+      SELECT d."sessionId" FROM devices d WHERE d.id IN (:tokens)
                                          )
   `, {tokens})
 }
