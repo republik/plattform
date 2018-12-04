4e935f1b60d570a69148f2a762cf7d96cf9e745f
Patrick Recher <p@tte.io>
2018-12-04 16:46:33 +0100

    fix(discussion): lock down discussions query

--- packages/discussions/graphql/resolvers/_queries/discussions.js
+++ packages/discussions/graphql/resolvers/_queries/discussions.js
@@ -1,2 +1,6 @@
-module.exports = async (_, args, { pgdb }) =>
-  pgdb.public.discussions.find({ hidden: false })
+const { Roles } = require('@orbiting/backend-modules-auth')
+
+module.exports = async (_, args, { pgdb, user: me }) => {
+  Roles.ensureUserIsInRoles(me, ['member', 'editor', 'supporter', 'admin'])
+  return pgdb.public.discussions.find({ hidden: false })
+}
