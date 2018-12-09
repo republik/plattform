2fd63b7634a3aa40ede78d7af21810af4226140d
Patrick Recher <p@tte.io>
2018-12-09 20:07:17 +0100

    fix(crowdfundings): mailLog.userId correctly reference user table

--- servers/republik/migrations/sqls/20181130161210-mail-log-up.sql
+++ servers/republik/migrations/sqls/20181130161210-mail-log-up.sql
@@ -3,9 +3,9 @@ CREATE EXTENSION IF NOT EXISTS "citext";
 CREATE TABLE "mailLog" (
   "id"              uuid primary key not null default uuid_generate_v4(),
   "type"            character varying not null,
-  "userId"          uuid,
-  "membershipIds"   uuid[],
+  "userId"          uuid references "users" on update cascade on delete set null,
   "email"           citext not null,
+  "membershipIds"   uuid[],
   "info"            jsonb,
   "resultOk"        boolean,
   "resultPayload"   jsonb,
