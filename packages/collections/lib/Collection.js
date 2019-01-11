aeaeb7562c6546b8997bd15a075e0d1fcdac50d9
Thomas Preusse <tpreusse@users.noreply.github.com>
2019-01-11 02:17:24 +0100

    fix(collections): single item get

--- packages/collections/lib/Collection.js
+++ packages/collections/lib/Collection.js
@@ -29,7 +29,7 @@ const findDocumentItems = (args, { pgdb }) =>
   )
 
 const getDocumentItem = (args, { pgdb }) =>
-  pgdb.query(`
+  pgdb.queryOne(`
     SELECT i.*
     FROM "collectionDocumentItems" i
     WHERE
