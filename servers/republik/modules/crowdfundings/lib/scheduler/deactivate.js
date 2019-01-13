0b75624cd615c2d191b8059c1fc6a231612ddac7
Patrick Venetz <venetz@gmail.com>
2019-01-13 17:17:01 +0100

    feat(crowdfundings): Use transaction and invalidate user cache

--- servers/republik/modules/crowdfundings/lib/scheduler/deactivate.js
+++ servers/republik/modules/crowdfundings/lib/scheduler/deactivate.js
@@ -2,6 +2,8 @@ const debug = require('debug')('crowdfundings:lib:scheduler:deactivate')
 const moment = require('moment')
 const Promise = require('bluebird')
 
+const createCache = require('../cache')
+
 // Amount of days before a cancelled membership is deactivated after membership
 // periods end
 const CANCELLED_GRACE_PERIOD_DAYS = 0
@@ -48,12 +50,25 @@ const deactivate = async (args, { pgdb, mail: { enforceSubscriptions } }) => {
   await Promise.map(
     memberships,
     async membership => {
-      await pgdb.public.memberships.update(
-        { id: membership.id },
-        { active: false, renew: false, updatedAt: moment() }
-      )
+      const transaction = await pgdb.transactionBegin()
 
-      await enforceSubscriptions({ userId: membership.userId, pgdb })
+      try {
+        await transaction.public.memberships.update(
+          { id: membership.id },
+          { active: false, renew: false, updatedAt: moment() }
+        )
+
+        await enforceSubscriptions({ userId: membership.userId, pgdb })
+
+        const cache = createCache({ prefix: `User:${membership.userId}` })
+        cache.invalidate()
+
+        await transaction.transactionCommit()
+      } catch (e) {
+        await transaction.transactionRollback()
+        console.log('transaction rollback', { membership, error: e })
+        throw e
+      }
     },
     { concurrency: 10 }
   )
