0964ffd8c1b1e6b072d25b397829b853c91fc380
Daniel Pfänder <daniel.pfaender@project-r.construction>
2019-01-28 11:15:24 +0100

    fix(mediaResponses): renaming leftover

--- servers/republik/graphql/resolvers/_queries/mediaResponses.js
+++ servers/republik/graphql/resolvers/_queries/mediaResponses.js
@@ -1,5 +1,5 @@
 module.exports = async (_, args, {pgdb}) => {
-  const data = await pgdb.public.gsheets.findOneFieldOnly({name: 'reports'}, 'data')
+  const data = await pgdb.public.gsheets.findOneFieldOnly({name: 'mediaResponses'}, 'data')
   if (!data) {
     return []
   }
