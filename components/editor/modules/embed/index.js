bfc87e87f5b31863e892f8151077d56c38998997
Patrick Recher <p@tte.io>
2019-02-05 12:52:18 +0100

    feat(embed): request and store mediaId

--- components/editor/modules/embed/index.js
+++ components/editor/modules/embed/index.js
@@ -28,6 +28,7 @@ query getVideoEmbed($id: ID!, $embedType: EmbedType!) {
       userProfileImageUrl
       aspectRatio
       durationMs
+      mediaId
     }
     ... on VimeoEmbed {
       platform
@@ -47,6 +48,7 @@ query getVideoEmbed($id: ID!, $embedType: EmbedType!) {
         thumbnail
       }
       durationMs
+      mediaId
     }
   }
 }
