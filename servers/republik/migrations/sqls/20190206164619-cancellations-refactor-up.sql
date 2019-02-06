f036f5e4c5b497d1e66e0c2d1c00a97dbf4e90b5
Patrick Recher <p@tte.io>
2019-02-06 19:04:05 +0100

    feat(cancellations): add script updateCancellations, update migration

--- servers/republik/migrations/sqls/20190206164619-cancellations-refactor-up.sql
+++ servers/republik/migrations/sqls/20190206164619-cancellations-refactor-up.sql
@@ -1,25 +1,37 @@
-ALTER DOMAIN cancel_category DROP CONSTRAINT cancel_category_check
+ALTER DOMAIN cancel_category DROP CONSTRAINT cancel_category_check;
 
 ALTER DOMAIN cancel_category ADD CONSTRAINT cancel_category_check
   CHECK(
     VALUE IN (
       'EDITORIAL',
+      'EDITORAL_NARCISSISTIC',
       'NO_TIME',
       'TOO_EXPENSIVE',
-      'OTHER',
-      'VOID',
-      'SYSTEM',
-      'LOGIN_TECH'
       'NO_MONEY',
+      'LOGIN_TECH',
       'PAPER',
-      'EDITORIAL',
       'EXPECTIONS',
       'RARELY_READ',
       'TOO_MUCH_TO_READ',
-      'EDITORAL_NARCISSISTIC',
       'CROWFUNDING_ONLY',
-      'SEVERAL_REASONS'
+      'SEVERAL_REASONS',
+      'OTHER',
+      'VOID',
+      'SYSTEM'
     )
   );
 
+ALTER TABLE "membershipCancellations"
+  ADD COLUMN "suppressConfirmation" boolean not null default false,
+  ADD COLUMN "suppressWinback" boolean not null default false,
+  ADD COLUMN "cancelledViaSupport" boolean not null default false
+;
 
+UPDATE "membershipCancellations" SET
+  "suppressConfirmation" = "suppressNotifications",
+  "suppressWinback" = "suppressNotifications"
+;
+
+ALTER TABLE "membershipCancellations"
+  DROP COLUMN "suppressNotifications"
+;
