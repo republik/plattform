9fec89a3a652194557d2e65888a233f92d057373
Patrick Venetz <venetz@gmail.com>
2019-02-22 10:38:19 +0100

    refactor(republik): Move memberStats.count to lib file

--- servers/republik/graphql/resolvers/_queries/memberStats.js
+++ servers/republik/graphql/resolvers/_queries/memberStats.js
@@ -1,17 +1,5 @@
-const { PARKING_USER_ID } = process.env
+const { count } = require('../../../lib/memberStats')
 
-module.exports = (_, args, { pgdb, user: me }) => ({
-  count: async () => {
-    return pgdb.queryOneField(`
-      SELECT
-        count(*)
-      FROM
-        users u
-      WHERE
-        u.roles @> '["member"]' AND
-        u.id != :excludeUserId
-    `, {
-      excludeUserId: PARKING_USER_ID
-    })
-  }
+module.exports = (_, args, context) => ({
+  count: count(context)
 })
