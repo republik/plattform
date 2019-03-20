07e5e49af1fbb64ea645ba496fac2069328829b6
Patrick Venetz <venetz@gmail.com>
2019-03-20 10:06:35 +0100

    Add VersionControl/BranchingNotice
    A component that subscribes to repository
    updates and renders a button or icon if another
    commit can be detected.

---
+++ components/VersionControl/BranchingNotice.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import { Subscription } from 'react-apollo'
+import NewerVersionIcon from 'react-icons/lib/md/call-split'
+
+import { Button, colors } from '@project-r/styleguide'
+
+import { repoSubscription } from './index'
+import { Link } from '../../lib/routes'
+
+const WarningIcon = ({ repoId }) => (
+  <div title='Neuere Version verfügbar'>
+    <Link route='repo/tree' params={{ repoId: repoId.split('/') }}>
+      <Button style={{
+        backgroundColor: colors.social,
+        borderColor: colors.social,
+        color: '#fff',
+        height: 40,
+        marginRight: 4,
+        marginTop: 27,
+        minWidth: 40,
+        padding: 0,
+        width: 40
+      }}>
+        <NewerVersionIcon style={{ marginTop: -5 }} />
+      </Button>
+    </Link>
+  </div>
+)
+
+const WarningButton = ({ repoId }) => (
+  <Link route='repo/tree' params={{ repoId: repoId.split('/') }}>
+    <Button
+      style={{
+        backgroundColor: colors.social,
+        borderColor: colors.social,
+        marginBottom: 10
+      }}
+      primary
+      block
+    >
+      <NewerVersionIcon style={{ marginBottom: 4, marginRight: 4 }} /> Neuere Version
+    </Button>
+  </Link>
+)
+
+export default ({ asIcon, repoId, currentCommitId }) => (
+  <Subscription
+    subscription={repoSubscription}
+    variables={{ repoId }}>
+    {({ data = {}, loading = true }) => {
+      const lastestCommit = data.repoUpdate && data.repoUpdate.commits.nodes[0]
+
+      if (loading || lastestCommit.id === currentCommitId) {
+        return null
+      }
+
+      if (asIcon) {
+        return <WarningIcon repoId={repoId} />
+      }
+
+      return <WarningButton repoId={repoId} />
+    }}
+  </Subscription>
+)
