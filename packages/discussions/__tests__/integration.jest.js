952bf884370a72e74c860c9eac60af3fb4c58195
Patrick Recher <p@tte.io>
2019-03-27 11:39:20 +0100

    fix(tests): cleanup obsolete globals getter/setter

--- packages/discussions/__tests__/integration.jest.js
+++ packages/discussions/__tests__/integration.jest.js
@@ -1,33 +1,22 @@
 const Instance = require('@orbiting/backend-modules-test/instance')
-const {
-  setGlobals,
-  getGlobal
-} = require('@orbiting/backend-modules-test/globals')
 
 const testName = 'discussions'
 
 describe('discussions', () => {
-  beforeAll( async () => {
+  beforeAll(async () => {
     const instance = await Instance({
       serverName: 'republik',
       testName
     })
-    setGlobals(testName, { instance })
+    global.instance = instance
   }, 60000)
 
-  afterAll( async () => {
-    await getGlobal(testName, 'instance').closeAndCleanup()
+  afterAll(async () => {
+    global.instance.closeAndCleanup()
   }, 30000)
 
   test('test', async () => {
-    const instance = getGlobal(testName, 'instance')
-    /*
-    await new Promise( resolve => {
-      setTimeout( ()=> resolve(), 40000)
-    })
-    */
+    const { instance } = global
     expect(await instance.pgdb.public.users.count()).toEqual(0)
-
   }, 60000)
-
 })
