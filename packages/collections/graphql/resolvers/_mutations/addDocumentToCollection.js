95ee55643dcaa5c54604c1d1ac43310770dee3a8
Patrick Venetz <venetz@gmail.com>
2019-04-02 13:43:23 +0200

    fix(collections): Remove transactions in some resolvers
    Initialized tansactions were setup but never used.

--- packages/collections/graphql/resolvers/_mutations/addDocumentToCollection.js
+++ packages/collections/graphql/resolvers/_mutations/addDocumentToCollection.js
@@ -2,39 +2,31 @@ const { Roles } = require('@orbiting/backend-modules-auth')
 const Collection = require('../../../lib/Collection')
 
 module.exports = async (_, { documentId, collectionName, data }, context) => {
-  const { pgdb, user: me, t, loaders } = context
+  const { user: me, t, loaders } = context
   Roles.ensureUserHasRole(me, 'member')
 
-  const transaction = await pgdb.transactionBegin()
-  try {
-    const collection = await Collection.byNameForUser(collectionName, me.id, context)
-    if (!collection) {
-      throw new Error(t(`api/collections/collection/404`))
-    }
-
-    const repoId = Buffer.from(documentId, 'base64')
-      .toString('utf-8')
-      .split('/')
-      .slice(0, 2)
-      .join('/')
-    const doc = await loaders.Document.byRepoId.load(repoId)
-    if (!doc) {
-      throw new Error(t(`api/collections/document/404`))
-    }
-
-    const item = await Collection.upsertDocumentItem(
-      me.id,
-      collection.id,
-      repoId,
-      data,
-      context
-    )
-
-    await transaction.transactionCommit()
-
-    return item
-  } catch (e) {
-    await transaction.transactionRollback()
-    throw e
+  const collection = await Collection.byNameForUser(collectionName, me.id, context)
+  if (!collection) {
+    throw new Error(t(`api/collections/collection/404`))
   }
+
+  const repoId = Buffer.from(documentId, 'base64')
+    .toString('utf-8')
+    .split('/')
+    .slice(0, 2)
+    .join('/')
+  const doc = await loaders.Document.byRepoId.load(repoId)
+  if (!doc) {
+    throw new Error(t(`api/collections/document/404`))
+  }
+
+  const item = await Collection.upsertDocumentItem(
+    me.id,
+    collection.id,
+    repoId,
+    data,
+    context
+  )
+
+  return item
 }
