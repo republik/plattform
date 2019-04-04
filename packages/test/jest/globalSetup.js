816a72b98722d3372f9c3a49ed022a85aa403ef5
Patrick Recher <p@tte.io>
2019-04-04 11:43:13 +0200

    cleanup

--- packages/test/jest/globalSetup.js
+++
@@ -1,6 +0,0 @@
-const { cleanup } = require('../Sandbox/pool')
-
-module.exports = async () => {
-  console.log('globalTeardown')
-  return cleanup(global.originalEnv.REDIS_URL)
-}
--- packages/test/jest/globalTeardown.js
+++
@@ -1,9 +0,0 @@
-// https://jestjs.io/docs/en/configuration#globalsetup-string
-// Note: Any global variables that are defined through globalSetup can only be read in globalTeardown. You cannot retrieve globals defined here in your test suites.
-
-const { init } = require('../Sandbox/pool')
-
-module.exports = () => {
-  global.originalEnv = { ...process.env }
-  return init(global.originalEnv.REDIS_URL)
-}
