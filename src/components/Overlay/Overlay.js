1a3bb7ae96a04352ff7b4ae60492b978d602dda5
Thomas Preusse <tpreusse@users.noreply.github.com>
2019-04-17 18:14:03 +0200

    fix(Overlay): avoid scroll event when scroll blocking
    setting document.body.style.position = 'fixed' triggered a scroll event

--- src/components/Overlay/Overlay.js
+++ src/components/Overlay/Overlay.js
@@ -51,30 +51,18 @@ class Overlay extends PureComponent {
     })
 
     // The code below is used to block scrolling of the page behind the overlay.
-    // The trick is to add overflow:hidden and position:relative to the body
-    // element. The later scrolls the page to the top, to counter that we shift
-    // the whole page up by the appropriate offset and restore the scroll offset
-    // when the overlay is dismissed.
-    this.pageYOffset = window.pageYOffset
-    document.documentElement.style.top = `-${this.pageYOffset}px`
-    document.documentElement.style.position = 'relative'
+    // The trick is to add overflow:hidden and block touchmove events
     document.body.style.overflow = 'hidden'
-    document.body.style.position = 'fixed'
-    document.body.style.left = '0'
-    document.body.style.right = '0'
+    this.onTouchMove = e => e.preventDefault()
+    document.body.addEventListener('touchmove', this.onTouchMove, { passive: false })
   }
 
   componentWillUnmount () {
     clearTimeout(this.fadeInTimeout)
 
-    // Remove scroll block and scroll page back to its original Y-offset.
-    document.documentElement.style.top = ''
-    document.documentElement.style.position = ''
+    // Remove scroll block
     document.body.style.overflow = ''
-    document.body.style.position = ''
-    document.body.style.left = ''
-    document.body.style.right = ''
-    window.scrollTo(0, this.pageYOffset)
+    document.body.removeEventListener('touchmove', this.onTouchMove)
   }
 
   render () {
