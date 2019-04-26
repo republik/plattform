8b3d44b5282bfc0694336eb4f8fe516c1c1bb7c7
Patrick Recher <p@tte.io>
2019-04-26 14:24:44 +0200

    refactor(tests): run 7 tests in parallel

--- jest.config.js
+++ jest.config.js
@@ -58,7 +58,7 @@ module.exports = {
   // A path to a module which exports an async function that is triggered once after all test suites
   // globalTeardown: null,
 
-  maxConcurrency: 5,
+  maxConcurrency: 7,
 
   // A set of global variables that need to be available in all test environments
   // globals: {},
