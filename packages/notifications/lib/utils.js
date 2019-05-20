46734cfef4b55fda6be52c72e26eabb62023814d
Patrick Recher <p@tte.io>
2019-05-20 11:08:34 +0200

    fix(notifications): test mode for deleteSessionForDevices

--- packages/notifications/lib/utils.js
+++ packages/notifications/lib/utils.js
@@ -1,9 +1,12 @@
 const deleteSessionForDevices = async (tokens, pgdb) => {
+  console.log(`TEST-deleteSessionForDevices length: ${tokens.length}`, tokens)
+  /*
   return pgdb.query(`
     DELETE FROM sessions s WHERE s.id IN (
       SELECT d."sessionId" FROM devices d WHERE ARRAY[d.token] && :tokens
     )
   `, {tokens})
+  */
 }
 
 module.exports = {
