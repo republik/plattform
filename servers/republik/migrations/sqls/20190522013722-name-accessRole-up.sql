cd905a9c3f7f50657d53474874b52beb2bd01fb0
Patrick Recher <p@tte.io>
2019-05-22 19:50:49 +0200

    fix(republik): nameAccessRole PUBLIC for all editors, admins and supporters

--- servers/republik/migrations/sqls/20190522013722-name-accessRole-up.sql
+++ servers/republik/migrations/sqls/20190522013722-name-accessRole-up.sql
@@ -1,3 +1,7 @@
 ALTER TABLE users
-  ADD COLUMN "nameAccessRole" "accessRole" not null default 'MEMBER'
+  ADD COLUMN "nameAccessRole" "accessRole" NOT NULL DEFAULT 'MEMBER' CHECK ("nameAccessRole" IN ('MEMBER', 'PUBLIC'))
 ;
+
+UPDATE users
+SET "nameAccessRole" = 'PUBLIC'
+WHERE roles @> '[ "editor" ]' OR roles @> '[ "admin" ]' OR roles @> '[ "supporter" ]';
