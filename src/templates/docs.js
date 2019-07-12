d1d5d6fa3c55cab65293ba165815cbfa101b1713
Thomas Preusse <tpreusse@users.noreply.github.com>
2019-07-12 14:05:09 +0200

    docs: reduce source code overload
    was mostly an issue with charts data and templates

--- src/templates/docs.js
+++ src/templates/docs.js
@@ -3,14 +3,5 @@ import { renderMdast } from 'mdast-react-render'
 import { parse } from '@orbiting/remark-preset'
 
 export const Markdown = ({children, schema}) => {
-  return (
-    <div>
-      {renderMdast(parse(children), schema)}
-      <pre style={{backgroundColor: '#fff', padding: 20, margin: '20px -20px -20px -20px', overflow: 'auto'}}>
-        <code style={{fontFamily: '"Roboto Mono", monospace'}}>
-          {children.trim()}
-        </code>
-      </pre>
-    </div>
-  )
+  return renderMdast(parse(children), schema)
 }
