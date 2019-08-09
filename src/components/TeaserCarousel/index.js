2d9a84eade89f7ef235c260f874d14d87240caf1
Luc <luc.guillemot@gmail.com>
2019-08-09 13:25:03 +0200

    fix: export ArticleCount component

--- src/components/TeaserCarousel/index.js
+++ src/components/TeaserCarousel/index.js
@@ -7,3 +7,4 @@ export { default as TeaserFrontCarouselHeadline } from "./Headline";
 export { default as TeaserFrontCarouselLead } from "./Lead";
 export { default as TeaserFrontCarouselTag } from "./Tag";
 export { default as TeaserFrontCarouselFormat } from "./Format";
+export { default as TeaserFrontCarouselArticleCount } from "./ArticleCount";
