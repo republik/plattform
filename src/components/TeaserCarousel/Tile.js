3cfeecbe4b5f8cc20fe2006349180f67bbccbd2d
Luc <luc.guillemot@gmail.com>
2019-08-13 10:00:49 +0200

    fix(Carousel/Tile): make Tile its own component

---
+++ src/components/TeaserCarousel/Tile.js
@@ -0,0 +1,125 @@
+import { css } from 'glamor'
+import React from 'react'
+import { TeaserFrontCarouselArticleCount } from '.'
+import colors from '../../theme/colors'
+import { FigureImage } from '../Figure'
+import LazyLoad from '../LazyLoad'
+import { mUp } from '../TeaserFront/mediaQueries'
+import Text from '../TeaserFront/Text'
+import { sansSerifRegular18 } from '../Typography/styles'
+
+const IMAGE_SIZE = {
+  small: { maxWidth: 160, maxHeight: 120 },
+  medium: { maxWidth: 160, maxHeight: 120 },
+  large: { maxWidth: 160, maxHeight: 120 }
+}
+
+const styles = {
+  tile: css({
+    margin: '0 7px 0 0',
+    textAlign: 'center',
+    width: '33%',
+    minWidth: 300,
+    maxWidth: 450,
+    display: 'flex',
+    justifyContent: 'center',
+    ...sansSerifRegular18,
+    ':last-of-type': { margin: 0 },
+    [mUp]: {
+      minWidth: 248
+    }
+  }),
+
+  container: css({
+    width: '100%' // IE11 needs this
+  }),
+
+  imageContainer: css({
+    margin: '0 auto 14px auto',
+    maxWidth: IMAGE_SIZE.large.maxWidth,
+    maxHeight: IMAGE_SIZE.large.maxHeight
+  }),
+
+  imageContainerBigger: css({
+    margin: '0 auto'
+  }),
+
+  image: css({
+    height: '100%',
+    maxWidth: '100%',
+    maxHeight: IMAGE_SIZE.large.maxHeight,
+    objectFit: 'scale-down'
+  }),
+
+  imageBigger: css({
+    maxWidth: '100%',
+    objectFit: 'scale-down'
+  })
+}
+
+const TeaserFrontCarouselTile = ({
+  color = '#000',
+  bgColor = 'unset',
+  noOutline = false,
+  count,
+  bigger,
+  image,
+  alt,
+  onClick,
+  aboveTheFold,
+  children
+}) => {
+  const imageProps =
+    image && FigureImage.utils.getResizedSrcs(image, IMAGE_SIZE.small, false)
+  let tileStyle = css(styles.tile, {
+    border: noOutline ? 'none' : `1px solid ${colors.outline}`,
+    color,
+    backgroundColor: bgColor,
+    cursor: onClick ? 'pointer' : 'default',
+    padding: bigger ? '0 0 40px 0' : '30px 15px',
+    alignItems: bigger ? 'flex-start' : 'center'
+  })
+
+  let containerStyle = css(styles.container, {
+    margin: bigger ? '0' : '0 auto'
+  })
+  let imageContainerStyles = bigger
+    ? styles.imageContainerBigger
+    : styles.imageContainer
+  let imageStyles = bigger ? styles.imageBigger : styles.image
+
+  return (
+    <div {...tileStyle} onClick={onClick} className="tile">
+      <div {...containerStyle}>
+        {/* Image */}
+        {imageProps && (
+          <figure {...imageContainerStyles}>
+            <LazyLoad visible={aboveTheFold}>
+              <img
+                src={imageProps.src}
+                srcSet={imageProps.srcSet}
+                alt={alt}
+                {...imageStyles}
+              />
+            </LazyLoad>
+          </figure>
+        )}
+
+        {/* Body */}
+        <div>
+          <Text color={color} margin={'0 auto'}>
+            {children}
+            {count && (
+              <TeaserFrontCarouselArticleCount
+                count={count}
+                bgColor={color}
+                color={bgColor}
+              />
+            )}
+          </Text>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default TeaserFrontCarouselTile
--- src/components/TeaserCarousel/index.js
+++ src/components/TeaserCarousel/index.js
@@ -1,12 +1,9 @@
 import * as _TeaserFrontCarouselHeadline from './Headline'
 
-export {
-  default as TeaserFrontCarouselTile,
-  TeaserFrontCarousel
-} from './Carousel'
-
 export const TeaserFrontCarouselHeadline = { ..._TeaserFrontCarouselHeadline }
 
+export { default as TeaserFrontCarousel } from './Carousel'
+export { default as TeaserFrontCarouselTile } from './Tile'
 export { default as TeaserFrontCarouselRow } from './Row'
 export { default as TeaserFrontCarouselLead } from './Lead'
 export { default as TeaserFrontCarouselTag } from './Tag'
