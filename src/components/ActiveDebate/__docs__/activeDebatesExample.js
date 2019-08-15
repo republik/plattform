e2717ca0d0164f31fab6800d744bf0f83a511978
Luc <luc.guillemot@gmail.com>
2019-08-15 16:31:31 +0200

    docs(ActiveDebates): add example without highlight

--- src/components/ActiveDebate/__docs__/activeDebatesExample.js
+++ src/components/ActiveDebate/__docs__/activeDebatesExample.js
@@ -318,3 +318,209 @@ export const activeDebates = {
     }
   ]
 }
+export const activeDebatesWithoutHighlight = {
+  hasHighlight: false,
+  discussions: [
+    {
+      id: 'e9834874-8dd6-4cdc-927d-1bd4fc91c331',
+      title: 'Stell dir vor, die UBS wird klimaneutral',
+      path: '/2019/07/12/stell-dir-vor-die-ubs-wird-klimaneutral',
+      closed: false,
+      document: {
+        id:
+          'cmVwdWJsaWsvYXJ0aWNsZS1rYW5uLWRpZS11YnMtZGFzLWtsaW1hLXJldHRlbi8zNzQyYzNiYWU4YmZlOTk5NDNiOTg2M2Q3MzI3OGFkMGFhNTU1NmEyL3Yz',
+        meta: {
+          title: 'Stell dir vor, die UBS wird klimaneutral',
+          path: '/2019/08/05/stell-dir-vor-die-ubs-wird-klimaneutral',
+          template: 'article',
+          ownDiscussion: {
+            id: 'e9834874-8dd6-4cdc-927d-1bd4fc91c331',
+            closed: false,
+            __typename: 'Discussion'
+          },
+          __typename: 'Meta'
+        },
+        __typename: 'Document'
+      },
+      __typename: 'Discussion',
+      comments: {
+        totalCount: 20,
+        nodes: [
+          {
+            id: 'b049b9bf-3870-4602-9e7a-1f477a40012a',
+            parentIds: [],
+            createdAt: '2000-01-01',
+            updatedAt: new Date().toISOString(),
+            preview: {
+              string:
+                'Einmal mehr geht es bei dieser Debatte um eine Huhn Ei Debatte. Ist es Aufgabe der Bank ihren Beitrag zu leisten oder in einer direkten Demokratie wie der Schweiz an den Stimmbürgern, die entsprechenden Rahmenbedingunge zu schaffen. Die',
+              more: true,
+              __typename: 'Preview'
+            },
+            displayAuthor: {
+              id: 'b',
+              name: 'U. A. L.',
+              slug: 'eab892d9-c885-4df2-9e09-5e44238dd5bc',
+              credential: null,
+              profilePicture: null,
+              __typename: 'DisplayUser'
+            },
+            __typename: 'Comment'
+          }
+        ]
+      }
+    },
+    {
+      id: '3c22807a-c1a2-46a6-ac56-93eb09a9dc29',
+      title: 'Wenn Big Tech in Bern nach Regulierung ruft',
+      path: '/2019/08/06/wenn-big-tech-in-bern-nach-regulierung-ruft',
+      closed: false,
+      document: {
+        id:
+          'cmVwdWJsaWsvYXJ0aWNsZS13ZW5uLWJpZy10ZWNoLWluLWJlcm4tbmFjaC1yZWd1bGllcnVuZy1zY2hyZWl0L2NmYmU1MzhjYjlhNzNhZWJlZDE4ODgwNmJkNjkxZjU1MjdmMmJiOWIvdjI=',
+        meta: {
+          title: 'Wenn Big Tech in Bern nach Regulierung ruft',
+          path: '/2019/08/06/wenn-big-tech-in-bern-nach-regulierung-ruft',
+          template: 'article',
+          ownDiscussion: {
+            id: '3c22807a-c1a2-46a6-ac56-93eb09a9dc29',
+            closed: false,
+            __typename: 'Discussion'
+          },
+          __typename: 'Meta'
+        },
+        __typename: 'Document'
+      },
+      __typename: 'Discussion',
+      comments: {
+        totalCount: 46,
+        nodes: [
+          {
+            id: '6f540ae7-7b38-486b-b397-d332042f9abb',
+            parentIds: [],
+            createdAt: '2000-01-01',
+            updatedAt: new Date().toISOString(),
+            preview: {
+              string:
+                'Betreffend: Überforderung: Einige geladene Partei­vertreter, die ebenfalls am Treffen teilnahmen, wollen bemerkt haben, dass die Lobbyisten den Bundes­behörden fachlich überlegen waren. Auch würden nicht alle das von Facebook verwendete',
+              more: true,
+              __typename: 'Preview'
+            },
+            displayAuthor: {
+              id: 'c',
+              name: 'M. B.',
+              slug: null,
+              credential: null,
+              profilePicture: null,
+              __typename: 'DisplayUser'
+            },
+            __typename: 'Comment'
+          }
+        ],
+        __typename: 'CommentConnection'
+      }
+    },
+    {
+      id: '6247e48c-5a69-458f-b223-adcd580bbc1e',
+      title: 'Brandgefährlicher Akku',
+      path: '/2019/08/07/brandgefaehrlicher-akku',
+      closed: false,
+      document: {
+        id:
+          'cmVwdWJsaWsvYXJ0aWNsZS1icmFuZGdlZmFlaHJsaWNoZS1zcGllbHpldWdlL2UyM2Y4ZmUxNjg0YTkwNTIyZTZkZjcyMTE0NGEyZTVjMzhmZDhhMGUvdjI=',
+        meta: {
+          title: 'Brandgefährlicher Akku',
+          path: '/2019/08/07/brandgefaehrlicher-akku',
+          template: 'article',
+          ownDiscussion: {
+            id: '6247e48c-5a69-458f-b223-adcd580bbc1e',
+            closed: false,
+            __typename: 'Discussion'
+          },
+          __typename: 'Meta'
+        },
+        __typename: 'Document'
+      },
+      __typename: 'Discussion',
+      comments: {
+        totalCount: 19,
+        nodes: [
+          {
+            id: '1a1f7c72-56ab-492b-83d4-d8d8a726f0cf',
+            parentIds: [],
+            createdAt: '2000-01-01',
+            updatedAt: new Date().toISOString(),
+            preview: {
+              string:
+                'Ohnein. Jetzt springt auch die "Republik" auf den Zug auf und suggeriert, dass Elektromobilität schlecht sei wegen angeblicher Brandgefahr. Damit nun auch Republikleser mit gutem Gewissen weiterdieseln können... Elektroautos brennen pro',
+              more: true,
+              __typename: 'Preview'
+            },
+            displayAuthor: {
+              id:
+                'f815cc7cd2b9a3aed4190d6be5753a835822784b9bd202d5c54b681efd2036fe',
+              name: 'Simon Schlauri',
+              slug: 'simonschlauri',
+              credential: null,
+              profilePicture:
+                'https://cdn.republik.space/s3/republik-assets/portraits/e7b5fec7809718a9f7b8c038c2747fd6.jpeg.webp?size=900x854&resize=384x384&bw=true',
+              __typename: 'DisplayUser'
+            },
+            __typename: 'Comment'
+          }
+        ],
+        __typename: 'CommentConnection'
+      }
+    },
+    {
+      id: '01b099e4-46d0-4cbd-a55f-1949c7ad7a3b',
+      title: 'Was tun gegen steigende Mieten?',
+      path: '/2019/08/07/was-tun-gegen-steigende-mieten',
+      closed: false,
+      document: {
+        id:
+          'cmVwdWJsaWsvYXJ0aWNsZS1taWV0ZXItcmF1c3d1ZXJmZS1pbi1iYXNlbC9lY2Q4OTU3YzIwNDEzOWFkNDllZWU5NWFiZmRkNTVmODlmOTNmYTlmL3Y0',
+        meta: {
+          title: 'Was tun gegen steigende Mieten?',
+          path: '/2019/08/07/was-tun-gegen-steigende-mieten',
+          template: 'article',
+          ownDiscussion: {
+            id: '01b099e4-46d0-4cbd-a55f-1949c7ad7a3b',
+            closed: false,
+            __typename: 'Discussion'
+          },
+          __typename: 'Meta'
+        },
+        __typename: 'Document'
+      },
+      __typename: 'Discussion',
+      comments: {
+        totalCount: 17,
+        nodes: [
+          {
+            id: '2e40ce5a-2075-461f-9ca8-95a77d2e948a',
+            parentIds: [],
+            createdAt: '2000-01-01',
+            updatedAt: new Date().toISOString(),
+            preview: {
+              string:
+                'Immer wieder wichtiges Thema, auch im Arc lémanique; die besondere Situation in Basel wurde von der WOZ schon vor Monaten beleuchtet. Was mir an der Analyse fehlt, ist nicht spezifisch baslerisch: das schweizerische Pensionssystem, seit',
+              more: true,
+              __typename: 'Preview'
+            },
+            displayAuthor: {
+              id: 'd',
+              name: 'R. L.',
+              slug: null,
+              credential: null,
+              profilePicture: null,
+              __typename: 'DisplayUser'
+            },
+            __typename: 'Comment'
+          }
+        ],
+        __typename: 'CommentConnection'
+      }
+    }
+  ]
+}
