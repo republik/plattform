7f777b574cc0c862219792bf952052f4b9da0873
Luc <luc.guillemot@gmail.com>
2019-08-15 13:39:47 +0200

    feat(DebateHeader): is its own module and an <a>

---
+++ src/components/ActiveDebate/DebateHeader.js
@@ -0,0 +1,39 @@
+import { css } from 'glamor'
+import PropTypes from 'prop-types'
+import React from 'react'
+import { IconLink } from '../Discussion/Internal/Comment'
+import { sansSerifRegular16 } from '../Typography/styles'
+import colors from '../../theme/colors'
+
+const styles = {
+  header: css({
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    margin: '7px 0 0 0',
+    textDecoration: 'none',
+    cursor: 'pointer',
+    color: colors.text
+  }),
+  title: css({
+    ...sansSerifRegular16,
+    marginRight: 10
+  })
+}
+
+const DebateHeader = ({ documentTitle, commentCount, href }) => {
+  return (
+    <a href={href} {...styles.header}>
+      {documentTitle && <div {...styles.title}>{documentTitle}</div>}
+      <IconLink discussionCommentsCount={commentCount} small />
+    </a>
+  )
+}
+
+export default DebateHeader
+
+DebateHeader.propTypes = {
+  documentTitle: PropTypes.string,
+  commentCount: PropTypes.number,
+  href: PropTypes.string
+}
