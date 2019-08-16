056767fd1d76bce2bb4053da2df59c061e2f61b1
Luc <luc.guillemot@gmail.com>
2019-08-16 17:13:05 +0200

    chore(Teaser): clean up

--- src/components/TeaserActiveDebates/index.js
+++ src/components/TeaserActiveDebates/index.js
@@ -1,4 +1,4 @@
-export { default as ActiveDebates } from './ActiveDebates'
+export { default as TeaserActiveDebates } from './ActiveDebates'
 export { default as ActiveDebateHeader } from './DebateHeader'
 export { default as ActiveDebateTeaser } from './DebateTeaser'
 export { default as ActiveDebateComment } from './DebateComment'
--- src/components/TeaserSharedComponent/docs.md
+++ src/components/TeaserSharedComponent/docs.md
@@ -5,7 +5,7 @@ A section title. If `href` is defined, an icon with a chevron right will be disp
 Supported props:
 - `onClick` (function): function triggered on click.
 - `href` (string): link.
-- `children`: children component (section title)
+- `children`: children component (section title).
 
 ```react|span-3
 <TeaserSectionTitle
