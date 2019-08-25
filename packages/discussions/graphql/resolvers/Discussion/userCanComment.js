766250ad241ef12d948f663d6940dbae705d0a75
Patrick Venetz <venetz@gmail.com>
2019-08-25 21:21:13 +0200

    fix(discussions): Check for existing user object

--- packages/discussions/graphql/resolvers/Discussion/userCanComment.js
+++ packages/discussions/graphql/resolvers/Discussion/userCanComment.js
@@ -5,7 +5,7 @@ module.exports = async ({ minInterval, id }, _, context) => {
   const { user, pgdb } = context
 
   const isMember = Roles.userIsInRoles(user, ['member'])
-  const hasActiveMembership = await hasUserActiveMembership(user, pgdb)
+  const hasActiveMembership = !!user && await hasUserActiveMembership(user, pgdb)
 
   return isMember && hasActiveMembership
 }
