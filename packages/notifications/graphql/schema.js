d2ae21be87e50b01e28e7e7eedb02679a3e8b5ee
Patrick Recher <p@tte.io>
2019-08-27 14:39:51 +0200

    fix(notifications): cleanup

--- packages/notifications/graphql/schema.js
+++ packages/notifications/graphql/schema.js
@@ -5,6 +5,8 @@ schema {
 
 type mutations {
   upsertDevice(token: ID!, information: DeviceInformationInput!): Device!
+
+  # deprecated: not used in app anymore, evict if no API calls tracked anymore
   rollDeviceToken(oldToken: String!, newToken: String!): Device!
 
   # users can remove their devices
