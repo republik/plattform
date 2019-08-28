b0515e4307f13dc146a4d5c87825af877602fe6a
Patrick Venetz <venetz@gmail.com>
2019-08-28 11:45:45 +0200

    refactor(cards): Revised script to import candidate data from provider

--- packages/cards/script/import-smartvote-data.js
+++ packages/cards/script/import-smartvote-data.js
@@ -15,9 +15,42 @@ const parse = dsvFormat(';').parse
 
 const probableTrue = (probability) => Math.random() < probability
 
+const maybeString = (value) => {
+  if (!value) {
+    return null
+  }
+
+  const string = String(value).trim()
+
+  return string
+}
+
 const maybeNumber = (value) => {
-  const number = Number(value.replace(/,/g, '.'))
+  if (!value) {
+    return null
+  }
+
+  const number = Number(String(value).replace(/,/g, '.'))
+
+  if (number === 0) {
+    return 0
+  }
+
   return number || (value || null)
+}
+
+const maybeSocialHandle = (url) => {
+  if (!url) {
+    return null
+  }
+
+  const match = String(url).match(/(?:facebook|twitter)\.com\/([^/]+)/)
+
+  if (!match) {
+    return null
+  }
+
+  return match[1]
 }
 
 const sanitizeMetaDistrict = (value) => {
@@ -28,33 +61,32 @@ const sanitizeMetaDistrict = (value) => {
   return value
 }
 
-const extractSocialHandle = (url) => {
-  const match = url.match(/(?:facebook|twitter)\.com\/([^/]+)/)
-
-  if (match) {
-    return match[1]
-  }
-
-  return false
-}
-
 const assignElection = (payload) => {
-  if (payload.meta.electionId === '223') {
+  if (payload._import.electionId === '222') {
     return {
-      councilOfStates: Object.assign({}, payload.councilOfStates, {
+      nationalCouncil: Object.assign({}, payload.nationalCouncil, {
+        listName: payload._import.listName,
+        listNumbers: payload._import.listNumbers,
+        listPlaces: payload._import.listPlaces,
         candidacy: true,
-        incumbent: payload.meta.incumbent,
-        elected: payload.meta.elected
+        incumbent: payload._import.incumbent,
+        elected: payload._import.elected
+      }),
+      _import: Object.assign({}, payload._import, {
+        credentialDescription: 'Nationalratskandidatur 2019'
       })
     }
   }
 
-  if (payload.meta.electionId === '222') {
+  if (payload._import.electionId === '223') {
     return {
-      nationalCouncil: Object.assign({}, payload.nationalCouncil, {
+      councilOfStates: Object.assign({}, payload.councilOfStates, {
         candidacy: true,
-        incumbent: payload.meta.incumbent,
-        elected: payload.meta.elected
+        incumbent: payload._import.incumbent,
+        elected: payload._import.elected
+      }),
+      _import: Object.assign({}, payload._import, {
+        credentialDescription: 'Ständeratskandidatur 2019'
       })
     }
   }
@@ -65,34 +97,34 @@ const assignElection = (payload) => {
 const toPayload = (row) => {
   const payload = {
     yearOfBirth: maybeNumber(row.year_of_birth),
-    occupation: row.occupation,
-    listName: row.list || null,
-    listNumbers: [row.candidate_no_1, row.candidate_no_2].filter(Boolean) || null,
-    listPlaces: [row.list_place_1, row.list_place_2].filter(Boolean) || null,
-    party: row.party_short || null,
+    occupation: maybeString(row.occupation),
+    party: maybeString(row.party_short),
     smartvoteCleavage:
       row.cleavage_1
         ? [1, 2, 3, 4, 5, 6, 7, 8].map((index) => maybeNumber(row[`cleavage_${index}`]))
         : null,
     vestedInterestsSmartvote:
       [1, 2, 3, 4, 5, 6, 7, 8].map((index) => {
-        if (!row[`interest_name_${index}`]) {
+        if (!maybeString(row[`interest_name_${index}`])) {
           return
         }
 
         return {
-          name: row[`interest_name_${index}`] || null,
-          position: row[`interest_position_${index}`] || null,
-          entity: row[`interest_legal_form_${index}`] || null
+          name: maybeString(row[`interest_name_${index}`]),
+          position: maybeString(row[`interest_position_${index}`]),
+          entity: maybeString(row[`interest_legal_form_${index}`])
         }
       }).filter(Boolean) || null,
     vestedInterestsLobbywatch:
       probableTrue(0.2)
         ? sampleDataLobbywatch
         : null,
-    campaignBudget: row.campaign_budget || null,
-    campaignBudgetComment: row.cb_comment || null,
+    campaignBudget: maybeNumber(row.campaign_budget),
+    campaignBudgetComment: maybeString(row.cb_comment),
     nationalCouncil: {
+      listName: null,
+      listNumbers: [],
+      listPlaces: [],
       candidacy: false,
       incumbent: false,
       elected: false,
@@ -105,18 +137,29 @@ const toPayload = (row) => {
       electionPlausability: 0 // @TODO: Evaluate Wahlwahrschenlichkeit
     },
     meta: {
-      userId: row.ID_user || null,
-      candidateId: row.ID_Candidate || null,
-      firstName: row.firstname || null,
-      lastName: row.lastname || null,
-      email: row['e-mail_public'] || null,
-      facebookId: extractSocialHandle(row['LINK_facebook']) || null,
-      twitterHandle: extractSocialHandle(row['LINK_twitter']) || null,
-      publicUrl: row['LINK_personal_website'] || null,
+      userId: maybeString(row.ID_user),
+      email: maybeString(row['e-mail_public'])
+    },
+    _import: {
+      candidateId: maybeString(row.ID_Candidate),
       district: sanitizeMetaDistrict(row.district) || null,
-      electionId: row.ID_election || null,
+      firstName: maybeString(row.firstname),
+      lastName: maybeString(row.lastname),
+      facebookId: maybeSocialHandle(row['LINK_facebook']),
+      twitterHandle: maybeSocialHandle(row['LINK_twitter']),
+      publicUrl: maybeString(row['LINK_personal_website']),
+      electionId: maybeString(row.ID_election),
+      listName: maybeString(row.list),
+      listNumbers: [maybeString(row.candidate_no_1), maybeString(row.candidate_no_2)].filter(Boolean) || null,
+      listPlaces: [maybeNumber(row.list_place_1), maybeNumber(row.list_place_2)].filter(Boolean) || null,
       incumbent: row.incumbent !== '0',
-      elected: row.elected !== '0'
+      elected: row.elected !== '0',
+      credentialDescription: null,
+      suggestedUsernames: [
+        slug(`${row.firstname.slice(0, 1)}${row.lastname.replace(/[^\w]/g, '')}`),
+        slug(`${row.firstname.slice(0, 2)}${row.lastname.replace(/[^\w]/g, '')}`),
+        slug(`${row.firstname.slice(0, 1)}${row.lastname}${row.year_of_birth.slice(2, 4)}`)
+      ]
     }
   }
 
@@ -133,7 +176,7 @@ PgDb.connect().then(async pgdb => {
   /**
    * Upsert CardGroups
    */
-  const districts = new Set(payloads.map(p => p.meta.district))
+  const districts = new Set(payloads.map(p => p._import.district))
   const groups = await pgdb.public.cardGroups.findAll()
 
   await Promise.each(districts, async district => {
@@ -156,56 +199,76 @@ PgDb.connect().then(async pgdb => {
    */
   const cards = await pgdb.public.cards.findAll()
 
-  const userEmails = payloads.map(payload => payload.meta.email).filter(Boolean)
-  const tempEmails = payloads.map(payload => `card-wahl2019-${payload.meta.identifier}@republik.ch`)
-  const users = await pgdb.public.users.find({
-    email: userEmails.concat(tempEmails)
-  })
+  const emails =
+    payloads
+      .map(payload => payload._import.email)
+      .filter(Boolean)
+      .concat(payloads.map(payload => `wahl2019-${payload.meta.userId}@republik.ch`))
+  const users =
+    emails.length > 0
+      ? await pgdb.public.users.find({ email: emails })
+      : []
+
+  const credentials =
+    cards.length > 0
+      ? await pgdb.public.credentials.find({ userId: cards.map(card => card.userId) })
+      : []
+
+  const usernamesTaken = (await pgdb.public.users.find({
+    username: payloads.map(payload => payload._import.suggestedUsernames).flat()
+  })).map(user => user.username)
 
   const now = new Date()
 
   await Promise.each(payloads, async payload => {
     const identifier = payload.meta.userId
-    const district = payload.meta.district
+    const district = payload._import.district
 
-    const name = [payload.meta.firstName, payload.meta.lastName].join(' ')
+    const name = [payload._import.firstName, payload._import.lastName].join(' ')
 
     console.log(`Card "${name}" (ID: "${identifier}")`)
 
-    const card = cards.find(card => card.payload.meta.userId === identifier)
+    const card = cards.find(card => card.payload.meta.userId === identifier) || {}
 
-    if (!card) {
+    if (!card.payload) {
       console.log('  is missing, creating...')
       const group = groups.find(({ name }) => district === name)
 
-      const tempEmail = `card-wahl2019-${identifier}@republik.ch`
+      const tempEmail = `wahl2019-${identifier}@republik.ch`
 
       const user =
-        users.find(({ email }) => email === payload.meta.email || email === tempEmail) ||
+        users.find(({ email }) => {
+          return (payload._import.email && email.toLowerCase() === payload._import.email.toLowerCase()) ||
+          email.toLowerCase() === tempEmail.toLowerCase()
+        }) ||
         await pgdb.public.users.insertAndGet({
           email: tempEmail,
-          firstName: payload.meta.firstName,
-          lastName: payload.meta.lastName,
-          facebookId: payload.meta.facebookId,
-          twitterHandle: payload.meta.twitterHandle,
-          publicUrl: payload.meta.publicUrl,
-          username: `card-wahl2019-${identifier}`,
+          firstName: payload._import.firstName,
+          lastName: payload._import.lastName,
+          facebookId: payload._import.facebookId,
+          twitterHandle: payload._import.twitterHandle,
+          publicUrl: payload._import.publicUrl,
+          username: payload._import.suggestedUsernames.find(suggestedUsername => !usernamesTaken.includes(suggestedUsername)),
           verified: false,
           hasPublicProfile: true,
           isListed: true
         })
 
+      usernamesTaken.push(user.username)
+
+      const { _import, ...insertablePayload } = payload
+
       const insertedCard = await pgdb.public.cards.insertAndGet({
-        payload,
+        payload: insertablePayload,
         cardGroupId: group.id,
         userId: user.id
       })
 
+      Object.assign(card, insertedCard)
       cards.push(insertedCard)
     } else {
-      const updatedPayload = Object.assign({}, card.payload, assignElection(payload))
-
       console.log('  exists')
+      const { _import, ...updatedPayload } = Object.assign({}, card.payload, assignElection(payload))
 
       try {
         assert.deepStrictEqual(updatedPayload, card.payload)
@@ -219,7 +282,26 @@ PgDb.connect().then(async pgdb => {
             updatedAt: now
           }
         )
+
+        Object.assign(card, { payload: updatedPayload })
       }
+    }
+
+    /**
+     * Roles (credentials)
+     */
+    const existingCredential = credentials.find(credential => {
+      return credential.userId === card.userId &&
+      credential.description === payload._import.credentialDescription
+    })
+
+    if (!existingCredential) {
+      await pgdb.public.credentials.insertAndGet({
+        userId: card.userId,
+        description: payload._import.credentialDescription,
+        isListed: !credentials.find(credential => credential.userId === card.userId),
+        verified: true
+      })
     }
   })
 
