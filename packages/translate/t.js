b49ef129ef6cdac7c34f134ff665b97e94e759d4
Patrick Recher <p@tte.io>
2019-09-02 18:28:33 +0200

    feat(translations): merge all translations into one file

--- packages/translate/t.js
+++ packages/translate/t.js
@@ -1,3 +1,3 @@
-const { getFormatter } = require('@orbiting/backend-modules-translate')
+const getFormatter = require('./getFormatter')
 const MESSAGES = require('./translations.json').data
 module.exports = getFormatter(MESSAGES)
--- servers/republik/lib/t.js
+++
@@ -1,8 +0,0 @@
-const _ = {
-  uniqBy: require('lodash/uniqBy')
-}
-const { getFormatter } = require('@orbiting/backend-modules-translate')
-const MESSAGES_CF = require('../modules/crowdfundings/lib/translations.json')
-const MESSAGES = require('./translations.json').data
-  .concat(MESSAGES_CF.data)
-module.exports = getFormatter(_.uniqBy(MESSAGES, 'key'))
