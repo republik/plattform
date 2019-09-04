5407575582404dd3b3a1d833696525f0e842c2e0
Patrick Venetz <venetz@gmail.com>
2019-09-04 11:51:05 +0200

    feat(cards): Shuffle cards with User.id as seed when logged in

--- packages/cards/graphql/resolvers/_queries/cards.js
+++ packages/cards/graphql/resolvers/_queries/cards.js
@@ -1,12 +1,16 @@
+const shuffleSeed = require('shuffle-seed')
+
 const { paginate } = require('@orbiting/backend-modules-utils')
 
 const defaults = {
   first: 10
 }
 
-module.exports = async (_, args, { pgdb }) => {
+module.exports = async (_, args, { pgdb, user }) => {
+  const cards = await pgdb.public.cards.findAll()
+
   return paginate(
     Object.assign({}, defaults, args),
-    await pgdb.public.cards.findAll()
+    shuffleSeed.shuffle(cards, (user && user.id) || 'Republik Rendezvous')
   )
 }
--- packages/cards/loaders/Card.js
+++ packages/cards/loaders/Card.js
@@ -1,3 +1,5 @@
+const shuffleSeed = require('shuffle-seed')
+
 const createDataLoader = require('@orbiting/backend-modules-dataloader')
 
 module.exports = (context) => ({
@@ -5,12 +7,18 @@ module.exports = (context) => ({
     ids => context.pgdb.public.cards.find({ id: ids })
   ),
   byUserId: createDataLoader(
-    userIds => context.pgdb.public.cards.find({ userId: userIds }),
+    async userIds => {
+      const cards = await context.pgdb.public.cards.find({ userId: userIds })
+      return shuffleSeed.shuffle(cards, (context.user && context.user.id) || 'Republik Rendezvous')
+    },
     null,
     (key, rows) => rows.filter(row => row.userId === key)
   ),
   byCardGroupId: createDataLoader(
-    cardGroupIds => context.pgdb.public.cards.find({ cardGroupId: cardGroupIds }),
+    async cardGroupIds => {
+      const cards = await context.pgdb.public.cards.find({ cardGroupId: cardGroupIds })
+      return shuffleSeed.shuffle(cards, (context.user && context.user.id) || 'Republik Rendezvous')
+    },
     null,
     (key, rows) => rows.filter(row => row.cardGroupId === key)
   )
