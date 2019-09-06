c6426c9bdebfbd43b3e80d326465ea88502ca691
Patrick Recher <p@tte.io>
2019-09-06 16:58:24 +0200

    fix(publikator): return already resolved meta fields

--- servers/publikator/graphql/resolvers/Meta.js
+++ servers/publikator/graphql/resolvers/Meta.js
@@ -8,12 +8,16 @@ const repo = require('./Repo')
 const commit = require('./Commit')
 
 const resolveRepoId = field => async (meta, args, context) => {
+  if (typeof meta[field] === 'object') {
+    return meta[field]
+  }
+
   const repoId = resolve.getRepoId(meta[field])
   if (!repoId) {
     return null
   }
 
-  const latestCommit = await repo.latestCommit({id: repoId}, null, context)
+  const latestCommit = await repo.latestCommit({ id: repoId }, null, context)
   const doc = await commit.document(latestCommit, {}, context)
 
   return doc || null
