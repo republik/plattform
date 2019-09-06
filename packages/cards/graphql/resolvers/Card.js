90b639b455395c5531574a1cdc0ee8eb864c3aa7
Patrick Venetz <venetz@gmail.com>
2019-09-06 13:57:59 +0200

    fix(cards): Remove unnecessary paginaton in Card.group resolver

--- packages/cards/graphql/resolvers/Card.js
+++ packages/cards/graphql/resolvers/Card.js
@@ -1,19 +1,10 @@
-const { paginate } = require('@orbiting/backend-modules-utils')
-
-const defaults = {
-  first: 10
-}
-
 module.exports = {
   async user (card, args, { loaders }) {
     return loaders.User.byId.load(card.userId)
   },
 
   async group (card, args, { loaders }) {
-    return paginate(
-      Object.assign({}, defaults, args),
-      await loaders.CardGroup.byId.load(card.cardGroupId)
-    )
+    return loaders.CardGroup.byId.load(card.cardGroupId)
   },
 
   payload (card) {
