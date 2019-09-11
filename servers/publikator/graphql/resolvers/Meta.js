e54c668f59c2076e3a64a1dcaf808ef6c654f4cd
Patrick Recher <p@tte.io>
2019-09-11 16:09:58 +0200

    fix(publikator): resolve meta.series of unpublished episodes

--- servers/publikator/graphql/resolvers/Meta.js
+++ servers/publikator/graphql/resolvers/Meta.js
@@ -21,10 +21,17 @@ const resolveRepoId = field => async (meta, args, context) => {
   const latestCommit = await repo.latestCommit({ id: repoId }, null, context)
   const doc = await commit.document(latestCommit, {}, context)
 
+  // for series we don't want to return the document
+  // but meta.series of the master
+  if (field === 'series' && doc) {
+    return doc.content.meta.series
+  }
+
   return doc || null
 }
 
 module.exports = {
   format: resolveRepoId('format'),
-  dossier: resolveRepoId('dossier')
+  dossier: resolveRepoId('dossier'),
+  series: resolveRepoId('series')
 }
