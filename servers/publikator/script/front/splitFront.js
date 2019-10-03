10cc20a50d71e33354fb7fae36f746a86a906c41
Thomas Preusse <tpreusse@users.noreply.github.com>
2019-10-03 14:10:07 +0200

    feat(publikator): split front by id

--- servers/publikator/script/front/splitFront.js
+++ servers/publikator/script/front/splitFront.js
@@ -3,6 +3,7 @@
 //
 // usage: place the magazine repo folder (containing article.md and images folder)
 // next to this script and run it
+// ./splitFront.js --id 2l7waBIDo --path ~/Articles/magazine
 
 const Promise = require('bluebird')
 const { promises: fs } = require('fs')
@@ -11,23 +12,24 @@ const MDAST = require('@orbiting/remark-preset')
 const moment = require('moment')
 const { max } = require('d3-array')
 const visit = require('unist-util-visit')
-
-const { argv } = process
+const argv = require('yargs').argv
 
 const dateFormat = 'DD.MM.YYYY'
 const dateFormatFilesafe = 'YYYY-MM-DD'
 const getDateRegex = () => new RegExp(/\d\d\.\d\d\.\d\d\d\d/, 'gm')
 
-const splitDate = moment(argv[2])
-if (!argv[2] || !splitDate.isValid()) {
-  throw new Error('first argument must be a valid date to split teasers at')
+const splitDate = moment(argv.date)
+const splitId = argv.id
+
+if ((!argv.date || !splitDate.isValid()) && !splitId) {
+  throw new Error('You must provide --date or --id')
 }
 
-const repoDir = path.join(__dirname, 'magazine')
+const repoDir = argv.path || path.join(__dirname, 'magazine')
 const getNewRepoDir = (key) =>
-  path.join(__dirname, `magazine-${key}-${splitDate.format(dateFormatFilesafe)}`)
+  path.join(__dirname, `magazine-${key}-${splitId || splitDate.format(dateFormatFilesafe)}`)
 
-console.log(`Trying to split front at ${splitDate.format(dateFormat)}...`)
+console.log(`Trying to split front at ${splitId || splitDate.format(dateFormat)}...`)
 
 Promise.resolve().then(async () => {
   const articleRaw = await fs.readFile(
@@ -41,40 +43,51 @@ Promise.resolve().then(async () => {
     after: []
   }
 
-  // split nodes into before and after
-  let isAfter = false
-  let firstDateAfter
-  mdast.children
-    .reverse()
-    .forEach(rootChild => {
-      const nodes = rootChild.identifier === 'TEASERGROUP'
-        ? rootChild.children
-        : [rootChild]
+  if (splitId) {
+    const splitIndex = mdast.children.findIndex(
+      child => child.data.id === splitId
+    )
+    if (!splitIndex) {
+      throw new Error(`split id ${splitId} not found`)
+    }
+    newRootChildren.before = mdast.children.slice(0, splitIndex).reverse()
+    newRootChildren.after = mdast.children.slice(splitIndex).reverse()
+  } else {
+    // split nodes into before and after
+    let isAfter = false
+    let firstDateAfter
+    mdast.children
+      .reverse()
+      .forEach(rootChild => {
+        const nodes = rootChild.identifier === 'TEASERGROUP'
+          ? rootChild.children
+          : [rootChild]
 
-      const nodesString = JSON.stringify(nodes)
-      const regexResult = getDateRegex().exec(nodesString)
-      const dateString = max(regexResult || [])
+        const nodesString = JSON.stringify(nodes)
+        const regexResult = getDateRegex().exec(nodesString)
+        const dateString = max(regexResult || [])
 
-      const date = dateString && moment(dateString, dateFormat)
-      const key = date
-        ? (date.isBefore(splitDate) ? 'before' : 'after')
-        : (isAfter ? 'after' : 'before')
+        const date = dateString && moment(dateString, dateFormat)
+        const key = date
+          ? (date.isBefore(splitDate) ? 'before' : 'after')
+          : (isAfter ? 'after' : 'before')
 
-      if (key === 'before') {
-        if (isAfter) {
-          console.warn('child with date before split date encountered after children with date after')
+        if (key === 'before') {
+          if (isAfter) {
+            console.warn('child with date before split date encountered after children with date after')
+          }
+          newRootChildren.before.push(rootChild)
+        } else {
+          if (!isAfter) {
+            firstDateAfter = date
+          }
+          isAfter = true
+          newRootChildren.after.push(rootChild)
         }
-        newRootChildren.before.push(rootChild)
-      } else {
-        if (!isAfter) {
-          firstDateAfter = date
-        }
-        isAfter = true
-        newRootChildren.after.push(rootChild)
-      }
-    })
+      })
 
-  console.log(`first date after split: ${firstDateAfter.format(dateFormat)}`)
+    console.log(`first date after split: ${firstDateAfter.format(dateFormat)}`)
+  }
 
   // put content into two folders
   await Promise.each(
