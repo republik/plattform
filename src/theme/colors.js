86a6135aac19283d14a238600b4154eccebac803
Anna Traussnig <anna.traussnig@gmail.com>
2019-10-31 15:51:08 +0100

    feat(Colors): remove unused color

--- src/theme/colors.js
+++ src/theme/colors.js
@@ -23,7 +23,6 @@ import { getJson } from './env'
 const colors = {
   primary: '#00508C',
   primaryBg: '#BFE1FF',
-  primaryBright: '#47CC00',
   secondary: '#00335A',
   secondaryBg: '#D8EEFF',
   disabled: '#B8BDC1',
