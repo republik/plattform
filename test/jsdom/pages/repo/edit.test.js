bfe545a0bcc36c0919d1d90e9b87025161692d29
Andreas Moor <github@grmnd.ch>
2019-11-01 15:29:29 +0100

    fix tests

--- test/jsdom/pages/repo/edit.test.js
+++ test/jsdom/pages/repo/edit.test.js
@@ -1,30 +1,32 @@
-import React from 'react'
-import test from 'tape'
-import setupData from '../../../utils/setupData'
-import RouterProvider from '../../../utils/RouterProvider'
-import { mount } from '../../../utils/enzyme'
+import React from "react";
+import test from "tape";
+import setupData from "../../../utils/setupData";
+import RouterProvider from "../../../utils/RouterProvider";
+import { mount } from "../../../utils/enzyme";
 
-import { t } from '../../../../lib/withT'
-import { parse } from '@orbiting/remark-preset'
+import { RouterContext } from "next/router";
 
-import { EditorPage } from '../../../../pages/repo/edit'
+import { t } from "../../../../lib/withT";
+import { parse } from "@orbiting/remark-preset";
+
+import { EditorPage } from "../../../../pages/repo/edit";
 
 const me = {
-  __typename: 'User',
-  id: '8a724320-5d4e-4991-9055-ec03db7122c2',
-  name: 'Test Engine',
-  firstName: 'Test',
-  lastName: 'Engine',
-  email: 'test@project-r.construction',
-  roles: ['editor']
-}
+  __typename: "User",
+  id: "8a724320-5d4e-4991-9055-ec03db7122c2",
+  name: "Test Engine",
+  firstName: "Test",
+  lastName: "Engine",
+  email: "test@project-r.construction",
+  roles: ["editor"]
+};
 
 const testData = setupData({
   apollo: {
     data: {
       ROOT_QUERY: {
         me: {
-          type: 'id',
+          type: "id",
           id: `${me.__typename}:${me.id}`,
           generated: false
         }
@@ -32,21 +34,20 @@ const testData = setupData({
       [`${me.__typename}:${me.id}`]: me
     }
   }
-})
+});
 
-const EditorPageWithTestData = testData.withData(
-  EditorPage
-)
+const EditorPageWithTestData = testData.withData(EditorPage);
 
-test('EditorPage is write-able', assert => {
-  const router = {
-    query: {
-      repoId: 'orbiting/test',
-      commitId: '1'
-    }
+const router = {
+  query: {
+    repoId: "orbiting/test",
+    commitId: "1"
   }
+};
+
+test("EditorPage is write-able", assert => {
   const wrapper = mount(
-    <RouterProvider router={router}>
+    <RouterContext.Provider value={router}>
       <EditorPageWithTestData
         t={t}
         me={me}
@@ -59,7 +60,7 @@ test('EditorPage is write-able', assert => {
             commit: {
               document: {
                 meta: {
-                  template: 'article'
+                  template: "article"
                 },
                 content: parse(`---
 template: article
@@ -89,10 +90,10 @@ Text
           Promise.resolve({
             data: {
               commit: {
-                id: '2'
+                id: "2"
               }
             }
-          })
+          });
         }}
         uncommittedChanges={{
           loading: false,
@@ -104,19 +105,15 @@ Text
             data: {
               uncommittedChanges: true
             }
-          })
+          });
         }}
       />
-    </RouterProvider>
-  )
+    </RouterContext.Provider>
+  );
 
-  const page = wrapper.find(EditorPage).instance()
+  const page = wrapper.find(EditorPage).instance();
 
-  assert.equal(
-    page.state.readOnly,
-    false,
-    'is not readOnly when alone'
-  )
+  assert.equal(page.state.readOnly, false, "is not readOnly when alone");
 
-  assert.end()
-})
+  assert.end();
+});
