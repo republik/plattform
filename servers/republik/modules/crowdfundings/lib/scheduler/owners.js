6dd31c4f970c200f1e9cf0d6b9ab4342c804a2db
Patrick Venetz <venetz@gmail.com>
2019-11-29 08:48:43 +0100

    fix(crowdfundings): Stop T-30, T-7 emails and revise T-0 email

--- servers/republik/modules/crowdfundings/lib/scheduler/owners.js
+++ servers/republik/modules/crowdfundings/lib/scheduler/owners.js
@@ -35,7 +35,7 @@ const getMaxEndDate = (now, daysBeforeEndDate) =>
     .endOf('day')
 
 const createBuckets = (now) => [
-  {
+  /* {
     name: 'membership_owner_prolong_notice',
     endDate: {
       min: getMinEndDate(now, 22),
@@ -78,7 +78,7 @@ const createBuckets = (now) => [
       templateName: 'membership_owner_prolong_notice_7'
     },
     handler: mailings
-  },
+  }, */
   {
     name: 'membership_owner_prolong_notice_0',
     endDate: {
