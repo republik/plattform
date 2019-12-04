c7fff50dab7d0440f4f819d7747fe321a4bd6ca3
Patrick Venetz <venetz@gmail.com>
2019-12-04 09:59:21 +0100

    feat(republik): Add hiddenAt, disabledAt attributes and respect in queries, mutations
    * Add fields to packageOptions table
    * Alter custom package code to hide disabled, hidden options
    * Move Package.options query to Crowdfunding.packages
    * Change Crowdfunding.packages resolver to respect hiddenAt, disabledAt props and
    * Prevent submitting pledges w/ options which are disabled, while hidden ones are

--- servers/republik/modules/crowdfundings/graphql/resolvers/Crowdfunding.js
+++ servers/republik/modules/crowdfundings/graphql/resolvers/Crowdfunding.js
@@ -1,12 +1,43 @@
+const { ascending } = require('d3-array')
+
 module.exports = {
-  async packages (crowdfunding, args, {pgdb}) {
-    return pgdb.public.packages.find(
+  async packages (crowdfunding, args, { pgdb }) {
+    const now = new Date()
+
+    const packages = await pgdb.public.packages.find(
       { crowdfundingId: crowdfunding.id, custom: false },
       { orderBy: { order: 'asc' } }
     )
+
+    const packageOptions =
+      await pgdb.public.packageOptions.find(
+        {
+          packageId: packages.map(p => p.id),
+          and: [
+            { or: [{ 'disabledAt >': now }, { disabledAt: null }] },
+            { or: [{ 'hiddenAt >': now }, { hiddenAt: null }] }
+          ]
+        },
+        { orderBy: { order: 'asc' } }
+      )
+
+    return packages
+      .map(package_ => ({
+        ...package_,
+        options: packageOptions
+          .filter(po => po.packageId === package_.id)
+          .map(option => ({
+            ...option,
+            templateId: option.id,
+            package: package_
+          }))
+          .sort((a, b) => ascending(a.order, b.order))
+      }))
+      .filter(package_ => package_.options.length > 0)
+      .sort((a, b) => ascending(a.order, b.order))
   },
-  async goals (crowdfunding, args, {pgdb}) {
-    return pgdb.public.crowdfundingGoals.find({crowdfundingId: crowdfunding.id}, {
+  async goals (crowdfunding, args, { pgdb }) {
+    return pgdb.public.crowdfundingGoals.find({ crowdfundingId: crowdfunding.id }, {
       orderBy: ['people asc', 'money asc']
     })
   },
--- servers/republik/modules/crowdfundings/lib/CustomPackages/index.js
+++ servers/republik/modules/crowdfundings/lib/CustomPackages/index.js
@@ -331,8 +331,8 @@ const getCustomOptions = async (package_) => {
     }
   }
 */
-const resolvePackages = async ({ packages, pledger = {}, pgdb }) => {
-  debug('resolvePackages', packages.length)
+const resolvePackages = async ({ packages, pledger = {}, strict = false, pgdb }) => {
+  debug('resolvePackages', { packages: packages.length, strict })
 
   if (packages.length === 0) {
     debug('no packages to resolve')
@@ -391,10 +391,15 @@ const resolvePackages = async ({ packages, pledger = {}, pgdb }) => {
 
   Object.assign(pledger, { memberships })
 
+  const now = moment()
+
   const allPackageOptions =
     await pgdb.public.packageOptions.find({
       packageId: packages.map(package_ => package_.id),
-      disabled: false
+      and: [
+        { or: [{ 'disabledAt >': now }, { disabledAt: null }] },
+        strict && { or: [{ 'hiddenAt >': now }, { hiddenAt: null }] }
+      ].filter(Boolean)
     })
 
   const allRewards =
