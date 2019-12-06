5d94f2460d8b84a76d37db23df7b543f8de377b8
Patrick Venetz <venetz@gmail.com>
2019-12-06 14:39:41 +0100

    feat(utils): Add experimental helper to resolve w/ cached data if available

--- packages/utils/index.js
+++ packages/utils/index.js
@@ -4,5 +4,6 @@ module.exports = {
   paginate: require('./paginate'),
   naming: require('./naming'),
   mdastToString: require('./mdastToString'),
-  hasUserActiveMembership: require('./hasUserActiveMembership')
+  hasUserActiveMembership: require('./hasUserActiveMembership'),
+  resolveCacheFirst: require('./resolveCacheFirst')
 }
