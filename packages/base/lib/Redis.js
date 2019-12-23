84f5934ef93d6dd20a831fabcb8e6b2add030cc1
Patrick Venetz <venetz@gmail.com>
2019-12-23 12:55:28 +0100

    feat(base): Add redis.scanMap helper function
    Scans trough a key pattern and applies async mapFn(client, key) to each key.

--- packages/base/lib/Redis.js
+++ packages/base/lib/Redis.js
@@ -1,9 +1,9 @@
 const debug = require('debug')('base:lib:redis')
 const redis = require('redis')
-const bluebird = require('bluebird')
+const Promise = require('bluebird')
 
-bluebird.promisifyAll(redis.RedisClient.prototype)
-bluebird.promisifyAll(redis.Multi.prototype)
+Promise.promisifyAll(redis.RedisClient.prototype)
+Promise.promisifyAll(redis.Multi.prototype)
 
 const connect = () => {
   const url = process.env.REDIS_URL
@@ -13,6 +13,31 @@ const connect = () => {
 
   client.__defaultExpireSeconds = 3 * 7 * 24 * 60 * 60 // 3 weeks
   client.__shortExpireSeconds = 3 * 24 * 60 * 60 // 3 days
+
+  // Paginate through keys and apply async mapFn(client, key)
+  client.scanMap = async ({
+    pattern = '*',
+    mapFn = () => {},
+    cursor = 0,
+    results = []
+  }) => {
+    debug('scanMap iteration: %o', { cursor, pattern })
+    const [nextCursor, keys] = await client.scanAsync([cursor, 'MATCH', pattern])
+
+    debug('scanMap, scanned page: %o', { cursor, pattern, nextCursor: nextCursor !== '0', keys: keys.length })
+
+    const pageResults = await Promise.map(keys, mapFn.bind(this, client))
+
+    results = [...results, ...pageResults]
+
+    // nextCursor is "0" if scan is completed.
+    if (!!nextCursor && nextCursor !== '0') {
+      return client.scanMap({ pattern, mapFn, cursor: nextCursor, results })
+    }
+
+    debug('scanMap reached full iteration: %o', { results: results && results.length })
+    return results
+  }
 
   return client
 }
