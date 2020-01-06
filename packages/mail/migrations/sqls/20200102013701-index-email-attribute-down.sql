6385ada416fc565692d7e4e85532c3fb4f11bc9e
Patrick Venetz <venetz@gmail.com>
2020-01-06 10:50:41 +0100

    fix(mail): Switch DROP/ADD COLUMN stmts

--- packages/mail/migrations/sqls/20200102013701-index-email-attribute-down.sql
+++ packages/mail/migrations/sqls/20200102013701-index-email-attribute-down.sql
@@ -3,4 +3,4 @@ DROP INDEX IF EXISTS "public"."mailLog_createdAt_idx";
 DROP INDEX IF EXISTS "public"."mailLog_userId_createdAt_idx";
 DROP INDEX IF EXISTS "public"."mailLog_email_createdAt_idx";
 DROP INDEX IF EXISTS "public"."mailLog_result__id_idx";
-ALTER TABLE "public"."mailLog" ADD COLUMN "mandrillLastEvent" jsonb;
+ALTER TABLE "public"."mailLog" DROP COLUMN "mandrillLastEvent";
--- packages/mail/migrations/sqls/20200102013701-index-email-attribute-up.sql
+++ packages/mail/migrations/sqls/20200102013701-index-email-attribute-up.sql
@@ -1,4 +1,4 @@
-ALTER TABLE "public"."mailLog" DROP COLUMN "mandrillLastEvent";
+ALTER TABLE "public"."mailLog" ADD COLUMN "mandrillLastEvent" jsonb;
 CREATE INDEX "mailLog_email_idx" ON "public"."mailLog"("email");
 CREATE INDEX "mailLog_createdAt_idx" ON "public"."mailLog"("createdAt" DESC);
 CREATE INDEX "mailLog_userId_createdAt_idx" ON "public"."mailLog"("userId","createdAt" DESC);
