96c073a7acf886fab1c26c979b2cee4cb0f7e86f
patte <Patrick Recher>
2020-01-09 17:52:19 +0100

    feat(Video): checkbox isPrimary

--- components/editor/modules/embed/ui.js
+++ components/editor/modules/embed/ui.js
@@ -89,6 +89,24 @@ export default ({ TYPE, editorOptions }) => {
                           Cinemagraph
                         </Checkbox>
                       </div>
+                      <div style={{ margin: '10px 0' }}>
+                        <Checkbox
+                          checked={block.data.get('isPrimary')}
+                          onChange={event => {
+                            const checked = block.data.get('isPrimary')
+                            let change = value
+                              .change()
+                              .setNodeByKey(block.key, {
+                                data: block.data.merge({
+                                  isPrimary: !checked
+                                })
+                              })
+                            onChange(change)
+                          }}
+                        >
+                          Primäres Video
+                        </Checkbox>
+                      </div>
                     </Fragment>
                   )}
                 </p>
