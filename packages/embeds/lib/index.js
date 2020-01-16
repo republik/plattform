03a5b401280ad360740f4578b71380bad687c90d
patte <Patrick Recher>
2020-01-16 15:58:36 +0100

    cleanup

--- packages/embeds/lib/index.js
+++ packages/embeds/lib/index.js
@@ -5,13 +5,13 @@ const vimeo = require('./vimeo')
 const youtube = require('./youtube')
 
 module.exports = {
-  imageKeys: uniq([
-    ...require('./twitter').imageKeys,
-    ...require('./vimeo').imageKeys,
-    ...require('./youtube').imageKeys
-  ]),
   documentcloud: require('./documentcloud'),
   twitter,
   vimeo,
-  youtube
+  youtube,
+  imageKeys: uniq([
+    ...twitter.imageKeys,
+    ...vimeo.imageKeys,
+    ...youtube.imageKeys
+  ])
 }
