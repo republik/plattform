647616d521f75e3cd84cf69516c91c58a5754852
patte <p@tte.io>
2020-02-04 12:00:54 +0100

    typo

--- packages/discussions/graphql/resolvers/Comment.js
+++ packages/discussions/graphql/resolvers/Comment.js
@@ -284,7 +284,7 @@ module.exports = {
       return {
         document: doc,
         fragmentId,
-        iconUrl: `${FRONTEND_BASE_URL}/static/top-story-batch.png`
+        iconUrl: `${FRONTEND_BASE_URL}/static/top-story-badge.png`
       }
     }
   },
