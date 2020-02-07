1135b6a2dcca50f759f55c0ee606472b9e4dff43
patte <p@tte.io>
2020-02-07 19:43:38 +0100

    cleanup

--- packages/utils/truncate.js
+++ packages/utils/truncate.js
@@ -3,9 +3,9 @@ module.exports = (input, n) => {
     return input
   }
   let newString = input.substr(0, n)
-  const lastSpace = newString.lastIndexOf(' ')
-  if (lastSpace > 0) {
-    newString = newString.substr(0, newString.lastIndexOf(' '))
+  const lastSpaceIndex = newString.lastIndexOf(' ')
+  if (lastSpaceIndex > 0) {
+    newString = newString.substr(0, lastSpaceIndex)
   }
   return `${newString.trim()}...`
 }
