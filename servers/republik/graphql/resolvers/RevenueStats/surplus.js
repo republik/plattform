b1fecc1e1da8e59969e728460b0af5a34ee8b55b
patte <p@tte.io>
2020-02-28 03:17:05 +0100

    fix(republik): fix surplus counter, include !active ABO_GIVE

--- servers/republik/graphql/resolvers/RevenueStats/surplus.js
+++ servers/republik/graphql/resolvers/RevenueStats/surplus.js
@@ -13,17 +13,18 @@ WITH "totals" AS (
       FROM "packageOptions" pkgo
       JOIN "rewards" r ON r.id = pkgo."rewardId"
     )
-
     SELECT
       pay.id "paymentId",
       pay.total "paymentTotal",
       pay."createdAt" "paymentCreatedAt",
       p.id "pledgeId",
+      pkg.name "packageName",
 
       -- Memberships
       pom.id,
-      pom.amount "memberhipAmount",
-      pom.amount * 24000 "membershipTotal",
+      pom.amount * COALESCE(pom.periods, 1) "membershipAmount",
+      LEAST(pom.price, 24000) "membershipPrice",
+      pom.amount * COALESCE(pom.periods, 1) * LEAST(pom.price, 24000) "membershipTotal",
 
       -- Goodies
       pog.id,
@@ -33,6 +34,8 @@ WITH "totals" AS (
     FROM payments pay
     JOIN "pledgePayments" ppay ON ppay."paymentId" = pay.id
     JOIN "pledges" p ON p.id = ppay."pledgeId"
+
+    JOIN "packages" pkg ON p."packageId" = pkg.id
 
     JOIN "pledgeOptions" po ON po."pledgeId" = p.id
 
@@ -45,28 +48,55 @@ WITH "totals" AS (
       pog."templateId" IN (SELECT id FROM "packageOptionsWithRewardType" WHERE type != 'MembershipType')
 
     WHERE
-      (pay.total >= 24000 OR p.donation > 0) AND
       pay.status = 'PAID' AND
-      pay."createdAt" BETWEEN :min AND :max
-    GROUP BY pay.id, p.id, po.id, pom.id, pog.id
+      pay."createdAt" BETWEEN :min AND :max AND
+      pkg.name != 'MONTHLY_ABO'
+    GROUP BY pay.id, p.id, pkg.name, po.id, pom.id, pog.id
+    --ORDER BY pay."createdAt" DESC
+  ),
+  "membershipActivationStats" AS (
+    SELECT
+      p.id "pledgeId",
+      COUNT(*) FILTER (
+        WHERE m."active" = true
+      ) "numActive"
+    FROM
+      pledges p
+    JOIN
+      memberships m
+      ON m."pledgeId" = p.id
+    GROUP BY
+      p.id
   )
 
   SELECT
-    rd."paymentId",
-    rd."paymentCreatedAt",
-    rd."paymentTotal",
-    SUM(rd."membershipTotal") "membershipTotal",
-    SUM(rd."goodieTotal") "goodieTotal",
-    rd."paymentTotal" - COALESCE(SUM(rd."membershipTotal"), 0) - COALESCE(SUM(rd."goodieTotal"), 0) "surplusTotal"
+    --rd."paymentId",
+    --rd."paymentCreatedAt",
+    --rd."paymentTotal",
+    --SUM(rd."membershipTotal") "membershipTotal",
+    --SUM(rd."goodieTotal") "goodieTotal",
+    --mas."numActive",
+    --SUM(rd."membershipAmount") "membershipAmount",
+    --SUM(rd."membershipPrice") "membershipPrice",
+    --(mas."numActive" * COALESCE(SUM(rd."membershipTotal"), 0)/SUM("membershipAmount")) "subtract",
+    CASE
+      WHEN rd."packageName" = 'ABO_GIVE' THEN
+        rd."paymentTotal" - (mas."numActive" * COALESCE(SUM(rd."membershipTotal"), 0)/SUM("membershipAmount")) - COALESCE(SUM(rd."goodieTotal"), 0)
+      ELSE
+        rd."paymentTotal" - COALESCE(SUM(rd."membershipTotal"), 0) - COALESCE(SUM(rd."goodieTotal"), 0)
+    END AS "surplusTotal"
 
   FROM "resolvedData" rd
 
-  GROUP BY rd."paymentId", rd."paymentCreatedAt", rd."paymentTotal"
-)
+  LEFT JOIN "membershipActivationStats" mas
+  ON rd."pledgeId" = mas."pledgeId"
 
-SELECT COALESCE(SUM("surplusTotal"), 0)::int "total"
+  GROUP BY rd."paymentId", rd."paymentCreatedAt", rd."paymentTotal", rd."packageName", mas."pledgeId", mas."numActive"
+  --ORDER BY rd."paymentCreatedAt" DESC
+)
+SELECT SUM("surplusTotal") "total"
 FROM "totals"
-LIMIT 1
+WHERE "surplusTotal" > 0
 `
 
 const normalPaymentsQuery = `
