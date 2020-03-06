651a21b67233a534c776899314b8c8e8af88ef7a
patte <p@tte.io>
2020-03-06 17:09:18 +0100

    fix(servers): close connections on close, new ConnectionContext helper

---
+++ packages/base/lib/ConnectionContext.js
@@ -0,0 +1,25 @@
+const Elasticsearch = require('./Elasticsearch')
+const PgDb = require('./PgDb')
+const Redis = require('./Redis')
+const RedisPubSub = require('./RedisPubSub')
+
+const Promise = require('bluebird')
+
+const create = async (applicationName) => ({
+  pgdb: await PgDb.connect({ applicationName }),
+  redis: Redis.connect(),
+  pubsub: RedisPubSub.connect(),
+  elastic: Elasticsearch.connect()
+})
+
+const close = ({ pgdb, redis, pubsub, elastic }) => Promise.all([
+  PgDb.disconnect(pgdb),
+  Redis.disconnect(redis),
+  RedisPubSub.disconnect(pubsub),
+  Elasticsearch.disconnect(elastic)
+])
+
+module.exports = {
+  create,
+  close
+}
