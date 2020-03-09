6d18bb20f912213fdce2b0265075622f346e4cc6
patte <p@tte.io>
2020-03-09 11:04:46 +0100

    fix(discussions): add missing indices

---
+++ packages/discussions/migrations/sqls/20200309105709-missing-indices-down.sql
@@ -0,0 +1,5 @@
+DROP INDEX IF EXISTS "discussions_hidden_idx";
+DROP INDEX IF EXISTS "discussions_closed_idx";
+
+DROP INDEX IF EXISTS "comments_published_idx";
+DROP INDEX IF EXISTS "comments_admin_unpublished_idx";
