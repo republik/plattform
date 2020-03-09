439ebff974ceaeadf034e3e99796a26e4fc38769
patte <p@tte.io>
2020-03-09 11:04:15 +0100

    fix(access): fix indices names

--- packages/access/migrations/sqls/20200309104631-missing-indices-up.sql
+++ packages/access/migrations/sqls/20200309104631-missing-indices-up.sql
@@ -1,4 +1,4 @@
-CREATE INDEX IF NOT EXISTS "access_grants_access_campaign_id_idx" ON "accessGrants"("accessCampaignId");
-CREATE INDEX IF NOT EXISTS "access_grants_granter_user_id_idx" ON "accessGrants"("granterUserId");
-CREATE INDEX IF NOT EXISTS "access_grants_revoked_at_idx" ON "accessGrants"("revokedAt");
-CREATE INDEX IF NOT EXISTS "access_grants_invalidated_at_idx" ON "accessGrants"("invalidatedAt");
+CREATE INDEX IF NOT EXISTS "accessGrants_accessCampaignId_idx" ON "accessGrants"("accessCampaignId");
+CREATE INDEX IF NOT EXISTS "accessGrants_granterUserId_idx" ON "accessGrants"("granterUserId");
+CREATE INDEX IF NOT EXISTS "accessGrants_revokedAt_idx" ON "accessGrants"("revokedAt");
+CREATE INDEX IF NOT EXISTS "accessGrants_invalidatedAt_idx" ON "accessGrants"("invalidatedAt");
