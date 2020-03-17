fb1569d27a42ef3ee829b601241d5a22b8c9b893
Thomas Preusse <tpreusse@users.noreply.github.com>
2020-03-17 16:17:06 +0100

    omit meta in title serializer

--- components/editor/modules/title/index.js
+++ components/editor/modules/title/index.js
@@ -63,6 +63,7 @@ export default ({ rule, subModules, TYPE }) => {
         type: TYPE,
         data: {
           ...node.data,
+          meta,
           format,
           section
         },
@@ -71,7 +72,7 @@ export default ({ rule, subModules, TYPE }) => {
     },
     toMdast: (object, index, parent, rest) => {
       // omit format and section
-      const { format, section, ...data } = object.data
+      const { format, section, meta, ...data } = object.data
       return {
         type: 'zone',
         identifier,
