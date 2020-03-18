6162b78bfb75eb377291fe39d0e7e7a91598c3cc
patte <p@tte.io>
2020-03-18 13:16:39 +0100

    fix: make rel urls absolute for embeds

--- packages/embeds/lib/linkPreview/index.js
+++ packages/embeds/lib/linkPreview/index.js
@@ -101,17 +101,19 @@ const getLinkPreviewByUrl = async (url) => {
     return
   }
 
+  const ogImage = obj['og:image']
+  const siteImage =
+    obj['shortcut icon'] ||
+    obj.iconSmall ||
+    await getSiteImage(url, baseUrl)
+
   return {
     title: obj['og:title'],
     description: obj['og:description'],
-    imageUrl: obj['og:image'],
+    imageUrl: ogImage && new URL(ogImage, baseUrl).toString(),
     imageAlt: obj['og:image:alt'],
     siteName: obj['og:site_name'] || hostname,
-    siteImageUrl: (
-      obj['shortcut icon'] ||
-      obj.iconSmall ||
-      await getSiteImage(url, baseUrl)
-    )
+    siteImageUrl: siteImage && new URL(siteImage, baseUrl).toString()
   }
 }
 
