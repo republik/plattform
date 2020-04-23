93402c5dd27d46c2eb9accd08ecd0071d2b911ab
patte <p@tte.io>
2020-04-23 15:32:46 +0200

    cleanup gsheets

--- packages/gsheets/express/gsheets.js
+++ packages/gsheets/express/gsheets.js
@@ -9,41 +9,34 @@ const {
 
 module.exports = (server, pgdb) => {
   if (!GSHEETS_IMPORT) {
-    console.warn('missing env GSHEETS_IMPORT, importing via the /gsheets endpoint will not work')
+    console.warn('missing env GSHEETS_IMPORT, importing via the /gsheets/import endpoint will not work')
   } else {
-    server.use(
-      router.get('/gsheets/:key', async (req, res) => {
-        const { key } = req.params
-
-        await importSheet(key, pgdb)
-          .catch( e => {
-            return res.status(400).end(e.message)
-          })
-          .then( () => {
-            return res.status(200).end('success! new data published!')
-          })
-      })
-    )
+    const importCallback = async (req, res) => {
+      const { key } = req.params
+      await importSheet(key, pgdb)
+        .catch( e => { return res.status(400).end(e.message) })
+        .then( () => { return res.status(200).end('success! new data published!') })
+    }
+    router.get('/:key', importCallback)
+    router.get('/import/:key', importCallback)
   }
 
   if (!GSHEETS_EXPORT) {
-    console.warn('missing env GSHEETS_EXPORT, exporting via the /gsheets-export endpoint will not work')
+    console.warn('missing env GSHEETS_EXPORT, exporting via the /gsheets/export endpoint will not work')
   } else {
-    server.use(
-      router.get('/gsheets-export/:key', async (req, res) => {
-        const { key } = req.params
-
-        await exportToSheet(key, pgdb)
-          .catch( e => {
-            console.log(e)
-            return res.status(400).end(e.message)
-          })
-          .then( () => {
-            return res.status(200).end('success! new data published!')
-          })
-      })
-    )
+    const exportCallback = async (req, res) => {
+      const { key } = req.params
+      await exportToSheet(key, pgdb)
+        .catch( e => {
+          console.log(e)
+          return res.status(400).end(e.message)
+        })
+        .then( () => {
+          return res.status(200).end('success! new data published!')
+        })
+    }
+    router.get('/export/:key', exportCallback)
   }
 
-  return server
+  return server.use('/gsheets', router)
 }
