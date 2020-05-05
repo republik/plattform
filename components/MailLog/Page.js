fde618be149570c80456d6ede1f6110a1712d9d7
Patrick Venetz <venetz@gmail.com>
2020-05-05 07:41:07 +0200

    fix(MailLog): Adopt API change (errornous to hasError)

--- components/MailLog/Page.js
+++ components/MailLog/Page.js
@@ -5,9 +5,7 @@ import withT from '../../lib/withT'
 
 import {
   Loader,
-  A,
-  P,
-  Interaction
+  A
 } from '@project-r/styleguide'
 
 import {
@@ -19,8 +17,8 @@ import {
 import List from './List'
 
 const GET_MAILLOG = gql`
-query getMailLog($after: String, $errornous: Boolean) {
-  mailLog(first: 100, after: $after, filters: { errornous: $errornous }) {
+query getMailLog($after: String, $hasError: Boolean) {
+  mailLog(first: 20, after: $after, filters: { hasError: $hasError }) {
     pageInfo {
       hasNextPage
       endCursor
@@ -48,17 +46,17 @@ query getMailLog($after: String, $errornous: Boolean) {
 `
 
 const Page = withT(({ params, onChange }) => {
-  const { errornous = false } = params
+  const { hasError = false } = params
 
   const toggleFilterErrornous = e => {
     e.preventDefault()
-    onChange({ ...params, errornous: !errornous ? true : null })
+    onChange({ ...params, hasError: !hasError ? true : null })
   }
 
   return (
-    <Query query={GET_MAILLOG} variables={{ errornous: !!errornous }}>{({ loading, error, data, fetchMore }) => {
+    <Query query={GET_MAILLOG} variables={{ hasError: !!hasError }}>{({ loading, error, data, fetchMore }) => {
       const fetchMoreNodes = () => fetchMore({
-        variables: { after: data.mailLog.pageInfo.endCursor, errornous },
+        variables: { after: data.mailLog.pageInfo.endCursor, hasError: !!hasError },
         updateQuery: (previousResult, { fetchMoreResult }) => ({
           mailLog: {
             __typename: previousResult.mailLog.__typename,
@@ -75,10 +73,10 @@ const Page = withT(({ params, onChange }) => {
           render={() =>
             <Section>
               <SectionTitle>
-                E-Mails {errornous && '(problematische Zustellungen)'}
+                E-Mails {hasError && '(problematische Zustellungen)'}
               </SectionTitle>
               <SectionNav>
-                <A href='#' onClick={toggleFilterErrornous}>{errornous ? 'Alle E-Mails' : 'Nur problematische Zustellungen'}</A>
+                <A href='#' onClick={toggleFilterErrornous}>{hasError ? 'Alle E-Mails' : 'Nur problematische Zustellungen'}</A>
               </SectionNav>
               <List nodes={data.mailLog.nodes} />
               {data.mailLog.pageInfo.endCursor && (
