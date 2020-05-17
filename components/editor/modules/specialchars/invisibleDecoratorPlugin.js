3342e6cde16a0b6e545d350bb994e7e499862328
Thomas Preusse <tpreusse@users.noreply.github.com>
2020-05-17 13:12:55 +0200

    add u2022, u2027 and u2423 as invalids
    if they are ever copy pasted into an text it will become visible

--- components/editor/modules/specialchars/invisibleDecoratorPlugin.js
+++ components/editor/modules/specialchars/invisibleDecoratorPlugin.js
@@ -8,6 +8,9 @@ const NBSP_TYPE = 'SPECIALCHARS_NBSP'
 const CHARS = [
   ['\u2028', INVALID_TYPE],
   ['\u0308', INVALID_TYPE],
+  ['\u2022', INVALID_TYPE],
+  ['\u2423', INVALID_TYPE],
+  ['\u2423', INVALID_TYPE],
   ['\u00ad', HYPHEN_TYPE],
   ['\u00a0', NBSP_TYPE]
 ]
