c1b16583bfa2cafba7c90cd7aaf577470861e9e8
Patrick Venetz <venetz@gmail.com>
2020-06-29 15:33:35 +0200

    fix(republik): Catch error, log and throw generic API error

--- servers/republik/graphql/resolvers/MembershipStats/count.js
+++ servers/republik/graphql/resolvers/MembershipStats/count.js
@@ -2,8 +2,17 @@ const moment = require('moment')
 
 const { sumBucketProps } = require('../../../lib/MembershipStats/evolution')
 
-module.exports = (_, args, context) => sumBucketProps(
-  context,
-  moment().format('YYYY-MM'),
-  { add: ['active', 'overdue'] }
-)
+module.exports = async (_, args, context) => {
+  try {
+    const sum = await sumBucketProps(
+      context,
+      moment().format('YYYY-MM'),
+      { add: ['active', 'overdue'] }
+    )
+
+    return sum
+  } catch (e) {
+    console.error(e)
+    throw new Error(context.t('api/unexpected'))
+  }
+}
