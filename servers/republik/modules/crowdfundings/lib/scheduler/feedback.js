0b1cc475420059e383e9bb2593f947562dcf28e5
Patrick Venetz <venetz@gmail.com>
2020-07-10 15:06:49 +0200

    fix: Include active membership total in feedback inquiry, too

--- servers/republik/modules/crowdfundings/lib/scheduler/feedback.js
+++ servers/republik/modules/crowdfundings/lib/scheduler/feedback.js
@@ -3,6 +3,28 @@ const Promise = require('bluebird')
 
 const { transformUser } = require('@orbiting/backend-modules-auth')
 const { sendMailTemplate } = require('@orbiting/backend-modules-mail')
+const { countFormat } = require('@orbiting/backend-modules-formats')
+
+const { getCount: getMembershipStatsCount } = require('../../../../lib/MembershipStats/evolution')
+
+const getOptions = async (recipients, context) => {
+  const options = {}
+
+  try {
+    options.membershipsCount = await getMembershipStatsCount(context)
+    debug('options.membershipsCount', options.membershipsCount)
+  } catch (e) {
+    console.warn(e)
+  }
+
+  options.subscriptions = await context.pgdb.public.subscriptions.find({ userId: recipients.map(r => r.userId) })
+  debug('options.subscriptions', options.subscriptions.length)
+
+  options.grants = await context.pgdb.public.accessGrants.find({ granterUserId: recipients.map(r => r.userId) })
+  debug('options.grants', options.grants.length)
+
+  return options
+}
 
 const findRecipients = (context) => {
   return context.pgdb.query(`
@@ -46,7 +68,8 @@ const findRecipients = (context) => {
   `)
 }
 
-const sendMail = (subscriptions, grants, context) => recipient => {
+const sendMail = (options, context) => recipient => {
+  const { membershipsCount, subscriptions, grants } = options
   const { t } = context
 
   const recipientSubscriptions = subscriptions.filter(s => s.userId === recipient.userId)
@@ -58,6 +81,10 @@ const sendMail = (subscriptions, grants, context) => recipient => {
     {
       name: 'name',
       content: name.trim()
+    },
+    membershipsCount && {
+      name: 'memberships_count',
+      content: countFormat(membershipsCount)
     },
     {
       name: 'subscriptions_count',
@@ -92,15 +119,11 @@ const inform = async function (args, context) {
   const recipients = await findRecipients(context)
   debug('recipients', recipients.length)
 
-  const subscriptions = await context.pgdb.public.subscriptions.find({ userId: recipients.map(r => r.userId) })
-  debug('all subscriptions by recipients', subscriptions.length)
-
-  const grants = await context.pgdb.public.accessGrants.find({ granterUserId: recipients.map(r => r.userId) })
-  debug('all grants by recipients', grants.length)
+  const options = await getOptions(recipients, context)
 
   await Promise.map(
     recipients,
-    sendMail(subscriptions, grants, context),
+    sendMail(options, context),
     { concurrency: 1 }
   )
 }
