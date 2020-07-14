ab118b9c304b40ed4283a4b8dd598e1c83458d7d
patte <p@tte.io>
2020-07-14 17:27:32 +0200

    cleanup

--- servers/publikator/graphql/resolvers/_queries/repo.js
+++ servers/publikator/graphql/resolvers/_queries/repo.js
@@ -13,6 +13,9 @@ module.exports = async (_, args, context) => {
 
   const cachedRepo = body.hits.hits[0]
   if (cachedRepo) {
+    // repo.latestPublications must always be fresh
+    // otherwise resolvers/Repo latestPublications uses stale information after publishing a new doc
+    delete cachedRepo.latestPublications
     return client.mapHit(cachedRepo)
   }
 
