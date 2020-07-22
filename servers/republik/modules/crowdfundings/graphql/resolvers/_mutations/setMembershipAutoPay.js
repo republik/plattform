3333fd01eae8073685a72d53866385c5e563c159
Patrick Venetz <venetz@gmail.com>
2020-07-22 11:04:21 +0200

    fix(crowdfundings): Post Slack message if setMembershipAutoPay succeeds

--- servers/republik/modules/crowdfundings/graphql/resolvers/_mutations/setMembershipAutoPay.js
+++ servers/republik/modules/crowdfundings/graphql/resolvers/_mutations/setMembershipAutoPay.js
@@ -1,6 +1,7 @@
 const { Roles } = require('@orbiting/backend-modules-auth')
 const { autoPayIsMutable: autoPayIsMutableResolver } = require('../Membership')
 const createCache = require('../../../lib/cache')
+const { publishMonitor } = require('../../../../../lib/slack')
 
 module.exports = async (_, { id, autoPay }, context) => {
   const { user: me, t, req } = context
@@ -40,6 +41,16 @@ module.exports = async (_, { id, autoPay }, context) => {
     await createCache({
       prefix: `User:${membership.userId}`
     }, { redis: context.redis }).invalidate()
+
+    try {
+      await publishMonitor(
+        req.user,
+        `setMembershipAutoPay (id: ${id}) #${membership.sequenceNumber} to *${autoPay ? 'TRUE' : 'FALSE'}*\n{ADMIN_FRONTEND_BASE_URL}/users/${membership.userId}`
+      )
+    } catch (e) {
+      // swallow slack message
+      console.warn('publish to slack failed', { req: req._log(), error: e })
+    }
 
     return updatedMembership
   } catch (e) {
