1b3fa7f462848a772b277e26a302d70bcf90086f
Thomas Preusse <tpreusse@users.noreply.github.com>
2020-08-14 14:32:48 +0200

    fix(mail): rm brackets[] from plain text anchors, add space after urls

--- packages/mail/script/generateTextTemplates.js
+++ packages/mail/script/generateTextTemplates.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 const { promises: fs } = require('fs')
 const htmlToText = require('html-to-text')
+const defaultFormat = require('html-to-text/lib/formatter')
 const path = require('path')
 const Promise = require('bluebird')
 const recursive = require('recursive-readdir')
@@ -30,13 +31,52 @@ const run = async () => {
       await fs.writeFile(
         file.replace(/\.html$/gi, '.txt'),
         htmlToText
-          .fromString(html, { ignoreImage: true })
-          .replace(/^ /gm, '')
-          .replace(/kontakt@republik\.ch \[kontakt@republik\.ch\]/gm, 'kontakt@republik.ch'),
+          .fromString(html, {
+            ignoreImage: true,
+            format: {
+              anchor: (elem, fn, options) => {
+                // based on https://github.com/werk85/node-html-to-text/blob/7894809a57e9066501b954dbd9c16fd34c3bcebf/lib/formatter.js#L148-L176
+                // custom: add extra space after urls
+                // backed in noLinkBrackets: true and hideLinkHrefIfSameAsText: true
+                function getHref () {
+                  if (!elem.attribs || !elem.attribs.href) { return undefined }
+                  const href = elem.attribs.href.replace(/^mailto:/, '')
+                  if (options.noAnchorUrl && href[0] === '#') { return undefined }
+                  return (options.linkHrefBaseUrl && href[0] === '/')
+                    ? options.linkHrefBaseUrl + href
+                    : href
+                }
+                function getText () {
+                  return fn(elem.children, options) || ''
+                }
+
+                const storedCharCount = options.lineCharCount
+                const text = getText()
+                const href = getHref()
+                const toHideSameLink = href === text.replace(/\n/g, '')
+                const result = (!href || toHideSameLink)
+                  ? text
+                  : (!text)
+                    ? href
+                    : text + ' ' + href + ' '
+
+                options.lineCharCount = storedCharCount
+                return defaultFormat.text({ data: result, type: 'text' }, options)
+              }
+            }
+          })
+          .replace(/^ /gm, ''),
         { encoding: 'utf8', flag: 'w' }
       )
     }
-  )
+  ).then(() => {
+    const custom = 'cf_comment_notification_new.txt signin.txt signin_code.txt'
+      .split(' ')
+      .filter(t => !argv.templates || argv.templates.includes(t))
+    if (custom.length) {
+      console.log('Make sure to restore plain text customizations: ' + custom.join(', '))
+    }
+  })
 }
 
 run()
