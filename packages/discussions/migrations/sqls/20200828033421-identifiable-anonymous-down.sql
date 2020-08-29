b71cadd64c0b60ac9f03a7e7c09d403ebf5fc86e
Thomas Preusse <tpreusse@users.noreply.github.com>
2020-08-29 10:24:18 +0200

    feat(discussion): uniq constraint for anonymous differentiator

--- packages/discussions/migrations/sqls/20200828033421-identifiable-anonymous-down.sql
+++ packages/discussions/migrations/sqls/20200828033421-identifiable-anonymous-down.sql
@@ -1,3 +1,4 @@
 ALTER TABLE "discussionPreferences"
+  DROP CONSTRAINT "discussionPreferences_anonymousDifferentiator_key",
   DROP COLUMN "anonymousDifferentiator"
 ;
--- packages/discussions/migrations/sqls/20200828033421-identifiable-anonymous-up.sql
+++ packages/discussions/migrations/sqls/20200828033421-identifiable-anonymous-up.sql
@@ -1,3 +1,7 @@
 ALTER TABLE "discussionPreferences"
-  ADD COLUMN "anonymousDifferentiator" integer
+  ADD COLUMN "anonymousDifferentiator" integer,
+  -- multiple null values for not anonymous are allowed
+  -- > Two null values are never considered equal in this comparison.
+  -- > https://www.postgresql.org/docs/12/ddl-constraints.html#DDL-CONSTRAINTS-UNIQUE-CONSTRAINTS
+  ADD CONSTRAINT "discussionPreferences_anonymousDifferentiator_key" UNIQUE ("discussionId", "anonymousDifferentiator")
 ;
