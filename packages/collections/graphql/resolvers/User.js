7c632f2c20432b330eda768e9edf003f9d29c9fe
Thomas Preusse <tpreusse@users.noreply.github.com>
2020-09-10 17:57:32 +0200

    feat(collections): Add User.collectionItems query

--- packages/collections/graphql/resolvers/User.js
+++ packages/collections/graphql/resolvers/User.js
@@ -1,5 +1,6 @@
 const { Roles } = require('@orbiting/backend-modules-auth')
 const Collection = require('../../lib/Collection')
+const { paginate } = require('@orbiting/backend-modules-utils')
 
 const accessRoles = ['member']
 const adminRoles = ['admin', 'supporter']
@@ -22,6 +23,23 @@ module.exports = {
       Roles.userIsInRoles(me, adminRoles)
     ) {
       return Collection.byNameForUser(name, user.id, context)
+    }
+  },
+  async collectionItems (user, args, context) {
+    const { user: me } = context
+    if (
+      (Roles.userIsMe(user, me) && Roles.userIsInRoles(user, accessRoles)) ||
+      Roles.userIsInRoles(me, adminRoles)
+    ) {
+      const collections = await Promise.all(args.names.map(name => Collection.byNameForUser(name, user.id, context)))
+      const items = await Collection.findDocumentItems(
+        {
+          collectionId: collections.map(collection => collection.id),
+          userId: user.id
+        },
+        context
+      )
+      return paginate(args, items)
     }
   }
 }
