5e5bde5ce86d1eff8632f5d97ff49816d19dfbed
Pierre Spring <pierre.spring@caillou.ch>
2020-09-10 12:01:56 +0200

    Use eslint and prettier for both JS and TS files

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+  },
+  overrides: [
+    {
+      files: ['**/*.ts'],
+      parser: '@typescript-eslint/parser',
+      plugins: ['@typescript-eslint'],
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier/@typescript-eslint',
+        'plugin:prettier/recommended',
+      ],
+      rules: { '@typescript-eslint/no-explicit-any': 'off' }
+    },
+    {
+      files: ['**/*.js'],
+      extends: 'standard',
+    },
+    {
+      files: ['**/*.jest.js'],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+}
