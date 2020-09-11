de0a328e3de44204cd104edafe2d2204b7818365
Thomas Preusse <tpreusse@users.noreply.github.com>
2020-09-11 12:00:05 +0200

    refactor(collections): dry canAccess function

--- packages/collections/graphql/resolvers/User.js
+++ packages/collections/graphql/resolvers/User.js
@@ -5,32 +5,28 @@ const { paginate } = require('@orbiting/backend-modules-utils')
 const accessRoles = ['member']
 const adminRoles = ['admin', 'supporter']
 
+const canAccess = (user, context) => {
+  const { user: me } = context
+  return (
+    (Roles.userIsMe(user, me) && Roles.userIsInRoles(user, accessRoles)) ||
+    Roles.userIsInRoles(me, adminRoles)
+  )
+}
+
 module.exports = {
   collections (user, args, context) {
-    const { user: me } = context
-    if (
-      (Roles.userIsMe(user, me) && Roles.userIsInRoles(user, accessRoles)) ||
-      Roles.userIsInRoles(me, adminRoles)
-    ) {
+    if (canAccess(user, context)) {
       return Collection.findForUser(user.id, context)
     }
     return []
   },
   collection (user, { name }, context) {
-    const { user: me } = context
-    if (
-      (Roles.userIsMe(user, me) && Roles.userIsInRoles(user, accessRoles)) ||
-      Roles.userIsInRoles(me, adminRoles)
-    ) {
+    if (canAccess(user, context)) {
       return Collection.byNameForUser(name, user.id, context)
     }
   },
   async collectionItems (user, args, context) {
-    const { user: me } = context
-    if (
-      (Roles.userIsMe(user, me) && Roles.userIsInRoles(user, accessRoles)) ||
-      Roles.userIsInRoles(me, adminRoles)
-    ) {
+    if (canAccess(user, context)) {
       const collections = await Promise.all(args.names.map(name => Collection.byNameForUser(name, user.id, context)))
       const items = await Collection.findDocumentItems(
         {
