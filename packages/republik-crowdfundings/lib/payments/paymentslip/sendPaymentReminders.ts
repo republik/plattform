4517f03d494bd3e55a956b52bff4fb96daec11eb
Pierre Spring <pierre.spring@caillou.ch>
2020-10-21 15:41:03 +0200

    DryRun sendPaymentReminders()
    DryRun can be configured through an environment variable,
    and overwritten through option parameter.

--- packages/republik-crowdfundings/lib/payments/paymentslip/sendPaymentReminders.ts
+++ packages/republik-crowdfundings/lib/payments/paymentslip/sendPaymentReminders.ts
@@ -9,10 +9,12 @@ import { publishFinance } from '@orbiting/backend-modules-republik/lib/slack'
 const { sendMailTemplate } = require('@orbiting/backend-modules-mail')
 const { PAYMENT_DEADLINE_DAYS } = require('./helpers')
 
-const daysTillFirstReminder = PAYMENT_DEADLINE_DAYS + 3
-const daysTillSecondReminder = daysTillFirstReminder + 30
-const daysTillCancelPledge = daysTillSecondReminder + 15
-const daysToConsider = daysTillCancelPledge + 30
+const DRY_RUN = process.env.DRY_RUN_SEND_PAYMENT_REMINDERS === 'true'
+
+const FIRST_REMINDER_DEADLINE_DAYS = PAYMENT_DEADLINE_DAYS + 3 // 3 days to consider weekends
+const SECOND_REMINDER_DEADLINE_DAYS = FIRST_REMINDER_DEADLINE_DAYS + 30
+const CANCEL_PLEDGE_DEADLINE_DAYS = SECOND_REMINDER_DEADLINE_DAYS + 15
+const DAYS_TO_CONSIDER = CANCEL_PLEDGE_DEADLINE_DAYS + 30
 
 interface OutstandingPayment {
   paymentId: Nominal<string, 'paymentId'>
@@ -26,9 +28,11 @@ interface OutstandingPayment {
   companyName: string
 }
 
-export async function sendPaymentReminders(context: Context): Promise<void> {
+export async function sendPaymentReminders(
+  context: Context,
+  { dryRun = DRY_RUN } = {},
+): Promise<void> {
   const { pgdb } = context
-
   if (await hasUnmatchedPayments(pgdb)) {
     return
   }
@@ -40,12 +44,13 @@ export async function sendPaymentReminders(context: Context): Promise<void> {
     secondRemindersSent,
     cancelMessages,
   ] = await Promise.all([
-    sendFirstReminder(outstandingPayments, context),
-    sendSecondReminder(outstandingPayments, context),
+    sendFirstReminder(outstandingPayments, context, dryRun),
+    sendSecondReminder(outstandingPayments, context, dryRun),
     getMessageForAccountsToCancel(outstandingPayments),
   ])
 
   let messageLines = [
+    ...(dryRun ? ['⚠️ DRY RUN, REMINDERS ARE NOT SENT ⚠️'] : []),
     '💌 Reminders Sent',
     '',
     `First reminders sent: ${firstRemindersSent}`,
@@ -67,23 +72,26 @@ export async function sendPaymentReminders(context: Context): Promise<void> {
 function getMessageForAccountsToCancel(
   outstandingPayments: OutstandingPayment[],
 ) {
-  const filterDate = daysAgo(daysTillCancelPledge)
+  const filterDate = daysAgo(CANCEL_PLEDGE_DEADLINE_DAYS)
   return outstandingPayments
     .filter(({ createdAt }) => createdAt < filterDate)
-    .map(({ userId, pledgeId }) => {
-      return [
-        '',
-        `User: {ADMIN_FRONTEND_BASE_URL}/users/${userId}`,
-        `Pledge ID: ${pledgeId}`,
-      ].join('\n')
-    })
+    .map(paymentToMessage)
+}
+
+function paymentToMessage({ userId, pledgeId }: OutstandingPayment) {
+  return [
+    '',
+    `User: {ADMIN_FRONTEND_BASE_URL}/users/${userId}`,
+    `Pledge ID: ${pledgeId}`,
+  ].join('\n')
 }
 
 function sendFirstReminder(
   outstandingPayments: OutstandingPayment[],
   context: Context,
+  dryRun: boolean,
 ) {
-  const filterDate = daysAgo(daysTillFirstReminder)
+  const filterDate = daysAgo(FIRST_REMINDER_DEADLINE_DAYS)
   const filter = ({ createdAt, remindersSentAt }: OutstandingPayment) => {
     if (remindersSentAt !== null && remindersSentAt.length) {
       return false
@@ -92,14 +100,21 @@ function sendFirstReminder(
     return createdAt < filterDate
   }
 
-  return sendReminders({ outstandingPayments, filter, context, isLast: false })
+  return sendReminders({
+    outstandingPayments,
+    filter,
+    context,
+    isLast: false,
+    dryRun,
+  })
 }
 
 function sendSecondReminder(
   outstandingPayments: OutstandingPayment[],
   context: Context,
+  dryRun: boolean,
 ) {
-  const filterDate = daysAgo(daysTillSecondReminder)
+  const filterDate = daysAgo(SECOND_REMINDER_DEADLINE_DAYS)
 
   const filter = ({ createdAt, remindersSentAt }: OutstandingPayment) => {
     if (remindersSentAt?.length !== 1) {
@@ -109,7 +124,13 @@ function sendSecondReminder(
     return createdAt < filterDate
   }
 
-  return sendReminders({ outstandingPayments, filter, context, isLast: true })
+  return sendReminders({
+    outstandingPayments,
+    filter,
+    context,
+    isLast: true,
+    dryRun,
+  })
 }
 
 function daysAgo(n: number) {
@@ -120,14 +141,22 @@ interface sendRemindersParameters {
   filter: ({ createdAt, remindersSentAt }: OutstandingPayment) => boolean
   context: Context
   isLast: boolean
+  dryRun: boolean
 }
 async function sendReminders({
   outstandingPayments,
   filter,
   context,
   isLast,
+  dryRun,
 }: sendRemindersParameters) {
   const latePayments = outstandingPayments.filter(filter)
+
+  if (dryRun) {
+    await dryRunInform(latePayments, isLast)
+    return latePayments.length
+  }
+
   for (const payment of latePayments) {
     await send({
       payment,
@@ -137,6 +166,15 @@ async function sendReminders({
     await updatePayment({ payment, pgdb: context.pgdb })
   }
   return latePayments.length
+}
+
+async function dryRunInform(payments: OutstandingPayment[], isLast: boolean) {
+  const message = [
+    '⚠️ DRY RUN, REMINDERS ARE NOT SENT ⚠️',
+    `${isLast ? 'Second' : 'First'} reminder would be sent to:`,
+    ...payments.map(paymentToMessage),
+  ].join('\n')
+  await publishFinance(message)
 }
 
 async function updatePayment({
@@ -192,7 +230,7 @@ async function getOutstandingPayments(
   pgdb: PgDb,
 ): Promise<OutstandingPayment[]> {
   return await pgdb.query(outstandingPaymentsQuery, {
-    date: moment().subtract(daysToConsider, 'days'),
+    date: moment().subtract(DAYS_TO_CONSIDER, 'days'),
   })
 }
 
