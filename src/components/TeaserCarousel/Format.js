e0c8beed4ba839ef0266d5211e3723a42b1ec542
Olivier Baumann <olivier@iovi.io>
2020-11-05 11:43:09 +0100

    fix(TeaserCarousel): Format

--- src/components/TeaserCarousel/Format.js
+++ src/components/TeaserCarousel/Format.js
@@ -1,7 +1,6 @@
 import { css } from 'glamor'
 import PropTypes from 'prop-types'
 import React from 'react'
-import colors from '../../theme/colors'
 import { sansSerifMedium14 } from '../Typography/styles'
 import { useColorContext } from '../Colors/useColorContext'
 const styles = css({
@@ -9,7 +8,7 @@ const styles = css({
   margin: '0 0 10px 0'
 })
 
-const Format = ({ children, color = colors.feuilleton }) => {
+const Format = ({ children, color }) => {
   const [colorScheme] = useColorContext()
   return (
     <div {...colorScheme.set('color', color || 'text', 'format')} {...styles}>
@@ -23,7 +22,4 @@ export default Format
 Format.propTypes = {
   children: PropTypes.node,
   color: PropTypes.string
-}
-Format.defaultProps = {
-  bigger: colors.feuilleton
 }
