592e42537fc43e0b86aa4fef15ac76bfd6e36dc6
Anna Traussnig <anna.traussnig@gmail.com>
2020-11-11 14:35:49 +0100

    add ColorContextProvider light for metadata

--- components/editor/modules/meta/index.js
+++ components/editor/modules/meta/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import MetaData from './ui'
+import { ColorContextProvider } from '@project-r/styleguide'
 
 export default ({ rule, TYPE, context = {} }) => {
   const options = rule.editorOptions || {}
@@ -14,15 +15,18 @@ export default ({ rule, TYPE, context = {} }) => {
         renderEditor({ value, children }, editor) {
           return (
             <div>
-              {children}
-              <MetaData
-                value={value}
-                editor={editor}
-                {...options}
-                mdastSchema={context.mdastSchema}
-                contextMeta={context.meta}
-                isTemplate={context.isTemplate}
-              />
+              <ColorContextProvider colorSchemeKey='light'>
+                {children}
+                {children}
+                <MetaData
+                  value={value}
+                  editor={editor}
+                  {...options}
+                  mdastSchema={context.mdastSchema}
+                  contextMeta={context.meta}
+                  isTemplate={context.isTemplate}
+                />
+              </ColorContextProvider>
             </div>
           )
         }
