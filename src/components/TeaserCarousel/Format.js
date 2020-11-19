cd98c159ee2f75de24d2075c09664bcb24c26764
Thomas Preusse <tpreusse@users.noreply.github.com>
2020-11-19 23:48:26 +0100

    fix(TeaserCarousel): format color

--- src/components/TeaserCarousel/Format.js
+++ src/components/TeaserCarousel/Format.js
@@ -3,15 +3,20 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import { sansSerifMedium14 } from '../Typography/styles'
 import { useColorContext } from '../Colors/useColorContext'
+import CarouselContext, { defaultValue } from './Context'
+
 const styles = css({
   ...sansSerifMedium14,
   margin: '0 0 10px 0'
 })
 
 const Format = ({ children, color }) => {
+  const context = React.useContext(CarouselContext)
+  const textColor = color || context.color
+  const mapping = textColor === defaultValue.color ? 'format' : undefined
   const [colorScheme] = useColorContext()
   return (
-    <div {...colorScheme.set('color', color || 'text', 'format')} {...styles}>
+    <div {...colorScheme.set('color', textColor, mapping)} {...styles}>
       {children}
     </div>
   )
