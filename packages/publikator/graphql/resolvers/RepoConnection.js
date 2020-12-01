8d061711655f1b2c87d94f3eb7b46822bef1d882
Patrick Venetz <venetz@gmail.com>
2020-12-01 13:29:16 +0100

    chore(publikator): Remove unused variable

--- packages/publikator/graphql/resolvers/RepoConnection.js
+++ packages/publikator/graphql/resolvers/RepoConnection.js
@@ -2,11 +2,11 @@ const { getPhases } = require('../../lib/phases')
 
 module.exports = {
   phases(obj, args, context) {
-    const { t } = context
-
-    return getPhases().map(phase => {
+    return getPhases().map((phase) => {
       const { key, color, lock } = phase
-      const bucket = obj.aggregations.phases.buckets.find(bucket => bucket.key === phase.key)
+      const bucket = obj.aggregations.phases.buckets.find(
+        (bucket) => bucket.key === phase.key,
+      )
 
       return {
         key,
