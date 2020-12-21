16edc4e8c3696d97eb071517ed059e1ea2a056ad
Patrick Venetz <venetz@gmail.com>
2020-12-21 13:57:50 +0100

    fix(republik-crowdfundings): Transfer discussion suspensions while merging user

--- packages/republik-crowdfundings/graphql/resolvers/_mutations/mergeUsers.js
+++ packages/republik-crowdfundings/graphql/resolvers/_mutations/mergeUsers.js
@@ -141,6 +141,7 @@ module.exports = async (_, args, context) => {
     await transaction.public.comments.update(from, to)
     await transaction.public.credentials.update(from, to)
     await transaction.public.consents.update(from, to)
+    await transaction.public.discussionSuspensions.update(from, to)
 
     await mergeCustomers({
       targetUserId: targetUser.id,
@@ -221,7 +222,7 @@ module.exports = async (_, args, context) => {
       () =>
         transaction.public.mailLog.update(
           { userId: null, email: sourceUser.email },
-          to
+          to,
         ),
       () => transaction.public.notifications.update(from, to),
       () =>
