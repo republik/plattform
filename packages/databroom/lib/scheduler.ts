d9b303a4ffbef161f006d322d12744454df5666c
Patrick Venetz <venetz@gmail.com>
2021-01-03 15:17:56 +0100

    feat(databroom): Add lib/scheduler

---
+++ packages/databroom/lib/scheduler.ts
@@ -0,0 +1,44 @@
+import { debug as _debug } from 'debug'
+import Promise from 'bluebird'
+
+import { timeScheduler } from '@orbiting/backend-modules-schedulers'
+import { Context } from '@orbiting/backend-modules-types'
+
+import { setup } from '../lib'
+
+const DEV = process.env.NODE_ENV && process.env.NODE_ENV !== 'production'
+
+const lockTtlSecs = 60 * 5 // 5 minutes
+
+const init = async (context: Context) => {
+  const debug = _debug('databroom:lib:scheduler')
+  debug('init')
+
+  const scheduler = await timeScheduler.init({
+    name: 'databroom',
+    context,
+    runFunc: async (args: any, context: any) => {
+      const { dryRun } = args
+
+      const jobFns = await setup({ dryRun }, context)
+      debug('%i jobs set up', jobFns.length)
+    
+      await Promise.each(jobFns, fn => fn())
+    },
+    lockTtlSecs,
+    runAtTime: '03:00',
+    runInitially: DEV,
+  })
+
+  const close = async () => {
+    await scheduler.close()
+  }
+
+  return {
+    close,
+  }
+}
+
+module.exports = {
+  init,
+}
