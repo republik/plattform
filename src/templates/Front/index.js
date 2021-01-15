9c2ce2c19ef859f3f3c228f8b44a6e0eff26d01d
Anna Traussnig <anna.traussnig@gmail.com>
2021-01-15 11:37:56 +0100

    feat(FrontSchema): remove background color from front

--- src/templates/Front/index.js
+++ src/templates/Front/index.js
@@ -636,8 +636,7 @@ const createSchema = ({ Link = DefaultLink, t = () => '', ...rest } = {}) => {
           <div
             style={{
               width: '100%',
-              overflow: 'hidden',
-              backgroundColor: '#fff'
+              overflow: 'hidden'
             }}
           >
             {children}
