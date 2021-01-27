9610af39932a87573819662d38fb7defb0333290
Anna Traussnig <anna.traussnig@gmail.com>
2021-01-27 17:28:21 +0100

    fix(Callout): add padding option

--- src/components/Callout/CalloutMenu.js
+++ src/components/Callout/CalloutMenu.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import Callout from './index'
+import { css } from 'glamor'
+import { mUp } from '../../theme/mediaQueries'
 
 const hasAncestor = (node, predicate) => {
   if (predicate(node)) {
@@ -16,7 +18,8 @@ const CalloutMenu = ({
   Element,
   align,
   initiallyOpen,
-  contentPaddingMobile
+  contentPaddingMobile,
+  padded
 }) => {
   const [showMenu, setMenu] = React.useState(initiallyOpen)
   const toggleRef = React.useRef()
@@ -36,7 +39,11 @@ const CalloutMenu = ({
   }, [showMenu])
 
   return (
-    <div style={{ position: 'relative' }} ref={toggleRef}>
+    <div
+      {...(padded && styles.padded)}
+      style={{ position: 'relative' }}
+      ref={toggleRef}
+    >
       {showMenu && (
         <Callout
           onClose={() => setMenu(false)}
@@ -49,6 +56,15 @@ const CalloutMenu = ({
       <Element onClick={() => setMenu(!showMenu)} />
     </div>
   )
+}
+
+const styles = {
+  padded: css({
+    marginRight: 20,
+    [mUp]: {
+      marginRight: 24
+    }
+  })
 }
 
 export default CalloutMenu
