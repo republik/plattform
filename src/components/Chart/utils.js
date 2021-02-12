3bbd26176779b6a6003a4775ebaf47a7f79f6591
Anna Traussnig <anna.traussnig@gmail.com>
2021-02-12 14:46:13 +0100

    fix(BarChart): better contrast for inline text

--- src/components/Chart/utils.js
+++ src/components/Chart/utils.js
@@ -253,5 +253,5 @@ export const unsafeDatumFn = code => new Function('datum', `return ${code}`)
 export const getTextColor = bgColor => {
   const color = rgb(bgColor)
   const yiq = (color.r * 299 + color.g * 587 + color.b * 114) / 1000
-  return yiq >= 128 ? colors.light.text : colors.light.textInverted
+  return yiq >= 128 ? 'white' : 'black'
 }
