85b516d60563cc10739722cdb849dfaa424fd317
Sharon Funke <sharon.funke@voodoo-arts.net>
2021-03-02 15:10:05 +0100

    fix(republik) improve querying min year in trendYears

--- packages/republik/lib/trendYears.js
+++ packages/republik/lib/trendYears.js
@@ -1,5 +1,5 @@
 const getYears = async (pgdb) => {
-  const minYearPeriod = await pgdb.query(`
+  const minYear = await pgdb.queryOneField(`
     SELECT 
       EXTRACT(YEAR FROM "beginDate") "minYear"
     FROM "membershipPeriods"
@@ -7,12 +7,12 @@ const getYears = async (pgdb) => {
     LIMIT 1
   `)
 
-  const minYear = minYearPeriod[0].minYear
   const maxYear = new Date().getFullYear()
   const years = []
   for (let year = minYear; year <= maxYear; year++) {
     years.push(year.toString())
   }
+  console.log(years)
   return years
 }
 
