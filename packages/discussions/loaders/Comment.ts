0b4192d0205cd0fdc6ed908767f95c748e06d467
Patrick Venetz <venetz@gmail.com>
2021-04-30 08:05:17 +0200

    refactor(discussions): Migrate Comment loader to TypeScript

---
+++ packages/discussions/loaders/Comment.ts
@@ -0,0 +1,63 @@
+import { PgTable } from 'pogi'
+import { v4 } from 'is-uuid'
+
+import createDataLoader from '@orbiting/backend-modules-dataloader'
+import { GraphqlContext } from '@orbiting/backend-modules-types'
+
+interface Comment {
+  id: string
+  discussionId: string
+  parentIds?: string[]
+  userId?: string
+  content?: string
+  upVotes: number
+  downVotes: number
+  votes: CommentVote[]
+  hotness: number
+  depth: 0
+  published: boolean
+  adminUnpublished: boolean
+  reports?: CommentReport[]
+  createdAt?: Date
+  updatedAt?: Date
+  tags?: string[]
+  urls?: string[]
+  embedUrl?: string
+  mentioningRepoId?: string
+  mentioningFragmentId?: string
+  featuredAt?: Date
+  featuredContent?: string
+  featuredTargets?: string[]
+}
+
+interface CommentVote {
+  vote: 1 | -1
+  userId?: string
+}
+
+interface CommentReport {
+  userId: string
+  reportedAt: Date
+}
+
+export default module.exports = function (context: GraphqlContext) {
+  const comments: PgTable<Comment> = context.pgdb.public.comments
+
+  return {
+    byId: createDataLoader(async (ids: readonly string[]) => {
+      const commentIds = ids.filter(v4)
+
+      return (
+        (commentIds.length && (await comments.find({ id: commentIds }))) || []
+      )
+    }),
+    byParentId: createDataLoader(
+      async (parentIds: readonly string[]) => {
+        const or = parentIds.filter(v4).map((p) => ({ 'parentIds @>': [p] }))
+        return comments.find({ or })
+      },
+      null,
+      (key, rows) => rows.filter((row) => row.parentIds?.includes(key)),
+    ),
+  }
+}
