80d456412b5655505364f99ede6ad3e1be736bc1
Olivier Baumann <olivier@iovi.io>
2021-05-25 14:25:53 +0200

    fix(ShareImage): add missing lineheight

--- src/components/ShareImage/ShareImagePreview.js
+++ src/components/ShareImage/ShareImagePreview.js
@@ -167,6 +167,7 @@ const ShareImagePreview = ({
             fontSize: fontSize || SHARE_IMAGE_DEFAULTS.fontSize,
             color: inverted ? '#FFF' : '#000',
             width: shareImage && '80%',
+            lineHeight: 1.25,
             border:
               preview && textContainerOverflow ? '1px dotted red' : 'none',
             maxHeight:
