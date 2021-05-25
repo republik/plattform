dfb2b6c6072f5ff399adc1070e43b7c757a1b9cf
Patrick Venetz <venetz@gmail.com>
2021-05-25 16:10:05 +0200

    fix(republik-crowdfunding): Drop publishing payment intent event in stripe webhook handlers

--- packages/republik-crowdfundings/lib/payments/stripe/webhooks/invoicePaymentSucceeded.js
+++ packages/republik-crowdfundings/lib/payments/stripe/webhooks/invoicePaymentSucceeded.js
@@ -9,25 +9,10 @@ const { makePledgeSuccessfulWithCharge } = require('../../Pledge')
 // but not the charge details
 module.exports = {
   eventTypes: ['invoice.payment_succeeded'],
-  handle: async (event, pgdb, t, redis, connectionContext, companyId) => {
+  handle: async (event, t, connectionContext, companyId) => {
     const context = {
       ...connectionContext,
       t,
-    }
-
-    const paymentIntentId = event.data?.object?.payment_intent
-    if (paymentIntentId) {
-      // object.lines.data[0] is the subscription
-      const pledgeId = event.data?.object?.lines?.data[0]?.metadata?.pledgeId
-      // stripe/payPledge might be waiting for clientSecret
-      // publish that no auth is required
-      await redis.publish(
-        `pledge:${pledgeId}:paymentIntent`,
-        JSON.stringify({
-          id: paymentIntentId,
-          clientSecret: 'no-auth-required',
-        }),
-      )
     }
 
     const chargeId = event.data?.object?.charge
