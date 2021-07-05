89dec85b99bd181e2fc4ab81b74545b66c14210f
Anna Traussnig <anna.traussnig@gmail.com>
2021-07-05 15:52:26 +0200

    adapt diff modal to new OverlayToolbar API

--- components/Tree/Diff.js
+++ components/Tree/Diff.js
@@ -3,18 +3,14 @@ import { css } from 'glamor'
 import { Query } from 'react-apollo'
 import gql from 'graphql-tag'
 import ReactDiffViewer, { DiffMethod } from 'react-diff-viewer'
-import MdClose from 'react-icons/lib/md/close'
 import MdMoreHoriz from 'react-icons/lib/md/more-horiz'
 import MdWrapText from 'react-icons/lib/md/wrap-text'
 
 import {
   Overlay,
   OverlayToolbar,
-  OverlayToolbarConfirm,
-  OverlayToolbarClose,
   OverlayBody,
-  Loader,
-  Interaction
+  Loader
 } from '@project-r/styleguide'
 
 const styles = {
@@ -80,14 +76,10 @@ export default function TreeDiff(props) {
               onClose={handleOnClose}
               mUpStyle={{ maxWidth: '95vw', minHeight: 'none' }}
             >
-              <OverlayToolbar>
-                <Interaction.Emphasis
-                  style={{ padding: '15px 20px', fontSize: 16 }}
-                >
-                  {props.commit.message}
-                </Interaction.Emphasis>
-                <OverlayToolbarClose onClick={handleOnClose} />
-              </OverlayToolbar>
+              <OverlayToolbar
+                title={props.commit.message}
+                onClose={handleOnClose}
+              />
               <OverlayBody style={{ fontSize: 13 }}>
                 <Loader
                   loading={loading}
