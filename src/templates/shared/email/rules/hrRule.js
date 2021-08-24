e748f6ca74eadbbaa159c504a506f7dd69878dc3
Tobias Maier <trm217@protonmail.com>
2021-08-24 16:00:15 +0200

    fix(thematicBreak): fix email safe thematic-break

--- src/templates/shared/email/rules/hrRule.js
+++ src/templates/shared/email/rules/hrRule.js
@@ -2,7 +2,7 @@ import { matchType } from 'mdast-react-render/lib/utils'
 import HorizontalRule from '../components/HorizontalRule'
 
 const hrRule = {
-  matchMdast: matchType('hr'),
+  matchMdast: matchType('thematicBreak'),
   component: HorizontalRule
 }
 
