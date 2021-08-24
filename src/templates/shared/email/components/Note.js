5ac21cf19a60bef2237b5c8c6eda2fe56fc5ea30
Tobias Maier <trm217@protonmail.com>
2021-08-24 09:16:16 +0200

    feat(Note-component): add email safe Note component

--- src/templates/shared/email/components/Note.js
+++ src/templates/shared/email/components/Note.js
@@ -1,11 +1,24 @@
 import React from 'react'
-import { Note as WebNote } from '../../../../components/Typography/Editorial'
+import { Interaction } from '../../../../components/Typography'
+import colors from '../../../../theme/colors'
+import { fontFamilies } from '../../../../theme/fonts'
 
 export const Note = ({ children }) => <>{children}</>
 
-// todo: make email safe
-export const NoteParagraph = ({ children, attributes }) => {
-  return <WebNote attributes={attributes}>{children}</WebNote>
-}
+export const NoteParagraph = ({ children, attributes, ...props }) => (
+  <p
+    className={Interaction.fontRule}
+    style={{
+      color: colors.text,
+      fontSize: '15px',
+      lineHeight: '21px', // 1.3125rem
+      fontFamily: fontFamilies.sansSerifRegular
+    }}
+    {...attributes}
+    {...props}
+  >
+    {children}
+  </p>
+)
 
 export default NoteParagraph
