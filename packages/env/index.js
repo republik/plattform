0ff4dab7c30a82aca20be184a7e3ce1dc66e8802
Patrick Venetz <venetz@gmail.com>
2021-09-29 17:35:39 +0200

    feat(env): Return paths to used env files

--- packages/env/index.js
+++ packages/env/index.js
@@ -6,19 +6,25 @@ const OVERWRITE_ENV = process.env.OVERWRITE_ENV
 const config = (envFile) => {
   if (DEV) {
     const dotenv = require('dotenv')
+    const paths = new Set()
 
     if (OVERWRITE_ENV) {
-      dotenv.config({ path: OVERWRITE_ENV })
+      paths.add(OVERWRITE_ENV)
     }
 
-    const projectRootPath = path.join(__dirname, '../..')
     if (envFile) {
-      dotenv.config({ path: path.join(projectRootPath, envFile) })
+      paths.add(getPath(envFile))
     }
 
-    dotenv.config({ path: path.join(projectRootPath, '.env') })
+    paths.add(getPath('.env'))
+
+    Array.from(paths).forEach((path) => dotenv.config({ path }))
+
+    return { paths }
   }
 }
+
+const getPath = (envFile = '.env') => path.join(__dirname, '../..', envFile)
 
 module.exports = {
   config,
