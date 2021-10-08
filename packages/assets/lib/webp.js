925b4f3ec851c95150ff70656d5c0f3a30329b63
Thomas Preusse <tpreusse@users.noreply.github.com>
2021-10-08 13:53:01 +0200

    fix(assets): skip empty newsletter cover image src when adding format auto

--- packages/assets/lib/webp.js
+++ packages/assets/lib/webp.js
@@ -1,6 +1,9 @@
 const { URL } = require('url')
 
 const addFormatAuto = (urlString) => {
+  if (!urlString?.trim()) {
+    return urlString
+  }
   const url = new URL(urlString)
   if (!url.searchParams.has('format')) {
     url.searchParams.set('format', 'auto')
