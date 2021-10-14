ca84a3313d890ad8cb6f81dde7c8d6f99b1eeaf2
Patrick Venetz <venetz@gmail.com>
2021-10-14 21:45:04 +0200

    fix(embeds): Apply proxy

--- packages/embeds/graphql/resolvers/_queries/embed.js
+++ packages/embeds/graphql/resolvers/_queries/embed.js
@@ -2,14 +2,16 @@ const {
   Roles: { ensureUserHasRole },
 } = require('@orbiting/backend-modules-auth')
 
-const { getEmbedData } = require('../../../lib/fetchAndStore')
+const { getEmbedData, applyProxyUrls } = require('../../../lib/fetchAndStore')
 
 module.exports = async (_, args, { user, t }) => {
   ensureUserHasRole(user, 'editor')
 
   const { id, embedType } = args
+  const embed = await getEmbedData(args, t)
+
   return {
-    ...(await getEmbedData(args, t)),
+    ...applyProxyUrls(embed, embedType),
     __typename: embedType,
     mediaId: `${embedType}-${id}`,
   }
