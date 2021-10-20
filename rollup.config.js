8d9c384163021c11c8c1b8593a650618a7da933c
Tobias Maier <trm217@protonmail.com>
2021-10-20 14:22:27 +0200

    chore(build-setup): rename scripts and add terser

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,8 @@
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import { babel } from '@rollup/plugin-babel'
-import { nodeResolve } from '@rollup/plugin-node-resolve'
-import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+import { terser } from 'rollup-plugin-terser'
 import fileSize from 'rollup-plugin-filesize'
 import css from 'rollup-plugin-import-css'
 
@@ -23,13 +24,37 @@ export default {
   ],
   plugins: [
     peerDepsExternal(),
-    nodeResolve(),
-    babel({ babelHelpers: 'bundled', configFile: true }),
-    css(),
-    commonjs({ extensions: ['.js', '.ts'] }),
+    resolve(),
     /*typescript({ tsconfig: './tsconfig.json' }),*/
+    babel({
+      babelHelpers: 'bundled',
+      exclude: 'node_modules/**'
+    }),
+    commonjs(),
+    css(),
+    // terser(),
     fileSize()
   ],
-
-  external: ['react', 'react-dom']
+  external: [
+    'd3-array',
+    'd3-color',
+    'd3-dsv',
+    'd3-format',
+    'd3-geo',
+    'd3-scale',
+    'd3-shape',
+    'd3-time',
+    'd3-time-format',
+    'downshift',
+    'glamor',
+    'isomorphic-unfetch',
+    'lodash',
+    'mdast-react-render',
+    'prop-types',
+    'react',
+    'react-maskedinput',
+    'react-textarea-autosize',
+    'scroll-into-view',
+    'topojson'
+  ]
 }
