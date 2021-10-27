a0de01e5fd42d949898762b5cd4228ac0b65ccdd
Tobias Maier <trm217@protonmail.com>
2021-10-27 14:30:49 +0200

    refactor(scripts): move code exucuted in the script to /packages/stylguide

--- packages/styleguide/scripts/sg_link.sh
+++ packages/styleguide/scripts/sg_link.sh
@@ -1,5 +1,7 @@
-rm -rf .next
-npm link @project-r/styleguide
+cd packages/styleguide
+
+# link packages
+yarn link @project-r/styleguide
 rm -rf node_modules/glamor
 rm -rf node_modules/react
 rm -rf node_modules/react-dom
@@ -7,7 +9,4 @@ ln -s @project-r/styleguide/node_modules/glamor node_modules/glamor
 ln -s @project-r/styleguide/node_modules/react node_modules/react
 ln -s @project-r/styleguide/node_modules/react-dom node_modules/react-dom
 
-sed -i.bak 's/sg_link\.sh",/sg_link\.sh","preinstall": "yarn sg:unlink",/g' ./package.json
-rm package.json.bak
-
-echo '\n⚠️  Make sure to restart the next.js server. To unlink run yarn.\n'
+cd ../../
--- packages/styleguide/scripts/sg_unlink.sh
+++ packages/styleguide/scripts/sg_unlink.sh
@@ -1,11 +1,14 @@
-rm -rf .next
+cd packages/styleguide
+
+# wipe linked packages
 rm -rf node_modules/glamor
 rm -rf node_modules/react
 rm -rf node_modules/react-dom
 rm -rf node_modules/@project-r/styleguide
 rm -rf node_modules/.cache
 
-sed -i.bak 's/\s*"preinstall": "npm run sg:unlink",\s*//g' ./package.json
-rm package.json.bak
+cd ../../
+# fetch dependencies again
+yarn install
 
 echo '\n⚠️  Make sure to restart the next.js server.\n'
