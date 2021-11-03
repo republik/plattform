653d8d67a22b5e91d1c27a294a67760156e6a17f
Tobias Maier <trm217@protonmail.com>
2021-11-03 12:13:09 +0100

    chore(build-setup): improve how peerDependencies are read

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import commonjs from '@rollup/plugin-commonjs'
 import { babel } from '@rollup/plugin-babel'
 import fileSize from 'rollup-plugin-filesize'
 import typescript from 'rollup-plugin-typescript2'
+import pkg from './package.json'
 
 export default {
   input: './src/lib.ts',
@@ -39,27 +40,6 @@ export default {
     commonjs(),
     fileSize()
   ],
-  external: [
-    'd3-array',
-    'd3-color',
-    'd3-dsv',
-    'd3-format',
-    'd3-geo',
-    'd3-scale',
-    'd3-shape',
-    'd3-time',
-    'd3-time-format',
-    'downshift',
-    'glamor',
-    'isomorphic-unfetch',
-    'lodash',
-    'mdast-react-render',
-    'prop-types',
-    'react',
-    'react-dom',
-    'react-maskedinput',
-    'react-textarea-autosize',
-    'scroll-into-view',
-    'topojson'
-  ]
+  external:
+    Object.keys(pkg.peerDependencies ?? {})
 }
