2eedbd486d69f876da75e6497d5bafce4f5e9f8e
Felix Michel <felix_michel@gmx.ch>
2021-12-01 15:26:46 +0100

    add tabs to ChartEditor

--- components/editor/modules/chart/EditOverlay/ChartEditor.js
+++ components/editor/modules/chart/EditOverlay/ChartEditor.js
@@ -1,44 +1,124 @@
-import React from 'react'
+import React, { useState, useMemo } from 'react'
 import { tsvParse, csvFormat } from 'd3-dsv'
 import { JSONEditor, PlainEditor } from '../../../utils/CodeEditorFields'
 import SizeSelector from './SizeSelector'
 import WYSIWYGChartEditor from '@project-r/styleguide/lib/components/Chart/Editor'
 
+import { css } from 'glamor'
+import { fontStyles } from '@project-r/styleguide'
+import { useColorContext } from '@project-r/styleguide/lib/components/Colors/useColorContext'
+
+const styles = {
+  orderBy: css({
+    ...fontStyles.sansSerifRegular16,
+    outline: 'none',
+    WebkitAppearance: 'none',
+    background: 'transparent',
+    border: 'none',
+    padding: '0',
+    cursor: 'pointer',
+    marginRight: '20px'
+  }),
+  regular: css({
+    textDecoration: 'none'
+  }),
+  selected: css({
+    textDecoration: 'underline',
+    textDecorationSkip: 'ink'
+  }),
+  tabs: css({
+    margin: '20px 0px'
+  })
+}
+
+const tabs = [
+  {
+    value: 'data',
+    text: 'Daten & JSON'
+  },
+  { value: 'basic', text: 'Grundeinstellungen' },
+  {
+    value: 'advanced',
+    text: 'Erweiterte Optionen'
+  }
+]
+
 const ChartEditor = ({ data, onChange }) => {
+  const [colorScheme] = useColorContext()
+  const [activeTab, setActiveTab] = useState('basic')
+
+  const handleTabClick = item => {
+    setActiveTab(item.target.value)
+  }
+
+  const hoverRule = useMemo(() => {
+    return css({
+      '@media (hover)': {
+        ':hover': {
+          color: colorScheme.getCSSColor('textSoft')
+        }
+      }
+    })
+  }, [colorScheme])
+
   return (
     <>
-      {/* <SizeSelector onChange={onChange} data={data} /> */}
-      <PlainEditor
-        label='CSV Daten'
-        value={data.get('values')}
-        onChange={value => onChange(data.set('values', value))}
-        linesShown={10}
-        onPaste={e => {
-          const clipboardData = e.clipboardData || window.clipboardData
-          let parsedTsv
-          try {
-            parsedTsv = tsvParse(clipboardData.getData('Text'))
-          } catch (e) {}
-          if (parsedTsv && parsedTsv.columns.length > 1) {
-            e.preventDefault()
-            onChange(data.set('values', csvFormat(parsedTsv)))
-          }
-        }}
-      />
-      <WYSIWYGChartEditor
-        data={data.get('values')}
-        value={data.get('config')}
-        onChange={newConfig => {
-          onChange(data.set('config', newConfig))
-        }}
-      />
-      <JSONEditor
-        label='Einstellungen'
-        config={data.get('config')}
-        onChange={newConfig => {
-          onChange(data.set('config', newConfig))
-        }}
-      />
+      <div {...styles.tabs}>
+        {tabs.map(d => {
+          return (
+            <button
+              key={d.value}
+              {...styles.orderBy}
+              {...colorScheme.set('color', 'text')}
+              {...styles[activeTab === d.value ? 'selected' : 'regular']}
+              {...(activeTab !== d.value && hoverRule)}
+              value={d.value}
+              onClick={handleTabClick}
+            >
+              {d.text}
+            </button>
+          )
+        })}
+      </div>
+      <SizeSelector onChange={onChange} data={data} />
+      {activeTab !== 'data' && (
+        <WYSIWYGChartEditor
+          data={data.get('values')}
+          value={data.get('config')}
+          onChange={newConfig => {
+            onChange(data.set('config', newConfig))
+          }}
+          activeTab={activeTab}
+        />
+      )}
+      {activeTab === 'data' && (
+        <>
+          <PlainEditor
+            label='CSV Daten'
+            value={data.get('values')}
+            onChange={value => onChange(data.set('values', value))}
+            linesShown={10}
+            onPaste={e => {
+              const clipboardData = e.clipboardData || window.clipboardData
+              let parsedTsv
+              try {
+                parsedTsv = tsvParse(clipboardData.getData('Text'))
+              } catch (e) {}
+              if (parsedTsv && parsedTsv.columns.length > 1) {
+                e.preventDefault()
+                onChange(data.set('values', csvFormat(parsedTsv)))
+              }
+            }}
+          />
+          <JSONEditor
+            label='Einstellungen'
+            config={data.get('config')}
+            onChange={newConfig => {
+              onChange(data.set('config', newConfig))
+            }}
+          />
+        </>
+      )}
     </>
   )
 }
