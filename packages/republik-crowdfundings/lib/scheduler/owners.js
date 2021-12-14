24dcd5a4c710347f0983d24ee70cde482260520a
Patrick Venetz <venetz@gmail.com>
2021-12-14 08:59:56 +0100

    fix(republik-crowdfundings): Unrwap array values

--- packages/republik-crowdfundings/lib/scheduler/owners.js
+++ packages/republik-crowdfundings/lib/scheduler/owners.js
@@ -213,7 +213,7 @@ const createJobs = (now) => [
       autoPay,
     }) => {
       return (
-        [('ABO', 'BENEFACTOR_ABO')].includes(membershipType) &&
+        ['ABO', 'BENEFACTOR_ABO'].includes(membershipType) &&
         membershipAutoPay === true &&
         autoPay &&
         userId === autoPay.userId
