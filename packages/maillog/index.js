720d9fbd3f3096afd9dbf085539b2ae0d03690bb
Patrick Venetz <venetz@gmail.com>
2022-01-14 19:38:14 +0100

    feat(maillog): Remove schema, resolvers, loaders, lib

--- packages/maillog/index.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  graphql: require('./graphql'),
-}
--- packages/maillog/loaders/MailLog.js
+++
@@ -1,7 +0,0 @@
-const createDataLoader = require('@orbiting/backend-modules-dataloader')
-
-module.exports = (context) => ({
-  byId: createDataLoader((ids) =>
-    context.pgdb.public.mailLog.find({ id: ids }),
-  ),
-})
