5ff48b0131042be74b525e7d2fbf65c32dbca6bc
Olivier Baumann <olivier@iovi.io>
2022-02-09 10:27:36 +0100

    fix(Tile): show padding if outline and bigger

--- packages/styleguide/src/components/TeaserCarousel/Tile.js
+++ packages/styleguide/src/components/TeaserCarousel/Tile.js
@@ -135,7 +135,7 @@ const Tile = ({
       borderWidth: outline && 1,
       borderStyle: outline && 'solid',
       cursor: onClick ? 'pointer' : 'default',
-      padding: bigger ? '0 0 20px 0' : '30px 15px',
+      padding: bigger & !outline ? '0 0 20px 0' : '30px 15px',
       alignItems: bigger ? 'flex-start' : 'center',
     },
     context.tileCount < 3 && { width: `${100 / context.tileCount}%` },
