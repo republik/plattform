0b45841e1951b3d93aa0bae0ab5bdd5de68609dc
Tobias Maier <trm217@protonmail.com>
2022-02-11 13:56:13 +0100

    feat(ApplePay): check Apple Pay availability sync
    Also make sure this is only checked in case the browser is loaded in a secure context

--- apps/www/components/Payment/Form/ApplePay.tsx
+++ apps/www/components/Payment/Form/ApplePay.tsx
@@ -1,13 +1,39 @@
-/**
- * Check if the browser has Apple Pay support
- */
-export const isApplePayAvailable = async () => {
-  if (!ApplePaySession) {
+import { useEffect, useState } from 'react'
+
+// Check if the website is running in a secure document.
+function isInSecureDocument(): boolean {
+  return (
+    typeof window !== 'undefined' && window?.location?.protocol === 'https:'
+  )
+}
+
+// Declare ApplePaySession property to prevent TypeScript errors
+declare global {
+  interface Window {
+    ApplePaySession: ApplePaySession | undefined
+  }
+}
+
+function isApplePayAvailable(): boolean {
+  if (!window?.ApplePaySession) {
     return false
   }
-  const merchantIdentifier = 'example.com.store' // TODO: get from env
-  const canMakePayments = await ApplePaySession.canMakePaymentsWithActiveCard(
-    merchantIdentifier,
-  ).catch(() => false)
-  return canMakePayments
+  return ApplePaySession.canMakePayments()
+}
+
+export const useIsApplePayAvailable = () => {
+  const [isAvailable, setIsAvailable] = useState(
+    isInSecureDocument() && isApplePayAvailable(),
+  )
+
+  useEffect(() => {
+    try {
+      // Only check for Apple Pay on the client and when a secure connection is provided
+      setIsAvailable(isInSecureDocument() && isApplePayAvailable())
+    } catch (error) {
+      console.error('Error checking for Apple Pay availability', error)
+    }
+  }, [])
+
+  return isAvailable
 }
