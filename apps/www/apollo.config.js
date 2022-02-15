b91d084c86a44c42e0a126acaf8f585763e322ef
Tobias Maier <trm217@protonmail.com>
2022-02-15 13:49:46 +0100

    chore(www/Apollo): add apollo and scripts to generate types in app/www

---
+++ apps/www/apollo.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  client: {
+    service: {
+      name: 'republik-backend',
+      url: 'http://localhost:5010/graphql',
+    },
+    includes: [
+      './components/**/*.{ts,tsx}',
+      './lib/**/*.{ts,tsx}',
+      './pages/**/*.{ts,tsx}',
+    ],
+  },
+}
