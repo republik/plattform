de29fd1ea8bbfbfd8b8c484246b1c21c8ac7da62
Olivier Baumann <olivier@iovi.io>
2022-02-21 18:51:11 +0100

    fix(teilen): remove duplicate translations

--- apps/www/pages/teilen.js
+++ apps/www/pages/teilen.js
@@ -7,10 +7,9 @@ import { t } from '../lib/withT'
 import withDefaultSSR from '../lib/hocs/withDefaultSSR'
 import withT from '../lib/withT'
 import { CROWDFUNDING } from '../lib/constants'
-import useInNativeApp from '../lib/withInNativeApp'
 import { withMembership } from '../components/Auth/checkRoles'
 
-import { PackageItem, PackageBuffer } from '../components/Pledge/Accordion'
+import { PackageItem } from '../components/Pledge/Accordion'
 import AccessCampaigns from '../components/Access/Campaigns'
 import ShareChart from '../components/Access/Campaigns/ShareChart'
 import Frame from '../components/Frame'
@@ -25,32 +24,21 @@ const Page = ({ me, t }) => {
 
   return (
     <Frame meta={meta}>
-      <Interaction.H2>
-        <strong>{t('Share/title')}</strong>
-      </Interaction.H2>
-      <Interaction.P>{t('Share/lead')}</Interaction.P>
       {/* e3568e03-b6b3-46c5-b07a-e9afeea92023 "Teilen Sie Ihr Abonnement" */}
       <ShareChart accessCampaignId='e3568e03-b6b3-46c5-b07a-e9afeea92023' />
       <AccessCampaigns />
-      {/* TODO: add prolong*/}
       {!me ? (
         <div style={{ marginTop: 36 }}>
           <Interaction.H2>
-            <strong>Einloggen und Abo schenken</strong>
+            <strong>{t('Account/Access/Campaigns/login/title')}</strong>
           </Interaction.H2>
           <SignIn />
         </div>
       ) : !me.accessCampaigns ? (
         <div style={{ marginTop: 36 }}>
           <Interaction.H2>
-            <strong>Mitglied werden und Abo Teilen</strong>
+            <strong>{t('Account/Access/Campaigns/becomeMamber/title')}</strong>
           </Interaction.H2>
-          <Interaction.P>
-            <strong>
-              Mit einer Republik Mitgliedschaft können Sie ihr Abo für x Wochem
-              mit anderen Leuten teilen
-            </strong>
-          </Interaction.P>
           <Link
             href={{
               pathname: '/angebote',
