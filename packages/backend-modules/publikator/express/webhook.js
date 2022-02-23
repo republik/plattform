f7d95a85941866660a8cfba132277b72710e74dd
Patrick Venetz <venetz@gmail.com>
2022-02-23 21:21:38 +0100

    feat(publikator): add /publikator/webhook/syntheticReadAloud

---
+++ packages/backend-modules/publikator/express/webhook.js
@@ -0,0 +1,50 @@
+const bodyParser = require('body-parser')
+
+// await middleware(server, pgdb, t, redis, createGraphqlContext())
+module.exports = (server, pgdb, t, redis, context) => {
+  // Callback for assets which are ready
+  server.post(
+    '/publikator/webhook/syntheticReadAload', // @TODO: Find better naming than audio
+    bodyParser.json(),
+    async (req, res) => {
+      const { pgdb, loaders } = context
+      console.log('/publikator/webhook/syntheticReadAload', req.body)
+
+      res.status(200).json({ ok: true })
+
+      const { body } = req
+      const { derivativeId, audioDuration, audioUrl, s3 } = body
+
+      const derivative = await pgdb.publikator.derivatives.updateAndGetOne(
+        {
+          id: derivativeId,
+        },
+        {
+          status: 'Ready',
+          result: {
+            audioDuration,
+            audioUrl,
+            s3,
+          },
+          updatedAt: new Date(),
+          readyAt: new Date(),
+        },
+      )
+
+      // @TODO: This might go into seperate lib.
+      // Near duplicate of mutation generateDerivative
+      if (derivative) {
+        const commit = await loaders.Commit.byId.load(derivative.commitId)
+
+        await context.pubsub.publish('repoChange', {
+          repoChange: {
+            repoId: commit.repoId,
+            mutation: 'UPDATED',
+            commit,
+            derivative,
+          },
+        })
+      }
+    },
+  )
+}
