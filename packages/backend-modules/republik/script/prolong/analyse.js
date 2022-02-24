a677e5512b646110bebf1ec17e8834cf6c135557
Thomas Preusse <tpreusse@users.noreply.github.com>
2022-02-24 10:40:05 +0100

    feat(prolong): period change event based analysis

---
+++ packages/backend-modules/republik/script/prolong/analyse.js
@@ -0,0 +1,164 @@
+#!/usr/bin/env node
+require('@orbiting/backend-modules-env').config()
+const {
+  lib: { ConnectionContext },
+} = require('@orbiting/backend-modules-base')
+const { ascending } = require('d3-array')
+const { timeDay } = require('d3-time')
+const { nest } = require('d3-collection')
+const fs = require('fs')
+const path = require('path')
+
+const applicationName = 'backends republik script prolong analyse'
+
+ConnectionContext.create(applicationName)
+  .then(async (context) => {
+    const { pgdb } = context
+
+    const users = await pgdb.query(`
+      SELECT
+        u.id,
+        u."createdAt",
+        json_agg(
+          json_build_object(
+          'membership', m."sequenceNumber",
+          'membershipType', mt.name,
+          'kind', mp.kind,
+          'begin', mp."beginDate",
+          'end', mp."endDate"
+          ) ORDER BY mp."beginDate"
+        ) as "periods"
+      FROM
+        users u
+      JOIN
+        "memberships" m
+        ON m."userId" = u.id
+      JOIN
+        "membershipTypes" mt
+        ON m."membershipTypeId" = mt.id
+      JOIN
+        "membershipPeriods" mp
+        ON mp."membershipId" = m.id
+      WHERE u.email != 'jefferson@project-r.construction'
+      GROUP BY 1
+      -- ORDER BY COUNT(mp.id) DESC
+    `)
+    const endOfRecords = await pgdb.queryOneField(
+      `SELECT MAX(GREATEST("updatedAt", "createdAt")) FROM "membershipPeriods"`,
+    )
+
+    console.log('users', users.length, 'endOfRecords', endOfRecords)
+
+    const usersWithOverlaps = new Set()
+    const usersWithSwallowed = new Set()
+
+    users.forEach((user) => {
+      user.periods.forEach((period, i, all) => {
+        period.beginDate = new Date(period.begin)
+        period.endDate = new Date(period.end)
+        const prevPeriod = all[i - 1]
+        if (prevPeriod && period.beginDate < prevPeriod.endDate) {
+          usersWithOverlaps.add(user)
+          period.beginDate = prevPeriod.endDate
+          if (period.endDate < prevPeriod.endDate) {
+            usersWithSwallowed.add(user)
+            period.endDate = prevPeriod.endDate
+            period.empty = true
+          }
+        }
+      })
+
+      user.events = []
+      user.totalDays = user.periods
+        .filter((period) => !period.empty)
+        .reduce((totalDays, period, i, all) => {
+          const days = timeDay.count(period.beginDate, period.endDate)
+          const prevAge = Math.floor(totalDays / 365)
+          const newAge = Math.floor((totalDays + days) / 365)
+          const prevPeriod = all[i - 1]
+          if (prevPeriod) {
+            const gapless =
+              period.beginDate - prevPeriod.endDate < 1000 * 60 * 60 * 24
+            if (!gapless) {
+              user.events.push({
+                type: 'break',
+                membershipType: prevPeriod.membershipType,
+                date: prevPeriod.endDate,
+                year: prevPeriod.endDate.getFullYear(),
+                age: prevAge,
+              })
+            }
+
+            if (newAge > prevAge) {
+              user.events.push({
+                type: gapless ? 'anniversary' : 'return',
+                membershipType: period.membershipType,
+                date: period.beginDate,
+                year: period.beginDate.getFullYear(),
+                age: prevAge,
+              })
+            }
+          }
+          const nextPeriod = all[i + 1]
+          if (!nextPeriod && period.endDate < endOfRecords) {
+            user.events.push({
+              type: 'break',
+              membershipType: period.membershipType,
+              date: period.endDate,
+              year: period.endDate.getFullYear(),
+              age: newAge,
+            })
+          }
+          return totalDays + days
+        }, 0)
+      // console.log('total days', user.totalDays, user.id)
+      // console.log(user.events)
+      // console.log(user.periods.map(p => [p.membershipType, p.begin, p.end]))
+      // console.log('---')
+    })
+
+    const allEvents = users.map((user) => user.events).flat()
+
+    console.log(
+      JSON.stringify(
+        nest()
+          .key((d) => d.age)
+          .sortKeys(ascending)
+          .key((d) => d.year)
+          .sortKeys(ascending)
+          .key((d) => d.type)
+          .rollup((values) => values.length)
+          .entries(allEvents)
+          .map(({ key, values }) => {
+            return {
+              key,
+              values: values.map(({ key: year, values }) => {
+                const object = values.reduce(
+                  (obj, { key, value }) => {
+                    obj[key] = value
+                    return obj
+                  },
+                  { year },
+                )
+                object.rate =
+                  object.anniversary / (object.break + object.anniversary)
+                return object
+              }),
+            }
+          }),
+        undefined,
+        2,
+      ),
+    )
+
+    console.log('usersWithOverlaps', usersWithOverlaps.size)
+    console.log('usersWithSwallowed', usersWithSwallowed.size)
+
+    fs.writeFileSync(
+      path.resolve(__dirname, '..', 'data', 'prolongUsers.json'),
+      JSON.stringify(users, undefined, 2),
+    )
+
+    return context
+  })
+  .then((context) => ConnectionContext.close(context))
