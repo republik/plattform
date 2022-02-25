7a466509add16f21645528da65faa0c3b4a984d9
Patrick Venetz <venetz@gmail.com>
2022-02-25 17:25:42 +0100

    fix(publikator): refactor publication links

---
+++ apps/publikator/components/Publication/PublicationLink.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import LockIcon from 'react-icons/lib/md/lock'
+import PublicIcon from 'react-icons/lib/md/public'
+
+import { IconButton } from '@project-r/styleguide'
+
+import { FRONTEND_BASE_URL } from '../../lib/settings'
+
+const PublicationLink = ({ publication }) => {
+  if (!publication.live || !publication.document?.meta?.path) {
+    return
+  }
+
+  return (
+    <IconButton
+      Icon={publication.prepublication ? LockIcon : PublicIcon}
+      style={{ marginRight: 0 }}
+      title={publication.document.meta.title}
+      invert
+      label={publication.prepublication ? 'Vorschau öffnen' : 'Beitrag öffnen'}
+      fillColorName='primary'
+      href={`${FRONTEND_BASE_URL}${publication.document.meta.path}`}
+      target='_blank'
+    />
+  )
+}
+
+export default PublicationLink
