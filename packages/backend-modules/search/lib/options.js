92d7e3c66fe6301bb90220a87955eb5eb1004dd0
Patrick Venetz <venetz@gmail.com>
2022-03-03 12:49:20 +0100

    feat(search): introduce search.processor
    a processor may superseed other search query arguments. getting options
    and helper functions moved into a lib. option samples queries for nodes
    but won't create cursors

---
+++ packages/backend-modules/search/lib/options.js
@@ -0,0 +1,106 @@
+const debug = require('debug')('search:lib:options')
+
+const { DOCUMENTS_SAMPLES_REPO_IDS, DOCUMENTS_SAMPLES } = process.env
+
+const processors = {
+  formatFeedSamples: (args, context) => {
+    const { filter } = args
+    const { user } = context
+
+    if (
+      DOCUMENTS_SAMPLES_REPO_IDS &&
+      !user?.roles?.includes('member') &&
+      filter?.format &&
+      DOCUMENTS_SAMPLES_REPO_IDS.split(',').includes(filter?.format)
+    ) {
+      return {
+        filters: [
+          { key: 'format', value: filter.format },
+          { key: 'feed', value: true },
+          { key: 'type', value: 'Document' },
+        ],
+        sort: { key: 'publishedAt', direction: 'DESC' },
+        samples: DOCUMENTS_SAMPLES,
+      }
+    }
+  },
+}
+
+const encodeCursor = (options) => {
+  return Buffer.from(
+    JSON.stringify({
+      ...options,
+
+      // drop undesirable options
+      processor: undefined,
+      after: undefined,
+      before: undefined,
+      filter: undefined,
+      trackingId: undefined,
+    }),
+  ).toString('base64')
+}
+
+// Parses a base64 encoded cursor (e.g. {after}, {before}).
+// It won't contain any nullish props due to JSON stringify/parse.
+const decodeCursor = (cursor) => {
+  try {
+    if (!cursor) {
+      return false
+    }
+
+    return JSON.parse(Buffer.from(cursor, 'base64').toString())
+  } catch (e) {
+    console.warn('failed to parse cursor:', cursor, e)
+    return false
+  }
+}
+
+// Returns args as options w/o nullish props.
+const cleanArgs = (args) =>
+  Object.keys(args)
+    .filter((key) => args[key] ?? false)
+    .reduce((prev, curr) => ({ ...prev, [curr]: args[curr] }), {})
+
+// Processes search {args} and returns reasonable {options}.
+// It removes nullish props.
+const processArgs = (args, context) =>
+  processors?.[args?.processor]?.(args, context) || cleanArgs(args)
+
+// Returns options
+const getOptions = (args, context) => {
+  const { after, before } = args
+
+  const defaults = {
+    filter: {},
+    filters: [],
+    sort: { key: 'relevance', direction: 'DESC' },
+    first: 40,
+    from: 0,
+    samples: false,
+  }
+
+  /**
+   * If cursors {after} (1) or {before} (2) are provided, their contents
+   * replace options provided: Changing search term or {first} won't have
+   * any effect.
+   */
+  const options =
+    decodeCursor(after) || decodeCursor(before) || processArgs(args, context)
+
+  debug('getOptions: %o', {
+    ...defaults,
+    ...options,
+  })
+
+  return {
+    ...defaults,
+    ...options,
+  }
+}
+
+module.exports = {
+  encodeCursor,
+  getOptions,
+  getProcessorsList: () => Object.keys(processors),
+}
