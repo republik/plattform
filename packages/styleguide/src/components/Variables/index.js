df5ca95e03097392ef8f46f6f4e4f55999e13262
Thomas Preusse <tpreusse@users.noreply.github.com>
2022-03-05 00:58:20 +0100

    fix(newsletters): avoid «Invalid Merge Tag Content» with empty blocks

--- packages/styleguide/src/components/Variables/index.js
+++ packages/styleguide/src/components/Variables/index.js
@@ -2,11 +2,16 @@ import React, { useContext, Children } from 'react'
 
 export const VariableContext = React.createContext({})
 
+// Note: spaces around merge tag conditions are needed
+// to ensure correct parsing on mailchimp in all cases
+// other wise e.g. empty if block with just content in
+// the else case cause «Invalid Merge Tag Content»
+
 export const Variable = ({ variable, fallback }) => {
   const vars = useContext(VariableContext)
   if (vars._mergeTags) {
     return fallback
-      ? `*|IF:${vars[variable]}|* *|${vars[variable]}|* *|ELSE:|* ${fallback} *|END:IF|*`
+      ? ` *|IF:${vars[variable]}|* *|${vars[variable]}|* *|ELSE:|* ${fallback} *|END:IF|* `
       : `*|${vars[variable]}|*`
   }
   return vars[variable] || fallback || null
@@ -18,17 +23,17 @@ export const If = ({ present, children }) => {
     if (vars.groups?.[present]) {
       return (
         <>
-          {`*|INTERESTED:${vars.groups[present]}|*`}
+          {` *|INTERESTED:${vars.groups[present]}|* `}
           {children}
-          {'*|END:INTERESTED|*'}
+          {' *|END:INTERESTED|* '}
         </>
       )
     }
     return (
       <>
-        {`*|IF:${vars[present] || present}|*`}
+        {` *|IF:${vars[present] || present}|* `}
         {children}
-        {'*|END:IF|*'}
+        {' *|END:IF|* '}
       </>
     )
   }
@@ -44,7 +49,7 @@ export const Else = ({ children }) => {
   const vars = useContext(VariableContext)
   return (
     <>
-      {vars._mergeTags && '*|ELSE:|*'}
+      {vars._mergeTags && ' *|ELSE:|* '}
       {children}
     </>
   )
