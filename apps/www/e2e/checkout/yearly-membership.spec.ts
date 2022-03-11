d5a6a0e2a5e21e54ea3ef89dc69e64ab51ce50d0
Tobias Maier <trm217@protonmail.com>
2022-03-11 17:05:14 +0100

    feat(Playwright): add checkout e2e test

---
+++ apps/www/e2e/checkout/yearly-membership.spec.ts
@@ -0,0 +1,46 @@
+import { test, expect } from '@playwright/test'
+
+function getUserInput() {
+  const date = new Date(Date.now())
+  const year = date.getFullYear() + 1 // next year
+
+  return {
+    userInfo: {
+      email: `max.muster+${Date.now()}@example.com`,
+      firstName: 'Max',
+      lastName: 'Muster',
+    },
+    card: {
+      cardNumber: '4242 4242 4242 4242',
+      expiry: `01 / ${year.toString().slice(-2)}`,
+      cvc: '123',
+    },
+  }
+}
+
+test('can checkout with valid input', async ({ page }) => {
+  const userInput = getUserInput()
+
+  await page.goto('/angebote?package=ABO')
+
+  for (const [key, value] of Object.entries(userInput.userInfo)) {
+    await page.fill(`input[name="${key}"]`, value)
+  }
+
+  // Initialize Stripe by clicking on card field
+  await page.locator('text=Kreditkartennummer').click()
+  console.log(userInput.card.expiry)
+  for (const [key, value] of Object.entries(userInput.card)) {
+    await page
+      .frameLocator(`.StripeElement-${key} iframe`)
+      .locator('input[aria-label]')
+      .fill(value)
+  }
+
+  // Accept AGB
+  await page.locator('div:nth-child(8) label span span >> nth=0').click()
+
+  await page.locator('text=CHF 240 bezahlen').click()
+
+  await page.locator('text=Zugriff via E-Mail bestätigen')
+})
--- apps/www/playwright.config.ts
+++ apps/www/playwright.config.ts
@@ -1,17 +1,17 @@
 import type { PlaywrightTestConfig } from '@playwright/test'
 import { devices } from '@playwright/test'
-
+import dotenv from 'dotenv'
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// require('dotenv').config();
+dotenv.config()
 
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
 const config: PlaywrightTestConfig = {
-  testDir: './test',
+  testDir: './e2e',
   /* Maximum time one test can run for. */
   timeout: 30 * 1000,
   expect: {
@@ -34,10 +34,10 @@ const config: PlaywrightTestConfig = {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: process.env.PLAYWRIGHT_BASEURL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'retain-on-failure',
   },
 
   /* Configure projects for major browsers */
@@ -76,7 +76,7 @@ const config: PlaywrightTestConfig = {
   ],
 
   /* Folder for test artifacts such as screenshots, videos, traces, etc. */
-  // outputDir: 'test-results/',
+  // outputDir: 'playwright-results/',
 
   /* Run your local dev server before starting the tests */
   // webServer: {
