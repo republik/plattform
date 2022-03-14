a4c1b7ab52c37817fe05c5f08466e24e0c654a78
Patrick Venetz <venetz@gmail.com>
2022-03-14 14:30:20 +0100

    feat(publikator): disable cache if context.scope isn't request

--- packages/backend-modules/publikator/loaders/Derivative.ts
+++ packages/backend-modules/publikator/loaders/Derivative.ts
@@ -19,14 +19,17 @@ export interface DerivativeRow {
 export default module.exports = function (context: GraphqlContext) {
   const derivatives: PgTable<DerivativeRow> =
     context.pgdb.publikator.derivatives
+  const cache: boolean = context.scope === 'request'
   return {
-    byId: createDataLoader((ids: readonly string[]) =>
-      derivatives.find({ id: ids }, { orderBy: { createdAt: 'desc' } }),
+    byId: createDataLoader(
+      (ids: readonly string[]) =>
+        derivatives.find({ id: ids }, { orderBy: { createdAt: 'desc' } }),
+      { cache },
     ),
     byCommitId: createDataLoader(
       (ids: readonly string[]) =>
         derivatives.find({ commitId: ids }, { orderBy: { createdAt: 'desc' } }),
-      null,
+      { cache },
       (key, rows) => rows.filter((row) => row.commitId === key),
     ),
   }
