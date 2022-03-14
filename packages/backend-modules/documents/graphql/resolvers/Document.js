20e436065721ad977c50de811ce903b9356856e9
Patrick Venetz <venetz@gmail.com>
2022-03-14 09:15:33 +0100

    feat(documents): process IF hasAccess in document.content

--- packages/backend-modules/documents/graphql/resolvers/Document.js
+++ packages/backend-modules/documents/graphql/resolvers/Document.js
@@ -7,6 +7,7 @@ const {
   processRepoImageUrlsInMeta,
   processEmbedImageUrlsInContent,
   processNodeModifiersInContent,
+  processIfHasAccess,
 } = require('../../lib/process')
 const { getMeta } = require('../../lib/meta')
 
@@ -78,6 +79,7 @@ module.exports = {
 
       processMembersOnlyZonesInContent(doc.content, context.user)
       processNodeModifiersInContent(doc.content, context.user)
+      processIfHasAccess(doc.content, context.user)
     }
     return doc.content
   },
@@ -158,6 +160,7 @@ module.exports = {
 
         processMembersOnlyZonesInContent(node, context.user)
         processNodeModifiersInContent(node, context.user)
+        processIfHasAccess(node, context.user)
 
         return extractIdsFromNode(node, doc.meta.repoId)
       })
