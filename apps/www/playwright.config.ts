32d1dde4cf321ece4bfe5b3e1668ca09949879d6
Tobias Maier <trm217@protonmail.com>
2022-03-16 09:04:12 +0100

    chore(Playwright): move test to root of e2e folder
    Revert "chore(Playwright): move test to root of e2e folder"
    
    This reverts commit 93820d752c483381ccfd707d47990dc07b9d68b2.
    
    chore(Playwright): move test into separate testfile
    
    chore(Playwright): update reporter

--- apps/www/playwright.config.ts
+++ apps/www/playwright.config.ts
@@ -28,8 +28,7 @@ const config: PlaywrightTestConfig = {
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  reporter: process.env.CI ? 'github' : 'list',
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
