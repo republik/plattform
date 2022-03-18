5abd1d37d8c12ec9908c76e77687f266885bf816
Tobias Maier <trm217@protonmail.com>
2022-03-18 15:54:11 +0100

    refactor(PayWithWallet): update use-effect hooks inside usePaymentRequest

--- apps/www/components/Payment/PaymentRequest/usePaymentRequest.ts
+++ apps/www/components/Payment/PaymentRequest/usePaymentRequest.ts
@@ -1,4 +1,10 @@
-import { Dispatch, SetStateAction, useEffect, useState } from 'react'
+import {
+  Dispatch,
+  SetStateAction,
+  useCallback,
+  useEffect,
+  useState,
+} from 'react'
 import {
   Stripe,
   PaymentRequest,
@@ -73,11 +79,13 @@ interface PaymentRequestValues {
  * Hook used to wrap a Stripe.js payment-request object
  * Upon the first initialization stripe is lazy-loaded.
  *
+ * @param selectedPaymentMethod
+ * @param setSelectedPaymentMethod
  * @param options used to initialize the payment request
  */
 function usePaymentRequest({
   selectedPaymentMethod,
-  setSelectedPaymentMethod,
+  setSelectedPaymentMethod: setSelectedPaymentMethod,
   options,
 }: PaymentRequestParameterObject): PaymentRequestValues {
   const [stripe, setStripe] = useState<Stripe>(null)
@@ -96,134 +104,61 @@ function usePaymentRequest({
 
   const { t } = useTranslation()
 
-  useEffect(() => {
-    // In case the input options have changed (f.e. shipping address is now required)
-    // Reinitialize the payment request
-    if (
-      paymentRequest &&
-      JSON.stringify(lastOptions) !== JSON.stringify(options)
-    ) {
-      setLastOptions(options)
-      initializePaymentRequest(usedWallet)
-    }
-  }, [paymentRequest, lastOptions, options])
-
-  useEffect(() => {
-    const selectPMIsStripeWallet =
-      selectedPaymentMethod && selectedPaymentMethod.startsWith('STRIPE-WALLET')
-
-    const isUninitialized =
-      selectPMIsStripeWallet && status === PaymentRequestStatus.IDLE
-
-    const initializedWalletIsOutdated =
-      selectPMIsStripeWallet && usedWallet !== selectedPaymentMethod
-
-    const stripeWalletIsUnavailable =
-      selectPMIsStripeWallet && status === PaymentRequestStatus.UNAVAILABLE
-
-    // Handle (re-)initialization of the payment request or switching payment method if unavailable
-    if (
-      (isUninitialized || initializedWalletIsOutdated) &&
-      !stripeWalletIsUnavailable
-    ) {
-      initializePaymentRequest(selectedPaymentMethod as WalletPaymentMethod)
-        .then((status) => {
-          if (status === PaymentRequestStatus.UNAVAILABLE) {
-            setSelectedPaymentMethod('STRIPE')
-            setErrors({
-              stripeError: t(
-                'account/pledges/payment/methods/wallet-unavailable',
-                {
-                  wallet: t(
-                    'account/pledges/payment/method/' + selectedPaymentMethod,
-                  ),
-                },
-              ),
-              walletError: t('account/pledges/payment/methods/unavailable'),
-            })
-          } else {
-            setErrors({
-              stripeError: null,
-              walletError: null,
-            })
-          }
-        })
-        .catch((error) => {
-          console.error('Wallet initialization error', error)
-          setErrors({
-            stripeError: null,
-            walletError: t(
-              'account/pledges/payment/methods/initialization/error',
-            ),
-          })
-        })
-    }
-
-    if (stripeWalletIsUnavailable) {
+  const initializePaymentRequest = useCallback(
+    async (wallet: WalletPaymentMethod): Promise<PaymentRequestStatus> => {
+      setStatus(PaymentRequestStatus.LOADING)
       setErrors({
+        walletError: null,
         stripeError: null,
-        walletError: t('account/pledges/payment/methods/unavailable'),
       })
-    }
-  }, [selectedPaymentMethod, status, usedWallet])
+      let stripePromise = stripe
+      if (!stripe) {
+        const globalStripePromise = await loadStripe()
+        setStripe(globalStripePromise)
+        stripePromise = globalStripePromise
+      }
 
-  async function createPaymentRequest(
-    wallet: WalletPaymentMethod,
-  ): Promise<PaymentRequestStatus> {
-    let stripePromise = stripe
-    if (!stripe) {
-      const globalStripePromise = await loadStripe()
-      setStripe(globalStripePromise)
-      stripePromise = globalStripePromise
-    }
+      const newPaymentRequest = await stripePromise.paymentRequest(
+        makePaymentRequestOptions(
+          options,
+          getPaymentRequestWalletValue(wallet),
+        ),
+      )
 
-    const newPaymentRequest = await stripePromise.paymentRequest(
-      makePaymentRequestOptions(options, getPaymentRequestWalletValue(wallet)),
-    )
+      // Track used options to prevent useless re-initializations
+      setLastOptions(options)
+      // Track the wallet in case the previous payment request was initialized with a different one
+      setUsedWallet(wallet)
 
-    // Track used options to prevent useless re-initializations
-    setLastOptions(options)
-    // Track the wallet in case the previous payment request was initialized with a different one
-    setUsedWallet(wallet)
+      const canMakePaymentResult = await newPaymentRequest.canMakePayment()
 
-    const canMakePaymentResult = await newPaymentRequest.canMakePayment()
+      const initializationStage = paymentRequest
+        ? 're-initialization'
+        : 'initialization'
 
-    const initializationStage = paymentRequest
-      ? 're-initialization'
-      : 'initialization'
+      if (!canMakePaymentResult) {
+        setStatus(PaymentRequestStatus.UNAVAILABLE)
+        trackEvent([
+          'payment-request',
+          `${initializationStage} failed`,
+          wallet,
+          options.total.amount / 100,
+        ])
+        return PaymentRequestStatus.UNAVAILABLE
+      }
 
-    if (!canMakePaymentResult) {
-      setStatus(PaymentRequestStatus.UNAVAILABLE)
+      setPaymentRequest(newPaymentRequest)
+      setStatus(PaymentRequestStatus.READY)
       trackEvent([
         'payment-request',
-        `${initializationStage} failed`,
+        `${initializationStage} successful`,
         wallet,
         options.total.amount / 100,
       ])
-      return PaymentRequestStatus.UNAVAILABLE
-    }
-
-    setPaymentRequest(newPaymentRequest)
-    setStatus(PaymentRequestStatus.READY)
-    trackEvent([
-      'payment-request',
-      `${initializationStage} successful`,
-      wallet,
-      options.total.amount / 100,
-    ])
-    return PaymentRequestStatus.READY
-  }
-
-  async function initializePaymentRequest(
-    wallet: WalletPaymentMethod,
-  ): Promise<PaymentRequestStatus> {
-    setStatus(PaymentRequestStatus.LOADING)
-    setErrors({
-      walletError: null,
-      stripeError: null,
-    })
-    return createPaymentRequest(wallet)
-  }
+      return PaymentRequestStatus.READY
+    },
+    [options, paymentRequest, stripe],
+  )
 
   function show(
     handlePayment: PaymentHandler,
@@ -264,12 +199,93 @@ function usePaymentRequest({
 
       // The result of the following reinitialization is ignored
       // since we can only get to this point if it was successful before
-      createPaymentRequest(usedWallet)
+      initializePaymentRequest(usedWallet)
     })
 
     paymentRequest.show()
     setStatus(PaymentRequestStatus.SHOWING)
   }
+
+  useEffect(() => {
+    const selectPMIsStripeWallet =
+      selectedPaymentMethod && selectedPaymentMethod.startsWith('STRIPE-WALLET')
+
+    const isUninitialized =
+      selectPMIsStripeWallet && status === PaymentRequestStatus.IDLE
+
+    const initializedWalletIsOutdated =
+      selectPMIsStripeWallet && usedWallet !== selectedPaymentMethod
+
+    const stripeWalletIsUnavailable =
+      selectPMIsStripeWallet && status === PaymentRequestStatus.UNAVAILABLE
+
+    const optionsUsedToInitializeHaveChanged =
+      selectPMIsStripeWallet &&
+      JSON.stringify(lastOptions) !== JSON.stringify(options)
+
+    if (status === PaymentRequestStatus.LOADING) {
+      console.debug('Payment request is still loading')
+      return
+    }
+    console.debug('running hook')
+    // Handle (re-)initialization of the payment request or switching payment method if unavailable
+    if (
+      (isUninitialized ||
+        initializedWalletIsOutdated ||
+        optionsUsedToInitializeHaveChanged) &&
+      !stripeWalletIsUnavailable
+    ) {
+      console.debug('reinitializing payment request')
+      initializePaymentRequest(selectedPaymentMethod as WalletPaymentMethod)
+        .then((status) => {
+          if (status === PaymentRequestStatus.UNAVAILABLE) {
+            setSelectedPaymentMethod('STRIPE')
+            setErrors({
+              stripeError: t(
+                'account/pledges/payment/methods/wallet-unavailable',
+                {
+                  wallet: t(
+                    'account/pledges/payment/method/' + selectedPaymentMethod,
+                  ),
+                },
+              ),
+              walletError: t('account/pledges/payment/methods/unavailable'),
+            })
+          } else {
+            setErrors({
+              stripeError: null,
+              walletError: null,
+            })
+          }
+        })
+        .catch((error) => {
+          console.error('Wallet initialization error', error)
+          setErrors({
+            stripeError: null,
+            walletError: t(
+              'account/pledges/payment/methods/initialization/error',
+            ),
+          })
+        })
+    }
+
+    if (stripeWalletIsUnavailable && !errors.walletError) {
+      setErrors({
+        stripeError: null,
+        walletError: t('account/pledges/payment/methods/unavailable'),
+      })
+    }
+  }, [
+    errors.walletError,
+    initializePaymentRequest,
+    lastOptions,
+    options,
+    selectedPaymentMethod,
+    setSelectedPaymentMethod,
+    status,
+    t,
+    usedWallet,
+  ])
 
   return {
     status,
