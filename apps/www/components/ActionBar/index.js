41af590838eb77cba568cfa9c687bc66c7b2ed66
Tobias Maier <trm217@protonmail.com>
2022-03-23 08:51:09 +0100

    refactor(www/ActionBar): show single share button in article-bottom

--- apps/www/components/ActionBar/index.js
+++ apps/www/components/ActionBar/index.js
@@ -352,7 +352,7 @@ const ActionBar = ({
         }
       },
       label: !forceShortLabel ? t('article/actionbar/share') : '',
-      modes: ['articleTop', 'articleOverlay'],
+      modes: ['articleTop', 'articleOverlay', 'articleBottom'],
       show: true,
     },
     {
@@ -468,7 +468,7 @@ const ActionBar = ({
           ))}
         </div>
       )}
-      {(mode === 'articleBottom' || mode === 'seriesOverviewBottom') && (
+      {mode === 'seriesOverviewBottom' && (
         <>
           {!inNativeApp ? (
             <Interaction.P style={{ marginTop: 24 }}>
