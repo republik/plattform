a51e3a835e8194b50be741162bde4c3da4581c06
Tobias Maier <trm217@protonmail.com>
2022-03-25 13:44:00 +0100

    refactor(publikator/test): refactor headline serializer to use jest

--- apps/publikator/components/editor/modules/headline/serializer.test.js
+++ apps/publikator/components/editor/modules/headline/serializer.test.js
@@ -1,75 +1,73 @@
-import test from 'tape'
 import createHeadlineModule from './'
 import { parse, stringify } from '@orbiting/remark-preset'
 
-test('h1 serialization', (assert) => {
-  const module = createHeadlineModule({
-    TYPE: 'H1',
-    rule: {
-      editorOptions: {
-        depth: 1,
+describe('headline serializer test-suite', () => {
+  it('h1 serialization', () => {
+    const module = createHeadlineModule({
+      TYPE: 'H1',
+      rule: {
+        editorOptions: {
+          depth: 1,
+        },
       },
-    },
-    subModules: [],
+      subModules: [],
+    })
+
+    const serializer = module.helpers.serializer
+
+    const value = serializer.deserialize(parse('# Test'))
+    const node = value.document.nodes.first()
+
+    expect(node.kind).toBe('block')
+    expect(node.type).toBe('H1')
+    expect(node.text).toBe('Test')
+
+    expect(stringify(serializer.serialize(value)).trimRight()).toBe('# Test')
   })
 
-  const serializer = module.helpers.serializer
-
-  const value = serializer.deserialize(parse('# Test'))
-  const node = value.document.nodes.first()
-
-  assert.equal(node.kind, 'block')
-  assert.equal(node.type, 'H1')
-  assert.equal(node.text, 'Test')
-
-  assert.equal(stringify(serializer.serialize(value)).trimRight(), '# Test')
-  assert.end()
-})
-
-test('h2 serialization', (assert) => {
-  const module = createHeadlineModule({
-    TYPE: 'H2',
-    rule: {
-      editorOptions: {
-        depth: 2,
+  it('h2 serialization', () => {
+    const module = createHeadlineModule({
+      TYPE: 'H2',
+      rule: {
+        editorOptions: {
+          depth: 2,
+        },
       },
-    },
-    subModules: [],
+      subModules: [],
+    })
+
+    const serializer = module.helpers.serializer
+
+    const value = serializer.deserialize(parse('## Test'))
+    const node = value.document.nodes.first()
+
+    expect(node.kind).toBe('block')
+    expect(node.type).toBe('H2')
+    expect(node.text).toBe('Test')
+
+    expect(stringify(serializer.serialize(value)).trimRight()).toBe('## Test')
   })
 
-  const serializer = module.helpers.serializer
-
-  const value = serializer.deserialize(parse('## Test'))
-  const node = value.document.nodes.first()
-
-  assert.equal(node.kind, 'block')
-  assert.equal(node.type, 'H2')
-  assert.equal(node.text, 'Test')
-
-  assert.equal(stringify(serializer.serialize(value)).trimRight(), '## Test')
-  assert.end()
-})
-
-test('h3 serialization', (assert) => {
-  const module = createHeadlineModule({
-    TYPE: 'H3',
-    rule: {
-      editorOptions: {
-        depth: 3,
+  it('h3 serialization', () => {
+    const module = createHeadlineModule({
+      TYPE: 'H3',
+      rule: {
+        editorOptions: {
+          depth: 3,
+        },
       },
-    },
-    subModules: [],
+      subModules: [],
+    })
+
+    const serializer = module.helpers.serializer
+
+    const value = serializer.deserialize(parse('### Test'))
+    const node = value.document.nodes.first()
+
+    expect(node.kind).toBe('block')
+    expect(node.type).toBe('H3')
+    expect(node.text).toBe('Test')
+
+    expect(stringify(serializer.serialize(value)).trimRight()).toBe('### Test')
   })
-
-  const serializer = module.helpers.serializer
-
-  const value = serializer.deserialize(parse('### Test'))
-  const node = value.document.nodes.first()
-
-  assert.equal(node.kind, 'block')
-  assert.equal(node.type, 'H3')
-  assert.equal(node.text, 'Test')
-
-  assert.equal(stringify(serializer.serialize(value)).trimRight(), '### Test')
-  assert.end()
 })
