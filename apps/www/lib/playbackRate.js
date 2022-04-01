75af3c85e96f361b56017c22100a5e0394fdc3ac
Thomas Preusse <tpreusse@users.noreply.github.com>
2022-04-01 15:21:05 +0200

    fix(Media): playback rate local storage key spelling
    BREAKING CHANGE: I don't want to write a local storage migration

--- apps/www/lib/playbackRate.js
+++ apps/www/lib/playbackRate.js
@@ -1,4 +1,4 @@
 import createPersistedState from './hooks/use-persisted-state'
 
-export const PLAYBACK_RATE_KEY = 'republik-plyaback-rate'
+export const PLAYBACK_RATE_KEY = 'republik-playback-rate'
 export const usePlaybackRate = createPersistedState(PLAYBACK_RATE_KEY)
