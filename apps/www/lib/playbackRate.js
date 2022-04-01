cbeb2e69ae337710bd8e8f4423e9780f4bc77af6
Thomas Preusse <tpreusse@users.noreply.github.com>
2022-04-01 18:01:37 +0200

    feat(Media): migration for misspelled local storage key

--- apps/www/lib/playbackRate.js
+++ apps/www/lib/playbackRate.js
@@ -2,3 +2,16 @@ import createPersistedState from './hooks/use-persisted-state'
 
 export const PLAYBACK_RATE_KEY = 'republik-playback-rate'
 export const usePlaybackRate = createPersistedState(PLAYBACK_RATE_KEY)
+
+// MIGRATION ADDED ON 01.04.2022
+// - can be removed after 7 days
+if (typeof window !== undefined) {
+  try {
+    const oldKey = 'republik-plyaback-rate'
+    const oldItem = window.localStorage.getItem(oldKey)
+    if (oldItem) {
+      window.localStorage.setItem(PLAYBACK_RATE_KEY, oldItem)
+      window.localStorage.removeItem(oldKey)
+    }
+  } catch (e) {}
+}
