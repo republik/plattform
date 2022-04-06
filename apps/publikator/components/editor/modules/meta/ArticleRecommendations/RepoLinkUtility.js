d3614deff0cb50e7841d0efc2aa810fa005b9773
Tobias Maier <trm217@protonmail.com>
2022-04-06 11:47:37 +0200

    fix(publikator/ArticleRecommendations): add undefined checks to RepoLink util

--- apps/publikator/components/editor/modules/meta/ArticleRecommendations/RepoLinkUtility.js
+++ apps/publikator/components/editor/modules/meta/ArticleRecommendations/RepoLinkUtility.js
@@ -1,12 +1,12 @@
 const GITHUB_PREFIX = 'https://github.com/'
 
 export function createAbsolutRepoUrl(repoId) {
-  if (!repoId.startsWith(GITHUB_PREFIX)) {
+  if (repoId && !repoId.startsWith(GITHUB_PREFIX)) {
     return GITHUB_PREFIX + repoId
   }
   return repoId
 }
 
 export function createRelativeRepoUrl(repoId) {
-  return repoId.replace(GITHUB_PREFIX, '')
+  return repoId?.replace(GITHUB_PREFIX, '')
 }
