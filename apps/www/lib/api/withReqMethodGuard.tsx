8cdb77a5d37388df61b7b672a88c8fcb4e9b8996
Tobias Maier <trm217@protonmail.com>
2022-05-06 11:00:18 +0200

    refactor(www/api-route): remove generics in withReqMethodGuard

--- apps/www/lib/api/withReqMethodGuard.tsx
+++ apps/www/lib/api/withReqMethodGuard.tsx
@@ -1,27 +1,23 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import HTTPMethods from './HTTPMethods'
 
-type APIFunction = <T = unknown>(
-  req: NextApiRequest,
-  res: NextApiResponse<T>,
-) => void
+type APIFunction = (req: NextApiRequest, res: NextApiResponse) => void
 
 /**
  * Wraps a Next.js api-function and rejects requests
  * @constructor
  */
-function withReqMethodGuard<T = unknown>(
+function withReqMethodGuard(
   handler: APIFunction,
   acceptedMethods: HTTPMethods[],
 ): APIFunction {
   return async (req: NextApiRequest, res: NextApiResponse) => {
     // If the request method is not accepted, reject the request
     console.log('req.method', req.method)
+    // HTTPMethod has string values therefor we can use `as HTTPMethods`
     if (!req.method || !acceptedMethods.includes(req.method as HTTPMethods)) {
-      console.log('Method guard hit')
       return res.status(405).end()
     } else {
-      console.log('Method guard passed')
       return handler(req, res)
     }
   }
