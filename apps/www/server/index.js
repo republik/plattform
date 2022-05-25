0408133f5ba48d2d4b0dff36767ee7059c116ec6
Tobias Maier <trm217@protonmail.com>
2022-05-25 11:28:02 +0200

    refactor(www/express-server): use COOKIE_NAME in rate-limiter

--- apps/www/server/index.js
+++ apps/www/server/index.js
@@ -25,7 +25,7 @@ if (!PUBLIC_BASE_URL) {
 const app = next({
   dev: DEV,
   port: PORT,
-  hostname: new URL(PUBLIC_BASE_URL).hostname
+  hostname: new URL(PUBLIC_BASE_URL).hostname,
 })
 const handler = app.getRequestHandler()
 
@@ -174,7 +174,7 @@ app.prepare().then(() => {
         } = req
 
         // If user is logged in, 20 requests per minute are allowed. Otherwise, only 5 requests/min allowed.
-        if (cookie && cookie.includes('connect.sid')) {
+        if (cookie && cookie.includes(process.env.COOKIE_NAME)) {
           return 20
         }
         return 5
