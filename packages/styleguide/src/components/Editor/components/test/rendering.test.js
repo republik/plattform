66999b86f3e58c3512800f14ae6b47cca1d33222
Anna Traussnig <anna.traussnig@gmail.com>
2022-05-25 11:56:11 +0200

    feat(Editor): blockCode config

--- packages/styleguide/src/components/Editor/components/test/rendering.test.js
+++ packages/styleguide/src/components/Editor/components/test/rendering.test.js
@@ -4,10 +4,9 @@ import renderAsText from '../render/text'
 describe('Slate Rendering', () => {
   describe('convertToString()', () => {
     it('should work', async () => {
-      const value = tree1
-      const text = renderAsText(value)
+      const text = renderAsText(tree1)
       expect(text).toEqual(
-        'La vie de château Once upon a time, in a small castle, lived a bold lady. She was responsible for: the stables the dragons the chicken Basically lots of shit As the peasants say (True that.) Read her story here.',
+        'La vie de château Once upon a time, in a small castle, lived a bold lady. She was responsible for: the stables the dragons the chicken Basically lots of shit As the peasants say (True that.) Read her story here. The story machine says goodbye.',
       )
     })
   })
--- packages/styleguide/src/components/Editor/config/elements/blockCode.ts
+++ packages/styleguide/src/components/Editor/config/elements/blockCode.ts
@@ -2,4 +2,5 @@ import { ElementConfigI } from '../../custom-types'
 
 export const config: ElementConfigI = {
   component: 'blockCode',
+  structure: [{ type: ['text', 'break'], repeat: true }],
 }
