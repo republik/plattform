00094d026313e9555a419ce646a03f0c751f4917
Thomas Preusse <tpreusse@users.noreply.github.com>
2022-05-27 14:25:36 +0200

    fix: cookie name mishap in www

--- apps/www/lib/auth/constants.js
+++ apps/www/lib/auth/constants.js
@@ -1,4 +1,4 @@
-const COOKIE_NAME = process.env.COOKIE_DOMAIN || 'connect.sid'
+const COOKIE_NAME = process.env.COOKIE_NAME || 'connect.sid'
 const JWT_COOKIE_NAME = process.env.JWT_COOKIE_NAME || 'user-token'
 
 // also used in server/index.js without es module support
