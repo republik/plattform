2e494717727846eeecf7f015a375c6b23a94dcbb
Tobias Maier <trm217@protonmail.com>
2022-06-01 15:36:00 +0200

    refactor(admin): add placeholder test so that test script doesn't fail since no test found

---
+++ apps/admin/lib/placeholder.test.js
@@ -0,0 +1,6 @@
+// This test is needed to ensure that the test script does not enter with status code 1
+// Which it does when no tests are found.
+// Delete once more tests have been added.
+test('placeholder', () => {
+  expect(true).toBe(true)
+})
