ef10562de9bf3e5415a02f25b9870c343e441abb
Tobias Maier <trm217@protonmail.com>
2022-06-15 11:55:35 +0200

    chore(admin): setup analyze script

--- apps/admin/next.config.js
+++ apps/admin/next.config.js
@@ -1,22 +1,28 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
 const withTM = require('next-transpile-modules')(['@project-r/styleguide'])
 
-module.exports = withTM({
-  poweredByHeader: false,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/users',
-        permanent: false,
-      },
-      {
-        source: '/~:userId',
-        destination: '/users/:userId',
-        permanent: false,
-      },
-    ]
-  },
-})
+module.exports = withTM(
+  withBundleAnalyzer({
+    poweredByHeader: false,
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+    async redirects() {
+      return [
+        {
+          source: '/',
+          destination: '/users',
+          permanent: false,
+        },
+        {
+          source: '/~:userId',
+          destination: '/users/:userId',
+          permanent: false,
+        },
+      ]
+    },
+  }),
+)
