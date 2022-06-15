0f67e18f2cc1a8b6a3689f79ca8550f2c421e64b
Tobias Maier <trm217@protonmail.com>
2022-06-15 13:21:05 +0200

    feat(nextjs-apollo-config): add graphql as external dependency and setup bunde-analysis tool

--- packages/nextjs-apollo-client/vite.config.ts
+++ packages/nextjs-apollo-client/vite.config.ts
@@ -2,9 +2,17 @@ import react from '@vitejs/plugin-react'
 import path from 'node:path'
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
+import visualizer from 'rollup-plugin-visualizer'
 
 module.exports = defineConfig({
-  plugins: [react(), dts({ insertTypesEntry: true })],
+  plugins: [
+    react(),
+    dts({ insertTypesEntry: true }),
+    process.env.ANALYZE === 'true' &&
+      visualizer(() => ({
+        filename: path.join(__dirname, 'bundle-analysis.html'),
+      })),
+  ],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
@@ -15,12 +23,20 @@ module.exports = defineConfig({
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ['@apollo/client', 'next', 'react', 'react-dom', 'uuid'],
+      external: [
+        '@apollo/client',
+        'graphql',
+        'next',
+        'react',
+        'react-dom',
+        'uuid',
+      ],
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
         globals: {
           '@apollo/client': 'ApolloClient',
+          graphql: 'graphql',
           next: 'next',
           react: 'React',
           'react-dom': 'ReactDOM',
