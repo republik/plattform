3da03501f9bd2bd41611a1965d113fbf2c8b4680
Patrick Venetz <venetz@gmail.com>
2022-06-19 11:09:35 +0200

    fix(discussions): add temporary "slated" timestamp

--- packages/backend-modules/discussions/migrations/sqls/20220616173809-content-as-jsonb-down.sql
+++ packages/backend-modules/discussions/migrations/sqls/20220616173809-content-as-jsonb-down.sql
@@ -1,2 +1,3 @@
 ALTER TABLE "public"."comments" DROP COLUMN "content";
 ALTER TABLE "public"."comments" RENAME COLUMN "content__markdown" TO "content";
+ALTER TABLE "public"."comments" DROP COLUMN "slatedAt";
--- packages/backend-modules/discussions/migrations/sqls/20220616173809-content-as-jsonb-up.sql
+++ packages/backend-modules/discussions/migrations/sqls/20220616173809-content-as-jsonb-up.sql
@@ -1,2 +1,3 @@
+ALTER TABLE "public"."comments" ADD COLUMN "slatedAt" timestamp with time zone;
 ALTER TABLE "public"."comments" RENAME COLUMN "content" TO "content__markdown";
 ALTER TABLE "public"."comments" ADD COLUMN "content" jsonb DEFAULT '[]';
