6478b01ee780e9509309a62d589980a868b8c340
Patrick Venetz <venetz@gmail.com>
2022-06-19 15:02:11 +0200

    fix(discussion): rename content index

--- packages/backend-modules/discussions/migrations/sqls/20220616173809-content-as-jsonb-down.sql
+++ packages/backend-modules/discussions/migrations/sqls/20220616173809-content-as-jsonb-down.sql
@@ -1,3 +1,4 @@
 ALTER TABLE "public"."comments" DROP COLUMN "content";
 ALTER TABLE "public"."comments" RENAME COLUMN "content__markdown" TO "content";
+ALTER INDEX public."comments_content__markdown_idx" RENAME TO "comments_content_idx";
 ALTER TABLE "public"."comments" DROP COLUMN "slatedAt";
--- packages/backend-modules/discussions/migrations/sqls/20220616173809-content-as-jsonb-up.sql
+++ packages/backend-modules/discussions/migrations/sqls/20220616173809-content-as-jsonb-up.sql
@@ -1,3 +1,4 @@
 ALTER TABLE "public"."comments" ADD COLUMN "slatedAt" timestamp with time zone;
 ALTER TABLE "public"."comments" RENAME COLUMN "content" TO "content__markdown";
+ALTER INDEX public."comments_content_idx" RENAME TO "comments_content__markdown_idx";
 ALTER TABLE "public"."comments" ADD COLUMN "content" jsonb DEFAULT '[]';
