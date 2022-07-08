1f6834a2955c7fe7c2fee9a62f4cbfd6fb48d517
Thomas Preusse <tpreusse@users.noreply.github.com>
2022-07-08 15:04:31 +0200

    feat(questionnaire): setup submissions viewer

---
+++ apps/www/components/Questionnaire/Submissions/index.js
@@ -0,0 +1,87 @@
+import { gql, useQuery } from '@apollo/client'
+import { Editorial, Interaction, Loader } from '@project-r/styleguide'
+import { useRouter } from 'next/router'
+
+export const mainQuery = gql`
+  query getQuestionnaireSubmissions($slug: String!) {
+    questionnaire(slug: $slug) {
+      id
+      description
+      beginDate
+      endDate
+      userHasSubmitted
+      userSubmitDate
+      submissions {
+        totalCount
+        nodes {
+          id
+          displayAuthor {
+            name
+          }
+          answers {
+            totalCount
+            nodes {
+              id
+              question {
+                id
+                __typename
+                text
+              }
+              payload
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+const Answers = () => {
+  const {
+    query: { slug },
+  } = useRouter()
+  const { loading, error, data } = useQuery(mainQuery, {
+    variables: {
+      slug,
+    },
+  })
+
+  return (
+    <Loader
+      loading={loading}
+      error={error}
+      render={() => {
+        const {
+          questionnaire,
+          questionnaire: { submissions },
+        } = data
+        return (
+          <>
+            <Interaction.H2>{questionnaire.description}</Interaction.H2>
+            <Interaction.P>{submissions.totalCount} Teilnehmende</Interaction.P>
+            {submissions.nodes.map(({ id, displayAuthor, answers }) => {
+              return (
+                <div key={id} style={{ margin: '30px 0 60px' }}>
+                  <Interaction.H3>
+                    {answers.totalCount} Antworten von {displayAuthor.name}
+                  </Interaction.H3>
+                  {answers.nodes.map(({ id, question, payload }) => {
+                    return (
+                      <Editorial.P key={id}>
+                        <strong>{question.text}</strong>
+                        <br />
+                        {payload.value}
+                      </Editorial.P>
+                    )
+                  })}
+                </div>
+              )
+            })}
+          </>
+        )
+      }}
+    />
+  )
+}
+
+export default Answers
