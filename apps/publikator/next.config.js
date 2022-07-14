b2bf155e15a1f7dc8200e8d8dac70039bfdef47f
Tobias Maier <trm217@protonmail.com>
2022-07-14 11:35:41 +0200

    chore(publikator): setup next-bundle-analyzer and update next-eslint-config

--- apps/publikator/next.config.js
+++ apps/publikator/next.config.js
@@ -1,21 +1,27 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
 const withTM = require('next-transpile-modules')(['@project-r/styleguide'])
 
 /**
  * @type {import('next').NextConfig}
  */
-module.exports = withTM({
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  webpack: (config) => {
-    const alias = Object.assign({}, config.resolve.alias)
-    delete alias.url
-    config.resolve = {
-      ...config.resolve,
-      alias,
-    }
+module.exports = withBundleAnalyzer(
+  withTM({
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+    webpack: (config) => {
+      const alias = Object.assign({}, config.resolve.alias)
+      delete alias.url
+      config.resolve = {
+        ...config.resolve,
+        alias,
+      }
 
-    return config
-  },
-  poweredByHeader: false,
-})
+      return config
+    },
+    poweredByHeader: false,
+  }),
+)
