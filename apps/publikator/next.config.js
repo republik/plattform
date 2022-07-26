35f7ef887a59d439a05106da53c0beedfbc21812
Anna Traussnig <anna.traussnig@gmail.com>
2022-07-26 14:52:12 +0200

    feat(FlyerEditor): two slate versions in publikator

--- apps/publikator/next.config.js
+++ apps/publikator/next.config.js
@@ -1,15 +1,20 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
 const withTM = require('next-transpile-modules')(['@project-r/styleguide'])
 
-module.exports = withTM({
-  webpack: (config) => {
-    const alias = Object.assign({}, config.resolve.alias)
-    delete alias.url
-    config.resolve = {
-      ...config.resolve,
-      alias,
-    }
+module.exports = withTM(
+  withBundleAnalyzer({
+    webpack: (config) => {
+      const alias = Object.assign({}, config.resolve.alias)
+      delete alias.url
+      config.resolve = {
+        ...config.resolve,
+        alias,
+      }
 
-    return config
-  },
-  poweredByHeader: false,
-})
+      return config
+    },
+    poweredByHeader: false,
+  }),
+)
