7a9fb7e9d25430e990179e873e2ce219650025e1
Anna Traussnig <anna.traussnig@gmail.com>
2022-08-17 13:37:15 +0200

    feat(FlyerInPublikator): fix merge issue

--- apps/publikator/components/Repo/Add.js
+++ apps/publikator/components/Repo/Add.js
@@ -220,7 +220,7 @@ class RepoAdd extends Component {
   goToEdit({ slug }) {
     const { title, schema, templateRepoId } = this.state
     const { isTemplate } = this.props
-    const path = schema === 'flyer' ? 'flyer/edit' : 'repo/edit'
+    const path = schema === 'flyer' ? 'flyer' : 'repo'
     this.props.router
       .replace({
         pathname: `/${path}/${GITHUB_ORG}/${slug}/edit`,
