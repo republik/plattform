9d1d0903df0b772b3bf4c75ab7b7d4b072267c99
Patrick Venetz <venetz@gmail.com>
2022-09-01 16:37:06 +0200

    fix(collections): harden audio queue

--- packages/backend-modules/collections/lib/AudioQueue.js
+++ packages/backend-modules/collections/lib/AudioQueue.js
@@ -1,38 +1,75 @@
 const Promise = require('bluebird')
+const { v4: uuid } = require('uuid')
 
 const getCollectionName = () => 'audioqueue'
 
 const getRepoId = (entityId) => {
-  if (entityId) {
-    const [org, repoName] = Buffer.from(entityId, 'base64')
-      .toString('utf-8')
-      .split('/')
-    return [org, repoName].join('/')
+  try {
+    if (entityId) {
+      const [org, repoName] = Buffer.from(entityId, 'base64')
+        .toString('utf-8')
+        .split('/')
+      return [org, repoName].join('/')
+    }
+  } catch (e) {
+    // swallow error
   }
 
   return undefined
 }
 
+// A filter to omit an unwanted item
+const omitItem = (unwantedItem) => (item) => item.id !== unwantedItem?.id
+
+// A filter to keep items with a sequence between start and end
+const pickSequenceRange = (start, end) => (item) => {
+  const { sequence } = item.data
+  if (!sequence) {
+    return false
+  }
+
+  const min = Math.min(start, end)
+  const max = Math.max(start, end)
+
+  return sequence >= min && sequence <= max
+}
+
 const upsertItem = async (input, context) => {
-  const { id, entityId, sequence } = input
+  const { id, entityType, entityId, sequence } = input
   const { user: me, loaders, pgdb, t } = context
+
+  if (!!id && !uuid(id)) {
+    throw new Error(t('api/collections/audioQueue/error/invalidItemId'))
+  }
+
+  if (!!entityId && !entityType) {
+    throw new Error(t('api/collections/audioQueue/error/missingEntityType'))
+  }
+
+  if (!!entityId && !['Document'].includes(entityType)) {
+    throw new Error(t('api/collections/audioQueue/error/unsupportedEntityType'))
+  }
+
+  if (sequence && (!Number.isFinite(sequence) || sequence < 1)) {
+    throw new Error(t('api/collections/audioQueue/error/unsupportedSequence'))
+  }
 
   const collection = await loaders.Collection.byKeyObj.load({
     name: getCollectionName(),
   })
 
   if (!collection) {
-    throw new Error('Audio queue is missing')
+    throw new Error(t('api/collections/audioQueue/error/missingCollection'))
   }
 
   const repoId = getRepoId(entityId)
 
-  if (repoId) {
-    const doc = await loaders.Document.byRepoId.load(repoId)
+  if (!!entityId && !repoId) {
+    throw new Error(t('api/collections/audioQueue/error/invalidEntityId'))
+  }
 
-    if (!doc) {
-      throw new Error(t(`api/collections/document/404`))
-    }
+  if (repoId && !(await loaders.Document.byRepoId.load(repoId))) {
+    throw new Error(t('api/collections/audioQueue/error/missingDocument'))
   }
 
   const items = await pgdb.public.collectionDocumentItems.find({
@@ -44,73 +81,80 @@ const upsertItem = async (input, context) => {
     (item) => item.id === id || item.repoId === repoId,
   )
 
-  const maxSequence =
-    (items.length &&
-      Math.max(...items.map((item) => item.data?.sequence || 0))) ||
-    1
-
-  const isMaxSequenceOccupied = !!items
-    .filter((item) => item.id !== existingItem?.id)
-    .find((item) => item.data?.sequence === maxSequence)
-
-  const evaluatedMaxSequence =
-    (isMaxSequenceOccupied && maxSequence + 1) || maxSequence
-
-  const evaluatedSequence =
-    ((!sequence || sequence > maxSequence) && evaluatedMaxSequence) || sequence
-
-  const isSequenceOccupied = !!items
-    .filter((item) => item.id !== existingItem?.id)
-    .find((item) => item.data?.sequence === evaluatedSequence)
-
-  const updatedItem = {
-    collectionId: collection.id,
-    userId: me.id,
-    repoId,
-    data: {
-      ...existingItem?.data,
-      sequence: evaluatedSequence,
-    },
+  if (id && !existingItem) {
+    throw new Error(t('api/collections/audioQueue/error/missingItem'))
   }
 
-  if (isSequenceOccupied) {
-    await Promise.each(
-      items
-        .filter((item) => item.id !== existingItem?.id)
-        .filter((item) => item.data?.sequence >= evaluatedSequence),
-      async (item) => {
-        await pgdb.public.collectionDocumentItems.update(
-          { id: item.id },
-          {
-            data: { ...item.data, sequence: item.data?.sequence + 1 },
-          },
-        )
+  const currentSequence = existingItem?.data?.sequence
+
+  const nextSequence =
+    (items.length &&
+      Math.max(...items.map((item) => item.data?.sequence || 0))) + 1
+
+  const aimSequence =
+    (!sequence && existingItem && nextSequence - 1) ||
+    ((!sequence || sequence > nextSequence) && nextSequence) ||
+    sequence
+
+  const resequenceItems = items
+    .filter(omitItem(existingItem))
+    .filter(pickSequenceRange(currentSequence || nextSequence, aimSequence))
+
+  const resequenceModifier = currentSequence < aimSequence ? -1 : 1
+
+  await Promise.each(resequenceItems, async (item) => {
+    const { sequence } = item.data
+    if (!sequence) {
+      return
+    }
+
+    await pgdb.public.collectionDocumentItems.update(
+      { id: item.id },
+      {
+        data: {
+          ...item.data,
+          sequence: sequence + resequenceModifier,
+        },
       },
     )
-  }
+  })
 
   if (existingItem) {
     await pgdb.public.collectionDocumentItems.update(
+      { id: existingItem.id },
       {
-        id: existingItem.id,
+        data: {
+          ...existingItem.data,
+          sequence: aimSequence,
+        },
       },
-      updatedItem,
     )
   } else {
-    await pgdb.public.collectionDocumentItems.insert(updatedItem)
+    await pgdb.public.collectionDocumentItems.insert({
+      collectionId: collection.id,
+      userId: me.id,
+      repoId,
+      data: {
+        sequence: aimSequence,
+      },
+    })
   }
 }
 
 const removeItem = async (input, context) => {
   const { id } = input
-  const { user: me, loaders, pgdb } = context
+  const { user: me, loaders, pgdb, t } = context
+
+  if (!!id && !uuid(id)) {
+    throw new Error(t('api/collections/audioQueue/error/invalidItemId'))
+  }
 
   const collection = await loaders.Collection.byKeyObj.load({
     name: getCollectionName(),
   })
 
   if (!collection) {
-    throw new Error('Audio queue is missing')
+    throw new Error(t('api/collections/audioQueue/error/missingCollection'))
   }
 
   await pgdb.public.collectionDocumentItems.delete({
