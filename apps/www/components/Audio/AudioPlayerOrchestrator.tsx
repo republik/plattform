5e565d11b70c37c8931655c84730d9af9fa9bcc7
Tobias Maier <trm217@proton.me>
2022-09-21 08:50:07 +0200

    chore(www/audio): remove unused imports

--- apps/www/components/Audio/AudioPlayerOrchestrator.tsx
+++ apps/www/components/Audio/AudioPlayerOrchestrator.tsx
@@ -1,9 +1,5 @@
-import { useInNativeApp } from '../../lib/withInNativeApp'
-import compareVersion from '../../lib/react-native/CompareVersion'
-import { NEW_AUDIO_API_VERSION } from './constants'
 import AudioPlayerContainer from './AudioPlayerContainer'
 import dynamic from 'next/dynamic'
-import { useRouter } from 'next/router'
 import useAudioQueue from './hooks/useAudioQueue'
 
 const AudioPlayer = dynamic(() => import('./AudioPlayer/AudioPlayer'), {
@@ -24,7 +20,6 @@ const AudioPlayerOrchestrator = () => {
   if (!isAudioQueueAvailable) {
     return <LegacyAudioPlayer />
   }
-
   // Render new audio player if in web or in a native app using the new audio-player
   return (
     <AudioPlayerContainer>
