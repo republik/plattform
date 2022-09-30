1ac6e9038685078c1246c7b2be620b6dba2dbd52
Tobias Maier <trm217@proton.me>
2022-09-30 11:22:29 +0200

    chore(www/audio): start adding filter-buttons in all audio-tracks

--- apps/www/components/Audio/AudioPlayer/ui/tabs/LatestArticles.tsx
+++ apps/www/components/Audio/AudioPlayer/ui/tabs/LatestArticles.tsx
@@ -1,10 +1,12 @@
-import { useMemo } from 'react'
+import { ReactNode, useMemo, useState } from 'react'
 import { css } from 'glamor'
 import {
   IconButton,
   PlaylistAddIcon,
   DownloadIcon,
   LinkIcon,
+  fontStyles,
+  useColorContext,
 } from '@project-r/styleguide'
 import AudioListItem from './AudioListItem'
 import useAudioQueue from '../../../hooks/useAudioQueue'
@@ -14,14 +16,50 @@ import { AudioQueueItem } from '../../../graphql/AudioQueueHooks'
 import LoadingPlaceholder from './LoadingPlaceholder'
 
 const styles = {
+  filterButton: css({
+    border: 'none',
+    padding: 0,
+    font: 'inherit',
+    outline: 'inherit',
+    textAlign: 'start',
+    backgroundColor: 'transparent',
+    cursor: 'pointer',
+    ...fontStyles.sansSerifBold,
+    fontWeight: 'bold',
+  }),
+  filters: css({
+    marginTop: 12,
+    display: 'flex',
+    gap: 8,
+  }),
   list: css({
     listStyle: 'none',
     padding: 0,
     display: 'flex',
     flexDirection: 'column',
     gap: 24,
-    margin: '24px 0',
+    marginTop: 12,
+    marginBottom: 24,
   }),
+}
+
+type FilterButtonProps = {
+  children?: ReactNode
+  isActive?: boolean
+  onClick: () => void
+}
+
+const FilterButton = ({ children, onClick, isActive }: FilterButtonProps) => {
+  const [colorScheme] = useColorContext()
+  return (
+    <button
+      onClick={() => onClick()}
+      {...styles.filterButton}
+      {...colorScheme.set('color', isActive ? 'text' : 'disabled')}
+    >
+      {children}
+    </button>
+  )
 }
 
 type LatestArticlesProps = {
@@ -33,6 +71,7 @@ const LatestArticlesTab = ({
   handleOpenArticle,
   handleDownload,
 }: LatestArticlesProps) => {
+  const [filter, setFilter] = useState<'all' | 'read-aloud'>('all')
   const { t } = useTranslation()
   const { data, loading, error } = useLatestArticlesQuery({
     variables: {
@@ -68,10 +107,17 @@ const LatestArticlesTab = ({
   }
 
   const filteredArticles = useMemo(() => {
-    return data?.latestArticles?.nodes.filter(
+    const articleWithAudio = data?.latestArticles?.nodes.filter(
       (article) => !!article?.meta?.audioSource,
     )
-  }, [data])
+
+    return articleWithAudio?.filter((article) => {
+      if (filter === 'all') {
+        return true
+      }
+      return article?.meta?.audioSource?.kind === 'readAloud'
+    })
+  }, [data, filter])
 
   if (loading) {
     return <LoadingPlaceholder />
@@ -82,38 +128,54 @@ const LatestArticlesTab = ({
   }
 
   return (
-    <ul {...styles.list}>
-      {filteredArticles.map((article) => (
-        <li key={article.id}>
-          <AudioListItem
-            item={article}
-            isActive={checkIfActiveItem(article.id)}
-            beforeActionItem={
-              <IconButton
-                Icon={PlaylistAddIcon}
-                title={t('AudioPlayer/Queue/Add')}
-                onClick={() => handleAddToQueue(article.id)}
-                disabled={checkIfInQueue(article.id)}
-                style={{ marginRight: 0 }}
-              />
-            }
-            actions={[
-              {
-                Icon: DownloadIcon,
-                label: t('AudioPlayer/Queue/Download'),
-                onClick: () => handleDownload(article),
-              },
-              {
-                Icon: LinkIcon,
-                label: t('AudioPlayer/Queue/GoToItem'),
-                onClick: () => handleOpenArticle(article.meta.path),
-              },
-            ]}
-            onClick={() => handlePlay(article.id)}
-          />
-        </li>
-      ))}
-    </ul>
+    <div>
+      <div {...styles.filters}>
+        <FilterButton
+          isActive={filter === 'read-aloud'}
+          onClick={() => setFilter('read-aloud')}
+        >
+          Vorgelesen
+        </FilterButton>
+        <FilterButton
+          isActive={filter === 'all'}
+          onClick={() => setFilter('all')}
+        >
+          Alle
+        </FilterButton>
+      </div>
+      <ul {...styles.list}>
+        {filteredArticles.map((article) => (
+          <li key={article.id}>
+            <AudioListItem
+              item={article}
+              isActive={checkIfActiveItem(article.id)}
+              beforeActionItem={
+                <IconButton
+                  Icon={PlaylistAddIcon}
+                  title={t('AudioPlayer/Queue/Add')}
+                  onClick={() => handleAddToQueue(article.id)}
+                  disabled={checkIfInQueue(article.id)}
+                  style={{ marginRight: 0 }}
+                />
+              }
+              actions={[
+                {
+                  Icon: DownloadIcon,
+                  label: t('AudioPlayer/Queue/Download'),
+                  onClick: () => handleDownload(article),
+                },
+                {
+                  Icon: LinkIcon,
+                  label: t('AudioPlayer/Queue/GoToItem'),
+                  onClick: () => handleOpenArticle(article.meta.path),
+                },
+              ]}
+              onClick={() => handlePlay(article.id)}
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
