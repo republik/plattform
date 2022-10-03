cf38b898c17a6678475c6650995a28e370be6bdd
Anna Traussnig <anna.traussnig@gmail.com>
2022-10-03 15:47:56 +0200

    fix(ListConfig): small amends

--- packages/styleguide/src/components/Editor/config/elements/list/container.tsx
+++ packages/styleguide/src/components/Editor/config/elements/list/container.tsx
@@ -1,22 +1,27 @@
-import { ElementConfigI, NodeTemplate } from '../../../custom-types'
+import { ElementConfigI } from '../../../custom-types'
 import { UlIcon, OlIcon } from '../../../../Icons'
 
-const structure: NodeTemplate[] = [
-  { type: 'listItem', main: true, repeat: true },
-]
-// TODO: this prop can be deleted
+// TODO: 'ordered' prop can be deleted
 //  this should be done together with slate tree migration in BE
-//  otherwise phantom uncommitted changed will appear in publikator
-const props = ['ordered']
+//  otherwise phantom uncommitted changed will appear in Publikator
+const baseConfig: Partial<ElementConfigI> = {
+  structure: [{ type: 'listItem', main: true, repeat: true }],
+  props: ['ordered'],
+  attrs: {
+    blockUi: {
+      style: {
+        top: 4,
+      },
+    },
+  },
+}
 
 export const ulConfig: ElementConfigI = {
   button: { icon: UlIcon },
-  props,
-  structure,
+  ...baseConfig,
 }
 
 export const olConfig: ElementConfigI = {
   button: { icon: OlIcon },
-  props,
-  structure,
+  ...baseConfig,
 }
