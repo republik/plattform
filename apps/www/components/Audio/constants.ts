4cb06a37f7d592cbd94b39879bcc34d779fcba0b
Tobias Maier <trm217@proton.me>
2022-10-06 15:31:56 +0200

    fix(www): update hoert_hoert toggle to require 'true' as value

--- apps/www/components/Audio/constants.ts
+++ apps/www/components/Audio/constants.ts
@@ -3,5 +3,5 @@ export const NEW_AUDIO_API_VERSION = '2.2.0'
 
 export const NEXT_PUBLIC_FEAT_HOERT_HOERT = process.env
   .NEXT_PUBLIC_FEAT_HOERT_HOERT
-  ? Boolean(process.env.NEXT_PUBLIC_FEAT_HOERT_HOERT)
+  ? process.env.NEXT_PUBLIC_FEAT_HOERT_HOERT == 'true'
   : false
