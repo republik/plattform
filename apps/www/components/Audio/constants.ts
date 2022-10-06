54fc2742118974f9507a6ca1f2d90fcbbce38ff8
Tobias Maier <trm217@proton.me>
2022-10-06 11:22:45 +0200

    feat(www): add Feature_flag for hoert_hoert features

--- apps/www/components/Audio/constants.ts
+++ apps/www/components/Audio/constants.ts
@@ -1,2 +1,6 @@
 // Version that introduced the new Audio API introduced with 'Hört hört'
 export const NEW_AUDIO_API_VERSION = '2.2.0'
+
+export const FEAT_HOERT_HOERT = process.env.FEAT_HOERT_HOERT
+  ? Boolean(process.env.FEAT_HOERT_HOERT)
+  : false
