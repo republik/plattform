7a44057db80b4e1d2d86fb8fd456d30422aaee2e
Tobias Maier <trm217@proton.me>
2022-10-07 15:52:39 +0200

    feat(www/AudioCover): add isLoading state which renders a spinner above the cover

--- apps/www/components/Audio/AudioPlayer/ui/AudioCover.tsx
+++ apps/www/components/Audio/AudioPlayer/ui/AudioCover.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { css } from 'glamor'
-import { AudioCoverGenerator } from '@project-r/styleguide'
-import { parse } from 'url'
+import { AudioCoverGenerator, Spinner } from '@project-r/styleguide'
+import { clamp } from '../../helpers/clamp'
 
 type AudioCoverProps = {
   size: number
+  /**
+   * Multiplier to calculate the size of spinner
+   * relative to the `size`-prop.
+   */
+  spinnerSizeRelation?: number
   format?: any
   image?: string
   audioCoverCrop?: {
@@ -13,13 +18,26 @@ type AudioCoverProps = {
     width: number
     height: number
   }
+  isLoading?: boolean
 }
 
 const styles = {
+  coverWrapper: css({
+    position: 'relative',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  }),
   cover: css({
     aspectRatio: '1 / 1',
     objectFit: 'cover',
     height: 'auto',
+  }),
+  spinner: css({
+    position: 'absolute',
+    inset: 0,
+    background: 'rgba(50, 50, 50, 0.2)',
   }),
 }
 
@@ -37,9 +55,11 @@ const getResizefromURL = (url, size) => {
 
 const AudioCover = ({
   size,
+  spinnerSizeRelation = 0.5,
   format,
   image: imageUrl,
   audioCoverCrop,
+  isLoading,
 }: AudioCoverProps) => {
   if (imageUrl) {
     let resizeUrl
@@ -50,12 +70,22 @@ const AudioCover = ({
       resizeUrl = `${imageUrl}&resize=${getResizefromURL(imageUrl, size * 2)}`
     }
     return (
-      <img
-        src={resizeUrl || imageUrl}
-        {...styles.cover}
-        style={{ width: size }}
-        alt=''
-      />
+      <div {...styles.coverWrapper}>
+        <img
+          src={resizeUrl || imageUrl}
+          {...styles.cover}
+          style={{ width: size }}
+          alt=''
+        />
+        {isLoading && (
+          <div {...styles.spinner}>
+            <Spinner
+              size={Math.ceil(size * clamp(spinnerSizeRelation, 0, 1))}
+              color='#999'
+            />
+          </div>
+        )}
+      </div>
     )
   }
   return <AudioCoverGenerator format={format} previewSize={size} />
