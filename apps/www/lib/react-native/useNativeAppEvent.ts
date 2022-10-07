df006b61e6619174c704587849b2eb7c460ddb53
Tobias Maier <trm217@proton.me>
2022-10-07 23:11:40 +0200

    fix(www/useNativeAppEvent): improve performance of useNativeAppEvent-hook

--- apps/www/lib/react-native/useNativeAppEvent.ts
+++ apps/www/lib/react-native/useNativeAppEvent.ts
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 import AppMessageEventEmitter from './AppMessageEventEmitter'
 import { useInNativeApp } from '../withInNativeApp'
 
@@ -7,23 +7,35 @@ type EventHandler<E> = (eventData: E) => Promise<void> | void
 /**
  * useWebViewEvent allows to subscribe to events emitted by the web-ui.
  * @param eventName The name of the event to subscribe to.
- * @param handler The handler to call when the event is emitted.
+ * @param callback The callback to call when the event is emitted.
  */
 function useNativeAppEvent<E = Event>(
   eventName: string,
-  handler: EventHandler<E>,
+  callback: EventHandler<E>,
 ) {
   const { inNativeApp } = useInNativeApp()
+  const savedCallback = useRef<EventHandler<E>>(() => null)
+
+  useEffect(() => {
+    savedCallback.current = callback
+  })
+
   useEffect(() => {
     if (!inNativeApp) {
       return
     }
+    console.log('useNativeAppEvent setup', eventName)
+    const handler = (evenData: E) => {
+      console.log('useNativeAppEvent handler', eventName, evenData)
+      savedCallback?.current(evenData)
+    }
 
     AppMessageEventEmitter.addListener(eventName, handler)
     return () => {
+      console.log('useNativeAppEvent teardown', eventName)
       AppMessageEventEmitter.removeListener(eventName, handler)
     }
-  }, [eventName, handler])
+  }, [eventName])
 }
 
 export default useNativeAppEvent
