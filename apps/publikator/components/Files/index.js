a73658c0cb59f35952a608376a690707cb09606c
Patrick Venetz <venetz@gmail.com>
2022-10-11 22:53:33 +0200

    fix(apps/publikator): use Loader wrapper

--- apps/publikator/components/Files/index.js
+++ apps/publikator/components/Files/index.js
@@ -4,6 +4,7 @@ import { Container } from '@project-r/styleguide'
 import { getRepoIdFromQuery } from '../../lib/repoIdHelper'
 import { RepoFile } from '../../lib/graphql/fragments'
 
+import Loader from '../Loader'
 import Frame from '../Frame'
 import Nav from '../Edit/Nav'
 import { Table, Tr, Th } from '../Table'
@@ -15,6 +16,7 @@ import Row from './Row'
 const GET_FILES = gql`
   query getFiles($id: ID!) {
     repo(id: $id) {
+      id
       files {
         ...RepoFile
       }
@@ -28,35 +30,41 @@ const FilesPage = ({ router }) => {
   const repoId = getRepoIdFromQuery(router.query)
   const variables = { id: repoId }
 
-  const { data } = useQuery(GET_FILES, { variables })
+  const { data, loading, error } = useQuery(GET_FILES, { variables })
 
   return (
-    <Frame raw>
+    <Frame>
       <Frame.Header>
         <Frame.Header.Section align='left'>
           <Nav />
         </Frame.Header.Section>
       </Frame.Header>
       <Frame.Body raw>
-        <Container>
-          <Info />
-          <Upload repoId={repoId} />
-          {!!data?.repo?.files?.length && (
-            <Table>
-              <thead>
-                <Tr>
-                  <Th style={{ width: '70%' }}>Datei</Th>
-                  <Th style={{ width: '30%' }}></Th>
-                </Tr>
-              </thead>
-              <tbody>
-                {data.repo.files.map((file) => (
-                  <Row key={file.id} file={file} />
-                ))}
-              </tbody>
-            </Table>
+        <Loader
+          loading={loading}
+          error={error}
+          render={() => (
+            <Container>
+              <Info />
+              <Upload repoId={data.id} />
+              {!!data.repo.files.length && (
+                <Table>
+                  <thead>
+                    <Tr>
+                      <Th style={{ width: '70%' }}>Datei</Th>
+                      <Th style={{ width: '30%' }}></Th>
+                    </Tr>
+                  </thead>
+                  <tbody>
+                    {data.repo.files.map((file) => (
+                      <Row key={file.id} file={file} />
+                    ))}
+                  </tbody>
+                </Table>
+              )}
+            </Container>
           )}
-        </Container>
+        />
       </Frame.Body>
     </Frame>
   )
