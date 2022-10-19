54dc6118b7d7db8898092158b129c1132dbf155e
Tobias Maier <trm217@proton.me>
2022-10-19 11:13:25 +0200

    refactor(www/audio): remove premature queue-refetch logic

--- apps/www/components/Audio/AudioPlayerController.tsx
+++ apps/www/components/Audio/AudioPlayerController.tsx
@@ -100,7 +100,6 @@ const AudioPlayerController = ({ children }: AudioPlayerContainerProps) => {
     audioQueueIsLoading,
     addAudioQueueItem,
     removeAudioQueueItem,
-    refetchAudioQueue,
   } = useAudioQueue()
   const { getMediaProgress, saveMediaProgress } = useMediaProgress()
 
@@ -541,18 +540,6 @@ const AudioPlayerController = ({ children }: AudioPlayerContainerProps) => {
     }
     setInitialized(true)
   }, [audioQueue, initialized, audioQueueIsLoading, pathname, setUpAppPlayer])
-
-  // refetch the queue to check for possible changes once the tab is opened again
-  useEffect(() => {
-    const handler = async () => {
-      const documentIsVisible = document.visibilityState === 'visible'
-      if (documentIsVisible && !isPlaying) {
-        await refetchAudioQueue()
-      }
-    }
-    document.addEventListener('visibilitychange', handler)
-    return () => document.removeEventListener('visibilitychange', handler)
-  }, [])
 
   useAudioContextEvent<void>('togglePlayer', playQueue)
 
