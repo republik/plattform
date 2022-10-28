4b0482569907ed6f68497ed46b6b78dfcd21ce82
Felix Michel <felix_michel@gmx.ch>
2022-10-28 14:50:32 +0200

    feat(Carousel): drafty first version

--- apps/www/components/Marketing/Carousel.tsx
+++ apps/www/components/Marketing/Carousel.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {
   mediaQueries,
   IconButton,
   PlayCircleIcon,
   ArticleIcon,
+  colors,
 } from '@project-r/styleguide'
 import { css } from 'glamor'
 import { getImgSrc } from '../Overview/utils'
@@ -11,6 +12,7 @@ import { useQuery } from '@apollo/client'
 import { GET_DOCUMENT_AUDIO } from './graphql/DocumentAudio.graphql'
 import { trackEvent } from '../../lib/matomo'
 import { useAudioContext } from '../Audio/AudioProvider'
+import { calcLength } from 'framer-motion'
 
 export type CarouselProps = { carouselData: any }
 
@@ -22,6 +24,7 @@ type CarouselItem = {
 const PlayAudio: React.FC<{ path: string }> = ({ path }) => {
   const { data } = useQuery(GET_DOCUMENT_AUDIO, { variables: { path } })
   const { toggleAudioPlayer, isPlaying } = useAudioContext()
+
   if (!data?.document) return null
   return (
     <IconButton
@@ -46,42 +49,128 @@ const Carousel: React.FC<CarouselProps> = ({ carouselData }) => {
     ({ data }) => ({
       src: getImgSrc(data, '/', 1200),
       path: data.url,
+      ref: React.createRef(),
     }),
   )
-  const currentItem = items[0]
+
+  const moreItems = items.concat(items)
+
+  const [currentPosition, setCurrentPosition] = React.useState(0)
+
+  const translationStep = 100 / moreItems.length
+  const maxTranslation = (moreItems.length - 1) * translationStep
+
+  const handleClick = (type) => {
+    if (type === 'forward') {
+      setCurrentPosition(
+        currentPosition >= maxTranslation
+          ? maxTranslation
+          : currentPosition + translationStep,
+      )
+    }
+    if (type === 'backward') {
+      setCurrentPosition(
+        currentPosition <= 0 ? 0 : currentPosition - translationStep,
+      )
+    }
+  }
+
+  console.log(currentPosition)
+
   return (
-    <div {...styles.carousel}>
-      <div {...styles.image}>
-        <img src={currentItem.src} width='100%' />
-      </div>
-      <div {...styles.actions}>
-        <IconButton
-          href={currentItem.path}
-          Icon={ArticleIcon}
-          labelStyle={{ fontSize: 24 }}
-          fillColorName='default'
-          labelShort='Lesen'
-          label='Lesen'
-          size={30}
-        />
-        <PlayAudio path={currentItem.path} />
+    <div>
+      <div {...styles.slider}>
+        <div
+          {...styles.clickArea}
+          style={{ left: '0' }}
+          onClick={() => handleClick('backward')}
+        ></div>
+        <div
+          {...styles.clickArea}
+          style={{ left: 'calc(100% - 50px)' }}
+          onClick={() => handleClick('forward')}
+        ></div>
+        <ul
+          {...styles.sliderWrapper}
+          style={{ transform: `translateX(-${currentPosition}%)` }}
+        >
+          {moreItems.map((d, i) => (
+            <li {...styles.slide} key={i}>
+              <div {...styles.imageWrapper}>
+                <img {...styles.image} src={d.src} />
+                <div {...styles.actions}>
+                  <IconButton
+                    href={d.path}
+                    Icon={ArticleIcon}
+                    labelStyle={{ fontSize: 24 }}
+                    fillColorName='default'
+                    labelShort='Lesen'
+                    label='Lesen'
+                    size={30}
+                  />
+                  <PlayAudio path={d.path} />
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   )
 }
 
+const SLIDER_HEIGHT = '35vmin'
+const SLIDER_WIDTH = '40vmin'
+
 const styles = {
+  clickArea: css({
+    position: 'absolute',
+    width: '50px',
+    backgroundColor: 'yellow', //colors.dark.default,
+    height: '100%',
+    zIndex: 2,
+    opacity: 0.7,
+  }),
+  slider: css({
+    position: 'relative',
+    overflow: 'hidden',
+    margin: '0 auto',
+    height: SLIDER_HEIGHT,
+    width: SLIDER_WIDTH,
+  }),
+  sliderWrapper: css({
+    display: 'flex',
+    position: 'absolute',
+    transition: 'transform 500ms cubic-bezier(0.25, 1, 0.35, 1)',
+    margin: '0 auto',
+    listStyleType: 'none',
+  }),
+  slide: css({
+    display: 'flex',
+    height: SLIDER_HEIGHT,
+    width: SLIDER_WIDTH,
+    flex: 1,
+    flexDirection: 'column',
+    position: 'relative',
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignContent: 'space-around',
+    zIndex: 1,
+  }),
+  imageWrapper: css({
+    overflow: 'hidden',
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    alignItems: 'center',
+  }),
   image: css({
     width: '100%',
-    padding: '20px 80px',
-  }),
-  carousel: css({
-    width: '100%',
-    [mediaQueries.mUp]: {
-      width: '80%',
-    },
   }),
   actions: css({
+    marginTop: '20px',
+    position: 'relative',
     display: 'flex',
     justifyContent: 'center',
   }),
