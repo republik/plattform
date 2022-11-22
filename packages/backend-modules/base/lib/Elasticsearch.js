8314d1b6a7d647d29cbd394b595a9d42ada5899b
Patrick Venetz <venetz@gmail.com>
2022-11-22 14:12:51 +0100

    feat(base): accept node URL when connecting

--- packages/backend-modules/base/lib/Elasticsearch.js
+++ packages/backend-modules/base/lib/Elasticsearch.js
@@ -1,8 +1,11 @@
 const { Client } = require('@elastic/elasticsearch')
 
-const connect = () =>
+const connect = (node) =>
   new Client({
-    node: process.env.ELASTIC_URL || 'http://elastic:elastic@localhost:9200',
+    node:
+      node ||
+      process.env.ELASTIC_URL ||
+      'http://elastic:elastic@localhost:9200',
   })
 
 const disconnect = (client) => client.close()
