7f41cef0c59b4330ceea271391a8cc1ddf4474b4
Olivier Baumann <olivierbaumann@me.com>
2022-12-06 14:36:56 +0100

    fix(Mandrill): variables

---
+++ packages/backend-modules/mail/templates/variables_oli.js
@@ -0,0 +1,8 @@
+export const new_vars = {
+  hasMultipleOAmounts: `this.oamount > 1.`,
+  hasAnyPledgeMemberships: `pledger_memberships_count > 0.`,
+  hasOneGiftedMembership: `gifted_memberships_count == 1.`,
+  hasMultipleGiftedMemberships: `gifted_memberships_count > 1.`,
+  hasOneGoodie: `goodies_count == 1.`,
+  hasMultipleGoodies: `goodies_count > 1.`,
+}
