a8780b8cf307d6ed7757e6e1920e5205e700fb08
Tobias Maier <trm217@proton.me>
2022-12-07 18:15:57 +0100

    docs(www): add comments to useLocalCommentReports.ts

--- apps/www/components/Discussion/helpers/useLocalCommentReports.ts
+++ apps/www/components/Discussion/helpers/useLocalCommentReports.ts
@@ -5,6 +5,11 @@ import Nullable from '../../../lib/types/Nullable'
 const LOCAL_COMMENT_REPORTS_STORAGE_KEY = 'local-comment-reports'
 const LOCAL_COMMENT_REPORTS_TTL = 1000 * 60 * 60 * 24 * 30 // 30 days
 
+/**
+ * Parses the raw comment-reports from local storage and returns a map of comment ids to the date of the report
+ * @param data The raw comment-reports from local storage
+ * @returns A map of comment ids to the date of the report
+ */
 function parseCommentReports(data: Nullable<string>): Record<string, Date> {
   try {
     if (data != null && JSON.parse(data) instanceof Object) {
@@ -25,6 +30,11 @@ function parseCommentReports(data: Nullable<string>): Record<string, Date> {
   }
 }
 
+/**
+ * Serializes the comment-reports map to a string that can be stored in local storage
+ * @param data The comment-reports map
+ * @returns The serialized comment-reports map
+ */
 function serializeCommentReports(data: Record<string, Date>): string {
   const serializedData = Object.keys(data).reduce((acc, key) => {
     acc[key] = data[key].toISOString()
@@ -37,6 +47,26 @@ const usePersistedSerializedLocalCommentReports = createPersistedState<
   string | null
 >(LOCAL_COMMENT_REPORTS_STORAGE_KEY)
 
+/**
+ * Checks if any of the reports in the map are older than the TTL
+ * @param reports The comment-reports map
+ * @returns True if any of the reports are older than the TTL, false otherwise
+ */
+function hasOutdatedReports(reports: Record<string, Date>): boolean {
+  return Object.keys(reports).some((key) => {
+    const parsedDate = new Date(reports[key])
+    return (
+      new Date().getTime() - parsedDate.getTime() > LOCAL_COMMENT_REPORTS_TTL
+    )
+  })
+}
+
+/**
+ * A hook that provides functions for adding and checking comment reports stored in the local-storage
+ * @returns An object containing the following properties:
+ * - addLocalCommentReport: A function that adds a comment report to local storage
+ * - checkIfAlreadyReported: A function that checks if a comment has already been reported
+ */
 export function useLocalCommentReports() {
   const [rawReports, setRawData] =
     usePersistedSerializedLocalCommentReports<Nullable<string>>(null)
@@ -45,7 +75,14 @@ export function useLocalCommentReports() {
   // sync data from local storage
   useEffect(() => {
     if (rawReports) {
-      setReports(parseCommentReports(rawReports))
+      const parsedReports = parseCommentReports(rawReports)
+
+      // Delte reports that are older than the TTL
+      if (hasOutdatedReports(parsedReports)) {
+        setRawData(serializeCommentReports(parsedReports))
+      }
+
+      setReports(parsedReports)
     }
   }, [rawReports])
 
