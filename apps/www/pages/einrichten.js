05b59c87deadf156a9843f9cec68e4cf2df5b2b1
Tobias Maier <trm217@proton.me>
2022-12-15 17:31:29 +0100

    feat(www): extract enforceRoles from enforceMembership, to allow for onboarding of non-members

--- apps/www/pages/einrichten.js
+++ apps/www/pages/einrichten.js
@@ -1,7 +1,7 @@
 import Onboarding from '../components/Onboarding/Page'
 
-import { enforceMembership } from '../components/Auth/withMembership'
+import { enforceRoles } from '../components/Auth/withMembership'
 import { withDefaultSSR } from '../lib/apollo/helpers'
 
 /* instead of enforceMembership we need something else here, so that non-members but climate lab users can access onboarding page as well */
-export default withDefaultSSR(enforceMembership()(Onboarding))
+export default withDefaultSSR(enforceRoles(['member', 'climate'])(Onboarding))
