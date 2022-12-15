346db2c64654336f408db8405d30d778b91186d2
Sharon Funke <sharon.funke@voodoo-arts.net>
2022-12-15 17:10:47 +0100

    chore(einrichten): add comment

--- apps/www/pages/einrichten.js
+++ apps/www/pages/einrichten.js
@@ -3,4 +3,5 @@ import Onboarding from '../components/Onboarding/Page'
 import { enforceMembership } from '../components/Auth/withMembership'
 import { withDefaultSSR } from '../lib/apollo/helpers'
 
+/* instead of enforceMembership we need something else here, so that non-members but climate lab users can access onboarding page as well */
 export default withDefaultSSR(enforceMembership()(Onboarding))
