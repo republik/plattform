72755a91024dfdcc80b5e35bb0e5fb89e6e20872
Tobias Maier <trm217@proton.me>
2022-12-30 10:46:10 +0100

    fix(www/climate): fix faulty usage of colorScheme in the Button component

--- apps/www/components/Climatelab/shared/Button.tsx
+++ apps/www/components/Climatelab/shared/Button.tsx
@@ -24,7 +24,7 @@ const Button: FC<ButtonProps> = ({
       {...colorScheme.set('borderColor', 'climateBorder')}
       {...css({
         ':hover': {
-          backgroundColor: colorScheme.get('primaryHover'),
+          backgroundColor: colorScheme.getCSSColor('primaryHover'),
         },
       })}
       {...styles.clearFix}
