e1d0c2436e0723c38076b7df2da266a3bb9023e7
Felix Michel <felix_michel@gmx.ch>
2023-01-04 16:55:58 +0100

    fix(www): layout carousel

--- apps/www/components/Climatelab/Postcard/ImageChoice.tsx
+++ apps/www/components/Climatelab/Postcard/ImageChoice.tsx
@@ -11,7 +11,7 @@ const styles = {
     borderWidth: '3px',
     borderStyle: 'solid',
     maxWidth: '100%',
-    objectFit: 'contain',
+    objectFit: 'fill',
     borderRadius: '2px',
     transition: 'box-shadow 0.3s ease-out',
     ':hover': {
