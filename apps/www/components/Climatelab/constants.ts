7d7d2e52e0069e1c792dc2a18315fb585b062711
Tobias Maier <trm217@proton.me>
2023-01-05 12:59:19 +0100

    fix(www/climate): pass CDN_FRONTEND_BASE_URL to landing-page share-image

--- apps/www/components/Climatelab/constants.ts
+++ apps/www/components/Climatelab/constants.ts
@@ -1,9 +1,12 @@
+import { CDN_FRONTEND_BASE_URL } from '../../lib/constants'
+
 export const CLIMATE_LAB_ROLE = 'climate'
 export const CLIMATE_LAB_ACCESS_CAMPAIGN_ID =
   '3684f324-b694-4930-ad1a-d00a2e00934b'
 export const CLIMATE_LAB_CONTEXT = 'climate'
 
 export const CLIMATE_LAB_SHARE_IMAGE_URL =
+  CDN_FRONTEND_BASE_URL +
   '/static/climatelab/Klimaillu_Landingpage_5A47E1__Share.jpg'
 export const CLIMATE_LAB_IMG_URL =
   '/static/climatelab/Klimaillu_Landingpage_5A47E1_square_800px.jpg'
