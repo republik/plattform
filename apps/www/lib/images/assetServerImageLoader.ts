f24d69a0e11a27ff26c129cbef676fab4080b5e0
Tobias Maier <trm217@proton.me>
2023-01-05 15:34:12 +0100

    fix(www/asset-image): only append CDN_FRONTEND_BASE_URL if defined

--- apps/www/lib/images/assetServerImageLoader.ts
+++ apps/www/lib/images/assetServerImageLoader.ts
@@ -1,13 +1,23 @@
 import { ImageLoaderProps } from 'next/image'
-import { CDN_FRONTEND_BASE_URL } from '../constants'
+import { CDN_FRONTEND_BASE_URL, isDev } from '../constants'
 
 function assetServerImageLoader({
   src,
   width,
   quality = 75, // Quality is not yet supported by the assets-server
 }: ImageLoaderProps): string {
+  if (!CDN_FRONTEND_BASE_URL) {
+    if (isDev) {
+      console.warn(
+        'CDN_FRONTEND_BASE_URL is not set. Images will not be loaded from the asset-server.',
+      )
+    }
+    return src
+  }
   // Add asset-server prefix to relative URLs
-  const url = new URL(src.startsWith('/') ? CDN_FRONTEND_BASE_URL + src : src)
+  const url = new URL(
+    !src.startsWith('http') ? CDN_FRONTEND_BASE_URL + src : src,
+  )
   url.searchParams.set('resize', `${width}x`)
   url.searchParams.set('format', 'auto')
 
