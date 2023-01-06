5ef22cc715407d6b3783f482166df02ed9c24338
Tobias Maier <trm217@proton.me>
2023-01-06 14:06:37 +0100

    refactor(www/climate): render trial-form inside a loader

--- apps/www/components/Climatelab/LandingPage/ClimateLabTrialForm.tsx
+++ apps/www/components/Climatelab/LandingPage/ClimateLabTrialForm.tsx
@@ -1,5 +1,10 @@
 import { css } from 'glamor'
-import { Button, Interaction, useColorContext } from '@project-r/styleguide'
+import {
+  Button,
+  Interaction,
+  useColorContext,
+  Loader,
+} from '@project-r/styleguide'
 import {
   CLIMATE_LAB_ACCESS_CAMPAIGN_ID,
   CLIMATE_LAB_CONTEXT,
@@ -12,7 +17,7 @@ import { t } from '../../../lib/withT'
 
 const ClimateLabTrialform = () => {
   const [colorScheme] = useColorContext()
-  const { me } = useMe()
+  const { me, meLoading } = useMe()
   const isInClimateLab = me?.roles.some((role) => role === CLIMATE_LAB_ROLE)
 
   return (
@@ -21,30 +26,41 @@ const ClimateLabTrialform = () => {
       {...colorScheme.set('backgroundColor', 'default')}
       {...colorScheme.set('borderColor', 'climateBorder')}
     >
-      {isInClimateLab ? (
-        <>
-          <Interaction.H3>
-            {t('Climate/Trial/Heading/CampaignMember')}
-          </Interaction.H3>
-          <Button primary href={CLIMATE_LAB_URL} style={{ marginTop: 20 }}>
-            {t('Climate/Trial/Action/CampaignMember')}
-          </Button>
-        </>
-      ) : (
-        <div>
-          <Interaction.H3 style={{ marginBottom: -10 }}>
-            {t('Climate/Trial/Heading/NonCampaignMember')}
-          </Interaction.H3>
-          <TrialForm
-            accessCampaignId={CLIMATE_LAB_ACCESS_CAMPAIGN_ID}
-            campaign='climate'
-            context={CLIMATE_LAB_CONTEXT}
-            skipForMembers={false}
-            shouldSkipTrialForm={isInClimateLab}
-            payload={{}}
-          />
-        </div>
-      )}
+      <Loader
+        loading={meLoading}
+        render={() => (
+          <>
+            {isInClimateLab ? (
+              <>
+                <Interaction.H3>
+                  {t('Climate/Trial/Heading/CampaignMember')}
+                </Interaction.H3>
+                <Button
+                  primary
+                  href={CLIMATE_LAB_URL}
+                  style={{ marginTop: 20 }}
+                >
+                  {t('Climate/Trial/Action/CampaignMember')}
+                </Button>
+              </>
+            ) : (
+              <div>
+                <Interaction.H3 style={{ marginBottom: -10 }}>
+                  {t('Climate/Trial/Heading/NonCampaignMember')}
+                </Interaction.H3>
+                <TrialForm
+                  accessCampaignId={CLIMATE_LAB_ACCESS_CAMPAIGN_ID}
+                  campaign='climate'
+                  context={CLIMATE_LAB_CONTEXT}
+                  skipForMembers={false}
+                  shouldSkipTrialForm={isInClimateLab}
+                  payload={{}}
+                />
+              </div>
+            )}
+          </>
+        )}
+      />
     </div>
   )
 }
