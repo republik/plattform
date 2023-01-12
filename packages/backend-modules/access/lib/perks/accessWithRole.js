af74e8145fc0ef8b299449c1f9ae41a7876ebe02
Luciana Kolbeck <luciana.kolbeck@republik.ch>
2023-01-12 17:42:16 +0100

    fix: added check before removing member role granted by perks

--- packages/backend-modules/access/lib/perks/accessWithRole.js
+++ packages/backend-modules/access/lib/perks/accessWithRole.js
@@ -1,6 +1,8 @@
 const debug = require('debug')('access:lib:perks:accessWithRole')
 
-const { addRole, removeRole } = require('../memberships')
+const { findByRecipient } = require('../grants')
+const { addRole, removeRole, removeMemberRole } = require('../memberships')
+const memberRole = 'member'
 
 const give = async (campaign, grant, recipient, settings, t, pgdb) => {
   const isRoleAdded = await addRole(grant, recipient, pgdb, settings.role)
@@ -21,7 +23,17 @@ const give = async (campaign, grant, recipient, settings, t, pgdb) => {
 }
 
 const revoke = async (grant, recipient, settings, pgdb) => {
-  const isRoleRevoked = await removeRole(grant, recipient, pgdb, settings.role)
+  let isRoleRevoked = false
+  if (settings.role === memberRole) {
+    isRoleRevoked = await removeMemberRole(
+      grant,
+      recipient,
+      findByRecipient,
+      pgdb,
+    )
+  } else {
+    isRoleRevoked = await removeRole(grant, recipient, pgdb, settings.role)
+  }
   if (isRoleRevoked) {
     debug('revoke', {
       recipient: recipient.id,
