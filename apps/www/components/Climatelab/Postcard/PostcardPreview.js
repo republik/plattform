9f33c9c65017a7833d696d31374ad6284a0ffc83
Felix Michel <felix_michel@gmx.ch>
2023-01-16 18:09:48 +0100

    fix(www/gallery): postcard preview layout, line height

--- apps/www/components/Climatelab/Postcard/PostcardPreview.js
+++ apps/www/components/Climatelab/Postcard/PostcardPreview.js
@@ -5,6 +5,7 @@ import {
   fontFamilies,
   mediaQueries,
   useColorContext,
+  RawHtml,
 } from '@project-r/styleguide'
 import AssetImage from '../../../lib/images/AssetImage'
 import { useTranslation } from '../../../lib/withT'
@@ -24,13 +25,9 @@ const styles = {
     border: 'solid 2px f9f5ec',
     borderRadius: '2px',
     fontFamily: fontFamilies.sansSerifRegular,
-    fontSize: '12px',
-    lineHeight: '1.2',
     color: '#282828',
     [mediaQueries.mUp]: {
-      padding: '20px',
-      fontSize: '16px',
-      lineHeight: '1.5',
+      padding: '15px',
     },
   }),
   textArea: css({
@@ -38,39 +35,29 @@ const styles = {
     flexWrap: 'wrap',
     flexFlow: 'column',
     maxHeight: '100%',
-    wordBreak: 'normal',
-    overflowWrap: 'break-word',
+    lineHeight: '1.3',
     width: '67%',
-    borderRight: 'solid 1px #DADDDC',
-    marginBottom: '10px',
-    paddingRight: '10px',
-    [mediaQueries.mUp]: {
-      paddingRight: '20px',
-    },
-    ':first-child': {
-      flexBasis: '80%',
-    },
+    paddingRight: '15px',
   }),
   credit: css({
     position: 'absolute',
     bottom: 0,
     right: 0,
-    paddingBottom: '5px',
-    paddingRight: '10px',
+    paddingBottom: '3px',
+    paddingRight: '15px',
     fontSize: '0.5rem',
     [mediaQueries.mUp]: {
-      fontSize: '0.6rem',
+      fontSize: '0.65rem',
     },
   }),
   rightSide: css({
     display: 'flex',
     flexDirection: 'column',
     width: '33%',
-    paddingLeft: '10px',
+    paddingLeft: '15px',
+    marginBottom: '20px',
+    borderLeft: 'solid 1px #DADDDC',
     justifyContent: 'sprace-between',
-    [mediaQueries.mUp]: {
-      paddingLeft: '20px',
-    },
   }),
   image: css({
     borderImage: 'url(/static/climatelab/border-image.png) 32 round',
@@ -115,11 +102,21 @@ export const PostcardPreview = (props) => {
       </div>
 
       <div {...styles.textArea}>
-        <AutoTextSize mode='box'>{text}</AutoTextSize>
-
-        <AutoTextSize mode='oneline' maxFontSizePx={16}>
-          {author && author.name}
+        <AutoTextSize mode='box' maxFontSizePx={42}>
+          <RawHtml
+            dangerouslySetInnerHTML={{
+              __html: `${text} <br />${
+                author && author.name === 'Unbenannt' ? '' : author.name
+              }`,
+            }}
+          />
+          {/* {text} */}
         </AutoTextSize>
+        {/* <div style={{ width: '100%' }}>
+          <AutoTextSize mode='oneline' maxFontSizePx={16}>
+            {author && author.name}
+          </AutoTextSize>
+        </div> */}
       </div>
 
       <div {...styles.rightSide}>
