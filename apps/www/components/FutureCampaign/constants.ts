f224ca8f371215d786574320dbe8fc8316b77ae3
Olivier Baumann <olivierbaumann@me.com>
2023-02-09 09:54:21 -0300

    fix(copy): copy fix

--- apps/www/components/FutureCampaign/constants.ts
+++ apps/www/components/FutureCampaign/constants.ts
@@ -21,7 +21,7 @@ export const SLIDER_STEPS = [
   }, //  5 <= Selected < average CHF}
   {
     label: 'Mit dem Schwarm schwimmen',
-    text: 'Wie gesagt: Wir wollen nichts Unbedachtes von Ihnen. Wählen Sie den Betrag, der für Sie stimmt. Im Schnitt zahlen neue Mitstreiter wie Sie.',
+    text: 'Wählen Sie den Betrag, der für Sie stimmt. Mitstreiter, die ihren Preis selber wählen, zahlen im Schnitt 120 CHF',
     goodie: true,
     goodieText: 'Zum Abo-Kauf schenken wir Ihnen eine Jubiläumstasche dazu.',
     bonusHint: '',
