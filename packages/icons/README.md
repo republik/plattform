68d8c1f48a5d94eeacba87fade6e45f7e4296e48
Tobias Maier <trm217@proton.me>
2023-02-15 14:10:14 +0100

    chore(icons): dynamically create entry-points for the package based on the folder structure
    Co-Authored-By: Jeremy Stucki <70969+jerstucki@users.noreply.github.com>

---
+++ packages/icons/README.md
@@ -0,0 +1,16 @@
+# @republik/icons
+
+## Adding an icon
+
+To add an icon, simply add a `.svg` file into the `icons` folder.
+Once you run the `build` script again, your icon will be ready to use.
+You can import it from the entrypoint generated from it's location within the icons
+folder. See the explanation below on how the imports are grouped.
+
+### How to import
+
+| Icon-path | How to import |
+| ------- | ------- |
+| `/lib/svg/MenuIcon.svg` | `import { MenuIcon } from '@republik/icons` |
+| `/lib/svg/md/SearchIcon.svg` | `import { SearchIcon } from '@republik/icons/md'` |
+| `/lib/svg/md/md/CopyIcon.svg` (Please don't nest subfolders) | `import { CopyIcon } from '@republik/icons/mdMd'` |
--- packages/icons/vite.config.ts
+++ packages/icons/vite.config.ts
@@ -1,7 +1,39 @@
 import { defineConfig } from 'vite'
-import { resolve } from 'path'
+import path from 'path'
+import glob from 'glob'
+import { fileURLToPath } from 'url'
 import react from '@vitejs/plugin-react'
 import dts from 'vite-plugin-dts'
+
+// Generate entry points to bundle all icons from the svg-folder
+// While providing a separate entry point for each sub-folder as well.
+const entryObject = Object.fromEntries(
+  glob
+    .sync('./lib/components/**/index.ts')
+    .map((file) => [
+      // This remove `src/` as well as the file extension from each
+      // file, so e.g. src/nested/foo.js becomes nested/foo
+      path.relative(
+        'lib',
+        file.slice(0, file.length - path.extname(file).length),
+      ),
+      // This expands the relative paths to absolute paths, so e.g.
+      // src/nested/foo becomes /project/src/nested/foo.js
+      fileURLToPath(new URL(file, import.meta.url)),
+    ])
+    .map((keyVal) => {
+      const key = keyVal[0]
+        .replace(/components\//, '')
+        .replace(/\/index$/, '')
+        // Transform 'md/md' to 'mdMd'
+        .replace(/\/[a-zA-Z0-9]{1}/, (chars) => chars[1].toUpperCase())
+
+      return [
+        key === 'index' ? 'base' : key, // special case for root index.ts
+        keyVal[1],
+      ]
+    }),
+)
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,25 +41,22 @@ export default defineConfig({
     react(),
     dts({
       insertTypesEntry: true,
-    }),    
+    }),
   ],
   build: {
     lib: {
-      entry: [
-        resolve(__dirname, 'lib/components/index.ts'),
-        resolve(__dirname, 'lib/components/md/index.ts'),
-        resolve(__dirname, 'lib/components/hr/index.ts'),
-      ],
-      name: "RepublikIcons",
-      fileName: 'republik-icons',
+      entry: entryObject,
+      name: 'RepublikIcons',
     },
     rollupOptions: {
-      external: ['react'],
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
       output: {
         globals: {
           react: 'React',
+          'react-dom': 'ReactDOM',
+          'react/jsx-runtime': 'jsxRuntime',
         },
       },
-    }
-  }
+    },
+  },
 })
