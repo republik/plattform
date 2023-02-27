ab45e49eb3ea7e431bac5b9073de26e3cab1a668
Anna Traussnig <anna.traussnig@gmail.com>
2023-02-27 11:57:00 +0100

    feat(ClimateQuestionnaire): fix dark mode colors

--- apps/www/components/Questionnaire/Submissions/QuestionChart.tsx
+++ apps/www/components/Questionnaire/Submissions/QuestionChart.tsx
@@ -3,6 +3,8 @@ import { format } from 'd3-format'
 import { scaleLinear } from 'd3-scale'
 import { css } from 'glamor'
 import { Fragment } from 'react'
+import { useColorContext } from '@project-r/styleguide'
+
 type Props = {
   answers: { answer: string; value: number }[]
   key: string
@@ -43,29 +45,28 @@ const pct = format('.1~%') // rounded to 1/10
 // const pct = format('.0%') // rounded
 
 export const QuestionSummaryChart = ({ answers }: Props) => {
+  const [colorScheme] = useColorContext()
   const y = scaleLinear([0, max(answers, (d) => d.value)], [0, 100])
 
   return (
-    <>
-      <div {...styles.chart}>
-        {answers.map((d) => {
-          return (
-            <Fragment key={d.answer}>
-              <div {...styles.barAndLabel}>
-                <div {...styles.label}>{pct(d.value)}</div>
+    <div {...styles.chart} {...colorScheme.set('color', 'text')}>
+      {answers.map((d) => {
+        return (
+          <Fragment key={d.answer}>
+            <div {...styles.barAndLabel}>
+              <div {...styles.label}>{pct(d.value)}</div>
 
-                <div
-                  {...styles.bar}
-                  style={{
-                    height: y(d.value),
-                  }}
-                ></div>
-              </div>
-              <div {...styles.label}>{d.answer}</div>
-            </Fragment>
-          )
-        })}
-      </div>
-    </>
+              <div
+                {...styles.bar}
+                style={{
+                  height: y(d.value),
+                }}
+              ></div>
+            </div>
+            <div {...styles.label}>{d.answer}</div>
+          </Fragment>
+        )
+      })}
+    </div>
   )
 }
