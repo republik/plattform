a1a662bd7f3872fd96cb25d6ad4b4e35c2428865
Felix Michel <felix_michel@gmx.ch>
2023-03-01 13:23:27 +0100

    fix(questionnaire): use flexColumns for masonry

--- apps/www/components/Questionnaire/Submissions/AnswersGrid.tsx
+++ apps/www/components/Questionnaire/Submissions/AnswersGrid.tsx
@@ -4,19 +4,23 @@ import { mediaQueries } from '@project-r/styleguide'
 
 const styles = {
   grid: css({
-    display: 'grid',
-    gap: '24px',
-    gridAutoRows: 'auto',
-    gridAutoFlow: 'row dense',
-    padding: '48px 0',
+    display: 'flex',
+    columnWidth: '360px',
+    flexWrap: 'wrap',
+    gap: '1rem',
+    marginBottom: 30,
     [mediaQueries.mUp]: {
-      gridTemplateColumns: 'repeat(auto-fill, minmax(360px, 1fr))',
+      columnWidth: '360px',
     },
   }),
   card: css({
+    breakInside: 'avoid',
     transition: 'transform .3s ease-in-out',
     ':hover': {
       transform: 'scale(1.03) !important',
+    },
+    [mediaQueries.mUp]: {
+      width: '360px',
     },
   }),
 }
@@ -26,20 +30,10 @@ export const AnswersGrid = ({ children }: { children: ReactNode }) => {
 }
 
 export const AnswersGridCard = ({
-  textLength,
   children,
 }: {
   textLength: number
   children: ReactNode
 }) => {
-  return (
-    <div
-      {...styles.card}
-      style={{
-        gridRow: `span ${Math.max(1, Math.round(textLength / 60))}`, // Tweak the numerator here to adjust relative height of the cards in the grid
-      }}
-    >
-      {children}
-    </div>
-  )
+  return <div {...styles.card}>{children}</div>
 }
