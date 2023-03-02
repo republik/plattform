10afd4e0d655ad4d44eb5e74a8c1334e4d586135
Felix Michel <felix_michel@gmx.ch>
2023-03-02 16:15:09 +0100

    chore(questionnaire): remove log

--- apps/www/components/Questionnaire/Submissions/QuestionFeatured.js
+++ apps/www/components/Questionnaire/Submissions/QuestionFeatured.js
@@ -144,8 +144,6 @@ const AnswerGridOverview = ({ slug, question, valueLength, hint }) => {
     },
   })
 
-  console.log(hint)
-
   // TODO filter answers by length, make Carousel with short answers and carousels with long answers
 
   return (
