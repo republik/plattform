65d13037f9f3e7715eb5b60917dad7cc0c8549bd
Tobias Maier <tobias.maier@republik.ch>
2023-03-08 11:47:50 +0100

    feat(icons): add 'size' prop to allow setting both width and height at once

--- packages/icons/.svgrrc.js
+++ packages/icons/.svgrrc.js
@@ -11,7 +11,27 @@ module.exports = {
   svgProps: {
     fill: "currentColor",
     stroke: "currentColor",
-    style: "{props.style || { verticalAlign: 'middle' }}"
+    style: "{props.style || { verticalAlign: 'middle' }}",
+    width: '{props.width || props.size || "1em"}',
+    height: '{props.height || props.size || "1em"}',
   },
-  indexTemplate: require("./lib/icon-template.cjs")
+  indexTemplate: require("./lib/icon-template.cjs"),
+  template: (variables, { tpl }) => {
+    return tpl`
+    ${variables.imports};
+    ${variables.interfaces};
+    const ${variables.componentName} = (
+      props: 
+        SVGProps<SVGSVGElement> & 
+        {
+          // Applies to both the width & height prop
+          size?: number | string 
+        } 
+      ) => (
+      ${variables.jsx}
+    );
+
+    ${variables.exports};
+    `
+  }
 }
