a40dd0743baf31a9f3e7977b11b66706d71daebb
Felix Michel <felix_michel@gmx.ch>
2023-03-09 15:00:28 +0100

    feat(EdgeQuestion): add CardsOverview with citations

--- apps/www/components/Climatelab/EdgeQuestion/index.tsx
+++ apps/www/components/Climatelab/EdgeQuestion/index.tsx
@@ -1,9 +1,33 @@
 import { css } from 'glamor'
-import { useColorContext } from '@project-r/styleguide'
-import { useEffect, useState } from 'react'
+import {
+  useColorContext,
+  Center,
+  Editorial,
+  inQuotes,
+} from '@project-r/styleguide'
 import QuestionScroll from './QuestionScroll'
 
-const styles = {}
+const styles = {
+  grid: css({
+    display: 'flex',
+    flexWrap: 'wrap',
+    gap: '1rem',
+    marginBottom: 60,
+  }),
+  card: css({
+    flex: '1 1 0',
+    backgroundColor: 'red',
+    background: 'rgba(255,255,255,0.5)',
+    borderRadius: 10,
+    padding: 24,
+    color: 'black',
+    height: '100%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    textAlign: 'center',
+  }),
+}
 
 const OVERVIEW_DATA = [
   {
@@ -21,11 +45,37 @@ type EdgeQuestionProps = { contentPath: string }
 
 const EdgeQuestion: React.FC<EdgeQuestionProps> = ({ contentPath }) => {
   return (
-    <div>
-      {OVERVIEW_DATA.map(({ name, excerpt }, idx) => {
-        return <div key={idx}>{excerpt}</div>
-      })}
+    <Center>
+      <CardsOverview data={OVERVIEW_DATA} />
       <QuestionScroll contentPath={contentPath} />
+    </Center>
+  )
+}
+
+const CardsOverview: React.FC<{
+  data: Array<{ name: string; excerpt: string }>
+}> = ({ data }) => {
+  return (
+    <div {...styles.grid}>
+      {data.map(({ name, excerpt }, idx) => {
+        return (
+          <div {...styles.card} key={idx}>
+            <div>
+              <Editorial.Question style={{ marginTop: 0 }}>
+                {inQuotes(excerpt)}
+              </Editorial.Question>
+              <Editorial.Credit
+                style={{
+                  marginTop: '0',
+                  paddingTop: '20px',
+                }}
+              >
+                Von <span style={{ textDecoration: 'underline' }}>{name}</span>
+              </Editorial.Credit>
+            </div>
+          </div>
+        )
+      })}
     </div>
   )
 }
