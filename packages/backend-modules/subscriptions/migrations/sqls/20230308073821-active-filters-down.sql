4ccdfb3185b5e59f3bfb3102d1f4596df87fbf62
Patrick Venetz <venetz@gmail.com>
2023-03-09 13:30:15 +0100

    fix(subscriptions): update filters for Document subscriptions

--- packages/backend-modules/subscriptions/migrations/sqls/20230308073821-active-filters-down.sql
+++ packages/backend-modules/subscriptions/migrations/sqls/20230308073821-active-filters-down.sql
@@ -1,4 +1,10 @@
 UPDATE subscriptions s SET filters = NULL
+WHERE s."objectType" = 'Document'
+  AND s.active = TRUE
+  AND s.filters ? 'Document'
+;
+
+UPDATE subscriptions s SET filters = NULL
 WHERE s."objectType" = 'User'
   AND s.active = TRUE
   AND s.filters ? 'Comment'
--- packages/backend-modules/subscriptions/migrations/sqls/20230308073821-active-filters-up.sql
+++ packages/backend-modules/subscriptions/migrations/sqls/20230308073821-active-filters-up.sql
@@ -3,3 +3,9 @@ WHERE s."objectType" = 'User'
   AND s.active = TRUE
   AND s.filters IS NULL
 ;
+
+UPDATE subscriptions s SET filters = '["Document"]'
+WHERE s."objectType" = 'Document'
+  AND s.active = TRUE
+  AND s.filters IS NULL
+;
