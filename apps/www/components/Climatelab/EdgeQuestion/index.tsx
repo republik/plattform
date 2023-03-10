9e88d79b2b91cc48233aabac5b60cb40a511c069
Felix Michel <felix_michel@gmx.ch>
2023-03-10 17:50:06 +0100

    feat(EdgeQuestion): colorful citations

--- apps/www/components/Climatelab/EdgeQuestion/index.tsx
+++ apps/www/components/Climatelab/EdgeQuestion/index.tsx
@@ -6,121 +6,175 @@ import {
   inQuotes,
   slug,
   Breakout,
-  Container,
+  fontStyles,
+  ColorContextProvider,
+  ColorContextLocalExtension,
 } from '@project-r/styleguide'
 import QuestionScroll from './QuestionScroll'
 import NextLink from 'next/link'
-import {
-  AnswersGrid,
-  AnswersGridCard,
-} from '../../Questionnaire/Submissions/AnswersGrid'
 
 const styles = {
+  grid: css({
+    display: 'flex',
+    flexDirection: 'column',
+    // gap: '1rem',
+    marginBottom: 60,
+  }),
   card: css({
-    background: 'rgba(255,255,255,0.5)',
-    borderRadius: 10,
-    padding: 24,
-    color: 'black',
-    height: '100%',
+    marginBottom: 10,
+    padding: 10,
+    maxWidth: '600px',
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
-    textAlign: 'center',
+    borderRadius: '10px 10px 10px 3px',
+  }),
+  boldCitation: css({
+    ...fontStyles.serifBold32,
   }),
 }
 
 const OVERVIEW_DATA = [
   {
     name: 'Rebecca Solnit',
+    tagline: 'Schriftstellerin',
     excerpt:
       'Die große Mehrheit der Menschen auf der Erde lebt ohnehin in Armut.',
+    color: 'color1',
   },
   {
     name: 'Marcel Hänggi',
+    tagline: 'Klimadude',
     excerpt: 'Ich wünschte mir mehr gesellschaftspolitische Vorstellungskraft.',
+    color: 'color2',
   },
   {
     name: 'Rebecca Solnit',
+    tagline: 'Schriftstellerin',
     excerpt:
       'Die große Mehrheit der Menschen auf der Erde lebt ohnehin in Armut.',
+    color: 'color4',
   },
   {
     name: 'Marcel Hänggi',
+    tagline: 'Klimadude',
     excerpt: 'Ich wünschte mir mehr gesellschaftspolitische Vorstellungskraft.',
+    color: 'color5',
   },
   {
     name: 'Rebecca Solnit',
+    tagline: 'Schriftstellerin',
     excerpt: 'Die große Mehrheit der Menschen',
+    color: 'color3',
   },
   {
     name: 'Marcel Hänggi',
+    tagline: 'Klimadude',
     excerpt: 'Gesellschaftspolitische Vorstellungskraft',
+    color: 'color6',
+  },
+  {
+    name: 'Marcel Hänggi',
+    tagline: 'Klimadude',
+    excerpt: 'Gesellschaftspolitische Vorstellungskraft',
+    color: 'color7',
   },
 ]
+
+const localColors = {
+  dark: {
+    color1: '#7BA7D2',
+    color2: '#1189C7',
+    color3: '#c6aa90',
+    color4: '#f06a54',
+    color5: '#b8b6bd',
+    color6: '#96c5e4',
+    color7: '#4CADAA',
+  },
+  light: {
+    color1: '#3f7dba',
+    color2: '#0c6695',
+    color3: '#a87e57',
+    color4: '#df3013',
+    color5: '#888590',
+    color6: '#4a9bd0',
+    color7: '#38817f',
+  },
+}
 
 type EdgeQuestionProps = { contentPath: string }
 
 const EdgeQuestion: React.FC<EdgeQuestionProps> = ({ contentPath }) => {
-  const [colorScheme] = useColorContext()
   return (
-    <>
-      <div
-        {...colorScheme.set('background-color', 'divider')}
-        style={{
-          // backgroundColor: '#5A47E1',
-          margin: '0 auto',
-          padding: '46px 0',
-        }}
-      >
-        <Container>
-          <CardsOverview data={OVERVIEW_DATA} />
-        </Container>
-      </div>
-      <Center>
-        <QuestionScroll contentPath={contentPath} />
-      </Center>
-    </>
+    <Center>
+      <Breakout size='breakout'>
+        <CardsOverview data={OVERVIEW_DATA} />
+      </Breakout>
+      <QuestionScroll contentPath={contentPath} />
+    </Center>
   )
 }
 
 const CardsOverview: React.FC<{
-  data: Array<{ name: string; excerpt: string }>
+  data: Array<{ name: string; excerpt: string; color: string; tagline: string }>
 }> = ({ data }) => {
   return (
-    <AnswersGrid>
-      {data.map(({ name, excerpt }, idx) => {
+    <div {...styles.grid}>
+      {data.map(({ name, excerpt, color, tagline }, idx) => {
         return (
-          <AnswersGridCard key={idx}>
+          <div
+            {...styles.card}
+            key={idx}
+            style={{
+              alignSelf: idx % 3 === 0 ? 'flex-end' : 'flex-start',
+              textAlign: idx % 3 === 0 ? 'right' : 'left',
+            }}
+          >
             <NextLink href={`#${slug(name)}`}>
               <a style={{ textDecoration: 'none' }}>
-                <div {...styles.card}>
-                  <div>
-                    <Editorial.Question
-                      style={{ marginTop: 0, color: 'black' }}
-                    >
-                      {inQuotes(excerpt)}
-                    </Editorial.Question>
-                    <Editorial.Credit
-                      style={{
-                        marginTop: '0',
-                        paddingTop: '20px',
-                        color: 'black',
-                      }}
-                    >
-                      Von{' '}
-                      <span style={{ textDecoration: 'underline' }}>
-                        {name}
-                      </span>
-                    </Editorial.Credit>
-                  </div>
-                </div>
+                <ColorContextLocalExtension localColors={localColors}>
+                  <GetColorScheme>
+                    {(colorScheme) => (
+                      <>
+                        <div>
+                          <Editorial.Question
+                            style={{ marginTop: 0 }}
+                            {...styles.boldCitation}
+                            {...colorScheme.set('color', color)}
+                          >
+                            {inQuotes(excerpt)}
+                          </Editorial.Question>
+                          <Editorial.Credit
+                            style={{
+                              marginTop: '0',
+                              paddingTop: '20px',
+                            }}
+                            {...colorScheme.set('color', color)}
+                          >
+                            <span>{name}</span>
+                            <span>
+                              {', '}
+                              {tagline}
+                            </span>
+                          </Editorial.Credit>
+                        </div>
+                      </>
+                    )}
+                  </GetColorScheme>
+                </ColorContextLocalExtension>
               </a>
             </NextLink>
-          </AnswersGridCard>
+          </div>
         )
       })}
-    </AnswersGrid>
+    </div>
   )
 }
 
 export default EdgeQuestion
+
+const GetColorScheme = ({ children }) => {
+  const [colorScheme] = useColorContext()
+
+  return children(colorScheme)
+}
