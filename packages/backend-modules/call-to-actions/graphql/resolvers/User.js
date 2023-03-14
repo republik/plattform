722431b142a9075d93298eab7f862044d8782e0e
Patrick Venetz <venetz@gmail.com>
2023-03-14 07:47:27 +0100

    fix(call-to-actions): return all CTA between beginAt, endAt

--- packages/backend-modules/call-to-actions/graphql/resolvers/User.js
+++ packages/backend-modules/call-to-actions/graphql/resolvers/User.js
@@ -15,42 +15,14 @@ module.exports = {
     return getCache(user.id, context).cache(async () => {
       const now = dayjs()
 
-      // Find latest ackknowledgedAt
-      const lastestAcknowledgedAt =
-        await context.pgdb.public.callToActions.findOneFieldOnly(
-          {
-            userId: user.id,
-            'acknowledgedAt !=': null,
-          },
-          'acknowledgedAt',
-          {
-            orderBy: { acknowledgedAt: 'desc' },
-            limit: 1,
-          },
-        )
-
-      if (lastestAcknowledgedAt) {
-        // Get end of day of acknowledgedAt as thresholdTimestamp
-        const thresholdTimestamp = dayjs(lastestAcknowledgedAt).endOf('day')
-
-        // Check if thresholdTimestamp is after now
-        if (!now.isAfter(thresholdTimestamp)) {
-          return []
-        }
-      }
-
       // Find oldest callToActions row by oldest beginAt
       return context.pgdb.public.callToActions.find(
         {
           userId: user.id,
           'beginAt <=': now,
           'endAt >': now,
-          acknowledgedAt: null,
         },
-        {
-          orderBy: 'beginAt',
-          limit: 1,
-        },
+        { orderBy: 'beginAt' },
       )
     })
   },
