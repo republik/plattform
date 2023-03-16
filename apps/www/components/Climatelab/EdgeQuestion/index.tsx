d9f53bc330522c769247b6ad5f173a34a2240ad4
Felix Michel <felix_michel@gmx.ch>
2023-03-16 14:23:45 +0100

    feat(edgequestion): excerpt link styling

--- apps/www/components/Climatelab/EdgeQuestion/index.tsx
+++ apps/www/components/Climatelab/EdgeQuestion/index.tsx
@@ -7,8 +7,8 @@ import {
   slug,
   Breakout,
   fontStyles,
-  ColorContextProvider,
   ColorContextLocalExtension,
+  ChevronRightIcon,
 } from '@project-r/styleguide'
 import QuestionScroll from './QuestionScroll'
 import NextLink from 'next/link'
@@ -23,11 +23,15 @@ const styles = {
   card: css({
     marginBottom: 10,
     padding: 10,
-    maxWidth: '600px',
+    maxWidth: '700px',
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
     borderRadius: '10px 10px 10px 3px',
+    transition: '500ms filter',
+    ':hover': {
+      filter: 'brightness(85%)',
+    },
   }),
   boldCitation: css({
     ...fontStyles.serifBold32,
@@ -36,48 +40,68 @@ const styles = {
 
 const OVERVIEW_DATA = [
   {
-    name: 'Rebecca Solnit',
-    tagline: 'Schriftstellerin',
+    name: 'Claudia Kemfert',
+    tagline:
+      'Leiterin der Abteilung Energie, Verkehr, Umwelt beim Deutschen Institut für Wirtschaftsforschung',
     excerpt:
-      'Die große Mehrheit der Menschen auf der Erde lebt ohnehin in Armut.',
+      'Die Uhr tickt. Es geht um nichts weniger, als die Erde ungefähr so zu erhalten, wie wir sie kennen.',
     color: 'color1',
   },
   {
-    name: 'Marcel Hänggi',
-    tagline: 'Klimadude',
-    excerpt: 'Ich wünschte mir mehr gesellschaftspolitische Vorstellungskraft.',
+    name: 'Patrick Hofstetter',
+    tagline: 'Klimaschutzexperte des WWF Schweiz',
+    excerpt:
+      'Ich wünsche mir, dass alle verstehen, dass die Klimakrise real ist, dass unsere Warnungen auf Fakten beruhen und dass die heute nötigen Investitionen und Verhaltensänderungen viel kleiner sind als alles, was uns das Klimachaos aufzwingen wird.',
     color: 'color2',
   },
   {
-    name: 'Rebecca Solnit',
-    tagline: 'Schriftstellerin',
+    name: 'Sherry Rehman',
+    tagline: 'Ministerin für Klimawandel von Pakistan',
     excerpt:
-      'Die große Mehrheit der Menschen auf der Erde lebt ohnehin in Armut.',
+      'Es gibt kein Patentrezept zur Lösung der Klimakrise. Die Klimakrise ist ein vielschichtiges Problem, das einen ganzheitlichen Ansatz erfordert.',
     color: 'color4',
   },
   {
-    name: 'Marcel Hänggi',
-    tagline: 'Klimadude',
-    excerpt: 'Ich wünschte mir mehr gesellschaftspolitische Vorstellungskraft.',
+    name: 'Bernd Ulrich',
+    tagline: 'Stellvertretender Chefredakteur, Die ZEIT',
+    excerpt:
+      'Ich würde mir wünschen, dass die Politiker:innen all ihr gewohntes und im letzten Jahrhundert erlerntes politisches Denken einmal durch die klimapolitische Relativitätstheorie schicken würden.',
     color: 'color5',
   },
   {
-    name: 'Rebecca Solnit',
-    tagline: 'Schriftstellerin',
-    excerpt: 'Die große Mehrheit der Menschen',
+    name: 'Mitzi Jonelle Tan',
+    tagline: '«Fridays for Future»-Aktivistin aus den Philippinen',
+    excerpt:
+      'Was den Klimawandel wirklich zur Krise werden lässt, ist die historische Ausbeutung von marginalisierten Gruppen, vor allem im Globalen Süden, vor allem durch den Globalen Norden.',
     color: 'color3',
   },
   {
-    name: 'Marcel Hänggi',
-    tagline: 'Klimadude',
-    excerpt: 'Gesellschaftspolitische Vorstellungskraft',
+    name: 'Rebecca Solnit',
+    tagline: 'Schriftstellerin, Mitgründerin des Klimaprojekts Not too Late',
+    excerpt:
+      'Die aktuelle Klimarevolution könnte uns in allen Bereichen wohlhabender machen. ',
     color: 'color6',
   },
   {
-    name: 'Marcel Hänggi',
-    tagline: 'Klimadude',
-    excerpt: 'Gesellschaftspolitische Vorstellungskraft',
+    name: 'Reinhard Steurer',
+    tagline:
+      'assoz. Professor für Klimapolitik an der Universität für Bodenkultur in Wien',
+    excerpt:
+      'Ich wünschte, eine große Mehrheit würde verstehen, dass Klimaschutz kein „grünes Anliegen“ ist. Es geht um den Schutz von Leben und von unserer Zivilisation.',
     color: 'color7',
+  },
+  {
+    name: 'Claudia Traidl-Hoffmann',
+    tagline: 'Professorin für Umweltmedizin',
+    excerpt:
+      'Selbst wenn wir es als Weltgemeinschaft schaffen sollten, die Erderwärmung auf 1,5 Grad zu begrenzen, bedeutet das nicht, dass alles beim Alten bleiben wird.',
+    color: 'color1',
+  },
+  {
+    name: 'Kimberly Nicholas',
+    tagline: 'Nachhaltigkeitswissenschaftlerin an der Universität Lund',
+    excerpt: 'It’s warming. It’s us. We’re sure. It’s bad. We can fix it.',
+    color: 'color2',
   },
 ]
 
@@ -158,6 +182,7 @@ const CardsOverview: React.FC<{
                             style={{
                               marginTop: '0',
                               paddingTop: '20px',
+                              textDecoration: 'underline',
                             }}
                             {...colorScheme.set('color', color)}
                           >
@@ -166,6 +191,7 @@ const CardsOverview: React.FC<{
                               {', '}
                               {tagline}
                             </span>
+                            <ChevronRightIcon />
                           </Editorial.Credit>
                         </div>
                       </>
