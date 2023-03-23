e1b289785a8c688a99cd61537a6dafb257360a6b
Felix Michel <felix_michel@gmx.ch>
2023-03-23 15:19:36 +0100

    feat(dot): circle it up

--- apps/www/components/Dot/index.tsx
+++ apps/www/components/Dot/index.tsx
@@ -9,19 +9,20 @@ import {
   InfoBoxTitle,
   InfoBoxText,
   useColorContext,
-  useHeaderHeight,
+  mediaQueries,
   fontStyles,
 } from '@project-r/styleguide'
 import Frame from '../Frame'
 
 // GENERIC-Y STUFF
 
-const RADIUS = 15
-const PADDING_TOP = 250
+const RADIUS = 9
+const SMALL_RADIUS = 6
+const PADDING_TOP = 150
 const PADDING_LEFT = 0
-const CIRCLE_PADDING = 5
+const CIRCLE_PADDING = 3
 const COLUMNS = 10
-const SIZE = RADIUS + CIRCLE_PADDING
+const SIZE = 2 * RADIUS + CIRCLE_PADDING
 
 const dataSet = [...Array(100)].map((d, i) => {
   const colIndex = i % COLUMNS
@@ -170,45 +171,49 @@ const StoryGraphic = ({ highlighted }: { highlighted: number }) => {
       <g transform={`translate(${PADDING_LEFT}, ${PADDING_TOP})`}>
         {dataSet.map((d, i) => {
           return (
-            <motion.rect
+            <motion.circle
               key={`ref-age-29-${i}`}
               transition={{ duration: 0.5 }}
               variants={defineVariants(
                 {
                   y: d.cy,
                   x: d.cx,
+                  r: 0,
                   opacity: 0,
                 },
                 {
                   step1: {
                     y: d.cy,
                     x: d.cx,
-                    width: d.r,
-                    height: d.r,
+                    r: i > 40 ? SMALL_RADIUS : d.r,
+                    // width: d.r,
+                    // height: d.r,
                     opacity: 1,
                     fill: i > 40 ? '#737373' : '#0ddfdc',
                   },
                   step2: {
                     y: d.cy,
                     x: d.cx,
-                    width: d.r,
-                    height: d.r,
+                    r: i > 62 ? SMALL_RADIUS : d.r,
+                    // width: d.r,
+                    // height: d.r,
                     opacity: 1,
                     fill:
                       i > 40 && i < 63
                         ? '#2efffc'
-                        : i >= 63
+                        : i > 62
                         ? '#737373'
                         : '#2a9492',
                   },
                   step3: {
                     y: d.cy,
                     x: d.cx,
-                    width: d.r,
-                    height: d.r,
+                    r: i > 72 ? SMALL_RADIUS : d.r,
+                    // width: d.r,
+                    // height: d.r,
                     opacity: 1,
                     fill:
-                      i > 63 && i < 73
+                      i > 62 && i < 73
                         ? '#2efffc'
                         : i >= 73
                         ? '#737373'
@@ -217,8 +222,9 @@ const StoryGraphic = ({ highlighted }: { highlighted: number }) => {
                   step4: {
                     y: d.cy,
                     x: d.cx,
-                    width: d.r,
-                    height: d.r,
+                    r: i > 77 ? SMALL_RADIUS : d.r,
+                    // width: d.r,
+                    // height: d.r,
                     opacity: 1,
                     fill:
                       i > 72 && i < 78
@@ -230,11 +236,12 @@ const StoryGraphic = ({ highlighted }: { highlighted: number }) => {
                   step5: {
                     y: d.cy,
                     x: d.cx,
-                    width: d.r,
-                    height: d.r,
+                    r: i > 82 ? SMALL_RADIUS : d.r,
+                    // width: d.r,
+                    // height: d.r,
                     opacity: 1,
                     fill:
-                      i > 78 && i < 83
+                      i > 77 && i < 83
                         ? '#2efffc'
                         : i >= 83
                         ? '#737373'
@@ -242,7 +249,7 @@ const StoryGraphic = ({ highlighted }: { highlighted: number }) => {
                   },
                 },
               )}
-            ></motion.rect>
+            ></motion.circle>
           )
         })}
 
@@ -250,31 +257,31 @@ const StoryGraphic = ({ highlighted }: { highlighted: number }) => {
           {...styles.label}
           {...colorScheme.set('fill', 'text')}
           variants={defineVariants(
-            { x: 20, y: 225, opacity: 0 },
+            { x: 5, y: 225, opacity: 0 },
             {
               step1: {
-                x: 20,
+                x: 5,
                 y: 225,
                 opacity: 1,
                 transition: { duration: 0.5 },
               },
               step2: {
-                x: 20,
+                x: 5,
                 y: 225,
                 opacity: 1,
               },
               step3: {
-                x: 20,
+                x: 5,
                 y: 225,
                 opacity: 1,
               },
               step4: {
-                x: 20,
+                x: 5,
                 y: 225,
                 opacity: 1,
               },
               step5: {
-                x: 20,
+                x: 5,
                 y: 225,
                 opacity: 1,
               },
@@ -291,7 +298,7 @@ const StoryGraphic = ({ highlighted }: { highlighted: number }) => {
       <g transform={`translate(${235}, ${PADDING_TOP})`}>
         {dataSet.map((d, i) => {
           return (
-            <motion.rect
+            <motion.circle
               key={`ref-age30-34-${i}`}
               transition={{ duration: 0.5 }}
               variants={defineVariants(
@@ -299,25 +306,28 @@ const StoryGraphic = ({ highlighted }: { highlighted: number }) => {
                   y: d.cy,
                   x: d.cx,
                   opacity: 0,
+                  r: 0,
                 },
                 {
                   step1: {
                     y: d.cy,
                     x: d.cx,
-                    width: d.r,
-                    height: d.r,
+                    r: i > 39 ? SMALL_RADIUS : d.r,
+                    // width: d.r,
+                    // height: d.r,
                     opacity: 1,
-                    fill: i >= 40 ? '#737373' : '#ab25ff',
+                    fill: i > 39 ? '#737373' : '#b743ff',
                   },
                   step2: {
                     y: d.cy,
                     x: d.cx,
-                    width: d.r,
-                    height: d.r,
+                    r: i > 60 ? SMALL_RADIUS : d.r,
+                    // width: d.r,
+                    // height: d.r,
                     opacity: 1,
                     fill:
-                      i >= 41 && i < 61
-                        ? '#ab25ff'
+                      i > 39 && i < 61
+                        ? '#b743ff'
                         : i >= 61
                         ? '#737373'
                         : '#b481d3',
@@ -325,12 +335,13 @@ const StoryGraphic = ({ highlighted }: { highlighted: number }) => {
                   step3: {
                     y: d.cy,
                     x: d.cx,
-                    width: d.r,
-                    height: d.r,
+                    r: i > 71 ? SMALL_RADIUS : d.r,
+                    // width: d.r,
+                    // height: d.r,
                     opacity: 1,
                     fill:
-                      i >= 62 && i < 72
-                        ? '#ab25ff'
+                      i > 60 && i < 72
+                        ? '#b743ff'
                         : i >= 72
                         ? '#737373'
                         : '#b481d3',
@@ -339,12 +350,13 @@ const StoryGraphic = ({ highlighted }: { highlighted: number }) => {
                   step4: {
                     y: d.cy,
                     x: d.cx,
-                    width: d.r,
-                    height: d.r,
+                    r: i > 76 ? SMALL_RADIUS : d.r,
+                    // width: d.r,
+                    // height: d.r,
                     opacity: 1,
                     fill:
-                      i >= 73 && i < 77
-                        ? '#ab25ff'
+                      i > 71 && i < 77
+                        ? '#b743ff'
                         : i >= 77
                         ? '#737373'
                         : '#b481d3',
@@ -352,19 +364,20 @@ const StoryGraphic = ({ highlighted }: { highlighted: number }) => {
                   step5: {
                     y: d.cy,
                     x: d.cx,
-                    width: d.r,
-                    height: d.r,
+                    r: i > 81 ? SMALL_RADIUS : d.r,
+                    // width: d.r,
+                    // height: d.r,
                     opacity: 1,
                     fill:
-                      i >= 78 && i < 82
-                        ? '#ab25ff'
+                      i > 76 && i < 82
+                        ? '#b743ff'
                         : i >= 82
                         ? '#737373'
                         : '#b481d3',
                   },
                 },
               )}
-            ></motion.rect>
+            ></motion.circle>
           )
         })}
 
@@ -372,31 +385,31 @@ const StoryGraphic = ({ highlighted }: { highlighted: number }) => {
           {...styles.label}
           {...colorScheme.set('fill', 'text')}
           variants={defineVariants(
-            { x: 20, y: 225, opacity: 0 },
+            { x: 5, y: 225, opacity: 0 },
             {
               step1: {
-                x: 20,
+                x: 5,
                 y: 225,
                 opacity: 1,
                 transition: { duration: 0.5 },
               },
               step2: {
-                x: 20,
+                x: 5,
                 y: 225,
                 opacity: 1,
               },
               step3: {
-                x: 20,
+                x: 5,
                 y: 225,
                 opacity: 1,
               },
               step4: {
-                x: 20,
+                x: 5,
                 y: 225,
                 opacity: 1,
               },
               step5: {
-                x: 20,
+                x: 5,
                 y: 225,
                 opacity: 1,
               },
@@ -413,7 +426,7 @@ const StoryGraphic = ({ highlighted }: { highlighted: number }) => {
       <g transform={`translate(${470}, ${PADDING_TOP})`}>
         {dataSet.map((d, i) => {
           return (
-            <motion.rect
+            <motion.circle
               key={`ref-age35-39-${i}`}
               transition={{ duration: 0.5 }}
               variants={defineVariants(
@@ -421,24 +434,27 @@ const StoryGraphic = ({ highlighted }: { highlighted: number }) => {
                   y: d.cy,
                   x: d.cx,
                   opacity: 0,
+                  r: 0,
                 },
                 {
                   step1: {
                     y: d.cy,
                     x: d.cx,
-                    width: d.r,
-                    height: d.r,
+                    r: i > 33 ? SMALL_RADIUS : d.r,
+                    // width: d.r,
+                    // height: d.r,
                     opacity: 1,
-                    fill: i >= 33 ? '#737373' : '#ffb34a',
+                    fill: i > 33 ? '#737373' : '#ffb34a',
                   },
                   step2: {
                     y: d.cy,
                     x: d.cx,
-                    width: d.r,
-                    height: d.r,
+                    r: i > 50 ? SMALL_RADIUS : d.r,
+                    // width: d.r,
+                    // height: d.r,
                     opacity: 1,
                     fill:
-                      i >= 34 && i < 50
+                      i >= 34 && i < 51
                         ? '#ffb34a'
                         : i >= 50
                         ? '#737373'
@@ -447,11 +463,12 @@ const StoryGraphic = ({ highlighted }: { highlighted: number }) => {
                   step3: {
                     y: d.cy,
                     x: d.cx,
-                    width: d.r,
-                    height: d.r,
+                    r: i > 59 ? SMALL_RADIUS : d.r,
+                    // width: d.r,
+                    // height: d.r,
                     opacity: 1,
                     fill:
-                      i >= 51 && i < 59
+                      i >= 51 && i < 60
                         ? '#ffb34a'
                         : i >= 59
                         ? '#737373'
@@ -460,8 +477,9 @@ const StoryGraphic = ({ highlighted }: { highlighted: number }) => {
                   step4: {
                     y: d.cy,
                     x: d.cx,
-                    width: d.r,
-                    height: d.r,
+                    r: i > 63 ? SMALL_RADIUS : d.r,
+                    // width: d.r,
+                    // height: d.r,
                     opacity: 1,
                     fill:
                       i >= 60 && i < 64
@@ -473,11 +491,12 @@ const StoryGraphic = ({ highlighted }: { highlighted: number }) => {
                   step5: {
                     y: d.cy,
                     x: d.cx,
-                    width: d.r,
-                    height: d.r,
+                    r: i > 67 ? SMALL_RADIUS : d.r,
+                    // width: d.r,
+                    // height: d.r,
                     opacity: 1,
                     fill:
-                      i >= 65 && i < 68
+                      i >= 64 && i < 68
                         ? '#ffb34a'
                         : i >= 68
                         ? '#737373'
@@ -485,38 +504,38 @@ const StoryGraphic = ({ highlighted }: { highlighted: number }) => {
                   },
                 },
               )}
-            ></motion.rect>
+            ></motion.circle>
           )
         })}
         <motion.text
           {...styles.label}
           {...colorScheme.set('fill', 'text')}
           variants={defineVariants(
-            { x: 20, y: 225, opacity: 0 },
+            { x: 5, y: 225, opacity: 0 },
             {
               step1: {
-                x: 20,
+                x: 5,
                 y: 225,
                 opacity: 1,
                 transition: { duration: 0.5 },
               },
               step2: {
-                x: 20,
+                x: 5,
                 y: 225,
                 opacity: 1,
               },
               step3: {
-                x: 20,
+                x: 5,
                 y: 225,
                 opacity: 1,
               },
               step4: {
-                x: 20,
+                x: 5,
                 y: 225,
                 opacity: 1,
               },
               step5: {
-                x: 20,
+                x: 5,
                 y: 225,
                 opacity: 1,
               },
@@ -533,7 +552,7 @@ const StoryGraphic = ({ highlighted }: { highlighted: number }) => {
       <g transform={`translate(${705}, ${PADDING_TOP})`}>
         {dataSet.map((d, i) => {
           return (
-            <motion.rect
+            <motion.circle
               key={`ref-age-40-${i}`}
               transition={{ duration: 0.5 }}
               variants={defineVariants(
@@ -541,24 +560,27 @@ const StoryGraphic = ({ highlighted }: { highlighted: number }) => {
                   y: d.cy,
                   x: d.cx,
                   opacity: 0,
+                  r: 0,
                 },
                 {
                   step1: {
                     y: d.cy,
                     x: d.cx,
-                    width: d.r,
-                    height: d.r,
+                    r: i > 17 ? SMALL_RADIUS : d.r,
+                    // width: d.r,
+                    // height: d.r,
                     opacity: 1,
-                    fill: i >= 18 ? '#737373' : '#ff5100',
+                    fill: i > 17 ? '#737373' : '#ff5100',
                   },
                   step2: {
                     y: d.cy,
                     x: d.cx,
-                    width: d.r,
-                    height: d.r,
+                    r: i > 26 ? SMALL_RADIUS : d.r,
+                    // width: d.r,
+                    // height: d.r,
                     opacity: 1,
                     fill:
-                      i >= 19 && i < 27
+                      i > 17 && i < 27
                         ? '#ff5100'
                         : i >= 27
                         ? '#737373'
@@ -567,11 +589,12 @@ const StoryGraphic = ({ highlighted }: { highlighted: number }) => {
                   step3: {
                     y: d.cy,
                     x: d.cx,
-                    width: d.r,
-                    height: d.r,
+                    r: i > 30 ? SMALL_RADIUS : d.r,
+                    // width: d.r,
+                    // height: d.r,
                     opacity: 1,
                     fill:
-                      i >= 28 && i < 31
+                      i > 26 && i < 31
                         ? '#ff5100'
                         : i >= 31
                         ? '#737373'
@@ -580,11 +603,12 @@ const StoryGraphic = ({ highlighted }: { highlighted: number }) => {
                   step4: {
                     y: d.cy,
                     x: d.cx,
-                    width: d.r,
-                    height: d.r,
+                    r: i > 33 ? SMALL_RADIUS : d.r,
+                    // width: d.r,
+                    // height: d.r,
                     opacity: 1,
                     fill:
-                      i >= 32 && i < 34
+                      i > 30 && i < 34
                         ? '#ff5100'
                         : i >= 34
                         ? '#737373'
@@ -593,11 +617,12 @@ const StoryGraphic = ({ highlighted }: { highlighted: number }) => {
                   step5: {
                     y: d.cy,
                     x: d.cx,
-                    width: d.r,
-                    height: d.r,
+                    r: i > 35 ? SMALL_RADIUS : d.r,
+                    // width: d.r,
+                    // height: d.r,
                     opacity: 1,
                     fill:
-                      i >= 34 && i < 36
+                      i > 33 && i < 36
                         ? '#ff5100'
                         : i >= 36
                         ? '#737373'
@@ -605,7 +630,7 @@ const StoryGraphic = ({ highlighted }: { highlighted: number }) => {
                   },
                 },
               )}
-            ></motion.rect>
+            ></motion.circle>
           )
         })}
 
@@ -613,31 +638,31 @@ const StoryGraphic = ({ highlighted }: { highlighted: number }) => {
           {...styles.label}
           {...colorScheme.set('fill', 'text')}
           variants={defineVariants(
-            { x: 20, y: 225, opacity: 0 },
+            { x: 5, y: 225, opacity: 0 },
             {
               step1: {
-                x: 20,
+                x: 5,
                 y: 225,
                 opacity: 1,
                 transition: { duration: 0.5 },
               },
               step2: {
-                x: 20,
+                x: 5,
                 y: 225,
                 opacity: 1,
               },
               step3: {
-                x: 20,
+                x: 5,
                 y: 225,
                 opacity: 1,
               },
               step4: {
-                x: 20,
+                x: 5,
                 y: 225,
                 opacity: 1,
               },
               step5: {
-                x: 20,
+                x: 5,
                 y: 225,
                 opacity: 1,
               },
@@ -876,7 +901,7 @@ const styles = {
       '30vh' /* don't use dvh here, otherwise the layout will jump when scrolling */,
     backdropFilter: 'blur(2px)',
     flexWrap: 'wrap',
-    marginBottom: 100,
+    marginBottom: '15vh',
   }),
   scrollyGraphicsChapters: css({
     width: '100%',
@@ -905,5 +930,8 @@ const styles = {
   }),
   label: css({
     ...fontStyles.sansSerifRegular23,
+    [mediaQueries.onlyS]: {
+      fontSize: '1.7rem',
+    },
   }),
 }
