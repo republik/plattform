c07b6f86c96bcb77a26eab3d8f602b3517e812d2
Jeremy Stucki <jeremy.stucki@gmail.com>
2023-03-29 17:51:02 +0200

    feat(docs): include vercel commit ref in docs base
    This way the "Edit this page" button should work in PRs too.

--- docs/theme.config.tsx
+++ docs/theme.config.tsx
@@ -4,7 +4,9 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: <strong>💅 Republik Styleguide</strong>,
-  docsRepositoryBase: 'https://github.com/republik/plattform/blob/main/docs/',
+  docsRepositoryBase: `https://github.com/republik/plattform/blob/${
+    process.env.VERCEL_GIT_COMMIT_REF ?? 'main'
+  }/docs/`,
   project: {
     link: 'https://github.com/republik/plattform',
   },
