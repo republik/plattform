467987fb16d16ce8696e7b2e71410b0bf06ce16d
Jeremy Stucki <jeremy.stucki@gmail.com>
2023-05-12 17:49:08 +0200

    test(audio): add some simple audio player tests

---
+++ e2e-tests/audio-player.spec.ts
@@ -0,0 +1,24 @@
+import { test, expect } from '@playwright/test'
+
+test('audio player shows up on article page', async ({ page }) => {
+  await page.goto(
+    'http://localhost:3010/2023/05/01/der-heilige-krieg-der-taliban-gegen-die-bildung-von-frauen',
+  )
+  const audioPlayerWrapper = page.locator('#audio-player-wrapper')
+
+  await expect(audioPlayerWrapper).not.toBeInViewport()
+
+  await page.getByRole('button', { name: 'Hören' }).click()
+
+  await expect(audioPlayerWrapper).toBeInViewport()
+
+  const audioElement = page.locator('[data-audioplayer-element="true"]')
+
+  await expect(audioElement).toHaveJSProperty('paused', false)
+
+  await page.getByRole('button', { name: 'Audioplayer schliessen' }).click()
+
+  await expect(audioElement).toHaveJSProperty('paused', true)
+
+  await expect(audioPlayerWrapper).not.toBeInViewport()
+})
