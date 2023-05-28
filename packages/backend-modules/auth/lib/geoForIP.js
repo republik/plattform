ce7d2ce908ee017658dcaa992ad54fd8c7871e8f
Patrick Venetz <venetz@gmail.com>
2023-05-28 12:34:04 +0200

    refactor(auth): replace geoip-database package w/ file

--- packages/backend-modules/auth/lib/geoForIP.js
+++ packages/backend-modules/auth/lib/geoForIP.js
@@ -1,27 +1,34 @@
-const geoipDatabase = require('geoip-database')
 const maxmind = require('maxmind')
-let cityLookup
+const path = require('path')
+
+const FILE_MISSING = Symbol('GeoLite2-City.mmdb missing')
+
+let lookup
+
+const init = async () => {
+  const relativeFilepath = '../../../../local/geo/GeoLite2-City.mmdb'
+  const filepath = path.resolve(__dirname, relativeFilepath)
+
+  return maxmind.open(filepath).catch((e) => {
+    console.warn("WARNING: GeoLite2-City.mmdb missing. Resolve IP won't work.")
+    return FILE_MISSING
+  })
+}
 
 module.exports = async (ip) => {
-  if (!cityLookup) {
-    cityLookup = await maxmind.open(geoipDatabase.city)
+  if (!lookup) {
+    lookup = await init()
   }
 
-  const geo = cityLookup.get(ip)
-  let country
-  // eslint-disable-next-line no-empty
-  try {
-    country = geo.country.names.de
-  } catch (e) {}
-  let countryEN
-  // eslint-disable-next-line no-empty
-  try {
-    countryEN = geo.country.names.en
-  } catch (e) {}
-  let city
-  // eslint-disable-next-line no-empty
-  try {
-    city = geo.city.names.de
-  } catch (e) {}
-  return { country, countryEN, city }
+  if (lookup === FILE_MISSING) {
+    return { country: null, countryEN: null, city: null }
+  }
+
+  const res = lookup.get(ip)
+
+  return {
+    country: res?.country?.names?.de,
+    countryEN: res?.country?.names?.en,
+    city: res?.city?.names?.de,
+  }
 }
