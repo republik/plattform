3192c41553b071d5ec8441d67e42f16efbaa5fb8
Felix Michel <felix_michel@gmx.ch>
2023-06-14 16:15:39 +0200

    feat(PoliticsQuestionnaire): wip for person view

---
+++ apps/www/pages/politikfragebogen/[id].js
@@ -0,0 +1,43 @@
+import { createGetServerSideProps } from '../../lib/apollo/helpers'
+import Page from '../../components/PoliticsQuestionnaire/Person'
+import fs from 'node:fs/promises'
+import path from 'node:path'
+import { csvParse } from 'd3'
+
+export default ({ responses, authorData }) => (
+  <Page responses={responses} authorData={authorData} />
+)
+
+export const getServerSideProps = createGetServerSideProps(
+  async ({
+    ctx: {
+      params: { id },
+    },
+  }) => {
+    const data = await fs.readFile(
+      path.join(
+        process.cwd(),
+        'public/static/politicsquestionnaire2023/submissions_dummy_data.csv',
+      ),
+      'utf-8',
+    )
+    const responses = csvParse(data)
+
+    const responsesById = responses.filter((d) => d.uuid === id)
+
+    const questionAnswerPair = responsesById.map((d) => {
+      return { question: d.questions, answer: d.answers }
+    })
+
+    const authorData = responsesById.map((d) => {
+      return { id: d.uuid, name: d.name }
+    })
+
+    return {
+      props: {
+        responses: questionAnswerPair,
+        authorData: authorData[0],
+      },
+    }
+  },
+)
