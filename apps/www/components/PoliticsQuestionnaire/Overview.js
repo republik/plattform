2a5278e7d6e0059bd37160c028099aae409d7b0e
Felix Michel <felix_michel@gmx.ch>
2023-06-14 11:36:16 +0200

    feat(PoliticsQuestionnaire): add background color, full width layout

--- apps/www/components/PoliticsQuestionnaire/Overview.js
+++ apps/www/components/PoliticsQuestionnaire/Overview.js
@@ -2,6 +2,8 @@ import { css } from 'glamor'
 import { csv, nest } from 'd3'
 import { useEffect, useState } from 'react'
 import { PUBLIC_BASE_URL } from '../../lib/constants'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 import {
   ColorContextProvider,
@@ -10,7 +12,14 @@ import {
   inQuotes,
   Interaction,
   NarrowContainer,
+  Container,
 } from '@project-r/styleguide'
+
+import { questionColor } from './config'
+
+// re-introduced since actionbar/article only expect a single share param
+// (otherwise share for multiple questions fails)
+export const QUESTION_SEPARATOR = ','
 
 import {
   AnswersGrid,
@@ -18,12 +27,8 @@ import {
 } from '../Questionnaire/Submissions/AnswersGrid'
 
 const SubmissionsOverview = ({ extract }) => {
-  const [politicsMetaData, setPoliticsMetaData] = useState([])
   const [submissionData, setSubmissionData] = useState([])
   useEffect(() => {
-    csv(
-      `${PUBLIC_BASE_URL}/static/politicsquestionnaire2023/politics_dummy_data.csv`,
-    ).then((data) => setPoliticsMetaData(data))
     csv(
       `${PUBLIC_BASE_URL}/static/politicsquestionnaire2023/submissions_dummy_data.csv`,
     ).then((data) => {
@@ -34,18 +39,64 @@ const SubmissionsOverview = ({ extract }) => {
       return setSubmissionData(groupedData)
     })
   }, [])
-  console.log({ politicsMetaData })
-  console.log({ submissionData })
 
   // the extract flag is only used for custom share for in the QuestionView
   if (extract) return null
 
-  return submissionData.map((question) => (
-    <AnswerGridOverview key={question.key} question={question} />
-  ))
+  return (
+    <div style={{ margin: '48px auto 0' }}>
+      {submissionData.map((question, idx) => (
+        <QuestionFeatured
+          key={question.key}
+          question={question}
+          bgColor={questionColor(idx)}
+        />
+      ))}
+    </div>
+  )
 }
 
 export default SubmissionsOverview
+
+const QuestionFeatured = ({ question, bgColor }) => {
+  // const router = useRouter()
+  // const { query } = router
+
+  // const questionRef = useRef()
+  // useEffect(() => {
+  //   if (query?.focus === questions[0].id) {
+  //     scrollIntoView(questionRef.current)
+  //   }
+  // }, [])
+
+  return (
+    <div
+      // ref={questionRef}
+      // id={questions[0].id}
+      style={{
+        padding: '0 0 46px 0',
+        // flexBasis: '50%',
+        backgroundColor: bgColor,
+        display: 'flex',
+      }}
+    >
+      <Container>
+        <AnswerGridOverview
+          // key={q.id}
+          question={question}
+        />
+
+        <NarrowContainer>
+          <Interaction.P style={{ textAlign: 'center' }}>
+            <QuestionLink question={question}>
+              <Editorial.A>Alle Antworten lesen</Editorial.A>
+            </QuestionLink>
+          </Interaction.P>
+        </NarrowContainer>
+      </Container>
+    </div>
+  )
+}
 
 const AnswerGridOverview = ({ question }) => {
   return (
@@ -62,14 +113,14 @@ const AnswerGridOverview = ({ question }) => {
       </NarrowContainer>
       <ColorContextProvider localColorVariables={colors} colorSchemeKey='light'>
         <AnswersGrid>
-          {question.values.map(({ uuid, answers }) => (
+          {question.values.map(({ uuid, answers, name }) => (
             <AnswersGridCard key={uuid}>
-              <div>
+              <SubmissionLink id={uuid}>
                 <a style={{ textDecoration: 'none' }}>
                   <div {...styles.answerCard}>
                     <div>
                       <Editorial.Question style={{ marginTop: 0 }}>
-                        {answers}
+                        {inQuotes(answers)}
                       </Editorial.Question>
                       <Editorial.Credit
                         style={{
@@ -79,18 +130,45 @@ const AnswerGridOverview = ({ question }) => {
                       >
                         Von{' '}
                         <span style={{ textDecoration: 'underline' }}>
-                          {uuid}
+                          {name}
                         </span>
                       </Editorial.Credit>
                     </div>
                   </div>
                 </a>
-              </div>
+              </SubmissionLink>
             </AnswersGridCard>
           ))}
         </AnswersGrid>
       </ColorContextProvider>
     </>
+  )
+}
+
+export const QuestionLink = ({ question, children }) => {
+  const router = useRouter()
+  const pathname = router.asPath.split('?')[0].split('#')[0]
+  return (
+    <Link
+      href={{
+        pathname,
+        query: {
+          share: question,
+        },
+      }}
+      shallow
+      passHref
+    >
+      {children}
+    </Link>
+  )
+}
+
+const SubmissionLink = ({ id, children }) => {
+  return (
+    <Link href={`/politikfragebogen/${id}`} passHref>
+      {children}
+    </Link>
   )
 }
 
