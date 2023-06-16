b3da7a24cb208f5fc728b3fb3562751989502e8c
Jeremy Stucki <jeremy.stucki@gmail.com>
2023-06-16 15:02:21 +0200

    feat: display time in article audio player

--- apps/www/components/ActionBar/Audio/ArticleAudioPlayer.tsx
+++ apps/www/components/ActionBar/Audio/ArticleAudioPlayer.tsx
@@ -1,6 +1,10 @@
+import { useEffect, useState } from 'react'
 import { useAudioContext } from '../../Audio/AudioProvider'
+import { useMediaProgress } from '../../Audio/MediaProgress'
+import { useGlobalAudioState } from '../../Audio/globalAudioState'
 import useAudioQueue from '../../Audio/hooks/useAudioQueue'
 import { AudioPlayerLocations } from '../../Audio/types/AudioActionTracking'
+import { renderTime } from '../../Audio/AudioPlayer/shared'
 
 type Props = {
   documentId: string
@@ -16,11 +20,33 @@ export const ArticleAudioPlayer = ({ documentId, documentMeta }: Props) => {
     checkIfActivePlayerItem,
     isPlaying,
   } = useAudioContext()
+  const { currentTime } = useGlobalAudioState()
   const { isAudioQueueAvailable, checkIfInQueue } = useAudioQueue()
 
   const isActiveAudioItem = checkIfActivePlayerItem(documentId)
   const itemPlaying = isPlaying && isActiveAudioItem
   const itemInAudioQueue = checkIfInQueue(documentId)
+  const { getMediaProgress } = useMediaProgress()
+
+  const [mediaProgress, setMediaProgress] = useState(0)
+
+  const currentDisplayTime =
+    isActiveAudioItem && currentTime > 0 ? currentTime : mediaProgress
+  const duration = documentMeta.audioSource.durationMs / 1000
+
+  useEffect(() => {
+    const updateMediaProgress = async () => {
+      const mp = await getMediaProgress({
+        mediaId: documentMeta.audioSource.mediaId,
+        durationMs: documentMeta.audioSource.durationMs,
+      })
+      setMediaProgress(mp || 0)
+    }
+
+    if (currentTime === 0) {
+      updateMediaProgress()
+    }
+  }, [documentMeta.audioSource.mediaId, currentTime])
 
   const play = () => {
     toggleAudioPlayer(
@@ -51,7 +77,8 @@ export const ArticleAudioPlayer = ({ documentId, documentMeta }: Props) => {
       >
         {itemPlaying ? 'Pause' : 'Play'}
       </button>
-      Plaing some shitz
+      Plaing some shitz ({renderTime(currentDisplayTime)} /{' '}
+      {renderTime(duration)})
     </div>
   )
 }
