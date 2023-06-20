6bc3c9ade75c44c1c5ac5ae2b632c2bc48368ca7
Tobias Maier <tobias.maier@republik.ch>
2023-06-20 09:23:41 +0200

    refactor(www): update error boundary

--- apps/www/lib/errors/PageErrorBoundary.tsx
+++ apps/www/lib/errors/PageErrorBoundary.tsx
@@ -3,6 +3,7 @@ import { ErrorBoundary } from 'react-error-boundary'
 import { reportError } from './reportError'
 import { A, Interaction, Button } from '@project-r/styleguide'
 import { PUBLIC_BASE_URL } from '../constants'
+import { divide } from 'lodash'
 
 type ErrorBoundaryProps = {
   children: ReactNode
@@ -14,31 +15,39 @@ function PageErrorBoundary({ children }: ErrorBoundaryProps) {
       FallbackComponent={({ error, resetErrorBoundary }) => (
         <div
           style={{
-            margin: '0 auto',
-            maxWidth: '60ch',
-            padding: '1rem',
             display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'flex-start',
-            gap: '1rem',
+            alignItems: 'center',
+            justifyContent: 'center',
+            minHeight: '80vh',
           }}
         >
-          <Interaction.H1>Es ist ein Fehler aufgetreten</Interaction.H1>
-          <Interaction.P>
-            Oftmals kann ein Neuladen der Seite das Problem beheben.
-          </Interaction.P>
-          <Interaction.P>
-            Sollte dieser Fehler zum wiederholten Male aufgetreten sein, wenden
-            Sie sich bitte an{' '}
-            <A
-              href={[
-                'mailto:kontakt@republik.ch',
-                '?subject=Fehlermeldung%20auf%20' +
-                  PUBLIC_BASE_URL +
-                  window.location.pathname,
-                '&body=' +
-                  encodeURIComponent(
-                    `
+          <div
+            style={{
+              margin: '0 auto',
+              maxWidth: '60ch',
+              padding: '1rem',
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'flex-start',
+              gap: '1rem',
+            }}
+          >
+            <Interaction.H1>Es ist ein Fehler aufgetreten</Interaction.H1>
+            <Button primary onClick={() => resetErrorBoundary()}>
+              Seite neu laden
+            </Button>
+            <Interaction.P>
+              Sollte dieser Fehler zum wiederholten Male aufgetreten sein,
+              wenden Sie sich bitte an{' '}
+              <A
+                href={[
+                  'mailto:kontakt@republik.ch',
+                  '?subject=Fehlermeldung%20auf%20' +
+                    PUBLIC_BASE_URL +
+                    window.location.pathname,
+                  '&body=' +
+                    encodeURIComponent(
+                      `
 Hallo Republik-Team, ich bin auf folgenden Fehler in der Webseite gestossen:
 
 URL: ${PUBLIC_BASE_URL}${window.location.pathname}
@@ -46,16 +55,14 @@ Fehler:
 ${error.message}
 ${error.stack}
                   `,
-                  ),
-              ].join('')}
-            >
-              kontakt@republik.ch
-            </A>
-            .
-          </Interaction.P>
-          <Button primary onClick={() => resetErrorBoundary()}>
-            Seite neu laden
-          </Button>
+                    ),
+                ].join('')}
+              >
+                kontakt@republik.ch
+              </A>
+              .
+            </Interaction.P>
+          </div>
         </div>
       )}
       onError={(error, info) => {
