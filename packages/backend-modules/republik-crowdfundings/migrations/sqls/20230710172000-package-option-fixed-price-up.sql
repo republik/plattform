78fb796b21bd6c702ffeb4172e8a91decfbf9430
Luciana Kolbeck <luciana.kolbeck@republik.ch>
2023-07-26 14:19:08 +0200

    fix: populate data as part of the migration

--- packages/backend-modules/republik-crowdfundings/migrations/sqls/20230710172000-package-option-fixed-price-up.sql
+++ packages/backend-modules/republik-crowdfundings/migrations/sqls/20230710172000-package-option-fixed-price-up.sql
@@ -5,3 +5,21 @@ ALTER TABLE "public"."packageOptions"
 ALTER TABLE "public"."packageOptions"
   ADD COLUMN "fixedPrice" boolean
 ;
+
+UPDATE "packageOptions" SET "fixedPrice" = false;
+
+UPDATE "packageOptions" 
+SET 
+"fixedPrice" = true
+WHERE "packageId" IN (
+  SELECT id FROM packages
+  WHERE name IN ('MONTHLY_ABO', 'YEARLY_ABO','LESHA'));
+  
+UPDATE "packageOptions" SET "payMoreSuggestion" = false;
+
+UPDATE "packageOptions" 
+SET 
+"payMoreSuggestion" = true
+WHERE "packageId" IN (
+  SELECT id FROM packages
+  WHERE name IN ('DONATE','ABO_GIVE','ABO_GIVE_MONTHS','YEARLY_ABO','LESHA'));
