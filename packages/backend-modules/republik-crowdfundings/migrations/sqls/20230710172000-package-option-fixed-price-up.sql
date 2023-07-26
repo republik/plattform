1a691b3967487a3c7623aaf1383c5f16bfc2c58e
Luciana Kolbeck <luciana.kolbeck@republik.ch>
2023-07-26 17:23:43 +0200

    fix: fix logic

--- packages/backend-modules/republik-crowdfundings/migrations/sqls/20230710172000-package-option-fixed-price-up.sql
+++ packages/backend-modules/republik-crowdfundings/migrations/sqls/20230710172000-package-option-fixed-price-up.sql
@@ -1,12 +1,10 @@
 ALTER TABLE "public"."packageOptions"
-  ADD COLUMN "payMoreSuggestion" boolean
+  ADD COLUMN "payMoreSuggestion" boolean NOT NULL DEFAULT TRUE
 ;
 
 ALTER TABLE "public"."packageOptions"
-  ADD COLUMN "fixedPrice" boolean
+  ADD COLUMN "fixedPrice" boolean NOT NULL DEFAULT FALSE
 ;
-
-UPDATE "packageOptions" SET "fixedPrice" = false;
 
 UPDATE "packageOptions" 
 SET 
@@ -14,12 +12,10 @@ SET
 WHERE "packageId" IN (
   SELECT id FROM packages
   WHERE name IN ('MONTHLY_ABO', 'YEARLY_ABO','LESHA'));
-  
-UPDATE "packageOptions" SET "payMoreSuggestion" = false;
 
 UPDATE "packageOptions" 
 SET 
-"payMoreSuggestion" = true
+"payMoreSuggestion" = FALSE
 WHERE "packageId" IN (
   SELECT id FROM packages
   WHERE name IN ('DONATE','ABO_GIVE','ABO_GIVE_MONTHS','YEARLY_ABO','LESHA'));
