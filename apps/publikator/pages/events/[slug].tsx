ce8b6ec77c05c71a2e5f01bdd376447e1273a48f
Olivier Baumann <olivierbaumann@me.com>
2023-08-04 11:35:36 +0200

    feat: add event page

---
+++ apps/publikator/pages/events/[slug].tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Event = () => {
+  return <div>1 Event</div>
+}
+
+export default Event
---
+++ apps/publikator/pages/events/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Events = () => {
+  return <div>Hello</div>
+}
+
+export default Events
