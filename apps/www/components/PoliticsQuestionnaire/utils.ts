207fa8e30206c2ede850062fad3aad18e1cf5fc3
Tobias Maier <tobias.maier@republik.ch>
2023-08-08 09:42:59 +0200

    feat(www): add types to utility function in politikfragebogen

--- apps/www/components/PoliticsQuestionnaire/utils.ts
+++ apps/www/components/PoliticsQuestionnaire/utils.ts
@@ -1,10 +1,10 @@
 import { QUESTION_TYPES } from './config'
 
-export const leftJoin = (objArr1, objArr2, key) =>
+export const leftJoin = (objArr1: object[], objArr2: object[], key: string) =>
   objArr1.map((anObj1) => ({
     ...objArr2.find((anObj2) => anObj1[key] === anObj2[key]),
     ...anObj1,
   }))
 
-export const getTypeBySlug = (slug) =>
+export const getTypeBySlug = (slug: string) =>
   QUESTION_TYPES.find((q) => q.questionSlug === slug).type
