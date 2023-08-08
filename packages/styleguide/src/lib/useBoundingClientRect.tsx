b2e8e8fa3e3e70f305b15505f297d385f4b38041
Tobias Maier <tobias.maier@republik.ch>
2023-08-08 14:53:29 +0200

    refactor(styleguide): convert useBoundingClientRect to TS

--- packages/styleguide/src/lib/useBoundingClientRect.tsx
+++ packages/styleguide/src/lib/useBoundingClientRect.tsx
@@ -7,11 +7,13 @@ import React from 'react'
  * Make sure to pass any dependencies to the hook which may change the bounding
  * client rect. Without the deps this hook will only measure the initial size.
  */
-export const useBoundingClientRect = (deps = []) => {
-  const [rect, setRect] = React.useState({})
+export function useBoundingClientRect(
+  deps: React.DependencyList = [],
+): [(_: HTMLElement) => void, DOMRect] {
+  const [rect, setRect] = React.useState<DOMRect>(null)
 
   return [
-    React.useCallback((el) => {
+    React.useCallback((el: HTMLElement) => {
       if (el) {
         setRect(el.getBoundingClientRect())
       }
