791b1fdd297924d14db0b1a6806bb97db59c3825
Felix Michel <felix_michel@gmx.ch>
2023-08-09 13:50:37 +0200

    fix(Data): fetch data from aws

--- apps/www/components/PoliticsQuestionnaire/loader.ts
+++ apps/www/components/PoliticsQuestionnaire/loader.ts
@@ -1,10 +1,5 @@
-import fs from 'node:fs/promises'
-import path from 'node:path'
+import { SUBMISSIONS_DATA_URL } from './config'
 
 export async function loadPoliticQuestionnaireCSV() {
-  const csvPath = path.join(
-    process.cwd(),
-    '/public/static/politicsquestionnaire2023/submissions_data.csv',
-  )
-  return fs.readFile(csvPath, 'utf-8')
+  return fetch(SUBMISSIONS_DATA_URL).then((res) => res.text())
 }
