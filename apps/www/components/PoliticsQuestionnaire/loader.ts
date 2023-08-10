a9927bc63cc02a2d0d0d5acc6506aa0fdc8cff5d
Tobias Maier <tobias.maier@republik.ch>
2023-08-10 09:58:44 +0200

    chore(www/politikerfragebogen): remove remaining console.warn

--- apps/www/components/PoliticsQuestionnaire/loader.ts
+++ apps/www/components/PoliticsQuestionnaire/loader.ts
@@ -3,7 +3,6 @@ import { SUBMISSIONS_DATA_URL } from './config'
 
 export async function loadPoliticQuestionnaireCSV() {
   return fetch(new URL(SUBMISSIONS_DATA_URL)).then((res) => {
-    console.warn(res)
     return res.text()
   })
 }
