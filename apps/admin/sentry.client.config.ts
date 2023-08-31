491775968b5250965b92c53a189bba7ee7d81035
Tobias Maier <tobias.maier@republik.ch>
2023-08-31 13:12:16 +0200

    Revert "feat(admin): remove static dsn entry from sentry configs"
    This reverts commit ac5b12ed40e6bb6ac8de8bf5d51195e56c7a0597.

--- apps/admin/sentry.client.config.ts
+++ apps/admin/sentry.client.config.ts
@@ -5,9 +5,26 @@
 import * as Sentry from '@sentry/nextjs'
 
 Sentry.init({
+  dsn: 'https://7f52fbd3b66fe6171fd2a59609212106@o4505721260212224.ingest.sentry.io/4505721312706560',
+
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
+
+  replaysOnErrorSampleRate: 1.0,
+
+  // This sets the sample rate to be 10%. You may want this to be 100% while
+  // in development and sample at a lower rate in production
+  // replaysSessionSampleRate: 0.1,
+
+  // You can remove this option if you're not planning to use the Sentry Session Replay feature:
+  // integrations: [
+  //   new Sentry.Replay({
+  //     // Additional Replay configuration goes in here, for example:
+  //     maskAllText: true,
+  //     blockAllMedia: true,
+  //   }),
+  // ],
 })
