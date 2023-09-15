743a8ce6b68a5ddc43879f4c110a6d426e253b22
Jeremy Stucki <jeremy.stucki@gmail.com>
2023-09-15 12:10:36 +0200

    feat: micro frame

--- apps/www/app/layout.tsx
+++ apps/www/app/layout.tsx
@@ -1,13 +1,41 @@
-export default function RootLayout({
+import Link from 'next/link'
+import { getClient } from './utils/ApolloClient'
+import { meQuery } from './utils/graphql/meQuery'
+
+export default async function RootLayout({
   // Layouts must accept a children prop.
   // This will be populated with nested layouts or pages
   children,
 }: {
   children: React.ReactNode
 }) {
+  const me = await getMe()
+
   return (
     <html lang='de'>
-      <body>ROOTLAYOUT{children}</body>
+      <body>
+        <Frame me={me} />
+        {children}
+      </body>
     </html>
+  )
+}
+
+const getMe = async () => {
+  const { data } = await getClient().query({ query: meQuery })
+
+  return data.me
+}
+
+const Frame = ({ me }) => {
+  return (
+    <div>
+      {' '}
+      {me ? (
+        `Hey, ${me.firstName}`
+      ) : (
+        <Link href='/anmelden'>Anmelden</Link>
+      )} | <Link href='/'>Ab zum Magazin</Link>
+    </div>
   )
 }
