2b3e3a6e201e277d9f27f80aaee654dea1d58a1d
Olivier Baumann <olivierbaumann@me.com>
2023-10-17 17:05:45 +0200

    fix(Author): rendering fix

--- packages/styleguide/src/templates/shared/email/components/Author.tsx
+++ packages/styleguide/src/templates/shared/email/components/Author.tsx
@@ -34,62 +34,66 @@ export const Author = ({
   }
   const credential = author.credentials?.find((c) => c.isListed)
 
-  !isLarge ? (
-    <table
-      border={0}
-      cellPadding='0'
-      cellSpacing='0'
-      width='100%'
-      style={{ marginTop: 48, marginBottom: 24 }}
-    >
-      <tr>
-        <td align='center'>
-          <img
-            width='80'
-            height='80'
-            src={author.portrait}
-            style={{
-              border: 0,
-              width: '80px !important',
-              height: '80px !important',
-              margin: 0,
-            }}
-          />
-          <div id='sender_bottom'>
-            {greeting}
-            <br />
-            <br />
-            <strong>{author.name}</strong>
-            <br />
-            <br />
-            {credential?.description}
-          </div>
-        </td>
-      </tr>
-    </table>
-  ) : (
-    <table id='sender' style={{ marginTop: 48, marginBottom: 24 }}>
-      <tr>
-        <td>
-          <img
-            width='72'
-            height='72'
-            src={author.portrait}
-            style={{
-              border: 0,
-              width: '72px !important',
-              height: '72px !important',
-              margin: 0,
-              maxWidth: '100% !important',
-            }}
-          />
-        </td>
-        <td id='sender' style={{ paddingLeft: 12, lineHeight: '160%' }}>
-          <strong>{author.name}</strong>
-          <br />
-          {credential?.description}
-        </td>
-      </tr>
-    </table>
+  return (
+    <>
+      {!isLarge ? (
+        <table
+          border={0}
+          cellPadding='0'
+          cellSpacing='0'
+          width='100%'
+          style={{ marginTop: 48, marginBottom: 24 }}
+        >
+          <tr>
+            <td align='center' style={{ lineHeight: '140%' }}>
+              <img
+                width='80'
+                height='80'
+                src={author.portrait}
+                style={{
+                  border: 0,
+                  width: '80px !important',
+                  height: '80px !important',
+                  margin: 0,
+                }}
+              />
+              <p id='sender_bottom'>
+                {greeting}
+                <br />
+                <br />
+                <strong>{author.name}</strong>
+                <br />
+                <br />
+                {credential?.description}
+              </p>
+            </td>
+          </tr>
+        </table>
+      ) : (
+        <table id='sender' style={{ marginTop: 48, marginBottom: 24 }}>
+          <tr>
+            <td>
+              <img
+                width='72'
+                height='72'
+                src={author.portrait}
+                style={{
+                  border: 0,
+                  width: '72px !important',
+                  height: '72px !important',
+                  margin: 0,
+                  maxWidth: '100% !important',
+                }}
+              />
+            </td>
+            <td id='sender' style={{ paddingLeft: 12, lineHeight: '160%' }}>
+              <strong>{author.name}</strong>
+              <br />
+              {credential?.description}
+            </td>
+          </tr>
+        </table>
+      )}
+    </>
   )
 }
