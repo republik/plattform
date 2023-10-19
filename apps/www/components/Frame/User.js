71cdec82571201b191932524df48384a8a991f52
Tobias Maier <tobias.maier@republik.ch>
2023-10-19 16:43:17 +0200

    feat(www): extract usernav into a separate page

--- apps/www/components/Frame/User.js
+++ apps/www/components/Frame/User.js
@@ -10,7 +10,7 @@ import {
   HEADER_HEIGHT_MOBILE,
   HEADER_HORIZONTAL_PADDING,
 } from '../constants'
-import withT, { useTranslation } from '../../lib/withT'
+import { useTranslation } from '../../lib/withT'
 import { ME_PORTRAIT_STORAGE_KEY } from '../../lib/context/MeContext'
 import { IconAccountBox } from '@republik/icons'
 
@@ -203,4 +203,4 @@ const styles = {
   }),
 }
 
-export default withT(User)
+export default User
