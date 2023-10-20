939b5957b5c41bc7b8499a45ef3620957068dccc
Luciana Kolbeck <8088524+lucianak@users.noreply.github.com>
2023-10-20 14:00:20 +0200

    fix: better error message

--- packages/backend-modules/embeds/lib/twitter/oauth.js
+++ packages/backend-modules/embeds/lib/twitter/oauth.js
@@ -15,10 +15,10 @@ grant_type=client_credentials
 const { TWITTER_APP_KEY, TWITTER_APP_SECRET } = process.env
 
 if (!TWITTER_APP_KEY) {
-  console.warn("missing TWITTER_APP_KEY. Twitter Embeds won't work.")
+  console.warn('Twitter-Embeds are no longer supported.')
 }
 if (!TWITTER_APP_SECRET) {
-  console.warn("missing TWITTER_APP_SECRET. Twitter Embeds won't work.")
+  console.warn('Twitter-Embeds are no longer supported.')
 }
 
 module.exports = async () => {
