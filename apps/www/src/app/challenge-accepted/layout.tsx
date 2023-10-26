acf488787bc6d112ed6c167d8b965afe35541cb3
Tobias Maier <tobias.maier@republik.ch>
2023-10-26 15:22:48 +0200

    feat(www/new-hope): show 404 in place of challenge-accepted pages for everyone who doesn't have one of the following roles 'editor', 'moderator' & 'admin'

--- apps/www/src/app/challenge-accepted/layout.tsx
+++ apps/www/src/app/challenge-accepted/layout.tsx
@@ -1,11 +1,22 @@
 import Footer from '@app/components/layout/footer'
 import { PageHeader } from '@app/components/layout/header'
+import { getMe } from '@app/lib/auth/me'
 import { css } from '@app/styled-system/css'
+import { notFound } from 'next/navigation'
 
-export default function Layout(props: {
+export default async function Layout(props: {
   children: React.ReactNode
   overlay: React.ReactNode
 }) {
+  const me = await getMe()
+
+  // TODO: temporary until the relase of «challenge accepted»
+  if (
+    !me?.roles.some((role) => ['editor', 'moderator', 'admin'].includes(role))
+  ) {
+    return notFound()
+  }
+
   return (
     <div data-page-theme='challenge-accepted'>
       <PageHeader />
