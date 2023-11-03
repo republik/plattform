358a371c1475ffec8a024fa65017b86e0fa111d2
Jeremy Stucki <jeremy.stucki@gmail.com>
2023-11-03 16:33:39 +0100

    fix: don't show article collection on person detail when there are no items

--- apps/www/src/app/challenge-accepted/person/[slug]/components/person-detail.tsx
+++ apps/www/src/app/challenge-accepted/person/[slug]/components/person-detail.tsx
@@ -113,18 +113,22 @@ export function PersonDetail({ person, isMember = false }: PersonDetailProps) {
         >
           <StructuredText data={person.bio.value} />
         </div>
-        <section className={css({ my: '16-32' })}>
-          <h2
-            className={css({
-              textStyle: 'h1Sans',
-              fontWeight: 'bold',
-              mb: '6',
-            })}
-          >
-            Von und mit {person.name}
-          </h2>
-          <CollectionRenderer items={person.items} isMember={isMember} />
-        </section>
+        {person.items.length > 0 ? (
+          <section className={css({ my: '16-32' })}>
+            <h2
+              className={css({
+                textStyle: 'h1Sans',
+                fontWeight: 'bold',
+                mb: '6',
+              })}
+            >
+              Von und mit {person.name}
+            </h2>
+            <CollectionRenderer items={person.items} isMember={isMember} />
+          </section>
+        ) : (
+          <div className={css({ mb: '16-32' })}></div>
+        )}
       </Container>
     </div>
   )
