314ec967ae42bde11c213611bf5eee6599afc216
Tobias Maier <tobias.maier@republik.ch>
2023-11-03 11:45:39 +0100

    chore(www/new-hope): remove unused code

--- apps/www/src/graphql/republik-api/app-sign-in.ts
+++ apps/www/src/graphql/republik-api/app-sign-in.ts
@@ -19,38 +19,6 @@ export type PendingAppSignInResult = {
   }
 }
 
-export const UNAUTHORIZED_SESSION_QUERY = gql`
-  query UnauthorizedSession(
-    $email: String!
-    $token: String!
-    $tokenType: SignInTokenType!
-  ) {
-    echo {
-      ipAddress
-      userAgent
-      country
-      city
-    }
-    target: unauthorizedSession(
-      email: $email
-      token: { type: $tokenType, payload: $token }
-    ) {
-      newUser
-      enabledSecondFactors
-      requiredConsents
-      requiredFields
-      session {
-        ipAddress
-        userAgent
-        country
-        city
-        phrase
-        isCurrent
-      }
-    }
-  }
-`
-
 export type SignInTokenType =
   | 'EMAIL_TOKEN'
   | 'EMAIL_CODE'
