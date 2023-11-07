0f1966e3001dff0909a5dbae88b752c83705ac14
Jeremy Stucki <jeremy.stucki@gmail.com>
2023-11-07 09:07:49 +0100

    fix: add matomo to app layout

--- apps/www/src/app/layout.tsx
+++ apps/www/src/app/layout.tsx
@@ -39,6 +39,33 @@ export default async function RootLayout({
             <NativeAppMessageSync />
           </ApolloWrapper>
         </ThemeProvider>
+        {process.env.NEXT_PUBLIC_MATOMO_URL_BASE &&
+          process.env.NEXT_PUBLIC_MATOMO_SITE_ID && (
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+            var _paq = _paq || [];
+            _paq.push(['enableLinkTracking']);
+            ${
+              process.env.NEXT_PUBLIC_BASE_URL.indexOf('https') === 0
+                ? "_paq.push(['setSecureCookie', true]);"
+                : ''
+            }
+            (function() {
+              _paq.push(['setTrackerUrl', '${
+                process.env.NEXT_PUBLIC_MATOMO_URL_BASE
+              }/matomo.php']);
+              _paq.push(['setSiteId', '${
+                process.env.NEXT_PUBLIC_MATOMO_SITE_ID
+              }']);
+              var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+              g.type='text/javascript'; g.async=true; g.defer=true; g.src='${
+                process.env.NEXT_PUBLIC_MATOMO_URL_BASE
+              }/matomo.js'; s.parentNode.insertBefore(g,s);
+            })();`,
+              }}
+            />
+          )}
       </body>
     </html>
   )
