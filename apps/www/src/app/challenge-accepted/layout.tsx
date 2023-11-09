f7eb0065475ca08fdea798702bf74143cad4126f
Tobias Maier <tobias.maier@republik.ch>
2023-11-09 11:03:56 +0100

    feat(www): create and start using the page-layout component

--- apps/www/src/app/challenge-accepted/layout.tsx
+++ apps/www/src/app/challenge-accepted/layout.tsx
@@ -1,25 +1,24 @@
-import Footer from '@app/components/layout/footer'
-import { PageHeader } from '@app/components/layout/header'
+import { PageLayout } from '@app/components/layout'
 import { css } from '@app/styled-system/css'
 
 export default async function Layout(props: { children: React.ReactNode }) {
   return (
     <div data-page-theme='challenge-accepted'>
-      <PageHeader />
-      <div
-        className={css({
-          color: 'text',
-          bg: 'pageBackground',
-          pb: '16-32',
-          pt: '4',
-        })}
-        style={{
-          minHeight: 'calc(100dvh - 69px)',
-        }}
-      >
-        {props.children}
-      </div>
-      <Footer />
+      <PageLayout>
+        <div
+          className={css({
+            color: 'text',
+            bg: 'pageBackground',
+            pb: '16-32',
+            pt: '4',
+          })}
+          style={{
+            minHeight: 'calc(100dvh - 69px)',
+          }}
+        >
+          {props.children}
+        </div>
+      </PageLayout>
     </div>
   )
 }
---
+++ apps/www/src/components/layout/index.tsx
@@ -0,0 +1,34 @@
+import { PageHeader } from '@app/components/layout/header'
+import Footer from './footer'
+import { css } from '@app/styled-system/css'
+import { getPlatformInformation } from '@app/lib/util/useragent/platform-information'
+
+type LayoutProps = {
+  children: React.ReactNode
+}
+
+/**
+ * The page-layout component is used to wrap the entire page. It contains the
+ * header, footer, and the main content of the page.
+ */
+export async function PageLayout({ children }: LayoutProps) {
+  const { isNativeApp } = getPlatformInformation()
+
+  return (
+    <div
+      className={css({
+        minHeight: '100dvh',
+        display: 'flex',
+        flexDirection: 'column',
+      })}
+    >
+      <PageHeader />
+      <div
+        className={css({ flexGrow: '1', backgroundColor: 'pageBackground' })}
+      >
+        {children}
+      </div>
+      {!isNativeApp && <Footer />}
+    </div>
+  )
+}
