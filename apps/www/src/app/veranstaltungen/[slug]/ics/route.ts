7dec098ae79ab88172ccbd07c0cc645e6d763258
Jeremy Stucki <jeremy.stucki@gmail.com>
2023-11-22 17:57:24 +0100

    refactor: use better ical library

--- apps/www/src/app/veranstaltungen/[slug]/ics/route.ts
+++ apps/www/src/app/veranstaltungen/[slug]/ics/route.ts
@@ -1,16 +1,10 @@
 import { EVENT_QUERY, EventRecordFields } from '@app/graphql/cms/events.query'
 import { useFragment } from '@app/graphql/gql'
 import { getCMSClient } from '@app/lib/apollo/cms-client'
+import dayjs from 'dayjs'
+import ical, { ICalCalendarMethod } from 'ical-generator'
 import { notFound } from 'next/navigation'
-import * as ics from 'ics'
-
-const getDateArray = (date: Date): [number, number, number, number, number] => [
-  date.getFullYear(),
-  date.getMonth() + 1,
-  date.getDate(),
-  date.getHours(),
-  date.getMinutes(),
-]
+import { v5 as uuidV5 } from 'uuid'
 
 export async function GET(
   request: Request,
@@ -27,23 +21,26 @@ export async function GET(
     return notFound()
   }
 
-  const start = new Date(Date.parse(event.startAt))
-  const end = event.endAt ? new Date(Date.parse(event.endAt)) : undefined
+  const url = `${process.env.NEXT_PUBLIC_BASE_URL}/veranstaltungen/${event.slug}`
 
-  const { error, value } = ics.createEvent({
-    start: getDateArray(start),
-    end: end ? getDateArray(end) : undefined,
-    duration: end ? undefined : { hours: 1 },
+  // Deterministic UUID
+  const id = uuidV5(url, uuidV5.URL)
+
+  const start = dayjs(event.startAt)
+  const end = event.endAt ? dayjs(event.endAt) : start.add(1, 'hour')
+
+  const calendar = ical({ method: ICalCalendarMethod.PUBLISH })
+
+  calendar.createEvent({
+    id,
+    start: start.toDate(),
+    end: end.toDate(),
     location: event.location,
-    url: `${process.env.NEXT_PUBLIC_BASE_URL}/veranstaltungen/${event.slug}`,
-    title: event.title,
+    url,
+    summary: event.title,
   })
 
-  if (error) {
-    throw Error(error.message)
-  }
-
-  return new Response(value, {
+  return new Response(calendar.toString(), {
     headers: {
       'Content-Type': 'text/calendar; charset=utf-8',
       'Content-Disposition': `attachment; filename="${event.title}.ics"`,
