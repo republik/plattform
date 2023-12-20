8d4cf7d554ae63ae3c18e309aa6bffe9d98d6423
Tobias Maier <tobias.maier@republik.ch>
2023-12-20 11:40:28 +0100

    feat(www): update graphql-codegen to split up codegen for schema file and query types

---
+++ apps/www/graphql/configs/codegen-dev.ts
@@ -0,0 +1,47 @@
+import type { CodegenConfig } from '@graphql-codegen/cli'
+
+import { loadEnvConfig } from '@next/env'
+
+loadEnvConfig(process.cwd())
+
+const config: CodegenConfig = {
+  overwrite: true,
+  generates: {
+    './src/graphql/cms/gql/': {
+      schema: './graphql/dato-cms.schema.graphql',
+      documents: ['./src/graphql/cms/**/*.{ts,tsx}'],
+      preset: 'client',
+      presetConfig: {
+        gqlTagName: 'gql',
+      },
+      config: {
+        scalars: {
+          ItemId: 'string',
+          IntType: 'number',
+          Date: 'string',
+          DateTime: 'string',
+        },
+      },
+      plugins: [],
+    },
+    './src/graphql/republik-api/gql/': {
+      schema: './graphql/republik-api.schema.graphql',
+      documents: ['./src/graphql/republik-api/**/*.{ts,tsx}'],
+      preset: 'client',
+      presetConfig: {
+        gqlTagName: 'gql',
+      },
+      config: {
+        scalars: {
+          ItemId: 'string',
+          IntType: 'number',
+          Date: 'string',
+          DateTime: 'string',
+        },
+      },
+      plugins: [],
+    },
+  },
+}
+
+export default config
