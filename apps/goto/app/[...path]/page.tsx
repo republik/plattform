d48ac8cbd0c415b3a6d25edd50a40c33e9712be3
Patrick Venetz <venetz@gmail.com>
2023-12-28 16:09:11 +0100

    chore(apps/goto): remove unused imports

--- apps/goto/app/[...path]/page.tsx
+++ apps/goto/app/[...path]/page.tsx
@@ -1,4 +1,4 @@
-import { notFound, redirect } from 'next/navigation'
+import { redirect } from 'next/navigation'
 
 /**
  * [Page] searchParams returns a plain JavaScript object and not a URLSearchParams instance.
--- apps/goto/app/page.tsx
+++ apps/goto/app/page.tsx
@@ -1,41 +1,11 @@
-import { notFound, redirect } from 'next/navigation'
+import { redirect } from 'next/navigation'
 
-/**
- * [Page] searchParams returns a plain JavaScript object and not a URLSearchParams instance.
- * @see https://nextjs.org/docs/app/api-reference/file-conventions/page#searchparams-optional
- */
-type PageProps = {
-  params: { path: string[] }
-  searchParams: { [key: string]: string | string[] | undefined }
-}
-
-/**
- * Applies searchParams to URL.searchParams
- *
- */
-const applySearchParams = (
-  url: URL,
-  searchParams: PageProps['searchParams'],
-) => {
-  Object.keys(searchParams).forEach((key) => {
-    const value = searchParams[key]
-
-    if (Array.isArray(value)) {
-      value.forEach((valuevalue) => {
-        url.searchParams.append(key, valuevalue)
-      })
-    } else {
-      url.searchParams.append(key, value)
-    }
-  })
-}
-
-export default function Page({ params, searchParams }: PageProps) {
+export default function Page() {
   if (!process.env.FRONTEND_BASE_URL) {
     throw new Error('nope')
   }
 
-  // Parse a fully qualified URL from params.path elements, using FRONTEND_BASE_URL
+  // Parse a fully qualified URL from FRONTEND_BASE_URL
   const url = new URL(process.env.FRONTEND_BASE_URL)
 
   // Haha, there is not page! Just a redirect.
