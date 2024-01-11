72748e4925bc29588b66f7dfba95de30f0b9e088
Jeremy Stucki <jeremy.stucki@gmail.com>
2024-01-11 14:58:35 +0100

    fix(www): rename draft API route params, improve security by checking for relative redirect path

--- apps/www/src/app/api/draft/preview/route.ts
+++ apps/www/src/app/api/draft/preview/route.ts
@@ -1,36 +1,26 @@
-// route handler with secret and slug
+// route handler with secret and redirect path
 import { draftMode } from 'next/headers'
 import { redirect } from 'next/navigation'
 
 export async function GET(request: Request) {
-  // Parse query string parameters
   const { searchParams } = new URL(request.url)
   const secret = searchParams.get('secret')
-  const slug = searchParams.get('slug')
+  const path = searchParams.get('path')
 
   // Check the secret and next parameters
   // This secret should only be known to this route handler and the CMS
   if (secret !== process.env.DRAFT_MODE_SECRET) {
-    return new Response('Invalid token', { status: 401 })
+    return new Response('Invalid query', { status: 401 })
   }
 
-  if (!slug) {
-    return new Response('Invalid slug', { status: 400 })
+  // Check that redirect path is relative, as it otherwise might lead to open redirect vulnerabilities
+  if (!path || !path.startsWith('/')) {
+    return new Response('Invalid query', { status: 401 })
   }
-
-  // // Fetch the headless CMS to check if the provided `slug` exists
-  // // getPostBySlug would implement the required fetching logic to the headless CMS
-  // const post = await getPostBySlug(slug)
-
-  // // If the slug doesn't exist prevent draft mode from being enabled
-  // if (!post) {
-  //   return new Response('Invalid slug', { status: 401 })
-  // }
 
   // Enable Draft Mode by setting the cookie
   draftMode().enable()
 
-  // Redirect to the path from the fetched post
-  // We don't redirect to searchParams.slug as that might lead to open redirect vulnerabilities
-  redirect(slug)
+  // Redirect to the path
+  redirect(path)
 }
