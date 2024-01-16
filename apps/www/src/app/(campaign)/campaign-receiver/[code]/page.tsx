6ca243e2b6eecba095e332c15c4386adfaeec95d
Jeremy Stucki <jeremy.stucki@gmail.com>
2024-01-16 17:20:56 +0100

    feat: make invite codes work in app dir

--- apps/www/src/app/(campaign)/campaign-receiver/[code]/page.tsx
+++ apps/www/src/app/(campaign)/campaign-receiver/[code]/page.tsx
@@ -1,7 +1,16 @@
-export default function Page({ params }) {
+import { UserInviterProfileInfoDocument } from '@app/graphql/republik-api/gql/graphql'
+import { getClient } from '@app/lib/apollo/client'
+
+export default async function Page({ params }) {
+  const { data } = await getClient().query({
+    query: UserInviterProfileInfoDocument,
+    variables: { accessToken: params.code },
+  })
+
   return (
     <div>
-      Hey, mach mit! Du wurdest eingeladen von <strong>{params.code}</strong>
+      Hey, mach mit! Du wurdest eingeladen von{' '}
+      <strong>{data.sender?.firstName}</strong>
     </div>
   )
 }
--- apps/www/src/app/(campaign)/campaign-sender/page.tsx
+++ apps/www/src/app/(campaign)/campaign-sender/page.tsx
@@ -1,3 +1,21 @@
-export default function Page() {
-  return <div>Hey</div>
+import { UserInviteLinkInfoDocument } from '@app/graphql/republik-api/gql/graphql'
+import { getClient } from '@app/lib/apollo/client'
+import Link from 'next/link'
+
+export default async function Page() {
+  const { data } = await getClient().query({
+    query: UserInviteLinkInfoDocument,
+  })
+
+  const url = `/campaign-receiver/${data.me?.accessToken}`
+
+  return (
+    <div>
+      Hey, du kannst jemanden einladen mit diesem Link:{' '}
+      <Link href={url}>
+        {process.env.NEXT_PUBLIC_BASE_URL}
+        {url}
+      </Link>
+    </div>
+  )
 }
