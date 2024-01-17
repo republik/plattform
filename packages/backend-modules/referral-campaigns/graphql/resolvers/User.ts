f539dc33d8b10c224707c320795a17902f4c0824
Tobias Maier <tobias.maier@republik.ch>
2024-01-17 13:39:39 +0100

    feat(api/referral-campaign): add initial code to package

--- packages/backend-modules/referral-campaigns/graphql/resolvers/User.ts
+++ packages/backend-modules/referral-campaigns/graphql/resolvers/User.ts
@@ -1,5 +1,15 @@
-module.exports = {
-  async referrals(user, args, { dataSources }) {
+import { UserRow, ConnectionContext } from '@orbiting/backend-modules-types'
+import debug from 'debug'
+
+type Args = {
+  campaign: string
+}
+
+exports = {
+  async referrals(user: UserRow, args: Args, ctx: ConnectionContext) {
+    const { pgdb } = ctx
+    const { campaign: campaignId } = args
+
     return 2
   },
 }
