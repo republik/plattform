8c779642391137857d3719a515bcbeca85382e31
Tobias Maier <tobias.maier@republik.ch>
2024-01-18 10:59:49 +0100

    fix(api/referral-campaign): use camelCase in naming of db-columns

--- packages/backend-modules/referral-campaigns/migrations/sqls/20240117120959-referral-campaign-ddl-up.sql
+++ packages/backend-modules/referral-campaigns/migrations/sqls/20240117120959-referral-campaign-ddl-up.sql
@@ -4,31 +4,31 @@ CREATE TABLE campaigns (
   id uuid NOT NULL,
   name text NOT NULL UNIQUE,
   description text NULL,
-  start_date timestamp with time zone NOT NULL,
-  end_date timestamp with time zone NOT NULL,
-  created_at timestamp with time zone NOT NULL,
-  updated_at timestamp with time zone NOT NULL,
+  beginDate timestamp with time zone NOT NULL,
+  endDate timestamp with time zone NOT NULL,
+  createdAt timestamp with time zone NOT NULL,
+  updatedAt timestamp with time zone NOT NULL,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE referrals (
   id uuid NOT NULL,
-  campaign_id uuid NULL,
-  referrer_id uuid NOT NULL,
-  pledge_id uuid NOT NULL,
-  created_at timestamp with time zone NOT NULL,
-  updated_at timestamp with time zone NOT NULL,
+  campaignId uuid NULL,
+  referrerId uuid NOT NULL,
+  pledgeId uuid NOT NULL,
+  createdAt timestamp with time zone NOT NULL,
+  updatedAt timestamp with time zone NOT NULL,
   PRIMARY KEY (id),
   FOREIGN KEY 
-    (campaign_id) REFERENCES campaigns (id)
+    (campaignId) REFERENCES campaigns (id)
     ON UPDATE CASCADE
     ON DELETE CASCADE,
   FOREIGN KEY 
-    (referrer_id) REFERENCES users (id)
+    (referrerId) REFERENCES users (id)
     ON UPDATE CASCADE
     ON DELETE CASCADE,
   FOREIGN KEY 
-    (pledge_id) REFERENCES pledges (id)
+    (pledgeId) REFERENCES pledges (id)
     ON UPDATE CASCADE
     ON DELETE CASCADE
 );
