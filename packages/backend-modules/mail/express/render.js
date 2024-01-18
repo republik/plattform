8ae1ba1b9d1bb3e2914f7b698a011f8b6586f75b
Olivier Baumann <olivierbaumann@me.com>
2024-01-18 11:18:25 +0100

    feat(Mail): allow query params in email renderer

--- packages/backend-modules/mail/express/render.js
+++ packages/backend-modules/mail/express/render.js
@@ -1,15 +1,38 @@
 const { getTemplates, envMergeVars } = require('../lib/sendMailTemplate')
 
+/**
+ * Convert the query-param object to the format of the 'envMergeVars' const
+ * { name: "Olivier" } -> [{name:"name", content: "Olivier"}]
+ */
+function convertVarObject(queryParams) {
+  if (typeof queryParams !== 'object' || queryParams === null) {
+    return []
+  }
+  return Object.keys(queryParams)
+    .map((key) => ({
+      name: key,
+      content: queryParams[key],
+    }))
+    .filter(Boolean)
+}
+
 module.exports = async (server) => {
   server.get('/mail/render/:template', async (req, res) => {
     const { html: template } = await getTemplates(req.params.template)
+    const queryMergeVars = convertVarObject(req?.query || {})
 
     if (!template) {
       return res.sendStatus(404)
     }
 
-    const mail = envMergeVars.reduce((template, mergeVar) => {
-      const { name, content } = mergeVar
+    const variables = [...envMergeVars, ...queryMergeVars].filter(
+      () => (value, index, self) => {
+        return self.indexOf(value) === index
+      },
+    )
+
+    const mail = variables.reduce((template, variable) => {
+      const { name, content } = variable
       return template.replace(new RegExp(`{{?${name}?}}`, 'ig'), content)
     }, template)
 
