4acbbfdef9ac52d4ea421abb1a65b1faf2222836
Tobias Maier <tobias.maier@republik.ch>
2024-01-18 17:07:04 +0100

    feat(api/auth): add migration to add alias field to user

---
+++ packages/backend-modules/migrations/migrations/20240118145152-user-alias-ddl.js
@@ -0,0 +1,8 @@
+const run = require('../run.js')
+
+const dir = './packages/backend-modules/auth/migrations/sqls'
+const file = '20240118145152-user-alias-ddl'
+
+exports.up = (db) => run(db, dir, `${file}-up.sql`)
+
+exports.down = (db) => run(db, dir, `${file}-down.sql`)
