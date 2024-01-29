65f67a68b5b2a402e8864b36d5314d6b8ac16d55
Jeremy Stucki <jeremy.stucki@gmail.com>
2024-01-29 18:00:14 +0100

    fix: throttle router.replace calls (Safari/Firefox will throw when too many updates are made)

--- apps/www/src/app/(campaign)/campaign-receiver/[code]/angebot/price-slider-with-state.tsx
+++ apps/www/src/app/(campaign)/campaign-receiver/[code]/angebot/price-slider-with-state.tsx
@@ -1,8 +1,9 @@
 'use client'
 
 import { css } from '@app/styled-system/css'
+import { throttle } from 'lodash'
 import { usePathname, useRouter, useSearchParams } from 'next/navigation'
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 import useResizeObserver from 'use-resize-observer'
 import { getSliderStepForValue } from './price-slider/helpers'
 import { PriceSlider } from './price-slider/price-slider-horizontal'
@@ -15,13 +16,20 @@ export const PriceSliderWithState = () => {
   const [step, setStep] = useState(initialStep)
   const { ref, width } = useResizeObserver()
 
+  const updateUrl = useCallback(
+    throttle((value) => {
+      router.replace(`${pathname}?price=${value}`)
+    }, 400),
+    [router, pathname],
+  )
+
   return (
     <div ref={ref} className={css({ width: '100%' })}>
       {width && (
         <PriceSlider
           onChange={(sliderStep) => {
             setStep(sliderStep)
-            router.replace(`${pathname}?price=${sliderStep.value}`)
+            updateUrl(sliderStep.value)
           }}
           step={step}
           width={width}
