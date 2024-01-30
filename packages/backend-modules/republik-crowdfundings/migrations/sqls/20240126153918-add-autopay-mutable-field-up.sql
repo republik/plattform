fb9c8cca7ab60093743a7d28330840892084adc0
Luciana <luciana.kolbeck@republik.ch>
2024-01-30 10:09:30 +0100

    fix: add BENEFACTOR_ABO to autoPayMutable membership types

--- packages/backend-modules/republik-crowdfundings/migrations/sqls/20240126153918-add-autopay-mutable-field-up.sql
+++ packages/backend-modules/republik-crowdfundings/migrations/sqls/20240126153918-add-autopay-mutable-field-up.sql
@@ -1,3 +1,3 @@
 -- migrate up here: CREATE TABLE...
 ALTER TABLE "public"."membershipTypes" ADD COLUMN "autoPayMutable" boolean NOT NULL DEFAULT false;
-UPDATE "public"."membershipTypes" SET "autoPayMutable" = TRUE WHERE "name" = 'ABO';
+UPDATE "public"."membershipTypes" SET "autoPayMutable" = TRUE WHERE "name" = 'ABO' OR "name"='BENEFACTOR_ABO';
