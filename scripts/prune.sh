245a3a2ee87a707f95001ff7799adc26878ed205
Luciana <luciana.kolbeck@republik.ch>
2024-02-05 14:31:35 +0100

    test: increase turbo prune verbosity

--- scripts/prune.sh
+++ scripts/prune.sh
@@ -7,7 +7,7 @@ then
   npx turbo prune --scope="@project-r/styleguide"
 else
   echo "Running prune for $SERVER-app"
-  npx turbo prune --scope="@orbiting/$SERVER-app"
+  npx turbo prune --scope="@orbiting/$SERVER-app" -vvv
 fi
 
 if [ ! -f out/yarn.lock ]
