721e65c5c85a9ea787dbec17d777d61ca4a22652
Tobias Maier <tobias.maier@republik.ch>
2024-02-27 07:47:53 +0100

    feat(www/sentry): run sentry installation wizard

---
+++ apps/www/sentry.edge.config.ts
@@ -0,0 +1,16 @@
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs'
+
+Sentry.init({
+  dsn: 'https://d5839f83efbf888e1f8108a48fecc287@o4505721260212224.ingest.sentry.io/4506817347977216',
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+})
