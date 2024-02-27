fe77af0eca4ff7e33fe8a92329e552eea0933e76
Jeremy Stucki <jeremy.stucki@gmail.com>
2024-02-27 16:44:08 +0100

    refactor: remove dark mode support from campaign

--- apps/www/src/theme/preset-campaign-2024.ts
+++ apps/www/src/theme/preset-campaign-2024.ts
@@ -6,10 +6,10 @@ export const presetCampaign2024 = definePreset({
       campaign2024: '[data-page-theme="campaign-2024"] &',
       campaign2024Inverted:
         '[data-page-theme="campaign-2024"]:where([data-theme-inverted]) &',
-      campaign2024Dark:
-        '[data-theme="dark"] [data-page-theme="campaign-2024"] &',
-      campaign2024DarkInverted:
-        '[data-theme="dark"] [data-page-theme="campaign-2024"]:where([data-theme-inverted]) &',
+      // campaign2024Dark:
+      //   '[data-theme="dark"] [data-page-theme="campaign-2024"] &',
+      // campaign2024DarkInverted:
+      //   '[data-theme="dark"] [data-page-theme="campaign-2024"]:where([data-theme-inverted]) &',
     },
   },
 
@@ -33,9 +33,6 @@ export const presetCampaign2024 = definePreset({
               _campaign2024: '{colors.campaign2024.red}',
               _campaign2024Inverted:
                 'linear-gradient(180deg, transparent 0%, {colors.campaign2024.red} 4rem, {colors.campaign2024.red} 100%)',
-              _campaign2024Dark: '{colors.campaign2024.yellow}',
-              _campaign2024DarkInverted:
-                'linear-gradient(180deg, transparent 0%, {colors.campaign2024.darkRed} 4rem, {colors.campaign2024.darkRed} 100%)',
             },
           },
         },
@@ -44,37 +41,29 @@ export const presetCampaign2024 = definePreset({
             value: {
               _campaign2024: '{colors.campaign2024.red}',
               _campaign2024Inverted: '{colors.campaign2024.yellow}',
-              _campaign2024Dark: '{colors.campaign2024.red}',
-              _campaign2024DarkInverted: '{colors.campaign2024.yellow}',
             },
           },
           overlay: {
             value: {
               _campaign2024: 'rgba(229,1,70, 0.05)',
               _campaign2024Inverted: 'rgba(255,255,255, 0.5)',
-              _campaign2024Dark: 'rgba(255,255,255, 0.05)',
             },
           },
           divider: {
             value: {
               _campaign2024: 'rgba(229,1,70,0.15)',
-              _campaign2024Dark: 'rgba(255,255,255, 0.15)',
             },
           },
           pageBackground: {
             value: {
               _campaign2024: '{colors.campaign2024.yellow}',
               _campaign2024Inverted: '{colors.campaign2024.red}',
-              _campaign2024Dark: '{colors.campaign2024.darkRed}',
-              _campaign2024DarkInverted: '{colors.campaign2024.darkRed}',
             },
           },
           contrast: {
             value: {
               _campaign2024: '{colors.campaign2024.red}',
               _campaign2024Inverted: '{colors.campaign2024.yellow}',
-              _campaign2024Dark: '{colors.campaign2024.yellow}',
-              _campaign2024DarkInverted: '{colors.campaign2024.red}',
             },
           },
           text: {
@@ -82,23 +71,18 @@ export const presetCampaign2024 = definePreset({
               value: {
                 _campaign2024: '{colors.campaign2024.red}',
                 _campaign2024Inverted: '{colors.campaign2024.yellow}',
-                _campaign2024Dark: '{colors.campaign2024.yellow}',
               },
             },
             inverted: {
               value: {
                 _campaign2024: '{colors.campaign2024.yellow}',
                 _campaign2024Inverted: '{colors.campaign2024.red}',
-                _campaign2024Dark: '{colors.campaign2024.red}',
-                _campaign2024DarkInverted: '{colors.campaign2024.darkRed}',
               },
             },
             primary: {
               value: {
                 _campaign2024: '{colors.campaign2024.yellow}',
                 _campaign2024Inverted: '{colors.campaign2024.red}',
-                _campaign2024Dark: '{colors.campaign2024.yellow}',
-                _campaign2024DarkInverted: '{colors.campaign2024.red}',
               },
             },
           },
@@ -106,7 +90,6 @@ export const presetCampaign2024 = definePreset({
             value: {
               _campaign2024: '{colors.campaign2024.red}',
               _campaign2024Inverted: '{colors.campaign2024.yellow}',
-              _campaign2024Dark: '{colors.campaign2024.yellow}',
             },
           },
         },
