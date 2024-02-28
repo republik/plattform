d40af424831ffef2471dd4434e5aea2da02be278
Tobias Maier <tobias.maier@republik.ch>
2024-02-28 11:40:08 +0100

    feat(www): support handling '/dialog' article url suffix to redirect to an articles discussion

--- apps/www/pages/[...path].tsx
+++ apps/www/pages/[...path].tsx
@@ -5,6 +5,7 @@ import { gql } from '@apollo/client'
 import { getDocument } from '../components/Article/graphql/getDocument'
 import { createGetStaticProps } from '../lib/apollo/helpers'
 import { isExternal } from '../components/StatusError'
+import { DocumentDiscussionIdDocument } from '@app/graphql/republik-api/gql/graphql'
 
 type Params = {
   path: string[]
@@ -13,7 +14,7 @@ type Params = {
 type Props = {
   payNoteTryOrBuy?: number
   payNoteSeed?: number
-  clientRedirection?: any
+  clientRedirection?: unknown
 }
 
 export const getStaticPaths: GetStaticPaths<Params> = async () => {
@@ -27,7 +28,8 @@ const REVALIDATE_SECONDS = 10
 
 export const getStaticProps = createGetStaticProps<Props, Params>(
   async (client, { params }) => {
-    const path = '/' + params.path.join('/')
+    const pathParts = params.path
+    const path = '/' + pathParts.join('/')
 
     const {
       data: { article },
@@ -81,6 +83,34 @@ export const getStaticProps = createGetStaticProps<Props, Params>(
           destination: redirection.target,
           statusCode: redirection.status,
         },
+      }
+    }
+
+    /**
+     * In case an article is not found and the sufix of the path is `/dialog`
+     * we check if the article has a discussion and redirect to the discussion.
+     *
+     * !!! This only works for documents that use the article template!
+     *
+     */
+    if (path.endsWith('/dialog')) {
+      const path = '/' + pathParts.slice(0, -1).join('/')
+      const discussionIdResult = await client.query({
+        query: DocumentDiscussionIdDocument,
+        variables: { path },
+      })
+
+      if (
+        discussionIdResult.data.document?.meta.template === 'article' &&
+        discussionIdResult.data.document?.meta.ownDiscussion?.id
+      ) {
+        return {
+          redirect: {
+            destination: `/dialog?t=article&id=${discussionIdResult.data.document.meta.ownDiscussion.id}`,
+            permanent: false,
+          },
+          revalidate: REVALIDATE_SECONDS,
+        }
       }
     }
 
