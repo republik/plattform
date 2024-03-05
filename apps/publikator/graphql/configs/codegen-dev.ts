88fa8367fcaa078cde15eec31f841a221d09ac14
Tobias Maier <tobias.maier@republik.ch>
2024-03-05 09:51:41 +0100

    feat: add gql-codegen setup to admin & publikator

---
+++ apps/publikator/graphql/configs/codegen-dev.ts
@@ -0,0 +1,30 @@
+import type { CodegenConfig } from '@graphql-codegen/cli'
+
+import { loadEnvConfig } from '@next/env'
+
+loadEnvConfig(process.cwd())
+
+const config: CodegenConfig = {
+  overwrite: true,
+  generates: {
+    './graphql/republik-api/gql/': {
+      schema: './graphql/republik-api.schema.graphql',
+      documents: ['./graphql/republik-api/**/*.{ts,tsx,gql,graphql}'],
+      preset: 'client',
+      presetConfig: {
+        gqlTagName: 'gql',
+      },
+      config: {
+        scalars: {
+          ItemId: 'string',
+          IntType: 'number',
+          Date: 'string',
+          DateTime: 'string',
+        },
+      },
+      plugins: [],
+    },
+  },
+}
+
+export default config
