b6601126ce8f202ce26e803150a32aa2144cd7c4
Tobias Maier <tobias.maier@republik.ch>
2024-03-13 14:55:26 +0100

    feat(www): ensure headers are same as before removing the express server

--- apps/www/next.config.js
+++ apps/www/next.config.js
@@ -86,23 +86,21 @@ const nextConfig = {
         headers: [
           // Security headers, peviously handled by helmet
           ...Object.entries({
-            'Content-Security-Policy': `default-src 'self';base-uri 'self';font-src 'self' https: data:;form-action 'self';frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src 'self';script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests`,
-            'Cross-Origin-Opener-Policy': 'same-origin',
-            'Cross-Origin-Resource-Policy': 'same-origin',
-            'Origin-Agent-Cluster': '?1',
+            // 'Content-Security-Policy': `default-src 'self';base-uri 'self';font-src 'self' https: data:;form-action 'self';frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src 'self';script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests`,
+            // 'Cross-Origin-Opener-Policy': 'same-origin',
+            // 'Cross-Origin-Resource-Policy': 'same-origin',
+            // 'Origin-Agent-Cluster': '?1',
             // Preload approval for 1 year
             'Referrer-Policy': 'no-referrer',
             'Strict-Transport-Security': `max-age=${
               60 * 60 * 24 * 365
             }; includeSubDomains; preload`,
             'X-Content-Type-Options': 'nosniff',
-            'X-DNS-Prefetch-Control': 'off',
             'X-Download-Options': 'noopen',
             'X-Frame-Options': 'SAMEORIGIN',
-            'X-Permitted-Cross-Domain-Policies': 'none',
             // removed by helmet by default, but we keep it for now
-            'X-Powered-By': '',
-            'X-XSS-Protection': '0',
+            'X-Powered-By': 'Republik',
+            'X-XSS-Protection': '1; mode=block',
           }).map(([key, value]) => ({
             key,
             value,
