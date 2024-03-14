5c157fdfb6194ea9d549a0a32e23d25e405de703
Jeremy Stucki <jeremy.stucki@gmail.com>
2024-03-14 11:02:41 +0100

    feat(publikator): allow unpublishing of files

---
+++ apps/publikator/components/Files/actions/Unpublish.js
@@ -0,0 +1,36 @@
+import { gql, useMutation } from '@apollo/client'
+import { Button } from '@project-r/styleguide'
+
+import { RepoFile } from '../../../lib/graphql/fragments'
+
+const MAKE_PRIVATE = gql`
+  mutation repoFileMakePrivate($id: ID!) {
+    repoFileMakePrivate(id: $id) {
+      ...RepoFile
+    }
+  }
+
+  ${RepoFile}
+`
+
+const Publish = ({ file }) => {
+  const [makePrivate, { loading }] = useMutation(MAKE_PRIVATE)
+
+  const onClick = () => {
+    if (
+      window.confirm(
+        'Achtung! Links auf diese Datei werden nicht mehr funktionieren.',
+      )
+    ) {
+      makePrivate({ variables: { id: file.id } })
+    }
+  }
+
+  return (
+    <Button onClick={onClick} disabled={loading} primary small>
+      Nicht mehr veröffentlichen
+    </Button>
+  )
+}
+
+export default Publish
