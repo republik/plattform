72c0cc06f9213735f8c315c059353079fd2d79aa
Luciana <luciana.kolbeck@republik.ch>
2024-03-21 16:19:49 +0100

    fix(migration): add missing migration file for datatrans change

---
+++ packages/backend-modules/migrations/migrations/20231217190623-datatrans.js
@@ -0,0 +1,8 @@
+const run = require('../run.js')
+
+const dir = '/packages/backend-modules/datatrans/migrations/sqls'
+const file = '20231217190623-datatrans'
+
+exports.up = (db) => run(db, dir, `${file}-up.sql`)
+
+exports.down = (db) => run(db, dir, `${file}-down.sql`)
