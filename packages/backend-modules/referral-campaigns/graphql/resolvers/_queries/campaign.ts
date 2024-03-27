8f538329de778eee1565a79454d48d8b2250b288
Henning Dahlheim <henning.dahlheim@republik.ch>
2024-03-27 17:43:02 +0100

    feat(referral-campaigns): add isActive field

--- packages/backend-modules/referral-campaigns/graphql/resolvers/_queries/campaign.ts
+++ packages/backend-modules/referral-campaigns/graphql/resolvers/_queries/campaign.ts
@@ -1,6 +1,7 @@
 import type { GraphqlContext } from '@orbiting/backend-modules-types'
+import type { Campaign } from '../../types'
 import { PGReferralsRepo } from '../../../lib/repo'
-import { Campaign } from '../../types'
+import { transformCampagin } from '../../../lib/campaign'
 
 type CampgainArgs = {
   slug: string
@@ -15,6 +16,9 @@ export = async function campaigns(
   ctx: GraphqlContext,
 ): Promise<Campaign | null> {
   const campaign = await new PGReferralsRepo(ctx.pgdb).getCampaignBySlug(slug)
+  if (!campaign) {
+    return null
+  }
 
-  return campaign
+  return transformCampagin(campaign)
 }
---
+++ packages/backend-modules/referral-campaigns/lib/campaign.ts
@@ -0,0 +1,12 @@
+import dayjs = require('dayjs')
+import isBetween = require('dayjs/plugin/isBetween')
+import { Campaign } from '../graphql/types'
+import { CampaignRow } from './types'
+dayjs.extend(isBetween)
+
+export function transformCampagin(campaign: CampaignRow): Campaign {
+  const now = new Date()
+
+  const isActive = dayjs(now).isBetween(campaign.beginDate, campaign.endDate)
+  return Object.assign({}, campaign, { isActive: isActive })
+}
