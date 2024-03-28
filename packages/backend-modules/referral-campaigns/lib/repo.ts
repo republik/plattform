382e935f2ec991f51ed1f00eb2b709e4f0d1aafe
Henning Dahlheim <henning.dahlheim@republik.ch>
2024-03-28 13:16:35 +0100

    Feat/referral campaign is active field (#769)
    * feat(referral-campaigns): add isActive field
    
    * fix(referral-campaigns): typo campagin -> campaign

--- packages/backend-modules/referral-campaigns/lib/repo.ts
+++ packages/backend-modules/referral-campaigns/lib/repo.ts
@@ -1,14 +1,14 @@
 import type { UserRow } from '@orbiting/backend-modules-types'
 import type { PgDb } from 'pogi'
-import type { Campaign } from '../graphql/types'
 import type {
   CampaignRewardRow,
   UserCampaignRewardsRow,
   ReferrersWithCountRow,
+  CampaignRow,
 } from './types'
 
 export interface ReferralCampaignRepo {
-  getCampaignBySlug(slug: string): Promise<Campaign | null>
+  getCampaignBySlug(slug: string): Promise<CampaignRow | null>
   getCampaignReferralCount(campaignId: string): Promise<number | number>
   getUserCampaignReferralCount(
     campaignId: string,
@@ -43,7 +43,7 @@ export class PGReferralsRepo implements ReferralCampaignRepo, ReferralCodeRepo {
     this.#pgdb = pgdb
   }
 
-  async getCampaignBySlug(slug: string): Promise<Campaign | null> {
+  async getCampaignBySlug(slug: string): Promise<CampaignRow | null> {
     return await this.#pgdb.public.campaigns.findOne({
       slug: slug,
     })
