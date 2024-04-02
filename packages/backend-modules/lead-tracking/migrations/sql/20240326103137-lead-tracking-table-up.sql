a8b4ab933fda46e3f888273fca00df23a4990ba6
Henning Dahlheim <henning.dahlheim@republik.ch>
2024-04-02 15:22:03 +0200

    chore(lead-tracking): fix migration, add import script

--- packages/backend-modules/lead-tracking/migrations/sql/20240326103137-lead-tracking-table-up.sql
+++ packages/backend-modules/lead-tracking/migrations/sql/20240326103137-lead-tracking-table-up.sql
@@ -1,10 +1,10 @@
 -- migrate up here: CREATE TABLE...
-CREATE TABLE IF NOT EXISTS leadTacking (
-  "id" uuid PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4()
-  "email" citext NOT NULL
-  "leadTag" citext NOT NULL
-  "createdAt"   timestamptz default now(),
-  "updatedAt"   timestamptz default now()
+CREATE TABLE IF NOT EXISTS "leadTracking" (
+  "id" uuid PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
+  "email" citext NOT NULL,
+  "leadTag" citext NOT NULL,
+  "createdAt" timestamptz default now(),
+  "updatedAt" timestamptz default now()
 );
 
-CREATE UNIQUE "email_lead_tag_idx" on leadTracking (email, leadTag);
+CREATE UNIQUE INDEX "email_lead_tag_idx" on "leadTracking" ("email", "leadTag");
