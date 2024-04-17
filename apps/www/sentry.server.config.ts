ac904d31b91f021d657f8be073ba0a95558587b6
Tobias Maier <tobias.maier@republik.ch>
2024-04-17 09:58:50 +0200

    feat(www): reduce tracesSampleRate in production to 0.25

--- apps/www/sentry.server.config.ts
+++ apps/www/sentry.server.config.ts
@@ -4,11 +4,13 @@
 
 import * as Sentry from '@sentry/nextjs'
 
+const isProduction = process.env.NODE_ENV === 'production'
+
 Sentry.init({
   dsn: 'https://d5839f83efbf888e1f8108a48fecc287@o4505721260212224.ingest.sentry.io/4506817347977216',
 
   // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
+  tracesSampleRate: isProduction ? 0.25 : 1,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
