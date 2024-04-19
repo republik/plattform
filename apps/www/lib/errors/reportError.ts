e92a1c45edb5cdc72a880373fc2a93456aa1315e
Tobias Maier <tobias.maier@republik.ch>
2024-04-19 10:34:11 +0200

    feat(web): hand the error of to Sentry when 'reportError' is called

--- apps/www/lib/errors/reportError.ts
+++ apps/www/lib/errors/reportError.ts
@@ -1,25 +1,10 @@
-let lastError: Error | string | undefined
+import * as Sentry from '@sentry/nextjs'
 
 export const reportError = async (context: string, error: Error | string) => {
-  // do not track server side
-  if (typeof window === 'undefined') {
-    return
-  }
-
-  // avoid double reporting from window.onerror
-  if (lastError?.toString().trim() === error.toString().trim()) {
-    return
-  }
-
-  lastError = error
-  const buildId = process.env.BUILD_ID
-
-  console.error(context, error)
-
-  await fetch('/api/reportError', {
-    method: 'POST',
-    body: `${context}\n${window.location.href}\n${
-      buildId ? `(buildId: ${buildId})\n` : ''
-    }${error}`,
+  Sentry.captureException(error, {
+    tags: {
+      origin: 'reportError-func',
+      reportErrorContext: context,
+    },
   })
 }
