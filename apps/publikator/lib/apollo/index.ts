e08fa6668b97dfd821efef00b0b4d30ccebc61bd
Tobias Maier <tobias.maier@republik.ch>
2024-04-24 10:28:55 +0200

    feat(publikator): add apollo-error-link to forward errors to sentry

--- apps/publikator/lib/apollo/index.ts
+++ apps/publikator/lib/apollo/index.ts
@@ -1,10 +1,12 @@
 import { createApolloClientUtilities } from '@republik/nextjs-apollo-client'
 
 import { API_URL, API_WS_URL } from '../settings'
+import { SentryErrorLink } from 'lib/sentry/apollo-error-link'
 
 export const { initializeApollo, withApollo } = createApolloClientUtilities({
   name: '@orbiting/publikator-app',
   version: process.env.BUILD_ID,
   apiUrl: API_URL,
   wsUrl: API_WS_URL,
+  errorLink: SentryErrorLink,
 })
