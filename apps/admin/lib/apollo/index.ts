20f8e60e37c3aa37e3b10cb845bc74d1984c350e
Tobias Maier <tobias.maier@republik.ch>
2024-04-26 16:41:18 +0200

    feat(frontends): get rid of sentry apollo error-links as they provide no added value

--- apps/admin/lib/apollo/index.ts
+++ apps/admin/lib/apollo/index.ts
@@ -3,13 +3,11 @@ import {
   makeWithDefaultSSR,
 } from '@republik/nextjs-apollo-client'
 import { API_URL } from '../../server/constants'
-import { SentryErrorLink } from 'lib/sentry/apollo-error-link'
 
 export const { withApollo, initializeApollo } = createApolloClientUtilities({
   name: '@orbiting/admin-app',
   version: process.env.BUILD_ID,
   apiUrl: API_URL,
-  errorLink: SentryErrorLink,
 })
 
 export const withDefaultSSR = makeWithDefaultSSR(initializeApollo)
