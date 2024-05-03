c6787e66e6bad34a8760746c6a36748413085520
Tobias Maier <tobias.maier@republik.ch>
2024-05-03 16:35:57 +0200

    test(www): add e2e test dir to jest ignore patterns

--- apps/www/jest.config.js
+++ apps/www/jest.config.js
@@ -7,12 +7,17 @@ const createJestConfig = nextJest({
 })
 
 // Add any custom config to be passed to Jest
+// jest config type
+/**
+ * @type {import('@jest/types').Config.InitialOptions}
+ */
 const customJestConfig = {
   // Add more setup options before each test is run
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
   moduleDirectories: ['node_modules', '<rootDir>/'],
   testEnvironment: 'jest-environment-jsdom',
+  testPathIgnorePatterns: ['<rootDir>/__tests__/e2e'],
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
