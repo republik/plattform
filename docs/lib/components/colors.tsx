36d44e6c2c3af3a2dad78e6a90fff6b6853a2e16
Jeremy Stucki <jeremy.stucki@gmail.com>
2024-05-03 15:01:50 +0200

    docs: fix type

--- docs/lib/components/colors.tsx
+++ docs/lib/components/colors.tsx
@@ -1,87 +1,6 @@
 import { css } from '@republik/theme/css'
 import { hstack } from '@republik/theme/patterns'
-import { token } from '@republik/theme/tokens'
-
-const colorTokens = [
-  'text',
-  'text.inverted',
-  'text.white',
-  'contrast',
-  'background',
-  'pageBackground',
-  'link',
-  'primary',
-  'primaryHover',
-  'overlay',
-  'error',
-  'divider',
-  'disabled',
-  'textSoft',
-]
-
-const legacyColorTokens = [
-  'logo',
-  'default',
-  'overlay',
-  'hover',
-  'alert',
-  'error',
-  'defaultInverted',
-  'overlayInverted',
-  'divider',
-  'dividerInverted',
-  'primary',
-  'primaryHover',
-  'primaryText',
-  'text',
-  'textInverted',
-  'textSoft',
-  'textSoftInverted',
-  'disabled',
-  'accentColorBriefing',
-  'accentColorInteraction',
-  'accentColorOppinion',
-  'accentColorFormats',
-  'accentColorMeta',
-  'accentColorAudio',
-  'accentColorFlyer',
-  'overlayShadow',
-  'fadeOutGradientDefault',
-  'fadeOutGradientDefault90',
-  'fadeOutGradientOverlay',
-  'displayLight',
-  'displayDark',
-  'sequential100',
-  'sequential95',
-  'sequential90',
-  'sequential85',
-  'sequential80',
-  'sequential75',
-  'sequential70',
-  'sequential65',
-  'sequential60',
-  'sequential55',
-  'sequential50',
-  'opposite100',
-  'opposite80',
-  'opposite60',
-  'neutral',
-  'discrete1',
-  'discrete2',
-  'discrete3',
-  'discrete4',
-  'discrete5',
-  'discrete6',
-  'discrete7',
-  'discrete8',
-  'discrete9',
-  'discrete10',
-  'chartsInverted',
-  'flyerBg',
-  'flyerText',
-  'flyerMetaText',
-  'flyerFormatText',
-]
+import { Token, token } from '@republik/theme/tokens'
 
 export function Colors({
   tokens,
@@ -108,7 +27,7 @@ export function Colors({
               name={name}
               value={
                 tokenType === 'pandacss'
-                  ? token.var(`colors.${name}`)
+                  ? token.var(`colors.${name}` as Token)
                   : `var(--color-${name})`
               }
             />
