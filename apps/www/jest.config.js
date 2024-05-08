aabf97b23068b26a154ff38ec3b58228ba9ef600
Tobias Maier <tobias.maier@republik.ch>
2024-05-08 14:58:08 +0200

    chore(www): add playwright setup

--- apps/www/jest.config.js
+++ apps/www/jest.config.js
@@ -13,6 +13,7 @@ const customJestConfig = {
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
   moduleDirectories: ['node_modules', '<rootDir>/'],
   testEnvironment: 'jest-environment-jsdom',
+  testPathIgnorePatterns: ['<rootDir>/__tests__/e2e'],
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
---
+++ apps/www/playwright.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from '@playwright/test'
+import { loadEnvConfig } from '@next/env'
+
+const nodeEnv = process.env.NODE_ENV || 'development'
+const isDev = nodeEnv === 'development'
+
+loadEnvConfig('.', isDev)
+
+export default defineConfig({
+  testDir: './__tests__/e2e/',
+  snapshotDir: './__tests__/__snapshots__/',
+  fullyParallel: false,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: process.env.CI ? 'github' : 'list',
+  // snapshotPathTemplate: '/{test}.snap',
+  use: {
+    actionTimeout: 0,
+    baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+    trace: 'on-first-retry',
+  },
+  webServer: {
+    command: isDev ? 'yarn dev' : 'yarn start',
+    url: process.env.NEXT_PUBLIC_BASE_URL,
+    stdout: 'pipe',
+    stderr: 'pipe',
+    timeout: 120_000, // building the app can take a while
+    reuseExistingServer: !isDev,
+  },
+})
