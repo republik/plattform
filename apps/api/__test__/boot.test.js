83bff2caa11fc5730a263b188c0883735459c196
Henning Dahlheim <henning.dahlheim@republik.ch>
2024-05-14 13:33:18 +0200

    test(api): add api boot test

---
+++ apps/api/__test__/boot.test.js
@@ -0,0 +1,43 @@
+require('@orbiting/backend-modules-env').config('apps/api/.env.test.local')
+const { Wait, DockerComposeEnvironment } = require('testcontainers')
+const { createDB } = require('@orbiting/backend-modules-migrations')
+const {
+  migrateUp,
+  seedSampleData,
+} = require('@orbiting/backend-modules-migrations/lib')
+
+describe('api boot tests', () => {
+  let composeEnv = null
+  let server = null
+
+  beforeAll(async () => {
+    composeEnv = await new DockerComposeEnvironment('./', [
+      'docker-compose-test.yml',
+    ])
+      .withWaitStrategy(
+        'redis-1',
+        Wait.forLogMessage('Ready to accept connections'),
+      )
+      .withWaitStrategy('postgres', Wait.forHealthCheck())
+      .up(['postgres', 'redis', 'elastic'])
+
+    await createDB(process.env.DATABASE_URL)
+
+    await migrateUp()
+
+    await seedSampleData(process.env.DATABASE_URL)
+
+    server = await require('../server').start()
+  }, 120000)
+
+  test('api is responding', async () => {
+    const res = await fetch('http://localhost:5010/graphiql')
+
+    await expect(res.status).toBe(200)
+  })
+
+  afterAll(async () => {
+    await server.close()
+    composeEnv.down()
+  })
+})
--- packages/backend-modules/migrations/lib/index.js
+++ packages/backend-modules/migrations/lib/index.js
@@ -1,4 +1,5 @@
 const { PgDb } = require('pogi')
+const DBMigrate = require('db-migrate')
 
 function parseDBConnectionString(urlString) {
   const url = new URL(urlString)
@@ -26,6 +27,69 @@ async function createDB(DATABASE_URL) {
   await pgdb.close()
 }
 
+async function seedSampleData(DATABASE_URL) {
+  if (!DATABASE_URL) {
+    throw new Error('DATABASE_URL is missing')
+  }
+
+  if (process.env.NODE_ENV === 'production') {
+    throw new Error('Seeding test data not allowed in production')
+  }
+
+  if (!process.env.DATABASE_SAMPLE_DATA_URL) {
+    throw new Error('DB sample data is missing')
+  }
+
+  const res = await fetch(process.env.DATABASE_SAMPLE_DATA_URL)
+  if (!res.ok) {
+    throw new Error(
+      `error fetching sample data: ${res.status} - ${res.statusText}`,
+    )
+  }
+
+  const pgdb = await PgDb.connect({
+    application_name: 'setup-seed',
+    connectionString: DATABASE_URL,
+  })
+
+  await pgdb.run(await res.text())
+
+  await pgdb.close()
+}
+
+async function migrateUp(upTo = undefined) {
+  const migrator = getMigrator()
+
+  return migrator.up(upTo)
+}
+
+async function migrateRest() {
+  const migrator = getMigrator()
+
+  return migrator.reset()
+}
+
+async function migrateDown(downTo = undefined) {
+  const migrator = getMigrator()
+
+  return migrator.down(downTo)
+}
+
+function getMigrator() {
+  const migrator = DBMigrate.getInstance()
+
+  migrator.setConfigParam(
+    'migrations-dir',
+    'packages/backend-modules/migrations/migrations',
+  )
+
+  return migrator
+}
+
 module.exports = {
   createDB,
+  migrateUp,
+  migrateDown,
+  migrateRest,
+  seedSampleData,
 }
