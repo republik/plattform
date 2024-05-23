3ddcdd262224ffdff3310f9662f7e09505b255ae
Tobias Maier <tobias.maier@republik.ch>
2024-05-23 12:07:47 +0200

    chore(styleguide): genereate TS decl in tsup onSuccess rather than with DTS

--- packages/styleguide/tsup.config.ts
+++ packages/styleguide/tsup.config.ts
@@ -1,9 +1,10 @@
+import { exec } from 'child_process'
 import { defineConfig } from 'tsup'
 
 const config = defineConfig({
   entryPoints: ['src/lib.ts', 'src/editor.ts', 'src/chart.ts'],
   format: ['cjs', 'esm'],
-  dts: true,
+  // dts: true,
   sourcemap: true,
   clean: true,
   minify: true,
@@ -12,6 +13,19 @@ const config = defineConfig({
   outDir: 'dist',
   loader: {
     '.js': 'jsx',
+  },
+  onSuccess: async () => {
+    await new Promise<void>((resolve, reject) => {
+      exec('tsc --emitDeclarationOnly --declaration', (err, stdout, stderr) => {
+        if (err) {
+          console.error(stderr)
+          reject(err)
+        }
+        console.log(stdout)
+        return resolve()
+      })
+    })
+    console.log('⚡ TypeScript declaration files generated!')
   },
 })
 
