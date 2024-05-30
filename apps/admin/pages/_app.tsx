b019a7bd88891f58d9f3757ff94c13d2cf3caeb5
Tobias Maier <tobias.maier@republik.ch>
2024-05-30 10:30:28 +0200

    feat(admin): remove Plausible

--- apps/admin/pages/_app.tsx
+++ apps/admin/pages/_app.tsx
@@ -4,19 +4,6 @@ import { RootColorVariables } from '@project-r/styleguide'
 
 import { PagePropsWithApollo } from '@republik/nextjs-apollo-client'
 import { withApollo } from '../lib/apollo'
-import PlausibleProvider from 'next-plausible'
-
-function PlausibleWrapper({ children }: { children: React.ReactNode }) {
-  const plausibleDomain = process.env.NEXT_PUBLIC_PLAUSIBLE_DOMAIN
-
-  if (!plausibleDomain) {
-    return <>{children}</>
-  }
-
-  return (
-    <PlausibleProvider domain={plausibleDomain}>{children}</PlausibleProvider>
-  )
-}
 
 type WebAppProps = {
   serverContext?: any
@@ -38,9 +25,7 @@ const WebApp = ({
         <meta name='viewport' content='width=device-width,initial-scale=1' />
       </Head>
       <RootColorVariables />
-      <PlausibleWrapper>
-        <Component serverContext={serverContext} {...otherPageProps} />
-      </PlausibleWrapper>
+      <Component serverContext={serverContext} {...otherPageProps} />
     </>
   )
 }
