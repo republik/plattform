f3ee83af7a109bccf2bbf7d1c15f4afbc21c2f40
Henning Dahlheim <henning.dahlheim@republik.ch>
2024-05-30 16:44:48 +0200

    feat(cors): allways allow request from localhost (#857)
    the new dynamic regex matcher prevents request proxied from localhost.
    
    There is no real reason why we should prevent localhost request similar to requests cli tools like curl

--- packages/backend-modules/base/lib/corsRegex.js
+++ packages/backend-modules/base/lib/corsRegex.js
@@ -7,14 +7,24 @@ function wildcardToRegex(pattern) {
   return new RegExp(`^${escapedPattern}$`)
 }
 
+const LOCALHOST_ORIGIN =
+  /https?:\/\/(?:localhost|127\.0\.0\.1|::1)(?::\d{1,5})?/
+
 function createCORSMatcher(allowedOrigins) {
   const regexPatterns = allowedOrigins.map(wildcardToRegex)
 
   return (origin, callback) => {
     if (!origin) {
-      // Allow non-origin requests (like mobile apps, curl requests, etc.)
+      debug(`allowing non web request`)
+      // Allow non-origin requests (like mobile apps, curl requests, etc.) and request from localhost
       return callback(null, true)
     }
+    if (LOCALHOST_ORIGIN.test(origin)) {
+      debug(`allowing request from local origin [${origin}]`)
+      // Allow non-origin requests (like mobile apps, curl requests, etc.) and request from localhost
+      return callback(null, true)
+    }
+
     debug(
       `checking [${origin}] against CORS allow list ${JSON.stringify(
         regexPatterns.map((r) => r.toString()),
