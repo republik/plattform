04222d97e20819dfcabbc25c4b9b35d1201d01cc
Tobias Maier <tobias.maier@republik.ch>
2024-05-30 15:05:05 +0200

    chore(styleguide): type comment as any 😭

--- packages/styleguide/src/components/Discussion/Internal/Comment/types.ts
+++ packages/styleguide/src/components/Discussion/Internal/Comment/types.ts
@@ -1,34 +1,4 @@
-import { ReactNode } from 'react'
-
-type Discussion = {
-  title?: string
-  comments?: {
-    totalCount: number
-  }
-}
-
-export type Comment = {
-  id: string
-  content: unknown
-  userVote?: 'UP' | 'DOWN'
-  upVotes: number
-  downVotes: number
-  userCanEdit: boolean
-  adminUnpublished: boolean
-  comments: {
-    totalCount: number
-    nodes: Comment[]
-  }
-  discussion: Discussion
-  published: boolean
-  createdAt: string
-  updatedAt: string
-  displayAuthor: DisplayAuthor
-  unavailable?: boolean
-  parentIds?: string[]
-  tags?: string[]
-  embed?: Embed
-}
+export type Comment = any
 
 export type DisplayAuthor = {
   name: string
@@ -39,19 +9,4 @@ export type DisplayAuthor = {
   }
 }
 
-export type Embed = {
-  __typename?: 'TwitterEmbed' | 'InstagramEmbed' | 'FacebookEmbed' | 'Embed'
-  url: string
-  title: string
-  imageUrl: string
-  imageAlt?: string
-  image: string
-  siteName: string
-  userName: string
-  userScreenName: unknown
-  siteImageUrl: string
-  userProfileImageUrl: string
-  html: string
-  description: string | ReactNode
-  createdAt: string
-}
+export type Embed = any
