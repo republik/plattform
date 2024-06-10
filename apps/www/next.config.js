847e100bd6b894241d2a8198f95b71cc65b68762
Jeremy Stucki <jeremy.stucki@gmail.com>
2024-06-10 17:09:29 +0200

    fix(www): set empty asset prefix

--- apps/www/next.config.js
+++ apps/www/next.config.js
@@ -28,9 +28,9 @@ const PUBLIC_BASE_URL = appendProtocol(
     process.env.NEXT_PUBLIC_VERCEL_URL,
 )
 
-const PUBLIC_CDN_URL =
-  appendProtocol(process.env.NEXT_PUBLIC_CDN_FRONTEND_BASE_URL) ||
-  PUBLIC_BASE_URL
+const PUBLIC_CDN_URL = process.env.NEXT_PUBLIC_CDN_FRONTEND_BASE_URL
+  ? appendProtocol(process.env.NEXT_PUBLIC_CDN_FRONTEND_BASE_URL)
+  : ''
 
 /**
  * @type {import('next').NextConfig}
