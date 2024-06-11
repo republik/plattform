dc2a214c118397cce2ac6b2be5e001d66dc81c0d
Henning Dahlheim <henning.dahlheim@republik.ch>
2024-06-11 16:18:59 +0200

    test(job-queue): start queue in test setup

--- packages/backend-modules/job-queue/__test__/queue.test.ts
+++ packages/backend-modules/job-queue/__test__/queue.test.ts
@@ -5,7 +5,7 @@ import {
   StartedPostgreSqlContainer,
 } from '@testcontainers/postgresql'
 import { JobState } from '../lib/types'
-import { Job } from 'pg-boss'
+import { Job, SendOptions } from 'pg-boss'
 
 const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
 
@@ -15,8 +15,21 @@ class DemoWorker extends BaseWorker<DemoWorkerArgs> {
   readonly queue = 'queue:demo'
 
   async perform(job: Job<DemoWorkerArgs>): Promise<void> {
-    job.data
+    // do some processing of the job data
     console.log(job.data)
+  }
+}
+
+class DemoErrorWorker extends BaseWorker<DemoWorkerArgs> {
+  readonly queue = 'queue:demo:error'
+  readonly options: SendOptions = {
+    retryLimit: 0,
+  }
+
+  async perform(job: Job<DemoWorkerArgs>): Promise<void> {
+    // if an error courses it gets stored into the output column
+    console.log(job.data)
+    throw new Error('Processing error')
   }
 }
 
@@ -32,18 +45,20 @@ describe('pg-boss worker test', () => {
       connectionString: postgresContainer.getConnectionUri(),
     })
     queue.registerWorker(DemoWorker)
+    queue.registerWorker(DemoErrorWorker)
+    await queue.start()
     await queue.startWorkers()
   }, 60000)
 
   beforeEach(async () => {
-    await queue.clearStorage()
+    // await queue.clearStorage()
   })
 
   afterAll(async () => {
     await queue.stop()
-  })
+  }, 20000)
 
-  test('queue class interface test', async () => {
+  it('processes send jobs', async () => {
     const jobID = await queue.send<DemoWorker>('queue:demo', {
       recipientEmail: 'example@republik.ch',
     })
@@ -66,5 +81,32 @@ describe('pg-boss worker test', () => {
 
     job = await queue.getJobById(jobID)
     expect(job?.state).toBe<JobState>('completed')
+  }, 60000)
+
+  it('saves errors to the job error column', async () => {
+    const jobID = await queue.send<DemoErrorWorker>('queue:demo:error', {
+      recipientEmail: 'example@republik.ch',
+    })
+
+    if (!jobID) {
+      throw Error('Job no queued')
+    }
+
+    const size = await queue.getQueueSize<DemoErrorWorker>('queue:demo:error')
+    expect(size).toBe(1)
+
+    let job = await queue.getJobById(jobID)
+
+    expect(job?.data).toStrictEqual<DemoWorkerArgs>({
+      recipientEmail: 'example@republik.ch',
+    })
+    expect(job?.state).toBe<JobState>('created')
+
+    await wait(3000)
+
+    job = await queue.getJobById(jobID)
+    expect(job?.state).toBe<JobState>('failed')
+    expect((job?.output as any).name).toBe('Error')
+    expect((job?.output as any).message).toBe('Processing error')
   }, 60000)
 })
