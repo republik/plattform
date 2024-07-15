1a41e6cd6695c69c8a10258e0ef3a42a898735f7
Luciana <luciana.kolbeck@republik.ch>
2024-07-15 18:22:02 +0200

    feat(mailchimp): add basic subscription state and nl link opt in merge fields

--- packages/backend-modules/mailchimp/lib/getMergeFieldsForUser.ts
+++ packages/backend-modules/mailchimp/lib/getMergeFieldsForUser.ts
@@ -1,7 +1,9 @@
 import { UserRow } from '@orbiting/backend-modules-types'
+import { getConsentLink } from '@orbiting/backend-modules-republik/lib/Newsletter'
 import { MergeFieldName, SegmentData } from '../types'
 
 type GetMergeFieldsForUserParams = { user: UserRow; segmentData: SegmentData }
+type SubscriptionState = 'Cancelled' | 'Autopay' | 'Active' | undefined
 type UserMergeFields = Partial<Record<MergeFieldName, string | number | Date>>
 
 export const mergeFieldNames = {
@@ -10,18 +12,20 @@ export const mergeFieldNames = {
   latestPledgeAmount: 'PL_AMOUNT',
   subscriptionEndDate: 'END_DATE',
   subscriptionType: 'SUB_TYPE',
+  subscriptionState: 'SUB_STATE',
+  newsletterOptInCa: 'NL_LINK_CA',
+  newsletterOptInWb: 'NL_LINK_WD',
 } as const
 
-export async function getMergeFieldsForUser({
+export function getMergeFieldsForUser({
   user,
   segmentData,
-}: GetMergeFieldsForUserParams): Promise<UserMergeFields> {
-  const latestMembershipPledgeId = segmentData.activeMembershipPeriod?.pledgeId
-  const filteredPledges = segmentData.pledges?.filter(
-    (pledge) => pledge.id === latestMembershipPledgeId,
-  )
-  const latestMembershipPledgeAmount =
-    filteredPledges?.length === 1 ? filteredPledges[0].total / 100 : 0
+}: GetMergeFieldsForUserParams): UserMergeFields {
+  
+  const latestMembershipPledgeAmount = getLatestMembershipPledgeAmount(segmentData)
+  const subscriptionState = getSubscriptionState(segmentData)
+  const linkCa = getConsentLink(user.email, 'CLIMATE')
+  const linkWdwww = getConsentLink(user.email, 'WDWWW')
 
   return {
     [mergeFieldNames.firstName]: user.firstName,
@@ -29,5 +33,28 @@ export async function getMergeFieldsForUser({
     [mergeFieldNames.latestPledgeAmount]: latestMembershipPledgeAmount,
     [mergeFieldNames.subscriptionEndDate]: segmentData.activeMembershipPeriod?.endDate,
     [mergeFieldNames.subscriptionType]: segmentData.activeMembership?.membershipTypeName,
+    [mergeFieldNames.subscriptionState]: subscriptionState,
+    [mergeFieldNames.newsletterOptInCa]: linkCa,
+    [mergeFieldNames.newsletterOptInWb]: linkWdwww,
   }
+}
+
+function getLatestMembershipPledgeAmount(segmentData: SegmentData): number {
+  const latestMembershipPledgeId = segmentData.activeMembershipPeriod?.pledgeId
+  const filteredPledges = segmentData.pledges?.filter(
+    (pledge) => pledge.id === latestMembershipPledgeId,
+  )
+  return filteredPledges?.length === 1 ? filteredPledges[0].total / 100 : 0
+}
+
+function getSubscriptionState(segmentData: SegmentData): SubscriptionState {
+  const { activeMembership } = segmentData
+  // Cancelled: Monatsabonnent*innen, die auf ein Jahresabo gewechselt haben, das aber noch nicht gestartet ist, fallen auch unter cancelled
+  if (!activeMembership?.renew) {
+    return 'Cancelled'
+  }
+  if (activeMembership.autoPay) {
+    return 'Autopay'
+  }
+  return 'Active'
 }
