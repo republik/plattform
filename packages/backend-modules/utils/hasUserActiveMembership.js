587d1e5f1953161301aa5e7f3f28f608a6205dc0
Henning Dahlheim <henning.dahlheim@republik.ch>
2024-08-09 15:56:00 +0200

    feat(utils): update hasUserActiveMembership to check subscriptions

--- packages/backend-modules/utils/hasUserActiveMembership.js
+++ packages/backend-modules/utils/hasUserActiveMembership.js
@@ -1,6 +1,19 @@
 module.exports = async (user, pgdb) => {
-  return !!(await pgdb.public.memberships.count({
+  // handle pledge based memberships
+  const hasActiveMembership = !!(await pgdb.public.memberships.count({
     userId: user.id,
     active: true,
   }))
+
+  if (hasActiveMembership) {
+    return true
+  }
+
+  // stripe subscriptions
+  const hasActiveSubscription = !!(await pgdb.payments.subscriptions.count({
+    userId: user.id,
+    and: [{ 'status !=': 'paused' }, { 'status !=': 'canceld' }],
+  }))
+
+  return hasActiveSubscription
 }
