7faf8d0eb012a7e6fbfd1750c9039e4ee572e3ec
Jeremy Stucki <jeremy.stucki@gmail.com>
2024-08-16 16:46:02 +0200

    fix(crowdfundings): return comapny enum from resolver

--- packages/backend-modules/republik-crowdfundings/graphql/resolvers/Package.js
+++ packages/backend-modules/republik-crowdfundings/graphql/resolvers/Package.js
@@ -1,5 +1,8 @@
 module.exports = {
   async company(package_, args, { pgdb }) {
-    return pgdb.public.companies.findOne({ id: package_.companyId })
+    const company = pgdb.public.companies.findOne({ id: package_.companyId })
+
+    // FIXME: resolve this properly
+    return company.name === 'REPUBLIK' ? 'REPUBLIK_AG' : 'PROJECT_R'
   },
 }
