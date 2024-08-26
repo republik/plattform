856f9386ef785c5c420a5b9c80c4d7f7bc5a3571
Henning Dahlheim <henning.dahlheim@republik.ch>
2024-08-26 15:38:54 +0200

    chore(payments): mock cancelMagazineSubscription mutation

--- packages/backend-modules/payments/graphql/schema.ts
+++ packages/backend-modules/payments/graphql/schema.ts
@@ -2,9 +2,14 @@ export = `
 
 schema {
   query: queries
+  mutation: mutations
 }
 
 type queries {
   paymentsIsRunning: Boolean
+}
+
+type mutations {
+  cancelMagazineSubscription(subscriptionId: String!): Boolean
 }
 `
