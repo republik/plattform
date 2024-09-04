4cca181e0ff39ca56911cfd9bf27c8e32652015b
Luciana <luciana.kolbeck@republik.ch>
2024-09-04 12:05:27 +0200

    fix(calculateKpis): slightly fix test data

--- packages/backend-modules/republik/__tests__/lib/calculateKpis/kpiCalculations.u.jest.ts
+++ packages/backend-modules/republik/__tests__/lib/calculateKpis/kpiCalculations.u.jest.ts
@@ -2,20 +2,20 @@ import dayjs from 'dayjs'
 import { calculateCancelledYearlyAbos, precomputeTransitoryLiabilities, YearlyAbo, YearlyAboPrecomputed } from '../../../lib/calculateKpis/kpiCalculations'
 
 describe('distribution of yearly abo totals between current and next fiscal year', () => {
-  const endFiscalYear = dayjs('2025-06-30')
+  const endFiscalYear = dayjs('2025-06-30T21:59:59.999Z')
   const yearlyAbos: YearlyAbo[] = [
     {createdAt: new Date('2024-07-01'), updatedAt: new Date('2024-07-01'), total: 12000},
     {createdAt: new Date('2024-08-01'), updatedAt: new Date('2024-08-01'), total: 12000}
   ]
   const results = precomputeTransitoryLiabilities(yearlyAbos, endFiscalYear)
-  expect(results[0].precomputed.totalFiscalYear).toBe(11934)
-  expect(results[0].precomputed.totalTransitoryLiabilities).toBe(66)
-  expect(results[1].precomputed.totalFiscalYear).toBe(10915)
-  expect(results[1].precomputed.totalTransitoryLiabilities).toBe(1085)
+  expect(results[0].precomputed.totalFiscalYear).toBe(11967)
+  expect(results[0].precomputed.totalTransitoryLiabilities).toBe(33)
+  expect(results[1].precomputed.totalFiscalYear).toBe(10948)
+  expect(results[1].precomputed.totalTransitoryLiabilities).toBe(1052)
 })
 
 describe('cancelled yearly abos', () => {
-  const endFiscalYear = dayjs('2025-06-30')
+  const endFiscalYear = dayjs('2025-06-30T21:59:59.999Z')
   
 
   test('only yearly abos that were bought and cancelled in the same fiscal year', () => {
@@ -24,8 +24,8 @@ describe('cancelled yearly abos', () => {
       {createdAt: new Date('2024-08-01'), updatedAt: new Date('2024-09-01'), total: 12000}
     ], endFiscalYear)
     const results = calculateCancelledYearlyAbos(yearlyAbos, endFiscalYear)
-    expect(results.StornierteJahresabonnementsAktuellesGeschaeftsjahr.Betrag).toBe(-(119.34 + 109.15))
-    expect(results.StornierteJahresabonnementsTransitorischePassive.Betrag).toBe(-(.66 + 10.85))
+    expect(results.StornierteJahresabonnementsAktuellesGeschaeftsjahr.Betrag).toBe(-(119.67 + 109.48))
+    expect(results.StornierteJahresabonnementsTransitorischePassive.Betrag).toBe(-(.33 + 10.52))
   })
 
   test('only yearly abos that were bought and cancelled in different fiscal years', () => {
@@ -44,7 +44,7 @@ describe('cancelled yearly abos', () => {
       {createdAt: new Date('2024-08-01'), updatedAt: new Date('2024-09-01'), total: 12000}
     ], endFiscalYear)
     const results = calculateCancelledYearlyAbos(yearlyAbos, endFiscalYear)
-    expect(results.StornierteJahresabonnementsAktuellesGeschaeftsjahr.Betrag).toBe(-(120 + 109.15))
-    expect(results.StornierteJahresabonnementsTransitorischePassive.Betrag).toBe(-(0 + 10.85))
+    expect(results.StornierteJahresabonnementsAktuellesGeschaeftsjahr.Betrag).toBe(-(120 + 109.48))
+    expect(results.StornierteJahresabonnementsTransitorischePassive.Betrag).toBe(-(0 + 10.52))
   })
 })
