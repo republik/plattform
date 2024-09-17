4f70d1ef37f25b94da4c2bfa512bba4fab04b876
Henning Dahlheim <henning.dahlheim@republik.ch>
2024-09-17 17:07:50 +0200

    fix(payments): always fetch expaned invoice from stripe

--- packages/backend-modules/payments/lib/handlers/stripe/invoiceUpdate.ts
+++ packages/backend-modules/payments/lib/handlers/stripe/invoiceUpdate.ts
@@ -1,26 +1,36 @@
 import Stripe from 'stripe'
 import { Company } from '../../types'
 import { PaymentService } from '../../payments'
+import { PaymentProvider } from '../../providers/provider'
 
 export async function processInvoiceUpdated(
   paymentService: PaymentService,
-  _company: Company,
+  company: Company,
   event:
     | Stripe.InvoiceUpdatedEvent
     | Stripe.InvoiceFinalizedEvent
     | Stripe.InvoiceVoidedEvent
     | Stripe.InvoicePaidEvent,
 ) {
+  const invoice = await PaymentProvider.forCompany(company).getInvoice(
+    event.data.object.id,
+  )
+
+  if (!invoice) {
+    console.error('unknown invoice %s', event.data.object.id)
+    return
+  }
+
   await paymentService.updateInvoice(
     {
-      externalId: event.data.object.id,
+      externalId: invoice.id,
     },
     {
-      total: event.data.object.total,
-      totalBeforeDiscount: event.data.object.subtotal,
-      items: event.data.object.lines.data,
-      discounts: event.data.object.discounts,
-      status: event.data.object.status as any,
+      total: invoice.total,
+      totalBeforeDiscount: invoice.subtotal,
+      items: invoice.lines.data,
+      discounts: invoice.discounts,
+      status: invoice.status as any,
     },
   )
 
