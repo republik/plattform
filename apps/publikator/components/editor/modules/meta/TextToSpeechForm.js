c88854222f933cfd6a5354bcbe638519c55a2af2
Anna Traussnig <anna.traussnig@gmail.com>
2024-09-26 09:37:00 +0300

    feat(SyntheticVoice): add warning when changing voice in publikator

--- apps/publikator/components/editor/modules/meta/TextToSpeechForm.js
+++ apps/publikator/components/editor/modules/meta/TextToSpeechForm.js
@@ -1,11 +1,12 @@
-import { Dropdown, Checkbox } from '@project-r/styleguide'
+import { Checkbox, Radio } from "@project-r/styleguide";
 
 import {
   MetaSection,
   MetaSectionTitle,
-  MetaOption,
-} from '../../../MetaDataForm/components/Layout'
+  MetaOption, MetaOptionGroupTitle
+} from "../../../MetaDataForm/components/Layout";
 import withT from '../../../../lib/withT'
+import { useState } from "react";
 
 const VOICES = [
   { value: 'huebsch-62964', text: 'Frau 1' },
@@ -17,6 +18,13 @@ const VOICES = [
 export default withT(({ t, editor, node, onInputChange }) => {
   const voice = node.data.get('syntheticVoice')
   const suppressed = node.data.get('suppressSyntheticReadAloud')
+  const [error, setError] = useState(undefined)
+
+  const checkVoice = () => {
+    if (voice) {
+      setError(t('metaData/tts/voice/warning'))
+    }
+  }
 
   return (
     <MetaSection>
@@ -31,20 +39,27 @@ export default withT(({ t, editor, node, onInputChange }) => {
         </Checkbox>
       </MetaOption>
       <MetaOption>
-        <Dropdown
-          black
-          label={t('metaData/tts/voice/dropdown')}
-          disabled={suppressed}
-          items={VOICES}
-          value={voice || ''}
-          onChange={({ value }) => {
-            editor.change((change) => {
-              change.setNodeByKey(node.key, {
-                data: node.data.set('syntheticVoice', value),
+        <MetaOptionGroupTitle>{t('metaData/tts/voice/dropdown')}</MetaOptionGroupTitle>
+        {VOICES.map((option, i) => (
+          <Radio
+            key={i}
+            checked={voice === option.value}
+            onChange={() => {
+              checkVoice(option.value)
+              editor.change((change) => {
+                change.setNodeByKey(node.key, {
+                  data: node.data.set('syntheticVoice', option.value),
+                })
               })
-            })
-          }}
-        />
+            }}
+            style={{ marginRight: 30 }}
+          >
+            {option.text}
+          </Radio>
+        ))}
+        { error && <p><small>
+          {error}
+        </small></p>}
       </MetaOption>
     </MetaSection>
   )
