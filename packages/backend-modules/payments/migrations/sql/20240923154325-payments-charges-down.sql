af1e990a959daded74f2381f0b63ae0b68175643
Henning Dahlheim <henning.dahlheim@republik.ch>
2024-10-02 11:18:16 +0200

    fix(payments): correct charges down migration

--- packages/backend-modules/payments/migrations/sql/20240923154325-payments-charges-down.sql
+++ packages/backend-modules/payments/migrations/sql/20240923154325-payments-charges-down.sql
@@ -1,31 +1,7 @@
 -- migrate down here: DROP TABLE...
 --
 
-CREATE TYPE payments.carge_status as ENUM (
-    'succeeded',
-    'pending',
-    'failed',
-);
+DROP TABLE IF EXISTS payments."charges";
 
-CREATE TABLE IF NOT EXIST payments."charges" (
-    "id" uuid default uuid_generate_v4() PRIMARY KEY,
-    "company" payments.company NOT NULL,
-    "userId" uuid NOT NULL, -- invoice that is being refunded
-    "invoiceId" uuid, -- invoice that is being refunded
-    "paid" boolean NOT NULL,
-    "amount" int NOT NULL,
-    "amount_captured" int,
-    "amount_refunded" int,
-    "refunded" boolean NOT NULL default false,
-    "status" payments.carge_status NOT NULL,
-    "provider" payments.payment_provider default 'STRIPE',
-    "externalId" text NOT NULL,
-    "createdAt" timestamptz DEFAULT now(),
-    "updatedAt" timestamptz DEFAULT now(),
-    "paidAt" timestamptz,
-    "refundedAt" timestamptz,
-    CONSTRAINT fk_charge_user_id FOREIGN KEY("userId")
-       REFERENCES public.users("id"),
-    CONSTRAINT fk_charge_invoice_id FOREIGN KEY("invoiceId")
-       REFERENCES payments.invoices("id")
-)
+DROP TYPE payments.charge_status;
+DROP TYPE payments.payment_method_type;
