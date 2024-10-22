cd5be51feadb4a5da42db4729efe4be91edb3bba
Anna Traussnig <anna.traussnig@gmail.com>
2024-10-22 17:51:51 +0300

    feat(datawrapper): plug in html illu code

--- packages/styleguide/src/components/Datawrapper/index.js
+++ packages/styleguide/src/components/Datawrapper/index.js
@@ -1,12 +1,63 @@
-import React from 'react'
+import React, { Component } from "react";
+import { css } from "glamor";
 
 
-const Datawrapper = ({ datawrapperId, alt }) => {
-  if (!datawrapperId) {
-    return <p>No Datawrapper ID</p>
+const styles = {
+  ai2html: css({
+    '& [data-min-width]': {
+      display: 'none',
+    },
+    '& [data-min-width]:last-child': {
+      display: 'block',
+    },
+  }),
+}
+
+class Datawrapper extends Component {
+  constructor(...args) {
+    super(...args)
+
+    this.setRef = (ref) => {
+      this.ref = ref
+    }
+    this.resize = () => {
+      if (this.ref) {
+        // ai2html specific resize
+        // - adapted from https://github.com/newsdev/ai2html/blob/b0bfda26aba31c5bf9d44c70dba02d4eec381ad6/_includes/resizer-script.html#L9
+        const width = this.ref.getBoundingClientRect().width
+        const elements = Array.prototype.slice.call(
+          this.ref.querySelectorAll('[data-min-width]'),
+        )
+        elements.forEach((element) => {
+          const minwidth = element.getAttribute('data-min-width')
+          const maxwidth = element.getAttribute('data-max-width')
+          if (+minwidth <= width && (+maxwidth >= width || maxwidth === null)) {
+            element.style.display = 'block'
+          } else {
+            element.style.display = 'none'
+          }
+        })
+      }
+    }
   }
+  componentDidMount() {
+    window.addEventListener('resize', this.resize)
+    this.resize()
+  }
+  componentDidUpdate() {
+    this.resize()
+  }
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.resize)
+  }
+  render() {
+    const { datawrapperId, alt } = this.props
 
-  const html = `<div style="min-height:496px" id="datawrapper-vis-${datawrapperId}">
+    if (!datawrapperId) {
+      return <p>No Datawrapper ID</p>
+    }
+
+    const html = `<div style="min-height:496px" id="datawrapper-vis-${datawrapperId}">
       <script 
         type="text/javascript" 
         defer 
@@ -19,13 +70,16 @@ const Datawrapper = ({ datawrapperId, alt }) => {
       </noscript>
     </div>`
 
-  return (
-    <div
-      dangerouslySetInnerHTML={{
-        __html: html,
-      }}
-    />
-  )
+    return (
+      <div
+        {...styles.ai2html}
+        ref={this.setRef}
+        dangerouslySetInnerHTML={{
+          __html: html,
+        }}
+      />
+    )
+  }
 }
 
 export default Datawrapper
