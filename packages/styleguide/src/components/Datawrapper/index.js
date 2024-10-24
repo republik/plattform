e2f0006c311db3da829eeb3970cf275eed874e76
Jeremy Stucki <jeremy.stucki@gmail.com>
2024-10-24 15:32:37 +0200

    feat(datawrapper): improve chart data loading

--- packages/styleguide/src/components/Datawrapper/index.js
+++ packages/styleguide/src/components/Datawrapper/index.js
@@ -1,32 +1,35 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Script from 'next/script'
 
 function Datawrapper({ datawrapperId, alt }) {
   const chartRef = useRef()
+  const [embedData, setEmbedData] = useState()
 
   useEffect(() => {
     if (datawrapperId) {
       fetch(`https://datawrapper.dwcdn.net/${datawrapperId}/embed.json`)
         .then((res) => res.json())
-        .then((embedData) => {
-          // then pass it to datawrapper.render call, along with { target, flags }
-
-          if (chartRef.current) {
-            window.datawrapper?.render(embedData, {
-              // the node that will be turned into the web component
-              target: chartRef.current,
-              // optionally include flags (e.g dark, fitchart) here
-              // see https://developer.datawrapper.de/docs/render-flags
-              flags: {},
-            })
-          }
+        .then((data) => {
+          setEmbedData(data)
         })
         .catch((e) => console.error(e))
     }
   }, [datawrapperId])
 
+  useEffect(() => {
+    if (embedData && chartRef.current) {
+      window.datawrapper?.render(embedData, {
+        // the node that will be turned into the web component
+        target: chartRef.current,
+        // optionally include flags (e.g dark, fitchart) here
+        // see https://developer.datawrapper.de/docs/render-flags
+        flags: {},
+      })
+    }
+  }, [embedData])
+
   return (
-    <div>
+    <div style={{ marginBottom: 30 }}>
       {datawrapperId ? null : 'Chart-ID fehlt'}
       <div ref={chartRef}></div>
       <Script src={`https://datawrapper.dwcdn.net/lib/datawrapper.js`} />
