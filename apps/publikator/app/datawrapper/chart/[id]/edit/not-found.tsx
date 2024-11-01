9d4265ff6707518ebd4ca319300a10794dad50c3
Jeremy Stucki <jeremy.stucki@gmail.com>
2024-11-01 15:34:33 +0100

    feat(datawrapper): add dw api and auto-login to edit chart

---
+++ apps/publikator/app/datawrapper/chart/[id]/edit/not-found.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+export default function ChartNotFound() {
+  return (
+    <div>
+      <p>
+        Diese Grafik kann nicht bearbeitet werden. Entweder:
+        <ul>
+          <li>Existiert die Grafik nicht (mehr),</li>
+          <li>
+            fehlt die Berechtigung, die Grafik zu bearbeiten (sie wurde
+            ausserhalb des Republik-Teams erstellt)
+          </li>
+          <li>oder irgend sonstwas ging schief.</li>
+        </ul>
+      </p>
+      <button
+        onClick={() => {
+          window.close()
+        }}
+      >
+        Fenster schliessen
+      </button>
+    </div>
+  )
+}
