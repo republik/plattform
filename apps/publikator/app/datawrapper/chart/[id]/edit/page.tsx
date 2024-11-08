c5646a29d53569835ee1e5d3d4d4beee8138f351
Jeremy Stucki <jeremy.stucki@gmail.com>
2024-11-08 16:37:05 +0100

    refactor: move role check into login fetch

--- apps/publikator/app/datawrapper/chart/[id]/edit/page.tsx
+++ apps/publikator/app/datawrapper/chart/[id]/edit/page.tsx
@@ -1,6 +1,13 @@
+import { meHasRole } from 'lib/graphql/me-has-role'
 import { notFound, redirect } from 'next/navigation'
 
 async function fetchLoginUrl(chartId: string) {
+  const isEditor = await meHasRole('editor')
+
+  if (!isEditor) {
+    return undefined
+  }
+
   const res = await fetch(`https://api.datawrapper.de/v3/auth/login-tokens`, {
     method: 'POST',
     headers: {
