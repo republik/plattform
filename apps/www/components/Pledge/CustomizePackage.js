48ccda8ff72338fc7284e78f759728ddaa55a22a
Olivier Baumann <olivierbaumann@me.com>
2024-12-04 13:52:05 +0100

    fix: revent unnecessary change

--- apps/www/components/Pledge/CustomizePackage.js
+++ apps/www/components/Pledge/CustomizePackage.js
@@ -987,7 +987,7 @@ class CustomizePackage extends Component {
           fields={configurableGoodieFields}
           showGoodiesTitle={!goodiesOnly}
         />
-        {!!userPrice && !fixedPrice (
+        {!!userPrice && !fixedPrice && (
           <div>
             <P>
               {t.first([
