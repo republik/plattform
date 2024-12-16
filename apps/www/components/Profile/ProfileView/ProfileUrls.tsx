2327e399a9dbfde3fc05e08c27a44f4a13329ab0
Olivier Baumann <olivierbaumann@me.com>
2024-12-16 13:37:05 +0100

    fix: better url handling in label parser

--- apps/www/components/Profile/ProfileView/ProfileUrls.tsx
+++ apps/www/components/Profile/ProfileView/ProfileUrls.tsx
@@ -2,24 +2,21 @@ import ProfileUrlIcon from '../Common/ProfileUrlIcon'
 import { IconButton } from '@project-r/styleguide'
 
 const ProfileUrlLabel = (url: string) => {
-  if (/bsky/.test(url)) {
-    const bskyUsername = url.match(/\/profile\/([^/\s]+)/)[1]
-    return bskyUsername || 'Bluesky'
+  if (/bsky\.app/.test(url)) {
+    return url?.match(/\/profile\/([^/\s]+)/)[1] ?? 'Bluesky'
   } else if (/facebook\.com/.test(url)) {
     return 'Facebook'
   } else if (/x\.com/.test(url)) {
-    const xId = url.match(/x\.com\/([^/\s]+)/)[1]
-    return xId || 'X'
+    return url?.match(/x\.com\/([^/\s]+)/)[1] ?? 'X'
   } else if (/threema\.id/.test(url)) {
-    const threemaID = url.match(/\/threema\.id\/([^/\s]+)/)[1]
-    return threemaID || 'Threema'
+    return url?.match(/\/threema\.id\/([^/\s]+)/)[1] ?? 'Threema'
   } else if (/signal\.me/.test(url)) {
     return 'Signal'
   } else if (/\/@.+@/.test(url) || /mastodon/.test(url)) {
     return 'Mastodon'
   } else {
     const urlObject = new URL(url)
-    return urlObject.host
+    return urlObject.host ?? url
   }
 }
 
