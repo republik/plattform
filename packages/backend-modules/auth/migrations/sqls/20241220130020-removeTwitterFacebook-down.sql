913d15bfece70d10b678bfd11fab35b688f95c3a
Olivier Baumann <olivierbaumann@me.com>
2024-12-30 17:57:19 +0100

    fix: fix SQL statement

--- packages/backend-modules/auth/migrations/sqls/20241220130020-removeTwitterFacebook-down.sql
+++ packages/backend-modules/auth/migrations/sqls/20241220130020-removeTwitterFacebook-down.sql
@@ -1,4 +1,4 @@
 ALTER TABLE users
-ADD COLUMN "publicUrl" text;
-ADD COLUMN "facebookId" text;
+ADD COLUMN "publicUrl" text,
+ADD COLUMN "facebookId" text,
 ADD COLUMN "publicUrl" text;
--- packages/backend-modules/auth/migrations/sqls/20241220130020-removeTwitterFacebook-up.sql
+++ packages/backend-modules/auth/migrations/sqls/20241220130020-removeTwitterFacebook-up.sql
@@ -1,4 +1,4 @@
 ALTER TABLE users
-DROP COLUMN "twitterHandle";
-DROP COLUMN "facebookId";
+DROP COLUMN "twitterHandle",
+DROP COLUMN "facebookId",
 DROP COLUMN "publicUrl";
