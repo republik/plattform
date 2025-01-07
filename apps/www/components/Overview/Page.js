3556d24ef32c61218ad683005864be7fee299f37
Luciana <luciana.kolbeck@republik.ch>
2025-01-07 11:02:29 +0100

    chore(page): add 2023 overview page

--- apps/www/components/Overview/Page.js
+++ apps/www/components/Overview/Page.js
@@ -30,7 +30,7 @@ const knownYears = {
   2020: { path: '/2020' },
   2021: { path: '/2021' },
   2022: { path: '/2022' },
-  // 2023: { path: '/2023' },
+  2023: { path: '/2023' },
 }
 
 const getAll = gql`
