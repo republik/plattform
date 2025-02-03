74d28ed7258f87147e4b544ae9bf0c9077e3a8b5
Anna Traussnig <anna.traussnig@gmail.com>
2025-02-03 10:24:13 +0200

    feat(Pdf): add SCREENSHOT_SERVER_BASE_URL variable

--- packages/backend-modules/assets/express/pdf.js
+++ packages/backend-modules/assets/express/pdf.js
@@ -1,16 +1,18 @@
 const { returnImage } = require('../lib')
-const { SCREENSHOT_BASE_URL, FRONTEND_BASE_URL } = process.env
+const { SCREENSHOT_SERVER_BASE_URL, FRONTEND_BASE_URL } = process.env
 
-if (!SCREENSHOT_BASE_URL) {
+if (!SCREENSHOT_SERVER_BASE_URL) {
   console.warn(
-    'missing env SCREENSHOT_BASE_URL, the /pdf endpoint will not work',
+    'missing env SCREENSHOT_SERVER_BASE_URL, the /pdf endpoint will not work',
   )
 }
 
 module.exports = (server) => {
   server.get('/pdf/:path(*)', async (req, res) => {
-    if (!SCREENSHOT_BASE_URL) {
-      console.warn('SCREENSHOT_BASE_URL not set unable to handle request')
+    if (!SCREENSHOT_SERVER_BASE_URL) {
+      console.warn(
+        'SCREENSHOT_SERVER_BASE_URL not set unable to handle request',
+      )
       return res.status(403).end()
     }
 
@@ -22,14 +24,15 @@ module.exports = (server) => {
     const { images, download, format, version, ...options } = req.query
 
     // build URL to fetch PDF from
-    const fetchUrl = new URL(`${SCREENSHOT_BASE_URL}/api/pdf`)
+    const fetchUrl = new URL(`${SCREENSHOT_SERVER_BASE_URL}/api/pdf`)
 
     // add params
     fetchUrl.searchParams.set('url', `${FRONTEND_BASE_URL}/${path}`)
     images && fetchUrl.searchParams.set('images', images)
     download && fetchUrl.searchParams.set('download', download)
     format && fetchUrl.searchParams.set('format', format)
-    // we regenerate the PDF if the version changes
+    // we regenerate the PDF if the "version" changes
+    // (the version is the publication date of the document)
     version && fetchUrl.searchParams.set('version', version)
 
     const result = await fetch(fetchUrl, { method: 'GET' }).catch((error) => {
