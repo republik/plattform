7c289844f7e98659fcccbeba16529c9587ed2076
Luciana <luciana.kolbeck@republik.ch>
2025-02-25 13:02:40 +0100

    fix(migration): run birthyear update separately from migration

--- packages/backend-modules/republik/migrations/sqls/20250218154508-user-birthyear-up.sql
+++ packages/backend-modules/republik/migrations/sqls/20250218154508-user-birthyear-up.sql
@@ -1,5 +1,6 @@
 -- migrate up here: CREATE TABLE...
 ALTER TABLE "public"."users" ADD COLUMN "birthyear" integer;
 
-UPDATE users SET "birthyear" = EXTRACT(YEAR FROM birthday)::int WHERE birthday IS NOT NULL;
+--run this separately
+--UPDATE users SET "birthyear" = EXTRACT(YEAR FROM birthday)::int WHERE birthday IS NOT NULL;
 
