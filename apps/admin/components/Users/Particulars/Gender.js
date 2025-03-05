b95ef8ba2b7d271e7e8fe7751a0dbdd3a1a58be8
Anna Traussnig <anna.traussnig@gmail.com>
2025-03-05 12:15:40 +0200

    add radio to reset gender

--- apps/admin/components/Users/Particulars/Gender.js
+++ apps/admin/components/Users/Particulars/Gender.js
@@ -1,17 +1,50 @@
-import { Field, Radio, Label, useColorContext } from '@project-r/styleguide'
-
 import withT from '../../../lib/withT'
 import compose from 'lodash/flowRight'
+
+import { useState } from 'react'
+
+import { css } from 'glamor'
+
+import { Field, Radio, Label, useColorContext } from '@project-r/styleguide'
+
+const styles = {
+  radio: css({
+    lineHeight: 1.1,
+    display: 'inline-block',
+    whiteSpace: 'nowrap',
+    marginBottom: 10,
+    marginRight: 15,
+    fontSize: 21,
+    marginTop: -1,
+  }),
+}
 
 // https://nibi.space/geschlechtsabfragen
 const GENDER_SUGGESTIONS = ['weiblich', 'männlich']
 const X_GENDER = 'weiteres'
+const NO_GENDER = 'keine Angabe'
 
 const GenderField = ({ values, onChange, t }) => {
   const [colorScheme] = useColorContext()
+  const [gender, setGender] = useState(values.gender)
+  const [customGender, setCustomGender] = useState(
+    !GENDER_SUGGESTIONS.some((suggestion) => values.gender === suggestion) &&
+      values.gender,
+  )
 
-  const currentGender = values.gender
-  const isX = !GENDER_SUGGESTIONS.some((gender) => gender === currentGender)
+  const isX = customGender || gender === X_GENDER
+
+  const onChangeHandler = (newGenderValue) => {
+    onChange({
+      values: {
+        gender: newGenderValue,
+      },
+      errors: {
+        gender: undefined,
+      },
+      dirty: { gender: true },
+    })
+  }
 
   return (
     <>
@@ -22,69 +55,49 @@ const GenderField = ({ values, onChange, t }) => {
           </span>
         </Label>
       </div>
-      {GENDER_SUGGESTIONS.map((gender) => (
-        <>
-          <span
-            style={{
-              display: 'inline-block',
-              whiteSpace: 'nowrap',
-              marginBottom: 10,
-              marginRight: 15,
-            }}
-          >
-            <Radio
-              value={currentGender}
-              checked={currentGender === gender}
-              onChange={() => {
-                onChange({
-                  values: {
-                    gender,
-                    genderCustom: undefined,
-                  },
-                  errors: {
-                    gender: undefined,
-                  },
-                  dirty: { gender: true },
-                })
-              }}
-            >
-              {gender}
-            </Radio>
-          </span>
-        </>
+      {GENDER_SUGGESTIONS.map((suggestion) => (
+        <Radio
+          value={suggestion}
+          key={suggestion}
+          checked={gender === suggestion}
+          onChange={() => {
+            setGender(suggestion)
+            setCustomGender(undefined)
+            onChangeHandler(suggestion)
+          }}
+        >
+          <span {...styles.radio}>{suggestion}</span>
+        </Radio>
       ))}
       <Radio
         value={X_GENDER}
         checked={isX}
         onChange={() => {
-          onChange({
-            values: {
-              gender: X_GENDER,
-              genderCustom: X_GENDER,
-            },
-            errors: {
-              gender: undefined,
-            },
-            dirty: { gender: true },
-          })
+          setGender(X_GENDER)
+          setCustomGender(X_GENDER)
+          onChangeHandler(X_GENDER)
         }}
       >
-        {X_GENDER}
+        <span {...styles.radio}>{X_GENDER}</span>
+      </Radio>
+      <Radio
+        value={NO_GENDER}
+        checked={!gender}
+        onChange={() => {
+          setGender(undefined)
+          setCustomGender(undefined)
+          onChangeHandler(null)
+        }}
+      >
+        <span {...styles.radio}>{NO_GENDER}</span>
       </Radio>
       <Field
         disabled={!isX}
         label={t('Account/Update/gender/label/custom')}
-        value={values.genderCustom || values.gender}
+        value={customGender || ''}
         onChange={(_, newValue) => {
-          onChange({
-            values: {
-              genderCustom: newValue,
-            },
-            errors: {
-              genderCustom: undefined,
-            },
-            dirty: { genderCustom: true },
-          })
+          setCustomGender(newValue)
+          onChangeHandler(newValue)
         }}
       />
     </>
