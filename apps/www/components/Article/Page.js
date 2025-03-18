1d26c3c6bf7b3743a287006dbc92e968c2ddb546
Anna Traussnig <anna.traussnig@gmail.com>
2025-03-18 15:23:37 +0200

    make SEO happy

--- apps/www/components/Article/Page.js
+++ apps/www/components/Article/Page.js
@@ -51,6 +51,7 @@ import { reportError } from 'lib/errors/reportError'
 import NewsletterTitleBlock from './components/NewsletterTitleBlock'
 import PublikatorLinkBlock from './components/PublikatorLinkBlock'
 import useSchema from './useSchema'
+import { useUserAgent } from 'lib/context/UserAgentContext'
 
 const EmptyComponent = ({ children }) => children
 
@@ -70,6 +71,7 @@ const ArticlePage = ({
   const { share, extract, showAll } = router.query
 
   const { me, meLoading, hasAccess, isEditor } = useMe()
+  const { isSearchBot } = useUserAgent()
 
   const { isAudioQueueAvailable } = useAudioQueue()
 
@@ -382,6 +384,9 @@ const ArticlePage = ({
 
           const showPodcastButtons = !!podcast && meta.template !== 'article'
 
+          const truncateContent =
+            meta.template === 'article' && !hasAccess && !isSearchBot
+
           return (
             <>
               <FontSizeSync />
@@ -470,11 +475,18 @@ const ArticlePage = ({
                           )}
                         </div>
                       )}
-                      {renderSchema(
-                        hasAccess
-                          ? splitContent.main
-                          : splitContent.mainTruncated,
-                      )}
+                      <div className='regwall'>
+                        {truncateContent ? (
+                          <>
+                            {renderSchema(splitContent.mainTruncated)}
+                            <div>
+                              REGWALLREGWALLREGWALLREGWALLREGWALLREGWALLREGWALLREGWALLREGWALL
+                            </div>
+                          </>
+                        ) : (
+                          <>{renderSchema(splitContent.main)}</>
+                        )}
+                      </div>
                     </article>
                     <ActionBarOverlay>{actionBarOverlay}</ActionBarOverlay>
                   </ProgressComponent>
