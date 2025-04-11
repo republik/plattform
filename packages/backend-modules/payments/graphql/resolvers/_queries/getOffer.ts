97bce0e686836586b68715b56d385cea093a7f9b
Henning Dahlheim <henning.dahlheim@republik.ch>
2025-04-11 12:03:34 +0200

    fix: remove entry offer from getOffer resolver

--- packages/backend-modules/payments/graphql/resolvers/_queries/getOffer.ts
+++ packages/backend-modules/payments/graphql/resolvers/_queries/getOffer.ts
@@ -1,21 +1,11 @@
 import { GraphqlContext } from '@orbiting/backend-modules-types'
-import {
-  activeOffers,
-  checkIntroductoryOfferEligibility,
-  INTRODUCTERY_OFFER_PROMO_CODE,
-  Shop,
-} from '../../../lib/shop'
+import { activeOffers, Shop } from '../../../lib/shop'
 
 export = async function getOffer(
   _root: never,
   args: { offerId: string; promoCode?: string },
-  ctx: GraphqlContext,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  _ctx: GraphqlContext,
 ) {
-  const promoCode =
-    (await checkIntroductoryOfferEligibility(ctx.pgdb, ctx.user)) &&
-    typeof args.promoCode === 'undefined'
-      ? INTRODUCTERY_OFFER_PROMO_CODE
-      : args.promoCode
-
-  return new Shop(activeOffers()).getOfferById(args.offerId, promoCode)
+  return new Shop(activeOffers()).getOfferById(args.offerId, args.promoCode)
 }
