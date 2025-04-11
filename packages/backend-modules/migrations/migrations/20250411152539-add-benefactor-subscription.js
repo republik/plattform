454745a09f633736ef10168b88098c12392607ab
Henning Dahlheim <henning.dahlheim@republik.ch>
2025-04-11 17:12:01 +0200

    chore: add BENEFACTOR_SUBSCRIPTION migration

---
+++ packages/backend-modules/migrations/migrations/20250411152539-add-benefactor-subscription.js
@@ -0,0 +1,10 @@
+const run = require('../run.js')
+
+const dir = 'packages/backend-modules/payments/migrations/sql'
+const file = '20250411152539-add-benefactor-subscription'
+
+exports.up = (db) =>
+  run(db, dir, `${file}-up.sql`)
+
+exports.down = (db) =>
+  run(db, dir, `${file}-down.sql`)
