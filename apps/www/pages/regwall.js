8985af928a9737bc5dd5e2072d3ca6711d5376d2
Anna Traussnig <anna.traussnig@gmail.com>
2025-04-15 10:25:41 +0300

    fix the build

--- apps/www/pages/regwall.js
+++
@@ -1,9 +0,0 @@
-import { withDefaultSSR } from '../lib/apollo/helpers'
-import Paywall from '@app/components/paywall'
-import Regwall from '@app/components/regwall'
-
-const Page = () => {
-  return <Paywall />
-}
-
-export default withDefaultSSR(Page)
