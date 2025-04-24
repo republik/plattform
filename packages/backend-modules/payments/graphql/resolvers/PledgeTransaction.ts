30a4a3b42db367a95298b5725f8baf1940b09c9b
Henning Dahlheim <henning.dahlheim@republik.ch>
2025-04-24 17:11:28 +0200

    chore: remove todo comment

--- packages/backend-modules/payments/graphql/resolvers/PledgeTransaction.ts
+++ packages/backend-modules/payments/graphql/resolvers/PledgeTransaction.ts
@@ -2,7 +2,6 @@ import { GraphqlContext } from '@orbiting/backend-modules-types'
 import { Transaction } from '../../lib/types'
 
 export = {
-  // !TODO: Implement pledge resolver
   pledge(transaction: Transaction, _args: never, ctx: GraphqlContext) {
     return ctx.loaders.PledgeLoader.byId.load(transaction.pledgeId)
   },
--- packages/backend-modules/payments/graphql/resolvers/SubscriptionTransaction.ts
+++ packages/backend-modules/payments/graphql/resolvers/SubscriptionTransaction.ts
@@ -2,7 +2,6 @@ import { GraphqlContext } from '@orbiting/backend-modules-types'
 import { Transaction } from '../../lib/types'
 
 export = {
-  // !TODO: Implement pledge resolver
   subscription(transaction: Transaction, _args: never, ctx: GraphqlContext) {
     return ctx.loaders.SubscriptionLoader.byId.load(transaction.subscriptionId)
   },
