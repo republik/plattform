1f5a6f2d2e0b63d2d470c5112dc106c492216285
Olivier Baumann <olivierbaumann@me.com>
2025-05-08 18:07:48 +0200

    fix fragment not exporting

--- apps/www/components/Onboarding/Sections/Usability.js
+++ apps/www/components/Onboarding/Sections/Usability.js
@@ -1,11 +1,24 @@
 import { Fragment } from 'react'
+import { gql } from '@apollo/client'
 
 import Section from '../Section'
 import ProgressSettings from '../../Account/ProgressSettings'
+
+
 import withT from '../../../lib/withT'
 
+
+export const fragments = {
+  user: gql`
+    fragment UsabilityUser on User {
+      id
+      PROGRESS: hasConsentedTo(name: "PROGRESS_OPT_OUT")
+    }
+  `,
+}
+
 const Usability = (props) => {
-  const { user, t } = props
+  const { t } = props
 
   return (
     <Section
