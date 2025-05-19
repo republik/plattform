bf2148918fb16c163d4aa6df2e7dfb5716a32ae4
Jeremy Stucki <jeremy.stucki@gmail.com>
2025-05-19 13:53:34 +0200

    use correct base url

--- apps/www/src/app/feed.xml/route.ts
+++ apps/www/src/app/feed.xml/route.ts
@@ -4,9 +4,9 @@ import { Feed } from 'feed'
 
 export async function GET() {
   const feed = new Feed({
-    id: 'https://www.republik.ch',
-    link: 'https://www.republik.ch',
-    title: 'Republik RSS Feed',
+    id: process.env.NEXT_PUBLIC_BASE_URL,
+    link: process.env.NEXT_PUBLIC_BASE_URL,
+    title: 'Republik Feed',
     copyright: 'Republik AG',
   })
 
@@ -22,7 +22,7 @@ export async function GET() {
         feed.addItem({
           id: btoa(`${base}/${repo}`),
           title: n.entity.meta.title,
-          link: `https://www.republik.ch${n.entity.meta.path}`,
+          link: `${process.env.NEXT_PUBLIC_BASE_URL}${n.entity.meta.path}`,
           description: n.entity.meta.description,
           author: n.entity.meta.contributors.map((a) => ({
             name: a.name,
