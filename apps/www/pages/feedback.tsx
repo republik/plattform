64be9f86e3e98cd4afdab40134a06c36f148177a
Olivier Baumann <olivierbaumann@me.com>
2025-06-11 16:37:44 +0200

    default ssr

--- apps/www/pages/feedback.tsx
+++ apps/www/pages/feedback.tsx
@@ -3,13 +3,10 @@ import Frame from '../components/Frame'
 import { GENERAL_FEEDBACK_DISCUSSION_ID } from '../lib/constants'
 import { MaybeDiscussionContextProvider } from '../pages/dialog'
 import Discussion from '../components/Discussion/Discussion'
-import {
-  Center,
-  Editorial,
-  Interaction,
-} from '@project-r/styleguide'
+import { Center, Editorial, Interaction } from '@project-r/styleguide'
 import { useTranslation } from '../lib/withT'
 import ActionBar from '../components/ActionBar'
+import { withDefaultSSR } from 'lib/apollo/helpers'
 
 const FeedbackDialogPage = () => {
   const { t } = useTranslation()
@@ -18,9 +15,7 @@ const FeedbackDialogPage = () => {
   return (
     <>
       <Frame hasOverviewNav raw formatColor='primary'>
-        <MaybeDiscussionContextProvider
-          discussionId={activeDiscussionId}
-        >
+        <MaybeDiscussionContextProvider discussionId={activeDiscussionId}>
           <Center>
             <div style={{ marginBottom: 30 }}>
               <Editorial.Format color='primary'>
@@ -32,12 +27,10 @@ const FeedbackDialogPage = () => {
                   {t('feedback/title')}
                 </Link>
               </Editorial.Format>
-              <Interaction.H1>
-                {t('feedback/general/title')}
-              </Interaction.H1>
-                <Interaction.P style={{ marginTop: 10 }}>
-                  {t('feedback/general/lead')}
-                </Interaction.P>
+              <Interaction.H1>{t('feedback/general/title')}</Interaction.H1>
+              <Interaction.P style={{ marginTop: 10 }}>
+                {t('feedback/general/lead')}
+              </Interaction.P>
               <br />
               <ActionBar discussion={activeDiscussionId} fontSize />
             </div>
@@ -49,4 +42,4 @@ const FeedbackDialogPage = () => {
   )
 }
 
-export default FeedbackDialogPage
+export default withDefaultSSR(FeedbackDialogPage)
