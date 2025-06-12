7b0e71d8a0624abeeef540909e25209749ee85e5
Henning Dahlheim <henning.dahlheim@republik.ch>
2025-06-12 13:39:26 +0200

    allow querying discussions by path

--- packages/backend-modules/discussions/loaders/Discussion.js
+++ packages/backend-modules/discussions/loaders/Discussion.js
@@ -22,6 +22,9 @@ module.exports = (context) => ({
   byId: createDataLoader((ids) =>
     context.pgdb.public.discussions.find({ id: ids }),
   ),
+  byPath: createDataLoader((paths) =>
+    context.pgdb.public.discussions.find({ path: paths }),
+  ),
   byRepoId: createDataLoader(
     (repoIds) => context.pgdb.public.discussions.find({ repoId: repoIds }),
     null,
@@ -79,7 +82,7 @@ module.exports = (context) => ({
             FROM discussions d
             WHERE ARRAY[d.id] && :ids
           )
-          
+
           SELECT "discussionId", value FROM tags t, jsonb_array_elements(t.tags) value
         ), counts AS (
           WITH data AS (
@@ -87,8 +90,8 @@ module.exports = (context) => ({
               c."discussionId" "discussionId",
               c.id,
               CASE
-                WHEN (coalesce(jsonb_array_length(c.tags), 0) > 0) THEN c.tags 
-                WHEN (coalesce(jsonb_array_length(cr.tags), 0) > 0) THEN cr.tags 
+                WHEN (coalesce(jsonb_array_length(c.tags), 0) > 0) THEN c.tags
+                WHEN (coalesce(jsonb_array_length(cr.tags), 0) > 0) THEN cr.tags
                 ELSE '[]'::jsonb
               END tags
             FROM comments c
@@ -99,10 +102,10 @@ module.exports = (context) => ({
 
           SELECT d."discussionId", value, COUNT(*) count
           FROM data d, jsonb_array_elements(d.tags) AS value
-          
+
           GROUP BY 1, 2
         )
-        
+
         SELECT edt."discussionId", edt.value, coalesce(c.count, 0) count
         FROM "expectedDiscussionTags" edt
         LEFT JOIN counts c
