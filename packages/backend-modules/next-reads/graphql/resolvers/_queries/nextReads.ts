5cbb2be348ea55bc93a0a789092e8ad34d16f51e
Henning Dahlheim <henning.dahlheim@republik.ch>
2025-06-16 17:38:46 +0200

    add PopularLast20DaysCommentsFeed to feed resolver

--- packages/backend-modules/next-reads/graphql/resolvers/_queries/nextReads.ts
+++ packages/backend-modules/next-reads/graphql/resolvers/_queries/nextReads.ts
@@ -1,5 +1,9 @@
 import { GraphqlContext } from '@orbiting/backend-modules-types'
-import { NextReadsResolverArgs, PopularLast7DaysFeed } from '../../../lib'
+import {
+  NextReadsResolverArgs,
+  PopularLast20DaysCommentsFeed,
+  PopularLast7DaysFeed,
+} from '../../../lib'
 
 export = async function nextReads(
   _root: never,
@@ -8,6 +12,10 @@ export = async function nextReads(
 ) {
   const resolver = [
     { id: 'POPULAR_LAST_7_DAYS', resolver: new PopularLast7DaysFeed(ctx.pgdb) },
+    {
+      id: 'POPULAR_OF_THE_LAST_20_WITH_COMMENTS_COUNT',
+      resolver: new PopularLast20DaysCommentsFeed(ctx.pgdb),
+    },
   ]
 
   const results = await Promise.all(
