b0801355b3fbd3e2dc5a89bc3811775816a43100
Anna Traussnig <anna.traussnig@gmail.com>
2025-06-17 12:09:03 +0300

    refactor

--- apps/www/src/components/next-reads/index.tsx
+++ apps/www/src/components/next-reads/index.tsx
@@ -6,33 +6,82 @@ import { useQuery } from '@apollo/client'
 import { NextReadsSection } from '@app/components/next-reads/section'
 import { css } from '@republik/theme/css'
 
+function CoverImage({ document }: { document: Document }) {
+  return (
+    <img
+      className={css({
+        aspectRatio: '1/1',
+        objectFit: 'cover',
+        width: '100%',
+        marginBottom: 4,
+      })}
+      src={
+        document.meta.facebookImage ||
+        document.meta.image ||
+        'https://a-z-animals.com/media/Penguin-Aptenodytes-Forsteri-walking-on-beach.jpg'
+      }
+      alt={document.meta.title}
+    />
+  )
+}
+
+function CategoryLabel({ document }: { document: Document }) {
+  const text = document.meta.format?.meta.title || document.meta.series?.title
+  if (!text) return null
+  return (
+    <h5
+      className={css({
+        // store the color in a css variable
+        color: document.meta.format?.meta.color || 'text',
+        fontFamily: 'gtAmericaStandard',
+        fontWeight: 500,
+        fontSize: 14,
+        lineHeight: 1,
+        letterSpacing: '-0.02em',
+        marginBottom: 2,
+      })}
+    >
+      {text}
+    </h5>
+  )
+}
+
+function MostRead({ document }: { document: Document }) {
+  return (
+    <div>
+      <CoverImage document={document} />
+      <CategoryLabel document={document} />
+      <h4
+        className={css({
+          fontFamily: 'rubis',
+          fontWeight: 500,
+          fontSize: 22,
+          marginBottom: 2,
+        })}
+      >
+        {document.meta.title}
+      </h4>
+      <p
+        className={css({
+          fontFamily: 'gtAmericaStandard',
+          fontWeight: 400,
+          fontSize: 14,
+          lineHeight: 1.2,
+        })}
+      >
+        {document.meta.estimatedReadingMinutes ||
+          document.meta.estimatedConsumptionMinutes}{' '}
+        Min
+      </p>
+    </div>
+  )
+}
+
 function RecommendedRead({ document }: { document: Document }) {
   return (
     <div>
-      <img
-        className={css({
-          aspectRatio: '4/3',
-          width: '100%',
-          marginBottom: 4,
-        })}
-        src={document.meta.facebookImage || document.meta.image}
-        alt={document.meta.title}
-      />
-      {document.meta.format && <h5>{document.meta.format.meta.title}</h5>}
-      {document.meta.series && (
-        <h5
-          className={css({
-            fontFamily: 'gtAmericaStandard',
-            fontWeight: 500,
-            fontSize: 14,
-            lineHeight: 1,
-            letterSpacing: '-0.02em',
-            marginBottom: 2,
-          })}
-        >
-          {document.meta.series.title}
-        </h5>
-      )}
+      <CoverImage document={document} />
+      <CategoryLabel document={document} />
       <h4
         className={css({
           fontFamily: 'rubis',
@@ -68,7 +117,7 @@ function RecommendedRead({ document }: { document: Document }) {
         {document.meta.contributors
           .filter((contributor) => contributor.kind.includes('Text'))
           .map((contributor) => contributor.name)
-          .join(',')}
+          .join(', ')}
       </p>
     </div>
   )
@@ -79,7 +128,9 @@ function NextReads() {
 
   if (loading) return null
 
-  const documents = data.documents.nodes.slice(0, 3)
+  const sameTopic = data.documents.nodes.slice(0, 3)
+  const mostRead = data.documents.nodes.slice(3, 11)
+
   return (
     <div
       className={css({
@@ -96,9 +147,26 @@ function NextReads() {
             gap: 4,
           })}
         >
-          {documents.map(({ entity }: { entity: Document }) => (
+          {sameTopic.map(({ entity }: { entity: Document }) => (
             <div key={entity.id} className={css({ marginBottom: '4' })}>
               <RecommendedRead document={entity} />
+            </div>
+          ))}
+        </div>
+      </NextReadsSection>
+
+      <NextReadsSection title='Most read'>
+        <div
+          className={css({
+            display: 'grid',
+            gridTemplateColumns: 'repeat(4, 1fr)',
+            gridTemplateRows: 'auto',
+            gap: 4,
+          })}
+        >
+          {mostRead.map(({ entity }: { entity: Document }) => (
+            <div key={entity.id} className={css({ marginBottom: '4' })}>
+              <MostRead document={entity} />
             </div>
           ))}
         </div>
