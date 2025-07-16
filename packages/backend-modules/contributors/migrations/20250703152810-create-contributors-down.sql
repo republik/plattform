fa77f61c9ec2957e89a6f2777bfa9ac173aff61e
Olivier Baumann <olivierbaumann@me.com>
2025-07-16 10:59:58 +0200

    migration fixes

--- packages/backend-modules/contributors/migrations/20250703152810-create-contributors-down.sql
+++ packages/backend-modules/contributors/migrations/20250703152810-create-contributors-down.sql
@@ -1,12 +1,5 @@
--- Drop the trigger
-DROP TRIGGER IF EXISTS update_contributors_updated_at ON contributors;
-
--- Drop the function
-DROP FUNCTION IF EXISTS update_updated_at_column();
-
 -- Drop the table (this will automatically drop all indexes)
 DROP TABLE IF EXISTS contributors;
 
 -- Drop the ENUM types
-DROP TYPE IF EXISTS employee_status_enum;
 DROP TYPE IF EXISTS gender_enum;
