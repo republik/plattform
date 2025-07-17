be682062c879e482f16d3c418d8221f8c9c45a72
Luciana <luciana.kolbeck@republik.ch>
2025-07-17 17:10:24 +0200

    chore(MagazineSubscription): move payment method into function

--- packages/backend-modules/payments/graphql/resolvers/MagazineSubscription.ts
+++ packages/backend-modules/payments/graphql/resolvers/MagazineSubscription.ts
@@ -54,59 +54,12 @@ export = {
 
   async paymentMethod(subscription: Subscription) {
     const paymentService = new PaymentService()
-
-    const sub = await paymentService.getSubscription(
+    const paymentMethod = await paymentService.getPaymentMethodForSubscription(
       subscription.company,
       subscription.externalId,
     )
-    if (!sub) {
-      return null
-    }
-
-    const customer = await paymentService.getCustomer(
-      subscription.company,
-      sub.customer as string,
-    )
-
-    if (!customer || customer.deleted) {
-      return null
-    }
-
-    const paymentMethodId =
-      sub.default_payment_method ||
-      customer.invoice_settings.default_payment_method
-
-    if (typeof paymentMethodId !== 'string') {
-      return null
-    }
-
-    const paymentMethod = await paymentService.getPaymentMethod(
-      subscription.company,
-      paymentMethodId,
-    )
-
-    if (!paymentMethod) {
-      return null
-    }
-
-    if (paymentMethod.card) {
-      return `${capitalize(paymentMethod.card.brand)} *${
-        paymentMethod.card.last4
-      }`
-    }
-
-    if (paymentMethod.paypal) {
-      return `Paypal`
-    }
-
-    if (paymentMethod.twint) {
-      return `Twint`
-    }
-
-    return null
+    return paymentMethod?.last4 ? `${paymentMethod.method} *${
+        paymentMethod.last4
+      }` : paymentMethod?.method
   },
-}
-
-function capitalize(str: string): string {
-  return str.charAt(0).toUpperCase() + str.slice(1)
 }
