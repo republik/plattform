2043785a91e30f29e4ccb438132f32fd7acfd577
Anna Traussnig <anna.traussnig@gmail.com>
2025-10-10 12:30:02 +0300

    try to fix fouc

--- packages/styleguide/src/components/StoryComponent/index.tsx
+++ packages/styleguide/src/components/StoryComponent/index.tsx
@@ -20,10 +20,14 @@ function StoryComponent({ tagname, componentData }: StoryComponentProps) {
 
   return (
     <>
+      <style>{`${tagname}:not(:defined) {
+        opacity: 0;
+      }`}</style>
       <CustomElement
         ref={ref}
         key={tagname}
         data={JSON.stringify(componentData)}
+        style={{}}
       />
       <Script type='module' src={src} strategy='lazyOnload' />
     </>
