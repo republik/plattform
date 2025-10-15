95a866b87db8f0d60fc318358dd9f8a298f0f840
Anna Traussnig <anna.traussnig@gmail.com>
2025-10-15 15:43:11 +0300

    set data programmatically as an attribute

--- packages/styleguide/src/components/StoryComponent/index.tsx
+++ packages/styleguide/src/components/StoryComponent/index.tsx
@@ -1,5 +1,5 @@
 import Script from 'next/script'
-import { useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 interface StoryComponentProps {
   tagname?: string
@@ -8,14 +8,33 @@ interface StoryComponentProps {
 
 function StoryComponent({ tagname, componentData }: StoryComponentProps) {
   const ref = useRef<HTMLElement>(null)
+  const [scriptLoaded, setScriptLoaded] = useState(false)
+  const [componentReady, setComponentReady] = useState(false)
+
+  useEffect(() => {
+    if (scriptLoaded) {
+      const checkRegistration = () => {
+        if (customElements.get(tagname)) {
+          setComponentReady(true)
+        } else {
+          setTimeout(checkRegistration, 50)
+        }
+      }
+      checkRegistration()
+    }
+  }, [scriptLoaded, tagname])
+
+  useEffect(() => {
+    if (componentReady && ref.current && componentData) {
+      const target = ref.current as any
+      target.data = componentData
+    }
+  }, [componentReady, componentData])
 
   if (!tagname) return <p>Tag name missing</p>
 
-  // TODO: onError
-  // TODO: custom props
-
   const src = `https://story.preview.republik.love/story-components/${tagname}/dist/index.js`
-  // const src = 'http://localhost:5173/dist/index.js' // dev purposes
+  // const src = 'http://localhost:5173/dist/index.js' // use locally running build
   const CustomElement = tagname as any
 
   return (
@@ -27,9 +46,13 @@ function StoryComponent({ tagname, componentData }: StoryComponentProps) {
         ref={ref}
         key={tagname}
         data={JSON.stringify(componentData)}
-        style={{}}
       />
-      <Script type='module' src={src} strategy='lazyOnload' />
+      <Script
+        type='module'
+        src={src}
+        strategy='lazyOnload'
+        onLoad={() => setScriptLoaded(true)}
+      />
     </>
   )
 }
