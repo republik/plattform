1970c8ef236c255ffb7495c9065d0ff773946a0d
Olivier Baumann <olivierbaumann@me.com>
2025-11-11 15:38:27 +0100

    add back dataloader

--- packages/backend-modules/collections/graphql/resolvers/_mutations/removeAudioQueueItem.js
+++ packages/backend-modules/collections/graphql/resolvers/_mutations/removeAudioQueueItem.js
@@ -1,31 +1,13 @@
 const { Roles } = require('@orbiting/backend-modules-auth')
-const { removeItem, getCollectionName } = require('../../../lib/AudioQueue')
-const { ascending } = require('d3-array')
+const { removeItem } = require('../../../lib/AudioQueue')
 
 module.exports = async (_, args, context) => {
   const { id } = args
-  const { user: me, loaders, pgdb } = context
+  const { user: me, loaders } = context
 
   Roles.ensureUserHasRole(me, 'member')
 
   await removeItem({ id }, context)
 
-  // Return only minimal data (IDs and sequences) without loading full documents
-  const collection = await loaders.Collection.byKeyObj.load({
-    name: getCollectionName(),
-  })
-
-  if (!collection) {
-    return []
-  }
-
-  const items = await pgdb.public.collectionDocumentItems.find(
-    {
-      collectionId: collection.id,
-      userId: me.id,
-    },
-    { orderBy: ['createdAt'] }
-  )
-
-  return items.sort((a, b) => ascending(a.data?.sequence, b.data?.sequence))
+  return loaders.AudioQueue.byUserId.load(me.id)
 }
