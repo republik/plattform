5db8bfa2f61aeec9e5352f85977b505f337cea6c
Henning Dahlheim <henning.dahlheim@republik.ch>
2025-11-17 17:49:27 +0100

    feat: improved logging

--- packages/backend-modules/base/express/gqlProtection.js
+++ packages/backend-modules/base/express/gqlProtection.js
@@ -1,36 +1,65 @@
+const { print, Kind } = require('graphql')
 const { ApolloArmor } = require('@escape.tech/graphql-armor')
 const { logger: baseLogger } = require('@orbiting/backend-modules-logger')
 
 const validationLogger = baseLogger.child({}, { msgPrefix: '[Graphql Armor] ' })
 
-function logRejection(_ctx, error) {
-  validationLogger.error({ error })
+function createRejectLogger(name) {
+  return function (ctx, error) {
+    const doc = ctx?.getDocument() ? print(ctx?.getDocument()) : null
+
+    validationLogger.error(
+      {
+        rule: name,
+        doc: doc,
+        error,
+      },
+      'request rejected',
+    )
+  }
 }
-function logAccept(ctx, details) {
-  validationLogger.info({ details })
+
+function createAcceptLogger(name) {
+  return function (ctx, details) {
+    const doc = ctx?.getDocument()
+
+    const operationDef = doc?.definitions?.find(
+      (def) => def.kind === Kind.OPERATION_DEFINITION,
+    )
+
+    const operation = operationDef.name
+      ? operationDef.name.value
+      : doc
+      ? print(doc)
+      : null
+
+    validationLogger.info(
+      { rule: name, value: details.n, operation },
+      'request accepted',
+    )
+  }
 }
 
 const gqlArmor = new ApolloArmor({
   maxAliases: {
     enabled: true,
     n: 5,
-    onReject: [logRejection],
+    onReject: [createRejectLogger('maxAliases')],
   },
   maxDepth: {
     enabled: true,
     n: 20,
-    onAccept: [logAccept],
-    onReject: [logRejection],
+    onAccept: [createAcceptLogger('maxDepth')],
+    onReject: [createRejectLogger('maxDepth')],
   },
   costLimit: {
     enabled: true,
-    limit: 10000,
-    onAccept: [logAccept],
-    onReject: [logRejection],
+    maxCost: 100000,
+    onAccept: [createAcceptLogger('costLimit')],
+    onReject: [createRejectLogger('costLimit')],
   },
   blockFieldSuggestion: {
     enabled: false,
-    onReject: [logRejection],
   },
 })
 
