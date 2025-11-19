1a5cb924f794ad4a5d177236a717ce233e88e5e5
Henning Dahlheim <henning.dahlheim@republik.ch>
2025-11-19 11:55:13 +0100

    Feat/graphql armor (#1246)
    * feat: add @escape.tech/graphql-armor
    
    * feat: update graphql-armor config
    
    * chore: move graphql-armor config into separate file
    
    * feat: improved logging
    
    * reduce complexity of critical queries
    
    * reduce complexity of feed queries
    
    * chore: apply PR feedback
    
    ---------
    
    Co-authored-by: Jeremy Stucki <jeremy.stucki@gmail.com>

---
+++ packages/backend-modules/base/express/gqlProtection.js
@@ -0,0 +1,88 @@
+const { print, Kind } = require('graphql')
+const { ApolloArmor } = require('@escape.tech/graphql-armor')
+const { logger: baseLogger } = require('@orbiting/backend-modules-logger')
+
+const validationLogger = baseLogger.child({}, { msgPrefix: '[Graphql Armor] ' })
+
+function getOperation(ctx) {
+  const doc = ctx?.getDocument()
+
+  const operationDef = doc?.definitions?.find(
+    (def) => def.kind === Kind.OPERATION_DEFINITION,
+  )
+
+  const operation = operationDef.name
+    ? operationDef.name.value
+    : doc
+    ? print(doc)
+    : 'Unknown operation'
+
+  return operation
+}
+
+function createRejectLogger(name) {
+  return function (ctx, error) {
+    const doc = ctx?.getDocument() ? print(ctx?.getDocument()) : null
+    const operation = getOperation(ctx)
+
+    validationLogger.error(
+      {
+        rule: name,
+        operation: operation,
+        doc: doc,
+        error,
+      },
+      `rule: [${name}] operation: [${operation}] request rejected`,
+    )
+  }
+}
+
+function createAcceptLogger(name) {
+  return function (ctx, details) {
+    const operation = getOperation(ctx)
+
+    validationLogger.debug(
+      { rule: name, value: details.n, operation },
+      `rule: [${name}] operation: [${operation}] request accepted`,
+    )
+  }
+}
+
+const gqlArmor = new ApolloArmor({
+  maxAliases: {
+    enabled: true,
+    n: 5,
+    onReject: [createRejectLogger('maxAliases')],
+  },
+  maxDepth: {
+    enabled: true,
+    n: 20,
+    onAccept: [createAcceptLogger('maxDepth')],
+    onReject: [createRejectLogger('maxDepth')],
+  },
+  costLimit: {
+    enabled: true,
+    maxCost: 100000,
+    onAccept: [createAcceptLogger('costLimit')],
+    onReject: [createRejectLogger('costLimit')],
+  },
+  maxDirectives: {
+    enabled: true,
+    n: 50,
+    onAccept: [createAcceptLogger('maxDirectives')],
+    onReject: [createRejectLogger('maxDirectives')],
+  },
+  maxTokens: {
+    enabled: true,
+    n: 1000,
+    onAccept: [createAcceptLogger('maxTokens')],
+    onReject: [createRejectLogger('maxTokens')],
+  },
+  blockFieldSuggestion: {
+    enabled: false,
+  },
+})
+
+const gqlProtection = gqlArmor.protect()
+
+exports.gqlProtection = gqlProtection
