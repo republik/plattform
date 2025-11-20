fce6f94fc7b529b92d594b2a870dd0cb08c10570
Henning Dahlheim <henning.dahlheim@republik.ch>
2025-11-20 14:02:25 +0100

    feat: add heroku metadata

--- apps/api/instrumentation.js
+++ apps/api/instrumentation.js
@@ -1,5 +1,8 @@
 const { NodeSDK } = require('@opentelemetry/sdk-node')
 
+const { resourceFromAttributes } = require('@opentelemetry/resources')
+const { ATTR_SERVICE_VERSION } = require('@opentelemetry/semantic-conventions')
+
 const {
   OTLPTraceExporter,
 } = require('@opentelemetry/exporter-trace-otlp-proto')
@@ -23,6 +26,14 @@ const {
 } = require('@opentelemetry/instrumentation-dataloader')
 
 const sdk = new NodeSDK({
+  resource: resourceFromAttributes({
+    [ATTR_SERVICE_VERSION]:
+      process.env.HEROKU_RELEASE_VERSION || 'unknown-version',
+    'service.instance.id': process.env.HEROKU_DYNO_ID,
+    'heroku.instance.name': process.env.DYNO,
+    'heroku.release.commit': process.env.HEROKU_SLUG_COMMIT,
+    'heroku.release.description': process.env.HEROKU_SLUG_DESCRIPTION,
+  }),
   traceExporter: new OTLPTraceExporter(),
   metricReader: new PeriodicExportingMetricReader({
     exporter: new OTLPMetricExporter(),
