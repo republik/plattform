4c3bede279da5280189bd901374f7c6b8869dbd3
Jeremy Stucki <jeremy.stucki@gmail.com>
2025-11-25 17:10:16 +0100

    clean up notification channels query

--- packages/backend-modules/push-notifications/graphql/resolvers/_mutations/upsertDevice.js
+++ packages/backend-modules/push-notifications/graphql/resolvers/_mutations/upsertDevice.js
@@ -79,17 +79,16 @@ module.exports = async (
         UPDATE
           users
         SET
-          "hadDevice" = true
-          ${
-            me._raw.notificationChannels.indexOf('APP') === -1 // avoid duplicates
-              ? ', "notificationChannels" = "notificationChannels" || \'["APP"]\''
-              : ''
-          }
+          "hadDevice" = true,
+          "notificationChannels" = "notificationChannels" || :channels
         WHERE
           id = :userId
       `,
         {
           userId: me.id,
+          channels: me._raw.notificationChannels.includes('APP')
+            ? '[]'
+            : '["APP"]',
         },
       )
     }
