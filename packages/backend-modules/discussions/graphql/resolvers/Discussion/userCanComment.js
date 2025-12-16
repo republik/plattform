1991b593a634ed8750fdd5249f8b1d15b5409b2c
Olivier Baumann <olivierbaumann@me.com>
2025-12-16 11:39:00 +0100

    remove allowedRoles from discussions

--- packages/backend-modules/discussions/graphql/resolvers/Discussion/userCanComment.js
+++ packages/backend-modules/discussions/graphql/resolvers/Discussion/userCanComment.js
@@ -2,9 +2,7 @@ const { Roles } = require('@orbiting/backend-modules-auth')
 const { hasUserActiveMembership } = require('@orbiting/backend-modules-utils')
 
 /**
- * User can comment when either:
- * - a) they have a role which is allowed to comment (discussion.allowedRoles)
- * - b) they have the member role and either debater role or an active membership
+ * User can comment when they have the member role and either debater role or an active membership
  */
 module.exports = async (discussion, _, context) => {
   const { closed } = discussion
@@ -15,19 +13,11 @@ module.exports = async (discussion, _, context) => {
     return false
   }
 
-  // a) User can comment, if it has some of the allowed roles but member
-  const allowedRoles = discussion.allowedRoles.filter((r) => r !== 'member')
-  if (Roles.userIsInRoles(user, allowedRoles)) {
-    return true
-  }
-
-  // b) User can comment, if discussion allows member role, user has member role
-  // and either debater role or an active membership.
-  const isMemberAllowed = !!discussion.allowedRoles.find((r) => r === 'member')
+  // User can comment, if user has member role and either debater role or an active membership
   const isMember = Roles.userIsInRoles(user, ['member'])
   const isDebater = Roles.userIsInRoles(user, ['debater'])
   const hasActiveMembership =
     !!user && (await hasUserActiveMembership(user, pgdb))
 
-  return isMemberAllowed && isMember && (isDebater || hasActiveMembership)
+  return isMember && (isDebater || hasActiveMembership)
 }
