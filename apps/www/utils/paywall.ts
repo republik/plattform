9309f275a63d1a8a2251c519e850da45dacf58e6
Jeremy Stucki <jeremy.stucki@gmail.com>
2026-01-27 18:06:28 +0100

    Fix localStorage security error in metering

--- apps/www/utils/paywall.ts
+++ apps/www/utils/paywall.ts
@@ -1,12 +1,22 @@
 export function trackArticleRead(articleId: string) {
-  const articlesRead = JSON.parse(localStorage.getItem('articlesRead') || '[]')
-  if (!articlesRead.includes(articleId)) {
-    articlesRead.push(articleId)
-    localStorage.setItem('articlesRead', JSON.stringify(articlesRead))
-  }
+  try {
+    const articlesRead = JSON.parse(
+      localStorage.getItem('articlesRead') || '[]',
+    )
+    if (!articlesRead.includes(articleId)) {
+      articlesRead.push(articleId)
+      localStorage.setItem('articlesRead', JSON.stringify(articlesRead))
+    }
+  } catch {}
 }
 
 export function hasExceededThreshold(threshold: number): boolean {
-  const articlesRead = JSON.parse(localStorage.getItem('articlesRead') || '[]')
-  return articlesRead.length > threshold
+  try {
+    const articlesRead = JSON.parse(
+      localStorage.getItem('articlesRead') || '[]',
+    )
+    return articlesRead.length > threshold
+  } catch {
+    return false
+  }
 }
