a7305535668db1df74e006e25d71ae4a548c63f3
Anna Traussnig <anna.traussnig@gmail.com>
2026-02-11 16:43:24 +0200

    nl form for logged out users

--- apps/www/src/components/newsletters/newsletter-subscribe.tsx
+++ apps/www/src/components/newsletters/newsletter-subscribe.tsx
@@ -3,9 +3,10 @@
 import {
   NewsletterName,
   NewsletterSettingsDocument,
+  SignUpForNewsletterDocument,
   UpdateNewsletterSubscriptionDocument,
 } from '#graphql/republik-api/__generated__/gql/graphql'
-import { useMutation, useQuery } from '@apollo/client'
+import { ApolloError, useMutation, useQuery } from '@apollo/client'
 import { ErrorMessage } from '@app/components/auth/login/error-message'
 import { isSubscribedToNewsletter } from '@app/components/newsletters/helpers'
 import { NewslettersStatus } from '@app/components/newsletters/newsletters-status'
@@ -17,11 +18,18 @@ import { useState } from 'react'
 import isEmail from 'validator/lib/isEmail'
 import { useTranslation } from '../../../lib/withT'
 
-function NewsletterSubscribeForm() {
+function NewsletterSubscribeForm({
+  newsletter,
+}: {
+  newsletter: NewsletterName
+}) {
   const { t } = useTranslation()
+  const track = useTrackEvent()
+  const [subscribe] = useMutation(SignUpForNewsletterDocument)
 
   const [isPending, setIsPending] = useState(false)
-  const [error, setError] = useState<string>()
+  const [confirmEmail, setConfirmEmail] = useState(false)
+  const [error, setError] = useState<ApolloError | string | undefined>()
 
   async function submitEmail(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault()
@@ -40,6 +48,34 @@ function NewsletterSubscribeForm() {
       setError(t('auth/login/email/missing'))
       return
     }
+
+    const result = await subscribe({
+      variables: {
+        email: email,
+        name: newsletter,
+        context: 'newsletter',
+      },
+    })
+
+    if (result.errors && result.errors.length > 0) {
+      setError(new ApolloError({ graphQLErrors: result.errors }))
+      return setIsPending(false)
+    }
+
+    track({
+      action: 'Anonymous Newsletter Subscribe',
+      name: newsletter,
+    })
+    setIsPending(false)
+    setConfirmEmail(true)
+  }
+
+  if (confirmEmail) {
+    return (
+      <p className={css({ textStyle: 'airy' })}>
+        {t('Auth/NewsletterSignUp/success')}
+      </p>
+    )
   }
 
   return (
@@ -49,14 +85,14 @@ function NewsletterSubscribeForm() {
           gap: '2',
           display: 'flex',
           alignItems: 'end',
+          flexDirection: 'column',
+          lg: { flexDirection: 'row' },
         })}
       >
         <FormField label='Ihre E-Mail-Adresse' name='email' type='email' />
-        <div>
-          <Button type='submit' disabled={isPending} loading={isPending}>
-            Newsletter abonnieren
-          </Button>
-        </div>
+        <Button type='submit' disabled={isPending} loading={isPending}>
+          Newsletter abonnieren
+        </Button>
       </div>
       {error && <ErrorMessage error={error} />}
     </form>
@@ -79,7 +115,7 @@ export function NewsletterSubscribeButton({
   if (!data) return null
 
   if (!data.me) {
-    return <NewsletterSubscribeForm />
+    return <NewsletterSubscribeForm newsletter={newsletter} />
   }
 
   const subscriptions = data.me.newsletterSettings.subscriptions
