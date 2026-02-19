dddd29a07d9cf2631052f940bd1a41ce61268022
Jeremy Stucki <jeremy.stucki@gmail.com>
2026-02-19 16:02:01 +0100

    use gateway token for gql codegen too

--- apps/publikator/graphql/configs/codegen-dev.ts
+++ apps/publikator/graphql/configs/codegen-dev.ts
@@ -8,7 +8,14 @@ const config: CodegenConfig = {
   overwrite: true,
   generates: {
     './graphql/republik-api/__generated__/gql/': {
-      schema: process.env.NEXT_PUBLIC_API_URL,
+      schema: {
+        [process.env.NEXT_PUBLIC_API_URL]: {
+          headers: {
+            'x-api-gateway-client': 'publikator',
+            'x-api-gateway-token': process.env.API_GATEWAY_TOKEN,
+          },
+        },
+      },
       documents: ['./graphql/republik-api/**/*.{ts,tsx,gql,graphql}'],
       preset: 'client',
       presetConfig: {
