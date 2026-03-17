ff6487415e851322f9a948cd8564c3466480bd9b
Jeremy Stucki <jeremy.stucki@gmail.com>
2026-03-17 10:23:09 +0100

    remove global header and footer from not-found page, unify error page layout

--- apps/www/src/app/not-found.tsx
+++ apps/www/src/app/not-found.tsx
@@ -1,54 +1,37 @@
-import Container from '@app/components/container'
-import { PageLayout } from '@app/components/layout'
+import { ErrorPage } from '@app/components/layout/error-page'
 import { css } from '@republik/theme/css'
-import { stack } from '@republik/theme/patterns'
 import Link from 'next/link'
-import React from 'react'
 
 function NotFound() {
   return (
-    <PageLayout>
-      <Container>
-        <div className={stack({ gap: '4', mt: '4', mb: '16' })}>
-          <h1
-            className={css({
-              textStyle: 'h1Sans',
-              mt: '8',
-              fontSize: '8xl',
-              marginY: '16',
-              textAlign: 'center',
-              md: {
-                fontSize: '300px',
-                marginY: '60px',
-              },
-            })}
-          >
-            404
-          </h1>
-          <p
-            className={css({
-              textStyle: 'sans',
-              fontSize: '1.3125rem',
-              lineHeight: '2rem',
-            })}
-          >
-            Seite nicht gefunden
-          </p>
-          <Link
-            href='/'
-            className={css({
-              color: 'primary',
-              textDecoration: 'none',
-              _hover: {
-                color: 'primaryHover',
-              },
-            })}
-          >
-            Zum Magazin
-          </Link>
-        </div>
-      </Container>
-    </PageLayout>
+    <ErrorPage>
+      <h1
+        className={css({
+          textStyle: 'h1Sans',
+          fontSize: '8xl',
+          textAlign: 'center',
+          lineHeight: '1',
+          md: {
+            fontSize: '300px',
+          },
+        })}
+      >
+        404
+      </h1>
+      <p>Die Seite konnte nicht gefunden werden</p>
+      <Link
+        href='/'
+        className={css({
+          color: 'primary',
+          textDecoration: 'none',
+          _hover: {
+            color: 'primaryHover',
+          },
+        })}
+      >
+        Zum Magazin
+      </Link>
+    </ErrorPage>
   )
 }
 
