5626411e3472e12cfc6eb18cdd66a4f88d453142
Henning Dahlheim <henning.dahlheim@republik.ch>
2026-03-19 11:11:52 +0100

    feat: add new member count for campaigns (#1352)
    * feat: add new member count for campaigns
    
    * fix: campaign member counting rule
    
    * fix: count result
    
    * chore: change any to never in referrals resolver

--- packages/backend-modules/referral-campaigns/graphql/resolvers/Campaign.ts
+++ packages/backend-modules/referral-campaigns/graphql/resolvers/Campaign.ts
@@ -3,11 +3,23 @@ import type { Campaign } from '../types'
 import { PGReferralsRepo } from '../../lib/repo'
 
 export = {
-  async referrals(campaign: Campaign, _args: any, ctx: GraphqlContext) {
+  async referrals(campaign: Campaign, _args: never, ctx: GraphqlContext) {
     const referralCount =
       (await new PGReferralsRepo(ctx.pgdb).getCampaignReferralCount(
         campaign.id,
       )) || 0
+    return { count: referralCount }
+  },
+  /**
+   * Returns the count of new memberships and subscriptions that sigend up
+   * during the campaign.
+   */
+  async newMembers(campaign: Campaign, _args: never, ctx: GraphqlContext) {
+    const referralCount =
+      (await new PGReferralsRepo(ctx.pgdb).getCampaignNewMemberCount(
+        campaign.id,
+      )) || 0
+
     return { count: referralCount }
   },
 }
