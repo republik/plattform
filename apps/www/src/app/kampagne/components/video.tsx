6e20ae034d3a38eb9460dee7fe095fcd48192204
Anna Traussnig <anna.traussnig@gmail.com>
2026-03-24 02:42:56 +0200

    host video on keycdn

--- apps/www/src/app/kampagne/components/video.tsx
+++ apps/www/src/app/kampagne/components/video.tsx
@@ -1,26 +1,22 @@
 'use client'
 
+import { usePlatformInformation } from '@app/lib/hooks/usePlatformInformation'
 import { css } from '@republik/theme/css'
 
 export function Video() {
+  const { isNativeApp } = usePlatformInformation()
+
+  // player causes page to redirect. will fix properly tomorrow
+  if (isNativeApp) {
+    return null
+  }
+
   return (
-    <div
-      className={css({
-        '& iframe': {
-          aspectRatio: '9/16',
-          width: '100%',
-          height: 'auto',
-        },
-      })}
-    >
-      <iframe
-        src='https://player.vimeo.com/video/1175425419'
-        frameBorder='0'
-        referrerPolicy='no-referrer'
-        allow='autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media'
-        sandbox='allow-scripts allow-same-origin allow-presentation'
-        title='Frühlingskampagne 2026'
-      ></iframe>
-    </div>
+    <video
+      src='https://cdn.repub.ch/s3/republik-assets/repos/republik/article-republik-versus-amazon-2-0/files/d880f823-6b30-48d0-8f56-289684d1c76f/wir_suchen_2000_neue_mitglieder-(1080p).mp4'
+      controls
+      playsInline
+      className={css({ aspectRatio: '9/16', width: '100%' })}
+    />
   )
 }
