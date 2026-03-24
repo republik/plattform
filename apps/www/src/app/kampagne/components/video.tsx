6abfdf64a58ab12dd5b3050ee7693ed9127b8478
Anna Traussnig <anna.traussnig@gmail.com>
2026-03-24 02:47:37 +0200

    remove native app check

--- apps/www/src/app/kampagne/components/video.tsx
+++ apps/www/src/app/kampagne/components/video.tsx
@@ -1,16 +1,6 @@
-'use client'
-
-import { usePlatformInformation } from '@app/lib/hooks/usePlatformInformation'
 import { css } from '@republik/theme/css'
 
 export function Video() {
-  const { isNativeApp } = usePlatformInformation()
-
-  // player causes page to redirect. will fix properly tomorrow
-  if (isNativeApp) {
-    return null
-  }
-
   return (
     <video
       src='https://cdn.repub.ch/s3/republik-assets/repos/republik/article-republik-versus-amazon-2-0/files/d880f823-6b30-48d0-8f56-289684d1c76f/wir_suchen_2000_neue_mitglieder-(1080p).mp4'
