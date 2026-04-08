b0b43d74ef791f4f1013139804b77fe8e814656f
Jeremy Stucki <jeremy.stucki@gmail.com>
2026-04-08 14:09:15 +0200

    fix base64 email merge field on user email change (#1380)
    * fix base64 email merge field on user email change
    
    * fix runOperations script

---
+++ packages/backend-modules/mailchimp/lib/operations/index.js
@@ -0,0 +1,9 @@
+const covidAccessTokens = require('./covidAccessTokens')
+const nameAndEmailBase64u = require('./nameAndEmailBase64u')
+
+module.exports = {
+  operations: {
+    covidAccessTokens,
+    nameAndEmailBase64u,
+  },
+}
