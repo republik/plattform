215a81065a4abb5411525a6a1cacc7d87fc1b0f0
Jeremy Stucki <jeremy.stucki@gmail.com>
2026-04-15 14:11:48 +0200

    move middleware/sentry files to src/

--- apps/www/src/instrumentation.ts
+++ apps/www/src/instrumentation.ts
@@ -2,11 +2,11 @@ import * as Sentry from '@sentry/nextjs'
 
 export async function register() {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
-    await import('./sentry.server.config')
+    await import('../sentry.server.config')
   }
 
   if (process.env.NEXT_RUNTIME === 'edge') {
-    await import('./sentry.edge.config')
+    await import('../sentry.edge.config')
   }
 }
 
