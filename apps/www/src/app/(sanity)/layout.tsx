5feabd61eb9baabc3f99c74c489a2e41ff53ecf5
Jeremy Stucki <jeremy.stucki@gmail.com>
2026-05-18 17:37:10 +0200

    a bit of page layout

--- apps/www/src/app/(sanity)/layout.tsx
+++ apps/www/src/app/(sanity)/layout.tsx
@@ -1,8 +1,11 @@
-import { draftMode } from 'next/headers'
-import { VisualEditing } from 'next-sanity/visual-editing'
 import { SanityLive } from '@/app/(sanity)/lib/live'
+import Container from '@/app/components/container'
+import { PageLayout } from '@/app/components/layout'
+import { css } from '@republik/theme/css'
+import { VisualEditing } from 'next-sanity/visual-editing'
+import { draftMode } from 'next/headers'
 import { DisableDraftMode } from './components/disable-draft-mode'
-// import {DisableDraftMode} from '@/app/(sanity)/components/disable-draft-mode'
+
 export default async function RootLayout({
   children,
 }: Readonly<{
@@ -10,7 +13,17 @@ export default async function RootLayout({
 }>) {
   return (
     <>
-      {children}
+      <PageLayout>
+        <div
+          className={css({
+            color: 'text',
+            pb: '16-32',
+            pt: '4',
+          })}
+        >
+          <Container>{children}</Container>
+        </div>
+      </PageLayout>
       <SanityLive />
       {(await draftMode()).isEnabled && (
         <>
