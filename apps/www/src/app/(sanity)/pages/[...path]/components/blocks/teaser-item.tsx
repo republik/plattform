58211cecaf877ae508850724c12feb6f2ea9e6ce
Anna Traussnig <anna.traussnig@gmail.com>
2026-07-07 13:02:32 +0300

    cleanup query

--- apps/www/src/app/(sanity)/pages/[...path]/components/blocks/teaser-item.tsx
+++ apps/www/src/app/(sanity)/pages/[...path]/components/blocks/teaser-item.tsx
@@ -1,30 +1,5 @@
 import { FrontTeaser } from '@/app/(sanity)/components/portable-text/front-teaser'
 import { TeaserBlockFragmentType } from '@/app/(sanity)/groq/teaser-block-fragment'
-import { defineQuery } from 'next-sanity'
-
-const TEASER_BLOCK_QUERY = defineQuery(
-  `*[_type == "page" && _id == $documentId][0]{
-    _id,
-    "block": pageBuilder[_key == $blockKey && _type == "teaserItem"][0]{
-      reference -> {
-        "slug": slug.current,
-        frontTeaser {
-          layout,
-          title,
-          lead,
-          image,
-          imageCredits,
-          imagePosition,
-          imagePadding,
-          textPosition,
-          textSize,
-          color,
-          backgroundColor
-        }
-      }
-    }
-  }`,
-)
 
 export async function TeaserItem({
   teaser,
