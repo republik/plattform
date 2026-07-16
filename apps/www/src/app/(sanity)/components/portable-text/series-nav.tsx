1f2043a98c1f601b95a1769f3675403df7f65230
Anna Traussnig <anna.traussnig@gmail.com>
2026-07-16 17:03:19 +0200

    add sie lesen detail

--- apps/www/src/app/(sanity)/components/portable-text/series-nav.tsx
+++ apps/www/src/app/(sanity)/components/portable-text/series-nav.tsx
@@ -1,13 +1,10 @@
-import { InlinePortableText } from '@/app/(sanity)/components/portable-text/render'
 import { getSeriesLabels } from '@/app/(sanity)/components/series-labels'
+import { InlineTeaser } from '@/app/(sanity)/components/teaser/inline'
 import { SERIES_NAV_QUERY } from '@/app/(sanity)/groq/series-nav-query'
 import { sanityFetch } from '@/app/(sanity)/lib/live'
-import { urlFor } from '@/app/(sanity)/lib/urlFor'
 import { Infobox } from '@/app/components/ui/infobox'
 import type { SeriesNav } from '@/sanity.types'
 import { css } from '@republik/theme/css'
-import { getImageDimensions } from '@sanity/asset-utils'
-import { Image } from 'next-sanity/image'
 import Link from 'next/link'
 
 export async function SeriesNav({
@@ -59,45 +56,9 @@ export async function SeriesNav({
             mx: 'auto',
           })}
         >
-          {series.episodes?.map((episode, i) => {
-            const src = episode.image
-              ? urlFor(episode.image).width(280).url()
-              : undefined
-            const dimensions = src ? getImageDimensions(src) : undefined
-
-            return (
-              <div
-                key={episode._id}
-                className={css({
-                  display: 'flex',
-                  flexDirection: 'column',
-                  gap: '1',
-                  alignItems: 'center',
-                  scrollSnapAlign: 'center',
-                })}
-              >
-                <h6 className={css({ fontSize: 's' })}>{labels[i]}</h6>
-                {episode.image && (
-                  <Image
-                    src={src}
-                    width={170}
-                    height={170 / dimensions.aspectRatio}
-                    alt=''
-                  />
-                )}
-                <h5
-                  className={css({
-                    textStyle: 'h3Serif',
-                    fontSize: 'medium',
-                    textAlign: 'left',
-                    width: '100%',
-                  })}
-                >
-                  <InlinePortableText value={episode.title} />
-                </h5>
-              </div>
-            )
-          })}
+          {series.episodes?.map((episode, i) => (
+            <InlineTeaser key={i} teaser={episode} label={labels[i]} />
+          ))}
         </div>
       </div>
     </>
---
+++ apps/www/src/app/(sanity)/components/teaser/inline/index.tsx
@@ -0,0 +1,62 @@
+'use client'
+
+import { LinkOverlay } from '@/app/(sanity)/components/teaser/_shared/helpers'
+import { TeaserFragmentType } from '@/app/(sanity)/groq/teaser-fragment'
+import { urlFor } from '@/app/(sanity)/lib/urlFor'
+import { css } from '@republik/theme/css'
+import { getImageDimensions } from '@sanity/asset-utils'
+import { Image } from 'next-sanity/image'
+import { usePathname } from 'next/navigation'
+
+export function InlineTeaser({
+  teaser,
+  label,
+}: {
+  teaser: TeaserFragmentType
+  label?: string
+}) {
+  const src = teaser.image ? urlFor(teaser.image).width(280).url() : undefined
+  const dimensions = src ? getImageDimensions(src) : undefined
+
+  const pathname = usePathname()
+  const isCurrentEpisode = pathname === `/articles${teaser.slug}`
+
+  return (
+    <div
+      key={teaser._id}
+      className={css({
+        position: 'relative',
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '1',
+        alignItems: 'center',
+        scrollSnapAlign: 'center',
+      })}
+    >
+      <h6
+        className={css({ fontSize: 's', fontFamily: 'gtAmericaStandard' })}
+        style={{ fontWeight: isCurrentEpisode ? 500 : 'normal' }}
+      >
+        {isCurrentEpisode ? `Sie lesen: ${label}` : label}
+      </h6>
+      {teaser.image && (
+        <Image
+          src={src}
+          width={170}
+          height={170 / dimensions.aspectRatio}
+          alt=''
+        />
+      )}
+      <h5
+        className={css({
+          textStyle: 'h3Serif',
+          fontSize: 'medium',
+          textAlign: 'left',
+          width: '100%',
+        })}
+      >
+        <LinkOverlay teaser={teaser} />
+      </h5>
+    </div>
+  )
+}
