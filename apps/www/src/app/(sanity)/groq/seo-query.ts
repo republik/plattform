17d0d3cacd0c47d2135a310fa5ef0045c87dd399
Olivier Baumann <olivierbaumann@me.com>
2026-07-21 10:41:53 +0200

    share image generation with next og route

--- apps/www/src/app/(sanity)/groq/seo-query.ts
+++ apps/www/src/app/(sanity)/groq/seo-query.ts
@@ -4,6 +4,14 @@ export const SEO_QUERY = defineQuery(
   `*[slug.current == $slug][0]{
     "title": coalesce(seo.title, pt::text(title)),
     "description": coalesce(seo.description, pt::text(description)),
-    "image": coalesce(seo.image, image)
+    "image": coalesce(seo.image, image),
+    "useImageBuilder": seo.useImageBuilder,
+    "imageBuilder": seo.imageBuilder,
+    "heading": pt::text(heading->title),
+    theme {
+      name,
+      accentColor,
+      darkMode
+    }
   }`,
 )
