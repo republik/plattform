35091df689874c799d9182d57e35c7960d82536f
Anna Traussnig <anna.traussnig@gmail.com>
2026-07-27 09:55:56 +0200

    edit theme to fit regwall pattern

--- packages/theme/src/recipes/editorial-content.ts
+++ packages/theme/src/recipes/editorial-content.ts
@@ -2,25 +2,32 @@ import { defineParts, defineRecipe } from '@pandacss/dev'
 
 const contentParts = defineParts({
   root: { selector: '&' },
-  allBlocks: { selector: '& > *' },
+  allBlocks: { selector: '& > *, .regwall > *' },
   allBlocksAfterHeadings: {
-    selector: '& > :is(h2,h3,h4,h5,h6) + *',
+    selector: '& > :is(h2,h3,h4,h5,h6) + *, .regwall > :is(h2,h3,h4,h5,h6) + *',
   },
-  breakoutBlocks: { selector: '& > .breakout' },
-  breakoutLeftBlocks: { selector: '& > .breakout-left' },
-  breakoutRightBlocks: { selector: '& > .breakout' },
-  fullWidthBlocks: { selector: '& > .full' },
-  paragraphs: { selector: '& > p' },
-  subheadings: { selector: '& > h2' },
-  heading: { selector: '& .page-heading' },
-  title: { selector: '& .page-title' },
-  titleAfterHeading: { selector: '& .page-heading + .page-title' },
-  lead: { selector: '& .page-lead' },
-  byline: { selector: '& .page-byline' },
-  unorderedLists: { selector: '& > ul' },
-  orderedLists: { selector: '& > ol' },
-  unorderedListItems: { selector: '& > ul li' },
-  orderedListItems: { selector: '& > ol li' },
+  breakoutBlocks: { selector: '& > .breakout, .regwall > .breakout' },
+  breakoutLeftBlocks: {
+    selector: '& > .breakout-left, .regwall > .breakout-left',
+  },
+  breakoutRightBlocks: {
+    selector: '& > .breakout-right, .regwall > .breakout-right',
+  },
+  fullWidthBlocks: { selector: '& > .full, .regwall > .full' },
+  paragraphs: { selector: '& > p, .regwall > p' },
+  subheadings: { selector: '& > h2, .regwall > h2' },
+  heading: { selector: '& .page-heading, .regwall .page-heading' },
+  title: { selector: '& .page-title, .regwall .page-title' },
+  titleAfterHeading: {
+    selector:
+      '& .page-heading + .page-title, .regwall .page-heading + .page-title',
+  },
+  lead: { selector: '& .page-lead, .regwall .page-lead' },
+  byline: { selector: '& .page-byline, .regwall .page-byline' },
+  unorderedLists: { selector: '& > ul, .regwall > ul' },
+  orderedLists: { selector: '& > ol, .regwall > ol' },
+  unorderedListItems: { selector: '& > ul li, .regwall > ul li' },
+  orderedListItems: { selector: '& > ol li, .regwall > ol li' },
 })
 
 export const editorialContentRecipe = defineRecipe({
