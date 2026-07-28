f5fc08708bca34362937a693def5711e013cfd6a
Jeremy Stucki <jeremy.stucki@gmail.com>
2026-07-28 14:01:51 +0200

    large teasers preview route (only accessible when draft mode is enabled)

--- apps/www/src/app/(sanity)/preview/layout.tsx
+++ apps/www/src/app/(sanity)/preview/layout.tsx
@@ -1,13 +1,16 @@
 import { PreviewPageLayout } from '@/app/(sanity)/components/preview/preview-page-layout'
 import { draftMode } from 'next/headers'
+import { notFound } from 'next/navigation'
 
-export default async function TeaserLayout({
+export default async function PreviewLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  // Routes in /preview should only be accessible when draft mode is enabled
   if ((await draftMode()).isEnabled) {
     return <PreviewPageLayout>{children}</PreviewPageLayout>
   }
-  return null
+
+  notFound()
 }
