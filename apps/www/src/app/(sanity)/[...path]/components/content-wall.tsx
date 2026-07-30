5912e32c6d751cc04726946f5549b05098bc8c4e
Anna Traussnig <anna.traussnig@gmail.com>
2026-07-30 14:30:26 +0200

    simplify regwall

--- apps/www/src/app/(sanity)/[...path]/components/content-wall.tsx
+++ apps/www/src/app/(sanity)/[...path]/components/content-wall.tsx
@@ -4,11 +4,11 @@ import { usePaynotes } from '@/app/(sanity)/components/paynotes/paynotes-context
 import Paywall from '@/app/(sanity)/components/paynotes/paywall'
 import Regwall from '@/app/(sanity)/components/paynotes/regwall'
 import { Article } from '@/sanity.types'
-import { css, cx } from '@republik/theme/css'
+import { css } from '@republik/theme/css'
 import { usePathname } from 'next/navigation'
 import { ReactNode, useEffect } from 'react'
 
-// both excerpt and fullContent should be rendered in the parent component, aka on the server
+// both excerpt and fullContent should be rendered in the parent component, (server-side)
 export function ContentWall({
   readingAccess,
   excerpt,
@@ -34,26 +34,23 @@ export function ContentWall({
 
   return (
     <>
-      <div className={cx('regwall', css({ position: 'relative' }))}>
-        {hasPaywall ? (
-          <>
-            <div
-              className={css({
-                position: 'absolute',
-                top: 0,
-                left: 0,
-                right: 0,
-                bottom: 0,
-                background:
-                  'linear-gradient(0deg, rgba(255,255,255,1) 0%, rgba(255,255,255,0) 100%)',
-              })}
-            />
-            {excerpt}
-          </>
-        ) : (
-          fullContent
-        )}
-      </div>
+      {hasPaywall ? (
+        <>
+          {excerpt}
+          <div
+            className={css({
+              width: '100%',
+              height: '200px',
+              mt: '-200px',
+              background:
+                'linear-gradient(0deg, rgba(255,255,255,1) 0%, rgba(255,255,255,0) 100%)',
+            })}
+          />
+        </>
+      ) : (
+        { fullContent }
+      )}
+
       <div className={css({ gridColumn: 'full' })}>
         <Regwall key={`regwall-${pathname}`} />
         <Paywall key={`paywall-${pathname}`} />
