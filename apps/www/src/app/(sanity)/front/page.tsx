da1cc49e7e529b99e5632e42570415c3356b3c30
Jeremy Stucki <jeremy.stucki@gmail.com>
2026-09-17 11:33:22 +0200

    front revalidate 0

--- apps/www/src/app/(sanity)/front/page.tsx
+++ apps/www/src/app/(sanity)/front/page.tsx
@@ -9,7 +9,7 @@ import { css } from '@republik/theme/css'
 import { Metadata } from 'next'
 import { notFound } from 'next/navigation'
 
-export const revalidate = 60
+export const revalidate = 0
 
 // Static SEO metadata: the Sanity front document has no metadata fields
 const title = 'Republik Magazin - Ohne Journalismus keine Demokratie'
