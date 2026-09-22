e6db92f88d9d7b891ee9f4e2153d33cdad4981ea
Jeremy Stucki <jeremy.stucki@gmail.com>
2026-09-22 14:11:35 +0200

    add permalink route (#1503)
    * add permalink route
    
    * permalink: redirect on 404

---
+++ apps/www/src/app/(sanity)/permalink/[id]/route.ts
@@ -0,0 +1,31 @@
+import { DOCUMENT_SLUG_BY_ID } from '@/app/(sanity)/groq/document-slug-by-id'
+import { sanityClientFetch } from '@/app/(sanity)/lib/fetch'
+import { NextResponse, type NextRequest } from 'next/server'
+
+export const revalidate = 60
+
+export async function GET(
+  req: NextRequest,
+  { params }: RouteContext<'/permalink/[id]'>,
+) {
+  const { id } = await params
+  const search = req.nextUrl.search
+
+  const slug = await sanityClientFetch(
+    DOCUMENT_SLUG_BY_ID,
+    { id },
+    { tag: 'permalink' },
+  )
+
+  if (!slug) {
+    // Redirect to /404, so user sees an error page instead of nothing
+    return NextResponse.redirect(
+      new URL('/404', process.env.NEXT_PUBLIC_BASE_URL),
+    )
+  }
+
+  const url = new URL(slug, process.env.NEXT_PUBLIC_BASE_URL)
+  url.search = search
+
+  return NextResponse.redirect(url)
+}
